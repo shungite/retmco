62117da539cf6460184e533f52981f8ed2aa0c13
Jon Snidal <jsnidal@gmail.com>
2012-10-10 08:43:09 -0500

    Concert custom post type

--- php/wp-content/themes/retlehs-roots-33d78b7/lib/post-types.php
+++ php/wp-content/themes/retlehs-roots-33d78b7/lib/post-types.php
@@ -1,3 +1,17 @@
 <?php
 
 // Custom post types
+
+add_action( 'init', 'create_my_post_types' );
+
+function create_my_post_types() {
+	register_post_type( 'concerts', 
+		array(
+			'labels' => array(
+				'name' => __( 'Concerts' ),
+				'singular_name' => __( 'Concert' )
+			),
+			'public' => true,
+		)
+	);
+}
