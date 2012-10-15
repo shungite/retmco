a5c3ea96b0990125641c5644d6191b8ef99f72d0
Jon Snidal <jsnidal@gmail.com>
2012-10-15 15:40:47 -0500

    Concerts

--- php/wp-content/themes/retlehs-roots-33d78b7/page-concert.php
+++ php/wp-content/themes/retlehs-roots-33d78b7/page-concert.php
@@ -4,16 +4,90 @@ Template Name: Concert Template
 */
 ?>
 
-Repertoire
-Jocelyn Morlock
-Nostalgia
-Wolfgang Amadeus Mozart
-Violin concerto No. 5, in A major, K 219 (Turkish)
-Arnold Schoenberg
-Verklärte Nacht
-Pre-concert event
+<img class="alignnone size-full wp-image-9" title="logosm" src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/09/logosm.png" alt="" width="150" height="78" />
+
+<div class="row">
+	<div class="span6">
+		<p>In its forty seasons, the MCO has introduced more up-and-coming young stars than you can shake a baton at. Karen Gomyo is well on her way. The Chicago Tribune called her “a first-rate artist of real musical command, vitality, brilliance and intensity.”</p>
+
+		<p>Karen Gomyo was born in Tokyo and grew up in Montreal and New York. She was the recipient of the prestigious Avery Fisher Career Grant in 2008. An extra treat: Karen Gomyo plays on a Stradivarius violin that was bought for her exclusive use by a private sponsor.</p>
+
+		<p>Guest conductor Scott Yoo has crafted a crafty programme for this show. From the Mozart concerto through Schoenberg’s one-movement string work to a reprise performance of Morlock’s Nostalgia, this repertoire will keep you on your toes!</p>
+
+	<div>
+		<ul class="nav nav-tabs" data-tabs="tabs">
+			<li class="active"><a href="#repertoire" data-toggle="tab">Repertoire</a></li>
+			<li><a href="#bios" data-toggle="tab">Artists' bios</a></li>
+			<li><a href="#notes" data-toggle="tab">Program notes</a></li>
+			<li><a href="#verk" data-toggle="tab">Verklarte</a></li>
+		</ul>
+	</div>
+
+<div class="tab-content">
+			<div class="tab-pane active" id="repertoire"><h5>Repertoire</h5
+		<p>Jocelyn Morlock<br />
+			Nostalgia</p>
+		<p>Wolfgang Amadeus Mozart<br />
+			Violin concerto No. 5, in A major, K 219 (Turkish)</p>
+		<p>Arnold Schoenberg<br />
+			Verklärte Nacht</p>
+<p>Pre-concert event
 Violin students of the Suzuki Talent Education
 Institute of Manitoba; 6:45 pm
 
-<?php get_template_part('templates/page', 'header'); ?>
-<?php get_template_part('templates/content', 'page'); ?>
+
+After beginning his musical studies at age three, Scott Yoo performed Mendelssohn’s violin concerto with the Boston Symphony at age twelve. He received first prize in the 1988 Josef Gingold International Violin Competition, the 1989 Young Concert Artists International Auditions, and the 1994 Avery Fisher Career Grant. After graduating with honours from Harvard University, Mr. Yoo founded the Metamorphosen Chamber Orchestra, conducting the ensemble in its series at Jordan Hall in Boston, and more than ninety performances on tour.
+Scott Yoo has collaborated with eminent artists Sarah Chang, Edgar Meyer, Benita Valente, and Dawn Upshaw. He is currently Music Director and Principal Conductor of the 39-year old Festival Mozaic, which presents over thirty orchestral, choral and chamber music concerts and events each year on California’s Central Coast.
+In the 2010/11, Yoo made his Carnegie Hall debut with the Saint Paul Chamber Orchestra and toured Europe with the Britten Symphonia and jazz pianist Brad Mehldau. He also continued his collaboration with Brad Mehldau at Disney Hall, an event produced by the Los Angeles Philharmonic Society.
+As a guest conductor, Mr. Yoo has led the Colorado, Dallas, Indianapolis, San Francisco and Utah Symphonies. He regularly conducts the Saint Paul Chamber Orchestra, and led their Elliott Carter Festival as well as numerous subscription series concerts. He has conducted the New World Symphony, the Civic Orchestra of Chicago, and the orchestras of Columbus, Hong Kong, Honolulu, Kansas City, Louisville, Mexico City, Nashville, and Phoenix. In Europe, he conducted the City of London Sinfonia, Orchestre de Bretagne, Odense Symphony and the Estonian National Symphony. He made his debut with the Seoul Philharmonic in 2007 and his debut with Yomiuri Nippon Orchestra in Tokyo in 2009. Scott also continues his long-standing relationship with the MCO.
+A proponent of the music of our time, Mr. Yoo has premiered 59 works by 30 composers. With Metamorphosen, he has recorded Mark O’Connor’s American Seasons for Sony Classical, John Harbison’s chamber orchestra works with soprano Dawn Upshaw for Archetype (nominated for a 1999 National Public Radio Performance Today Award) and song cycles of Earl Kim with sopranos Benita Valente and Karol Bennett for New World (named a 2001 ‘Critics’ Choice’ by The New York Times). His recent recording projects include the complete orchestral works of Earl Kim with the RTE National Orchestra of Ireland for Naxos, and works of Mozart and Elliott Carter for Bridge Records.
+Mr. Yoo studied violin with Roman Totenberg, Albert Markov, Paul Kantor and Dorothy DeLay, and conducting with Michael Gilbert and Michael Tilson-Thomas.
+
+
+Born in Tokyo, violinist Karen Gomyo grew up in Montréal and New York. Recipient of the prestigious Avery Fisher Career Grant in 2008, she has been hailed by the Chicago Tribune as “a first-rate artist of real musical command, vitality, brilliance and intensity.”
+Gomyo’s engagements as soloist have included those with the philharmonic orchestras of Los Angeles, Bergen, New York, Hong Kong and Liverpool, the Cleveland Orchestra, the symphony orchestras of San Francisco, Sydney, St. Louis, Cincinnati, Dallas, Houston, Montréal, Toronto, Vancouver and Tokyo — also the National Symphony of Washington DC. In Europe she has performed with the City of Birmingham Symphony, the Royal Scottish National Symphony, Orchestre National de Lille, the Scottish Chamber Orchestra, the Norwegian Opera Orchestra, Salzburg Camerata, the Vienna Chamber Orchestra and Den Haag Residentie Orkest. 
+She has worked with such conductors as Leonard Slatkin, Neeme Järvi, David Robertson, David Zinman, Gilbert Varga, Yannick Nézet-Séguin, Hannu Lintu, Andrey Boreyko, Hans Graf, Louis Langree, James Gaffigan, Andrew Grams, Pinchas Zukerman, Vasily Petrenko, Robin Ticciati and Jakub Hrusa. 
+Highlights in 2012 / 13 include concerts with the Minnesota Orchestra, the Hong Kong Philharmonic, and with the symphony orchestras of San Francisco, Montréal, Phoenix, Atlanta, Colorado, San Diego, Toronto, Detroit, Vancouver, Milan, Sao Paulo and Sydney. She also performs several recitals in the US and Europe. 
+In recital and chamber music, Gomyo has performed in festivals in the US (Aspen, Ravinia, Caramoor, Mostly Mozart), Austria, Germany, France, Norway, Ukraine, Holland, Spain, Italy, Japan, and Canada, collaborating with such artists as Heinrich Schiff, Lynn Harrell, Alisa Weilerstein, Christian Poltera, Donald Weilerstein, Isabelle van Keulen, Antoine Tamestit, Kathryn Stott and Anton Kuerti. 
+Karen Gomyo is deeply interested in the Nuevo Tango music of Astor Piazzolla, and in 2012, along with Piazzolla cohorts Pablo Ziegler (piano), Hector del Curto (Bandoneón), Claudio Ragazzi (electric guitar), and Pedro Giraudo (double bass) plus classical pianist Alessio Bax, she toured a unique program featuring the music of Piazzolla and the classical composers who influenced him. She will participate with these artists again at a week-long festival in Laguna Beach, California in early 2013. 
+In 2008 Gomyo performed at the First Symposium for the Victims of Terrorism held at the United Nations in New York, and in 2009 was the guest soloist for the New York Philharmonic’s Memorial Day concert at the Cathedral of St. John the Divine. 
+Karen Gomyo plays on a Stradivarius violin that was bought for her exclusive use by a private sponsor. She lives in New York City.
+
+
+Juno-nominated composer Jocelyn Morlock is one of Canada’s most distinctive voices. “A lyrical wonder, exquisite writing” with “an acute feeling for sonority” and an approach that is “deftly idiomatic” (Vancouver Sun), Morlock’s music has received numerous accolades, including: top 10 at the 2002 International Rostrum of Composers; Winner of the 2003 CMC Prairie Region Emerging Composers competition; winner of the Mayor’s Arts Awards in Vancouver (2008); two nominations for Best Classical Composition at the Western Canadian Music Awards (2006, 2010) and most recently a Juno Nomination for Classical Composition of the Year (2011, Exaudi.) She is currently (2012–2014) serving as inaugural Composer-in-Residence for Vancouver’s innovative concert series, Music on Main.
+Morlock’s international career was launched at the 1999 International Society for Contemporary Music’s World Music Days with performances of her quartet Bird in the Tangled Sky. Since then, she has composed for significant music competitions, including the 2008 Eckhardt-Gramatté National Music Competition (Involuntary Love Songs) and the 2005 Montréal International Music Competition (Amore, a tour de force vocal work that has since received more than 70 performances and numerous radio broadcasts.)
+Highlights of the past year’s premieres include Three Meditations on Light, written for the debut concert of the Couloir duo at Music on Main’s Modulus Festival; Luft, a ballet written for Turning Point Ensemble’s Rio Tinto Alcan prize-winning production Firebird 2011; and Aeromancy, a double cello concerto commissioned for the Gala opening concert of the first International Cello Festival of Canada in Winnipeg. In addition to continued collaboration with Music on Main, upcoming projects include a new flute concerto for Paolo Bortolussi and the Vancouver Island Symphony, and a violin and piano work for Duo Concertante.
+Nostalgia
+Jocelyn Morlock 
+Nostalgia was commissioned by the MCO, 
+who premiered it in 2007, conducted by Scott Yoo. The composer’s note follows:
+nos·tal·gi·a: A mixed feeling of happiness, sadness, and longing when recalling a person, place, or event from the past, or the past in general.
+Nostalgia has been a frequent topic of consideration for artists, from Tarkovsky and Takemitsu to Milan Kundera, and while I am daunted (and amazed) by their work, I am sufficiently fascinated by nostalgia to attempt to explore it in my own composition.
+Aside from the undoubted delights of glimpsing the past through a rosy, muzzy haze, nostalgia also has some darker facets; if one engages in an obsessive love for the past, one may lose sight of the present, and nothing appears quite as wonderful as that which is forever lost.
+I’ve taken as my starting point the Adagio of Bach’s Sonata for Viola da Gamba and Harpsichord, bwv 1027, a piece which for me is replete with nostalgia and past associations. The opening melody has a sweet sadness that I find irresistible — I can’t tell you how many times I’ve listened to it, but too many.
+While I mined the Bach for tiny fragments that I could use for my own musical purposes throughout my twelve-minute work, I believe that such referencing/borrowing may be audible only during its last couple of minutes, if at all. Rather than building a piece on Bach’s music, (in the post-modern style of, say, Schnittke), my intent was to refer to the many emotions I feel when listening to Bach to create a rumination upon this seductive but surreal world of memory. The music I’ve written is (I hope) by turns more mysterious and dreamy, disturbed, and in the end peaceful, though such peace may also be an illusion …
+I leave you with two quotes from my favourite author, Milan Kundera: “In the sunset of dissolution, everything is illuminated by the aura of nostalgia.” (From The Unbearable Lightness of Being, 1984.) “You can suffer nostalgia in the presence of the beloved if you glimpse a future where the beloved is no more.” (From Ignorance, 1998.)
+Violin Concerto No. 5
+in A Major, K. 219 — ‘Turkish’
+Wolfgang Amadeus Mozart
+Mozart, the greatest of all child prodigies in music, received from his father Leopold a thorough education in composition and performance. The latter included instruction in piano, harpsichord and violin. As an adult, Wolfgang concentrated his performing skills on the piano. In his early years, he appeared most often as a soloist on the violin, beginning with the concert tours his family made during the 1760s. His father once wrote to him that “You yourself do not know how well you play the violin … when you play with energy and with your whole heart and soul, yes indeed, it’s just as though you were the finest violinist in all of Europe.” This was no small compliment, coming from a renowned authority on the instrument. He published his widely used violin method in the year of Wolfgang’s birth. 
+In 1769, Wolfgang was awarded the post of Honorary Concertmaster in the Salzburg Court Orchestra. His duties included leading it from the first desk of violins (this being before the rise of the conductor, as we know the role), playing solos, and writing new music for it to perform. Three years would pass before he began receiving a salary for these considerable responsibilities. 
+Between June and December 1775, he composed all but the first of the five violin concertos that can be unquestionably attributed to him. He completed No. 5 on December 20, 1775. It is not only the most accomplished of the series but also the most unusual. The first movement has the tempo marking Allegro aperto, for example, meaning literally “quick and open.” He appears to be the only composer to use this adjective. He applied it to just eight other works, the vocal numbers of which are either, in the words of author Jean-Pierre Marty, “hymns to hope, to joy, to love, and to nature and to happiness,” or liturgical songs of praise. This concerto movement evokes a similar mood. 
+The soloist’s first entry is another example of the concerto’s uniqueness. It is remarkable for being quite different in tempo and mood — quiet and dreamy — from the preceding, almost martial orchestral introduction. It’s as if the violinist were saying to the orchestra, “catch your breath while I introduce myself.” And once the quicker tempo resumes, the soloist introduces a completely new theme. 
+The second movement is a true Adagio, slow and heartfelt, in contrast to the easy, flowing Andante that was typical of the era. Its lyrical intensity borders on the operatic. The finale, a rondo in the style of a minuet, is the source of the concerto’s nickname. It begins in appropriately courtly fashion. In the delightfully startling minor key episode mid-way through, Mozart instructs the cellos and basses to strike their strings with the wood of the bow, and asks the soloist for virtuoso pyrotechnics. These practices recall the Turkish military music that was all the rage in Austria at the time — but evokes the folk music of Hungary to equal degree. At that time, the word ‘Turkish’ was a catch-all term that might be applied to any music that evoked the colourful, exotic folk tunes of people living east of Austria. 
+Mozart borrowed the main ‘Turkish’ theme from a ballet, Le gelosie del seraglio (Harem Jealousy), that he had sketched for his opera Lucio Silla, but had not completed. The ‘Turkish’ episode was edited out of the concerto at some point, perhaps by someone with more ‘refined’ taste than the composer. It was reinstated in the score during the 1960s. 
+
+<h5 id="verk">Verklärte Nacht (Transfigured Night)</h5>
+Arnold Schoenberg
+The Schoenberg of atonality and serial composition — the innovative, controversial procedures that demonized him in the minds of many listeners — still lay ahead when he composed this rapturous, ultra-Romantic piece. 
+Until 1899, his creative output had been confined to songs, piano pieces and a string quartet. The conservative Romanticism of Johannes Brahms strongly influenced them all. That year, the poems of German author Richard Dehmel (1863-1920) inspired him not only to set some of them to music, but also to create an evocation of Dehmel’s poem, Transfigured Night. He scored it for six strings and cast it in the form of a single, continuous movement. It proved to be his most ambitious and most successful work to date. He prepared a version for string orchestra in 1917. The MCO will be performing the revision of it that he created in 1943.
+In 1950, he wrote, “My composition was, perhaps, somewhat different from other illustrative compositions, firstly, by not being for orchestra but for a chamber group and secondly, because it does not illustrate any action or drama, but was restricted to portraying nature and to expressing human feelings … in other words, it offers the possibility to be appreciated as ‘pure’ music.”
+Once Dehmel heard it, he wrote to Schoenberg that “I would consider it a sin of omission if I did not say a word of thanks to you for your wonderful sextet. I had intended to follow the motives of my text in your composition, but I soon forgot to do so, I was so enraptured by the music.” 
+The poem is a dialogue between two lovers, as they walk through a chilly moonlit forest. The woman reveals that she is expecting a former lover’s child. Her new lover argues that the depth of their feelings for each other will ensure that they will love the child as their own. 
+The music progresses from initial anxiety, through turbulent yearning, to a final sense of tranquil fulfilment. The original chamber version communicates a compelling sense of intimacy, but Schoenberg’s transcription for full orchestral strings brings added power and sumptuousness.</p>
+</div>
+	</div>
+</div>
+
+<?php get_template_part('templates/content', 'concert'); ?>
+
