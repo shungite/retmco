89cbfab969d8eaf078e721ac33e62d4040bab1cf
Jon Snidal <jsnidal@gmail.com>
2012-10-25 09:33:30 -0500

    Concert added

---
+++ php/wp-content/themes/retlehs-roots-33d78b7/page-concert4.php
@@ -0,0 +1,86 @@
+<?php
+/*
+Template Name: Page Concert4 Template
+*/
+?>
+<div class="container">
+
+<div class="row">
+
+	<div class="span11">
+		<img class="alignnone size-full wp-image-9" title="logosm" src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/09/logosm.png" alt="" width="150" height="78" />
+	</div>
+
+</div><!-- /row -->
+
+<div class="row">
+
+	<div class="span8">
+		<img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/head06feb13a.png" alt="" width="870" height="512">
+	</div>
+	<div class="span2">
+		<p>.</p>
+	</div>
+
+</div><!-- /row -->
+
+<div class="row">
+
+	<div class="span6">
+		<h4>A double celebration:<br />The Winnipeg Singers and the MCO</h4>
+		<p>Winnipeg was a very different place prior to 1972: there was no Prairie Theatre Exchange, no Manitoba Opera, no Plug In ICA, no Winnipeg Singers and — horror of horrors — no MCO!</p>
+		<p>At this concert we will celebrate the formation of two of the fine institutions that are the weft and warp of the fabric of Winnipeg’s rich and varied cultural scene.</p>
+		<p>The Choir and Orchestra will be conducted by one of Winnipeg’s busiest musicians, Yuri Klaz. Klaz also directs the First Mennonite Church Choir, the Shaarey Zedek Synagogue and continues as conductor of The Winnipeg Philharmonic Choir.</p>
+	</div>
+
+	<div class="span2"> 
+
+		<img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /><br />
+	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=89e3b17ba5474af284f562865c4b70e1"><img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/Adult.png" alt="" width="62" height="40" /></a><br />
+ 	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=696960f1b3b241bab82da74d814a3c13"><img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/Senior.png" alt="" width="62" height="40" /></a><br />
+	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=49f2aef74f6c4dabb1248afb38f3358d"><img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/Student.png" alt="" width="62" height="40" /></a>
+		<p class="fineprint">Click ticket above to add to shopping cart; adjust quantity in cart and return to purchase other types of tickets.</p>
+	</div>
+
+</div><!-- /row -->
+
+<div class="row">
+
+	<div class="span6">
+		<ul class="nav nav-tabs">
+			<li><a href="#repertoire" data-toggle="tab"><strong>Repertoire</strong></a></li>
+			<li><a href="#bios" data-toggle="tab"><strong>Artists&rsquo; bios</strong></a></li>
+			<li><a href="#notes" data-toggle="tab"><strong>Program notes</strong></a></li>
+		</ul>
+		<div class="tab-content">
+			<div class="tab-pane fade in active" id="repertoire">
+				<p><strong>Manitoba Chamber Orchestra</strong><br />Anne Manson, Music Director<br />Karl Stobbe, Concertmaster<br />Westminster United Church<br />6 February 2013</p>
+				<p>The Winnipeg Singers<br />Yuri Klaz, Music Director and Conductor</p>
+				<p><strong>Antonio Vivaldi</strong><br /><em>Credo</m>, in E Minor, RV 591</p>
+				<p><strong>Arvo P&auml;rt</strong><br /><em>Berliner Messe</em></p>
+				<p><strong>Thomas Tallis</strong><br /><em>Spem in allum</em></p>
+				<p><strong>Glenn Buhr</strong><br /><em>Ritchot Mass</em></p>
+				<p><strong>Imant Raminsh</strong><br />Psalm 121</p>
+				<p><strong>Felix Mendelssohn</strong><br /><em>Jesu, meine Freude,</em> chorale cantata<br />for chorus and strings</p>
+				<p>Concert sponsor / <strong>LBL Holdings</strong><br />Season sponsor / <strong><a href="http://www.greatwestlife.com/001/Home/index.htm">The Great-West Life Assurance Company</a></strong><br />Print media sponsor / <strong><a href="http://www.winnipegfreepress.com/">Winnipeg Free Press</a></strong><br />Radio media sponsors / <strong><a href="http://music.cbc.ca/#/radio2/">CBC Radio 2 98.3</a></strong>, <strong><a href="http://www.cbc.ca/programguide/daily/today/cbc_radio_one">CBC Radio One 990</a></strong>,<br /><strong><a href="http://www.espace.mu/Espace-Musique/">Espace musique 89,9</a></strong> and <strong><a href="http://www.goldenwestradio.com/">Golden West Radio</a></strong>
+			</div>
+			<div class="tab-pane fade" id="bios">
+				<p><strong>Yuri Klaz</strong></p>
+				<p><strong>The Winnipeg Singers</strong></p>
+				<p><strong>Arvo P&auml;rt</strong></p>
+				<p><strong>Glenn Buhr</strong></p>
+				<p><strong>Imant Raminsh</strong></p>
+			</div>
+			<div class="tab-pane fade" id="notes">
+			</div>
+		</div><!-- /tab-content -->
+	</div><!-- /span6 tab area -->
+	<div class="span5">
+		<p class="fineprint">.</p>
+	</div>
+
+</div><!-- /row -->
+
+<?php get_template_part('templates/content', 'concert'); ?>
+
+</div><!-- /container -->
