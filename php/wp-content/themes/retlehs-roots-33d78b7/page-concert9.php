dc57365ea1bbeed4cca6528cfcddb21949926652
Jon Snidal <jsnidal@gmail.com>
2012-10-25 11:56:20 -0500

    Concert add again

---
+++ php/wp-content/themes/retlehs-roots-33d78b7/page-concert9.php
@@ -0,0 +1,81 @@
+<?php
+/*
+Template Name: Page Concert9 Template
+*/
+?>
+<div class="container">
+
+<div class="row">
+
+	<div class="span11">
+		<img class="alignnone size-full wp-image-9" title="logosm" src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/09/logosm.png" alt="" width="150" height="78" />
+	</div>
+
+</div><!-- /row -->
+
+<div class="row">
+
+	<div class="span8">
+		<img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/head05jun13a.png" alt="" width="870" height="512">
+	</div>
+	<div class="span2">
+		<p>.</p>
+	</div>
+
+</div><!-- /row -->
+
+<div class="row">
+
+	<div class="span6">
+		<h4>English cellist<br />Colin Carr to play / conduct</h4>
+		<p>Colin Carr was the talk of the town in 2011. &ldquo;Everyone loved cellist Colin Carr when he performed with us during the International Cello Festival of Canada last year,&rdquo; says Anne Manson. &ldquo;We&rsquo;ll be continuing the tradition of having a player rather than a conductor lead the Orchestra.&rdquo;</p>
+		<p>&ldquo;The program includes a Mozart Divertimento, a Boccherini cello concerto and Tchaikovsky&rsquo;s <em>Souvenir de Florence</em>, which is a beautiful piece that we have wanted to play for a long time.&rdquo;</p>
+		<p>Carr appears throughout the world as a soloist, chamber musician, recording artist and teacher. He has played with most of the major orchestras throughout the world. Colin Carr&rsquo;s cello was made by Matteo Gofriller in Venice in 1730.</p>
+		<p><strong>Souvenir de Florence</strong></p>
+		<p>The <em>Souvenir de Florence</em> is a string sextet scored for two violins, two violas and two cellos, composed in the European summer of 1890. He dedicated the work to the St. Petersburg Chamber Music Society in response to his becoming an Honorary Member.</p>
+		<p>The work, in the traditional four-movement form, received its name after the composer sketched one of the principal themes while visiting Florence, Italy, where he composed <em>The Queen of Spades</em>. The work was revised between December 1891 and January 1892, before being premiered in 1892 (Wikipedia).
+	</div><!-- /span6 -->
+
+	<div class="span2"> 
+		<img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /><br />
+	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=353954ab67014802ab5262d5e4a2b9af"><img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/Adult.png" alt="" width="62" height="40" /></a><br />
+ 	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=65cf18fe94b34a2aa24d7f7e56e7dd71"><img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/Senior.png" alt="" width="62" height="40" /></a><br />
+	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=374bffd27d964cf382b3d3cf17768550"><img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/Student.png" alt="" width="62" height="40" /></a>
+		<p class="fineprint">Click ticket above to add to shopping cart; adjust quantity in cart and return to purchase other types of tickets.</p>
+	</div><!-- /span2 -->
+
+</div><!-- /row -->
+
+<div class="row">
+
+	<div class="span6">
+		<ul class="nav nav-tabs">
+			<li><a href="#repertoire" data-toggle="tab"><strong>Repertoire</strong></a></li>
+			<li><a href="#bios" data-toggle="tab"><strong>Artists&rsquo; bios</strong></a></li>
+			<li><a href="#notes" data-toggle="tab"><strong>Program notes</strong></a></li>
+		</ul>
+		<div class="tab-content">
+			<div class="tab-pane fade in active" id="repertoire">
+				<p><strong>Manitoba Chamber Orchestra</strong><br />Anne Manson, Music Director<br />Karl Stobbe, Concertmaster<br />Westminster United Church<br />5 June 2013</p>
+				<p>Colin Carr, conductor and cello soloist</p>
+				<p><strong>Wolfgang Amadeus Mozart</strong><br />Divertimento in D Major, K136/125a</p>
+				<p><strong>Luigi Boccherini</strong><br />Concerto No. 3, in G Major, for Cello and Piano, G480</p>
+				<p><strong>Pyotr Ilyich Tchaikovsky</strong><br /><em>Souvenir de Florence</em></p>
+				<p>Season sponsor / <strong><a href="http://www.greatwestlife.com/001/Home/index.htm">The Great-West Life Assurance Company</a></strong><br />Print media sponsor / <strong><a href="http://www.winnipegfreepress.com/">Winnipeg Free Press</a></strong><br />Radio media sponsors / <strong><a href="http://music.cbc.ca/#/radio2/">CBC Radio 2 98.3</a></strong>, <strong><a href="http://www.cbc.ca/programguide/daily/today/cbc_radio_one">CBC Radio One 990</a></strong>,<br /><strong><a href="http://www.espace.mu/Espace-Musique/">Espace musique 89,9</a></strong> and <strong><a href="http://www.goldenwestradio.com/">Golden West Radio</a></strong>
+			</div>
+			<div class="tab-pane fade" id="bios">
+				<p><strong>Colin Carr</strong></p>
+			</div>
+			<div class="tab-pane fade" id="notes">
+			</div>
+		</div><!-- /tab-content -->
+	</div><!-- /span6 tab area -->
+	<div class="span5">
+		<p class="fineprint">.</p>
+	</div>
+
+</div><!-- /row -->
+
+<?php get_template_part('templates/content', 'concert'); ?>
+
+</div><!-- /container -->
