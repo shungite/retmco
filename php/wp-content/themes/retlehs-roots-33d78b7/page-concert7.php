da5bd500d08f459b7c6083f7364d9a9a2b0acae1
Jon Snidal <jsnidal@gmail.com>
2012-10-25 10:53:00 -0500

    Concert add

---
+++ php/wp-content/themes/retlehs-roots-33d78b7/page-concert7.php
@@ -0,0 +1,85 @@
+<?php
+/*
+Template Name: Page Concert7 Template
+*/
+?>
+<div class="container">
+
+<div class="row">
+
+	<div class="span11">
+		<img class="alignnone size-full wp-image-9" title="logosm" src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/09/logosm.png" alt="" width="150" height="78" />
+	</div>
+
+</div><!-- /row -->
+
+<div class="row">
+
+	<div class="span8">
+		<img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/head02apr13a.png" alt="" width="870" height="512">
+	</div>
+	<div class="span2">
+		<p>.</p>
+	</div>
+
+</div><!-- /row -->
+
+<div class="row">
+
+	<div class="span6">
+		<h4>Time machine:<br />hear the first<br />MCO concert ever!</h4>
+		<p>After just one concert, Winnipeggers knew that we needed to have live chamber music performed in this town for at least the next forty years.</p>
+<p>Anne Manson: &ldquo;This concert will be an almost exact replica of the first concert MCO ever played, forty years ago. We are so happy to have Tracy Dahl back with us for Britten&rsquo;s Les Illuminations. And the audience will have a chance to get to know Caitlin Broms-Jacobs featured in a Vivaldi oboe concerto.&rdquo;</p>
+<p>Roy Goodman, who occupies his own volume in the tome of MCO&rsquo;s history (as Music Director from 2000 to 2005), will once again be at the podium for this happy occasion.</p>
+<p><strong>Maestro Roy Goodman</strong></p>
+<p>Roy Goodman is Principal Guest Conductor of the English Chamber Orchestra, Director Emeritus of the European Union Baroque Orchestra, Honorary President of the Royal Scottish National Orchestra Chorus and has appeared as guest conductor with 120 other orchestras and opera houses.</p>
+	</div><!-- /span6 -->
+
+	<div class="span2"> 
+		<img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /><br />
+	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=720d6e6e7bb549aa8f5532ef65ea4edd"><img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/Adult.png" alt="" width="62" height="40" /></a><br />
+ 	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=ce47fb63079d4f84b5907716f09cf6c7"><img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/Senior.png" alt="" width="62" height="40" /></a><br />
+	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=a3bd1ff75de449a3a8cf1a61948316b5"><img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/Student.png" alt="" width="62" height="40" /></a>
+		<p class="fineprint">Click ticket above to add to shopping cart; adjust quantity in cart and return to purchase other types of tickets.</p>
+	</div><!-- /span2 -->
+
+</div><!-- /row -->
+
+<div class="row">
+
+	<div class="span6">
+		<ul class="nav nav-tabs">
+			<li><a href="#repertoire" data-toggle="tab"><strong>Repertoire</strong></a></li>
+			<li><a href="#bios" data-toggle="tab"><strong>Artists&rsquo; bios</strong></a></li>
+			<li><a href="#notes" data-toggle="tab"><strong>Program notes</strong></a></li>
+		</ul>
+		<div class="tab-content">
+			<div class="tab-pane fade in active" id="repertoire">
+				<p><strong>Manitoba Chamber Orchestra</strong><br />Anne Manson, Music Director<br />Karl Stobbe, Concertmaster<br />Westminster United Church<br />2 April 2013</p>
+				<p>Roy Goodman, conductor<br />Tracy Dahl, soprano<br />Caitlin Broms-Jacobs, ,oboe</p>
+				<p><strong>Edvard Grieg</strong><br /><em>From Holberg’s Time</em> (Suite in olden style), Op. 40</p>
+				<p><strong>Benjamin Britten</strong><br /><em>Les Illuminations</em>, Op. 18</p>
+				<p><strong>Antonio Vivaldi</strong><br />Concerto in A Minor, for Oboe, Strings<br />and Harpsichord, RV 463 (F VII-13)</p>
+				<p><strong>John Estacio</strong><br /><em>Such Sweet Sorrow</em></p>
+				<p><strong>Gioachino Rossini</strong><br />Sonata No. 3, for Strings in C Major</p>
+				<p>Concert sponsor / <strong>Angela &mdash; Robert Ross</strong><br />Season sponsor / <strong><a href="http://www.greatwestlife.com/001/Home/index.htm">The Great-West Life Assurance Company</a></strong><br />Print media sponsor / <strong><a href="http://www.winnipegfreepress.com/">Winnipeg Free Press</a></strong><br />Radio media sponsors / <strong><a href="http://music.cbc.ca/#/radio2/">CBC Radio 2 98.3</a></strong>, <strong><a href="http://www.cbc.ca/programguide/daily/today/cbc_radio_one">CBC Radio One 990</a></strong>,<br /><strong><a href="http://www.espace.mu/Espace-Musique/">Espace musique 89,9</a></strong> and <strong><a href="http://www.goldenwestradio.com/">Golden West Radio</a></strong>
+			</div>
+			<div class="tab-pane fade" id="bios">
+				<p><strong>Roy Goodman</strong></p>
+				<p><strong>Tracy Dahl</strong></p>
+				<p><strong>Caitlin Broms-Jacobs</strong></p>
+				<p><strong>John Estacio</strong></p>
+			</div>
+			<div class="tab-pane fade" id="notes">
+			</div>
+		</div><!-- /tab-content -->
+	</div><!-- /span6 tab area -->
+	<div class="span5">
+		<p class="fineprint">.</p>
+	</div>
+
+</div><!-- /row -->
+
+<?php get_template_part('templates/content', 'concert'); ?>
+
+</div><!-- /container -->
