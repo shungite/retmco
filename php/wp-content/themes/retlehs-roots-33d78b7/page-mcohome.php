dde10cb1fe3a99500f0ad2606c181f7bbd8c51e8
Jon Snidal <jsnidal@gmail.com>
2012-10-26 15:43:56 -0500

    New home page work

---
+++ php/wp-content/themes/retlehs-roots-33d78b7/page-mcohome.php
@@ -0,0 +1,90 @@
+<?php
+/*
+Template Name: Page MCOhome Template
+*/
+?>
+<div class="container">
+
+<img class="alignnone size-full wp-image-9" title="logosm" src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/09/logosm.png" alt="" width="150" height="78" />
+<p>&nbsp;</p>
+
+<div class="row">
+	
+	<div class="span3" bgcolor="#666">
+		<div class="well">
+			<p>After beginning his musical studies at age three, Scott Yoo performed Mendelssohn&rsquo;s violin concerto with the Boston Symphony at age twelve. He received first prize in the 1988 Josef Gingold International Violin Competition, the 1989 Young Concert Artists International Auditions, and the 1994 Avery Fisher Career Grant. After graduating with honours from Harvard University, Mr. Yoo founded the Metamorphosen Chamber Orchestra, conducting the ensemble in its series at Jordan Hall in Boston, and more than ninety performances on tour.</p>
+			<p>Scott Yoo has collaborated with eminent artists Sarah Chang, Edgar Meyer, Benita Valente, and Dawn Upshaw. He is currently Music Director and Principal Conductor of the 39-year old Festival Mozaic, which presents over thirty orchestral, choral and chamber music concerts and events each year on California&rsquo;s Central Coast.</p>
+		</div><!-- /well -->
+	</div><!-- /span3 -->
+
+	<div class="span3" bgcolor="#666">
+		<div class="well">
+			<p>jkwhdf</p>
+		</div><!-- /well -->
+	</div><!-- /span3 -->
+
+	<div class="span3" bgcolor="#666">
+		<div class="well">
+			<p>jkwhdf</p>
+		</div><!-- /well -->
+	</div><!-- /span3 -->
+
+	<div class="span3">
+		<div class="row-pinK">
+			<p>jkwhdf</p>
+		</div><!-- /row-pink -->
+	</div><!-- /span3 -->
+
+</div><!-- /row -->
+
+<div class="row">
+
+	<div class="span8">
+		<a href="http://retmco-shungite.rhcloud.com/gomyo/"><img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/head21nov12a.png" width="870" height="512"></a>
+	</div><!-- /span8 -->
+
+</div><!-- /row -->
+
+<div class="row">
+
+	<div class="span6">
+		<h4>At our next-ish concert: &ldquo;...one of<br />the world’s new wonders of the violin.&rdquo;</h4>
+		<p>Violinist Karen Gomyo is making headlines around the world, in Denmark, Australia and in the USA. Now, she headlines with the MCO! It isn't too late to subscribe: make this the first in a six-concert series.</p>
+		<p>&nbsp;</p>
+	</div><!-- /span6 -->
+
+</div><!-- /row -->
+
+<div class="row">
+	<div class="span4">
+		<img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/YooEvent.png" alt="" width="100" height="100" />
+		<h4>Music &amp; conversation<br />with Scott Yoo</h4>
+		<p>Fundraiser: enjoy the first of two unique events celebrating MCO's 40th anniversary season. The first is an intimate evening at historic 218 Roslyn Road&mdash;with wine and delectable edibles!</p>
+		<a class="btn" href="#">Details »</a>
+	</div><!-- /span4 -->
+
+	<div class="span4">
+		<img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/tlcorner.png" alt="" width="100" height="100" />
+		<h4>Subscribe to MCO's<br />40th anniversary season!</h4>
+		<p>Winnipeg fell in love with live chamber music at the very first MCO concert! From that first season of just three concerts, the Orchestra has grown in every way imaginable.</p>
+		<a class="btn" href="#">Subscribe now »</a>
+	</div><!-- /span4 -->
+</div><!-- /row -->
+</div>
+
+
+<div class="row">
+
+	<div class="span2">
+		<p>&nbsp;</p>
+		<h6><i class="icon-warning-sign"></i><br />Warning!</h6>
+		<p class="fineprint">Please be patient as we rebuild our web site. We promise to reward you with better content and interactive features. In the meantime, feel free to send us some feedback...</p>
+		<a class="btn btn-mini" href="mailto:info@themco.ca?subject=Feedback ...">Complain »</a>
+		<p>&nbsp;</p>
+	</div><!-- /span2 -->
+
+</div><!-- /row -->	
+
+<?php get_template_part('templates/content', 'home'); ?>
+
+</div><!-- /container -->
