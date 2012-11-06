896d4ed52b60afa576de377617ffa6d68e73779f
Jon Snidal <jsnidal@gmail.com>
2012-11-06 13:58:13 -0600

    Home link

--- php/wp-content/themes/retlehs-roots-33d78b7/page-concert9.php
+++ php/wp-content/themes/retlehs-roots-33d78b7/page-concert9.php
@@ -8,7 +8,7 @@ Template Name: Page Concert9 Template
 <div class="row">
 
 	<div class="span11">
-		<img class="alignnone size-full wp-image-9" title="logosm" src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/09/logosm.png" alt="" width="150" height="78" />
+		<a href="http://www.themco.ca"><img class="alignnone size-full wp-image-9" title="logosm" src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/09/logosm.png" alt="" width="150" height="78" /></a>
 	</div>
 
 </div><!-- /row -->
