31b4f15112087e113fb1334ff20da3cc5f0de0e0
Jon Snidal <jsnidal@gmail.com>
2012-11-07 15:39:02 -0600

    Home work

--- php/wp-content/themes/retlehs-roots-33d78b7/page-home.php
+++ php/wp-content/themes/retlehs-roots-33d78b7/page-home.php
@@ -7,39 +7,40 @@ Template Name: Page Home Template
 
 <img class="alignnone size-full wp-image-9" title="logosm" src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/09/logosm.png" alt="" width="150" height="78" />
 
+<section>
+
 <div class="row">
 
 	<div class="span8">
 		<a href="http://www.themco.ca/gomyo/"><img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/head21nov12a.png" width="870" height="512"></a>
-	</div><!-- /span8 -->
 
-</div><!-- /row -->
-
-<div class="row">
-
-	<div class="span6">
-		<h4>At our next concert: &ldquo;...one of<br />the world’s new wonders of the violin.&rdquo;</h4>
+		<h4>At our next concert: &ldquo;... one of<br />the world’s new wonders of the violin.&rdquo;</h4>
 		<p>Violinist Karen Gomyo is making headlines around the world, in Denmark, Australia and in the USA. Now, she headlines with the MCO! It isn't too late to subscribe: make this the first in a six-concert series.</p>
 		<p>&nbsp;</p>
 	</div><!-- /span6 -->
 
 </div><!-- /row -->
 
+</section>
+
 <div class="row">
+
+<div class="span4">
+		<a href="http://www.themco.ca/subscribe/"><img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/11/SubscribeTodaySmall.png" alt="" width="148" height="100" /></a>
+		<h4>Celebrate MCO's<br />40th anniversary season<br />with a subscription!</h4>
+		<p>Winnipeg fell in love with live chamber music at the very first MCO concert! From that first season of just three concerts, the Orchestra has grown in every way imaginable.</p>
+		<a class="btn" href="http://www.themco.ca/subscribe/">Subscribe now »</a>
+	</div><!-- /span4 -->
+
 	<div class="span4">
 		<a href="http://www.themco.ca/yoo/"><img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/YooEvent.png" alt="" width="100" height="100" /></a>
 		<h4>Candlelight event:<br />Music &amp; conversation<br />with Scott Yoo</h4>
 		<p>Fundraiser: enjoy the first of two unique events celebrating MCO's 40th anniversary season. The first is an intimate evening at historic 218 Roslyn Road&mdash;with wine and delectable edibles!</p>
 		<a class="btn" href="http://www.themco.ca/yoo/">Details »</a>
 	</div><!-- /span4 -->
-
-	<div class="span4">
-		<a href="http://www.themco.ca/subscribe/"><img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/11/SubscribeTodaySmall.png" alt="" width="148" height="100" /></a>
-		<h4>Celebrate MCO's<br />40th anniversary season!</h4>
-		<p>Winnipeg fell in love with live chamber music at the very first MCO concert! From that first season of just three concerts, the Orchestra has grown in every way imaginable.</p>
-		<a class="btn" href="http://www.themco.ca/subscribe/">Subscribe now »</a>
-	</div><!-- /span4 -->
+	
 </div><!-- /row -->
+
 </div>
 
 <div class="row">
