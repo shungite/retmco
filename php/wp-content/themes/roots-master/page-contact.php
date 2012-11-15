1003501ec71e2479947c08fc0172fa810c9f1446
Jon Snidal <jsnidal@gmail.com>
2012-11-15 15:55:17 -0600

    Contact page

---
+++ php/wp-content/themes/roots-master/page-contact.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: Page Contact Template
+*/
+?>
+<div class="container">
+
+<div class="row">
+
+	<div class="span11">
+		<a href="http://www.themco.ca"><img class="alignnone size-full wp-image-9" title="logosm" src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/09/logosm.png" alt="" width="150" height="78" /></a>
+	</div>
+
+</div><!-- /row -->
+
+<?php get_template_part('templates/content', 'subscribe'); ?>
+
+</div><!-- /container -->
