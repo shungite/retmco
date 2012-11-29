166dbdc480d309cb1f9fe9efb9cd7173f2de6441
Jon Snidal <jsnidal@gmail.com>
2012-11-29 10:54:43 -0600

    Undo and new

--- php/wp-content/themes/roots-master/templates/head.php
+++ php/wp-content/themes/roots-master/templates/head.php
@@ -16,6 +16,11 @@
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
 
+  <?php
+  Header( "HTTP/1.1 301 Moved Permanently" ); 
+  Header( "Location: http://www.new-url.com" ); 
+  ?> 
+
   <?php wp_head(); ?>
 
   <?php if (wp_count_posts()->publish > 0) : ?>
