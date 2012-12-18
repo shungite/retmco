76b3c206664eef458c0cecc70aa89c781928a0ce
Jon Snidal <jsnidal@gmail.com>
2012-12-18 11:45:41 -0600

    Board edited

--- php/wp-content/themes/roots-master/page-board.php
+++ php/wp-content/themes/roots-master/page-board.php
@@ -16,18 +16,7 @@ Template Name: Page Board Template
 <div class="row">
 
 	<div class="span7">
-		<h4>Manitoba Chamber Orchestra<br />Board of directors &amp; staff</h4>
-	</div>
-
-</div><!-- /row -->
-
-<div class="row">
-
-	<div class="span4">
-		<div class="well">
-			<h4>Artistic mission statement</h4>
-			<p>To create and perform chamber orchestra repertoire at the highest artistic level possible for Manitobans, for Canadians, and for international audiences.</p>
-		</div>
+		<h4>Manitoba Chamber Orchestra<br />Board of directors &amp; administration</h4>
 	</div>
 
 </div><!-- /row -->
@@ -35,44 +24,51 @@ Template Name: Page Board Template
 <div class="row">
 
 	<div class="span6">
-		<p><strong>President</strong><br />
-		Jesus Angel Miguel Garcia</p>
-		<p><strong>Vice-President</strong><br />
-		Bill Neville</p>
-		<p><strong>Past President</strong><br />
-		Ingrid Moehlmann</p>
-		<p><strong>Treasurer</strong><br />
-		Gil Forrest</p>
-		<p><strong>Secretary</strong><br />
-		Alice MacKenzie</p>
-		<p><strong>Member at large</strong><br />
-		Jim Chapryk</p>
-		<p><strong>Members</strong></p>
-		<p>Lynne Axworthy<br />
-		Chloe Carpenter<br />
-		Judith Hall<br />
-		Joan Hardy<br />
-		Sue Irving<br />
-		Suzanne Kennelly<br />
-		Elaine Margolis<br />
-		Sandi Mielitz<br />
-		Beryl Peters<br />
-		Rishi Ramberran<br />
-		Nicola Schaefer<br />
-		Moti Shojania<br />
-		Alison Thiessen<br />
-		Elizabeth Tippett-Pope<br />
-		Faye Warren</p>
-		<p><strong>Staff</strong></p>
-		<p><strong>Managing Director</strong><br />Vicki Young</p>
-		<p><strong>Administrative Assistant</strong><br />Joan Gillis</p>
-		<p><strong>Administrative Assistant</strong><br />Corinne Friesen Loewen</p>
-		<p><strong>Bookeeper</strong><br />Pat Ireton</p>
-		<p><strong>Director of Development</strong><br />Shirley Elias</p>
-		<p><strong>Project support and Front-of-House Manager</strong><br />Elise Anderson</p>
-		<p><strong>Marketing</strong><br />Jon Snidal</p>
-		<p><strong>Program advertising</strong><br />Brian Chipney</p>
-		<p><strong>Stage Manager</strong><br />Matthew Underhill</p>
+		<div class="well">
+			<h4>Artistic mission statement</h4>
+			<p>To create and perform chamber orchestra repertoire at the highest artistic level possible for Manitobans, for Canadians, and for international audiences.</p>
+			<p><strong>President</strong><br />
+			Jesus Angel Miguel Garcia</p>
+			<p><strong>Vice-President</strong><br />
+			Bill Neville</p>
+			<p><strong>Past President</strong><br />
+			Ingrid Moehlmann</p>
+			<p><strong>Treasurer</strong><br />
+			Gil Forrest</p>
+			<p><strong>Secretary</strong><br />
+			Alice MacKenzie</p>
+			<p><strong>Member at large</strong><br />
+			Jim Chapryk</p>
+			<p><strong>Members</strong></p>
+			<p>Lynne Axworthy<br />
+			Chloe Carpenter<br />
+			Judith Hall<br />
+			Joan Hardy<br />
+			Sue Irving<br />
+			Suzanne Kennelly<br />
+			Elaine Margolis<br />
+			Sandi Mielitz<br />
+			Beryl Peters<br />
+			Rishi Ramberran<br />
+			Nicola Schaefer<br />
+			Moti Shojania<br />
+			Alison Thiessen<br />
+			Elizabeth Tippett-Pope<br />
+			Faye Warren</p>
+			<p><strong>Administration</strong></p>
+			<p><strong>Music Director &amp; Conductor</strong><br />Anne Manson</p>
+			<p><strong>Managing Director</strong><br />Vicki Young</p>
+			<p><strong>Administrative Assistant</strong><br />Joan Gillis</p>
+			<p><strong>Administrative Assistant</strong><br />Corinne Friesen Loewen</p>
+			<p><strong>Bookeeper</strong><br />Pat Ireton</p>
+			<p><strong>Director of Development</strong><br />Shirley Elias</p>
+			<p><strong>Project support and Front-of-House Manager</strong><br />Elise Anderson</p>
+			<p><strong>Marketing</strong><br />Jon Snidal</p>
+			<p><strong>Concert Manager</strong><br />Boyd MacKenzie</p>
+			<p><strong>Librarian</strong><br />Ray Chrunyk</p>
+			<p><strong>Program advertising</strong><br />Brian Chipney</p>
+			<p><strong>Stage Manager</strong><br />Matthew Underhill</p>
+		</div>
 	</div>
 
 </div><!-- /row -->
