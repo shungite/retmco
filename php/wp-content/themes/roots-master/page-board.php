51f95ce7381355139a50fc1d42b74850ce66fe99
Jon Snidal <jsnidal@gmail.com>
2012-12-18 08:47:01 -0600

    Board

---
+++ php/wp-content/themes/roots-master/page-board.php
@@ -0,0 +1,84 @@
+<?php
+/*
+Template Name: Page Board Template
+*/
+?>
+<div class="container">
+
+<div class="row">
+
+	<div class="span11">
+		<a href="http://www.themco.ca"><img class="alignnone size-full wp-image-9" title="logosm" src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/09/logosm.png" alt="" width="150" height="78" /></a>
+	</div>
+
+</div><!-- /row -->
+
+<!-- <div class="row">
+
+	<div class="span8">
+		<img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/11/headhist.png" alt="" width="870" height="256">
+	</div>
+	<div class="span2">
+		<p>.</p>
+	</div>
+
+</div><!-- /row -->
+
+<div class="row">
+
+	<div class="span7">
+		<h4>Manitoba Chamber Orchestra<br />Board of directors &amp; staff</h4>
+	</div>
+
+</div><!-- /row -->
+
+<div class="row">
+
+	<div class="span6">
+		<p><strong>President</strong><br />
+		Jesus Angel Miguel Garcia</p>
+		<p><strong>Vice-President</strong><br />
+		Bill Neville</p>
+		<p><strong>Past President</strong><br />
+		Ingrid Moehlmann</p>
+		<p><strong>Treasurer</strong><br />
+		Gil Forrest</p>
+		<p><strong>Secretary</strong><br />
+		Alice MacKenzie</p>
+		<p><strong>Member at large</strong><br />
+		Jim Chapryk</p>
+
+		<p><strong>Members</strong></p>
+		<p>Lynne Axworthy<br />
+		Chloe Carpenter<br />
+		Judith Hall<br />
+		Joan Hardy<br />
+		Sue Irving<br />
+		Suzanne Kennelly<br />
+		Elaine Margolis<br />
+		Sandi Mielitz<br />
+		Beryl Peters<br />
+		Rishi Ramberran<br />
+		Nicola Schaefer<br />
+		Moti Shojania<br />
+		Alison Thiessen<br />
+		Elizabeth Tippett-Pope<br />
+		Faye Warren</p>
+
+		<p><strong>Staff</strong></p>
+		<p><strong>Managing Director</strong><br />Vicki Young</p>
+		<p><strong>Administrative Assistant</strong><br />Joan Gillis</p>
+		<p><strong>Administrative Assistant</strong><br />Corinne Friesen Loewen</p>
+		<p><strong>Bookeeper</strong><br />Pat Ireton</p>
+		<p><strong>Director of Development</strong><br />Shirley Elias</p>
+		<p><strong>Project support and Front-of-House Manager</strong><br />Elise Anderson</p>
+		<p><strong>Marketing</strong><br />Jon Snidal</p>
+		<p><strong>Program advertising</strong><br />Brian Chipney</p>
+		<p><strong>Stage Manager</strong><br />Matthew Underhill</p>
+	</div>
+
+</div><!-- /row -->
+
+<?php get_template_part('templates/content', 'history'); ?>
+
+</div><!-- /container -->
