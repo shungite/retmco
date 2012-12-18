f8b98979a29716d219ba0ef9175a6f52a2a738ce
Jon Snidal <jsnidal@gmail.com>
2012-12-18 11:16:28 -0600

    News

---
+++ php/wp-content/themes/roots-master/page-news.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Template Name: Page News Template
+*/
+?>
+<div class="container">
+
+<div class="row">
+
+	<div class="span11">
+		<a href="http://www.themco.ca"><img class="alignnone size-full wp-image-9" title="logosm" src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/09/logosm.png" alt="" width="150" height="78" /></a>
+	</div>
+
+</div><!-- /row -->
+
+<div class="row">
+
+	<div class="span7">
+		<h4>News and recent activities</h4>
+	</div>
+
+</div><!-- /row -->
+
+<?php get_template_part('templates/content', 'news'); ?>
+
+</div><!-- /container -->
