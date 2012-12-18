9e77872fd748f7b8eae856dcdb1c7c2c1b129fe8
Jon Snidal <jsnidal@gmail.com>
2012-12-18 12:07:16 -0600

    Post font size

--- php/wp-content/themes/roots-master/templates/content.php
+++ php/wp-content/themes/roots-master/templates/content.php
@@ -9,7 +9,7 @@
 <?php while (have_posts()) : the_post(); ?>
   <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header>
-      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
     <div class="entry-summary">
