d417a54d71689c2c1c062db3187d7dd22d9e7214
Jon Snidal <jsnidal@gmail.com>
2013-01-07 12:24:21 -0600

    Building Concert custom post type

--- php/wp-content/themes/roots-master/lib/custom.php
+++ php/wp-content/themes/roots-master/lib/custom.php
@@ -9,9 +9,27 @@ function create_my_post_types() {
 		array(
 			'labels' => array(
 				'name' => __( 'Concerts' ),
-				'singular_name' => __( 'Concert' )
+				'description' => __( 'Manitoba Chamber Orchestra Concert' ),
+				'singular_name' => __( 'Concert' ),
+				'add_new' => __( 'Add New' ),
+				'add_new_item' => __( 'Add New Concert' ),
+				'edit' => __( 'Edit' ),
+				'edit_item' => __( 'Edit Concert' ),
+				'new_item' => __( 'New Concert' ),
+				'view' => __( 'View Concert' ),
+				'view_item' => __( 'View Concert' ),
+				'search_items' => __( 'Search Concerts' ),
+				'not_found' => __( 'No concerts found' ),
+				'not_found_in_trash' => __( 'No concerts found in Trash' ),
+				'parent' => __( 'Parent Concert' ),
 			),
 			'public' => true,
+			'show_ui' => true,
+			'publicly_queryable' => true,
+			'exclude_from_search' => false,
+			'hierarchical' => true,
+			'query_var' => true,
+			'menu_position' => 20
 		)
 	);
 }
