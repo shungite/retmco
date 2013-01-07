754fd64a2441c6555f225b1d9792b38f71f010b2
Jon Snidal <jsnidal@gmail.com>
2013-01-07 11:54:35 -0600

    Started custom post type

--- php/wp-content/themes/roots-master/lib/custom.php
+++ php/wp-content/themes/roots-master/lib/custom.php
@@ -1,3 +1,18 @@
 <?php
 
 // Custom functions
+
+add_action( 'init', 'create_my_post_types' );
+
+function create_my_post_types() {
+	register_post_type( 'concert',
+		array(
+			'labels' => array(
+				'name' => __( 'Concerts' ),
+				'singular_name' => __( 'Concert' )
+			),
+			'public' => true,
+		)
+	);
+}
+
