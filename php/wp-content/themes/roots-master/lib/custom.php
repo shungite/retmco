ff8bb20fb833ef63ae973fd73e3ccc9e43f20658
Jon Snidal <jsnidal@gmail.com>
2013-01-09 08:10:08 -0600

    Custom post type fixed

--- php/wp-content/themes/roots-master/lib/custom.php
+++ php/wp-content/themes/roots-master/lib/custom.php
@@ -2,36 +2,32 @@
 
 // Custom functions
 
-add_action( 'init', 'create_my_post_types' );
-
 function create_my_post_types() {
-	register_post_type( 'concert',
-		array(
-			'labels' => array(
-				'name' => __( 'Concerts' ),
-				'description' => __( 'Manitoba Chamber Orchestra Concert' ),
-				'singular_name' => __( 'Concert' ),
-				'add_new' => __( 'Add New' ),
-				'add_new_item' => __( 'Add New Concert' ),
-				'edit' => __( 'Edit' ),
-				'edit_item' => __( 'Edit Concert' ),
-				'new_item' => __( 'New Concert' ),
-				'view' => __( 'View Concert' ),
-				'view_item' => __( 'View Concert' ),
-				'search_items' => __( 'Search Concerts' ),
-				'not_found' => __( 'No concerts found' ),
-				'not_found_in_trash' => __( 'No concerts found in Trash' ),
-				'parent' => __( 'Parent Concert' ),
-			),
-			'public' => true,
-			'show_ui' => true,
-			'publicly_queryable' => true,
-			'exclude_from_search' => false,
-			'hierarchical' => true,
-			'query_var' => true,
-			'taxonomies' => array( 'post_tag', 'category '),
-			'menu_position' => 20
-		)
+	$labels = array(
+		'name' => _x( 'Concerts', 'post type general name' ),
+		'singular_name' => _x( 'Concert', 'post type singular name' ),
+		'add_new' => _x( 'Add New', 'concert' ),
+		'add_new_item' => __( 'Add New Concert' ),
+		'edit_item' => __( 'Edit Concert' ),
+		'new_item' => __( 'New Concert' ),
+		'all_items' => __( 'All Concerts' ),
+		'view_item' => __( 'View Concert' ),
+		'search_items' => __( 'Search Concerts' ),
+		'not_found' => __( 'No concerts found' ),
+		'not_found_in_trash' => __( 'No concerts found in Trash' ),
+		'parent_item_colon' => '',
+		'menu_name' => 'Concerts'
 	);
+	$args = array(
+		'labels' => $labels,
+		'description' => __( 'Holds Manitoba Chamber Orchestra Concerts' ),
+		'public' => true,
+		'menu_position' => 20,
+		'supports' => array( 'title', 'editor', 'thumbnail', 'excerpt', 'comments' ),
+		'has_archive' => true,
+		);
+	register_post_type( 'concert', $args );
 }
 
+add_action( 'init', 'create_my_post_types' );
+?>
