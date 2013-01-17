9703d048b63b41f44b2f97bca047031efed9f027
Jon Snidal <jsnidal@gmail.com>
2013-01-17 15:35:13 -0600

    Home page edits

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -12,37 +12,74 @@ Template Name: Page MCOhome Template
 <div class="row-fluid">
 	
 	<div class="span8">
-   	<img src="http://www.themco.ca/wp-content/uploads/2013/01/cover06feb131.png">
+   	<a href="http://www.themco.ca/concert/winnipeg-singers/"><img src="http://www.themco.ca/wp-content/uploads/2013/01/cover06feb131.png"></a>
+		<h4>A double celebration:<br />The Winnipeg Singers and the MCO</h4>
+		<p>The Winnipeg Singers and the MCO celebrate 40 years this season, and this concert is the birthday party! Yuri Klaz will conduct a fascinating program of choral works by Vivaldi, Tallis, Mendelssohn, Arvo P&auml;rt, Imant Ramish and Glenn Buhr.</p>
   </div>
-
 </div><!-- /row -->
 
 <div class="row-fluid">
 	<p>&nbsp;</p>
 </div>
 
+<div class="row-fluid">
 
-<div class="row">
 	<div class="span7">
+		<div class="well-white">
 		<div class="media">
 			<a class="pull-left" href="#extras" data-toggle="tab">
-			<img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2012/11/Haley_Portrait_086.png">
+			<img class="media-object img-rounded" src="http://www.themco.ca/wp-content/uploads/2012/11/janina_small.jpg" width="96px" height="96px">
 			</a>
 		<div class="media-body">
-			<p><strong><a href="#extras" data-toggle="tab">EXTRAS!</strong></a><br />Check out the 'Extras' tab below for Haley Rempel&rsquo;s &lsquo;Two-minute Talk,&rsquo; <em>On Franz Joseph Haydn</em>, an article on Marc-Andr&eacute; from <em>La Libert&eacute;</em> and more!</p>
-			<p class="fineprint">&nbsp;<p>
+			<p><strong><a href="http://www.themco.ca/janina/" data-toggle="tab">MCO 40th anniversary fund-raiser:<br>A Candlelight evening with Janina Fialkowska!</strong></a><br />Celebrate four decades of the MCO with world renowned pianist Janina Fialkowska in The Fort Garry's opulent Provencher Room.</p>
 		</div>
 	</div>
+</div>
+</div>
+
+<div class="span1">
+	<p class="buy"><strong>Buy<br>tickets<br>to this<br>event!</strong></p>
 </div>
 
 </div><!-- /row -->
 
+<div class="row-fluid">
 
+	<div class="span7">
+		<div class="well">
+		<div class="media">
+			<a class="pull-left" href="#extras" data-toggle="tab">
+			<img class="media-object img-rounded" src="http://www.themco.ca/wp-content/uploads/2012/11/janina_small.jpg" width="96px" height="96px">
+			</a>
+		<div class="media-body">
+			<p><strong><a href="#extras" data-toggle="tab">MCO fund-raiser:<br>A Candlelight evening with Janina Fialkowska!</strong></a><br />Check out the 'Extras' tab below for Haley Rempel&rsquo;s &lsquo;Two-minute Talk,&rsquo; <em>On Franz Joseph Haydn</em>, an article on Marc-Andr&eacute; from <em>La Libert&eacute;</em> and more!</p>
+		</div>
+	</div>
+</div>
+</div>
+
+</div><!-- /row -->
+
+<div class="row-fluid">
+
+	<div class="span7">
+		<div class="well-white">
+		<div class="media">
+			<a class="pull-left" href="#extras" data-toggle="tab">
+			<img class="media-object img-rounded" src="http://www.themco.ca/wp-content/uploads/2012/11/janina_small.jpg" width="96px" height="96px">
+			</a>
+		<div class="media-body">
+			<p><strong><a href="#extras" data-toggle="tab">MCO fund-raiser:<br>A Candlelight evening with Janina Fialkowska!</strong></a><br />Check out the 'Extras' tab below for Haley Rempel&rsquo;s &lsquo;Two-minute Talk,&rsquo; <em>On Franz Joseph Haydn</em>, an article on Marc-Andr&eacute; from <em>La Libert&eacute;</em> and more!</p>
+		</div>
+	</div>
+</div>
+</div>
+
+</div><!-- /row -->
 
 <div class="row">
 
 	<div class="span2">
-		<p>&nbsp;</p>
 		<h6><i class="icon-warning-sign"></i><br />Warning!</h6>
 		<p class="fineprint">Please be patient as we rebuild our web site. We promise to reward you with better content and interactive features. In the meantime, feel free to send us some feedback...</p>
 		<a class="btn btn-mini" href="mailto:info@themco.ca?subject=Feedback ...">Complain »</a>
