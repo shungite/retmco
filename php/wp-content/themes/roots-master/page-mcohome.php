5c6f5aec211a68953222b5d08f6baf04b70f3bf4
Jon Snidal <jsnidal@gmail.com>
2013-01-18 13:35:25 -0600

    Front page edits

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -14,13 +14,17 @@ Template Name: Page MCOhome Template
 	<div class="span8">
    	<a href="http://www.themco.ca/concert/winnipeg-singers/"><img src="http://www.themco.ca/wp-content/uploads/2013/01/cover06feb131.png"></a>
 		<h4>A double celebration:<br />The Winnipeg Singers and the MCO</h4>
-		<p>The Winnipeg Singers and the MCO celebrate 40 years this season, and this concert is the birthday party! Yuri Klaz will conduct a varied program of choral works by Vivaldi, Tallis, Mendelssohn, Arvo P&auml;rt, Imant Ramish and Glenn Buhr. <a href="http://www.themco.ca/concert/winnipeg-singers/ "><strong>Program notes, bios and more here.</strong></a>
+		<p>The Winnipeg Singers and the MCO celebrate 40 years this season, and this concert is the birthday party! Yuri Klaz will conduct a varied program of choral works by Vivaldi, Tallis, Mendelssohn, Arvo P&auml;rt, Imant Ramish and Glenn Buhr. Read lots more about the concert <a href="http://www.themco.ca/concert/winnipeg-singers/"><strong>here</strong></a>, or click below for quick tickets.<br>
+		<a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=89e3b17ba5474af284f562865c4b70e1"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/AdultC.png" alt="" width="62" height="40" /></a>
+ 	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=696960f1b3b241bab82da74d814a3c13"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/SeniorC.png" alt="" width="62" height="40" /></a>
+	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=49f2aef74f6c4dabb1248afb38f3358d"><img rel="tooltip" title="Click to add student ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/StudentC.png" alt="" width="62" height="40" /></a>
+
 		</p>
   </div>
 </div><!-- /row -->
 
 <div class="row-fluid">
-	<p>&nbsp;</p>
+	<small>&nbsp;</small>
 </div>
 
 <div class="row-fluid">
@@ -58,7 +62,15 @@ Template Name: Page MCOhome Template
 
 </div><!-- /row -->
 
-<div class="row">
+<div class="row-fluid">
+
+<div class="span4">
+	<div class="well">
+		<p><strong><a href="#">Sign up here for MCO&rsquo;s<br><em>Noteworthy</em> email newsletter!</strong></a></p>
+		<p>Our just-about-monthly missive is filled with information on upcoming concerts and special events. Unsubscribe at any time.</p>
+		<?php gravity_form(3, false, false, false, '', true, false); ?>
+	</div>
+</div>
 
 	<div class="span2">
 		<h6><i class="icon-warning-sign"></i><br />Warning!</h6>
@@ -67,7 +79,8 @@ Template Name: Page MCOhome Template
 		<p>&nbsp;</p>
 	</div><!-- /span2 -->
 
-</div><!-- /row -->	
+
+</div><!-- /row -->
 
 <?php get_template_part('templates/content', 'mcohome'); ?>
 
