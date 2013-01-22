16e7aaf5bd9e6b0975efae6fbe851c5dce7f6065
Jon Snidal <jsnidal@gmail.com>
2013-01-22 09:27:57 -0600

    Programs page updated

--- php/wp-content/themes/roots-master/page-programs.php
+++ php/wp-content/themes/roots-master/page-programs.php
@@ -18,21 +18,36 @@ Template Name: Page Programs Template
 
 <div class="row">
 
-	<div class="span6">
+	<div class="span7">
 		<h4>Students love the excitement<br />of live chamber music!</h4>
 		<p>Whether it&rsquo;s a school band or choir, Orff group, recorder ensemble, or private studio, children learn through music! There are so many reasons to bring the young people in your life to an MCO event &mdash; they won&rsquo;t hear anything like this anywhere else.</p>
 		<p>Help them create a cultural and musical framework for themselves. Show them where their music studies can lead &mdash; they can become an audience member, composer, conductor, musician, board member, donor, educator or CD purchaser. Have fun sharing music with them and with other avid music fans.</p>
 		<p><strong>School visits &mdash; the MCO comes to you</strong> / the MCO Outreach Ensemble is a roaming troupe of MCO players consisting of violin, viola, cello, double bass and guitar. It offers workshops and mini-concerts in schools throughout the province. Call 204.783.7377 for more info or to request a visit.</p>
+		<p>&nbsp;</p>
 			<div class="well">
 				<p><strong>Special matinee performance of <em>Brundib&agrave;r</em><br />1:15 pm, Wednesday, March 12th, Westminster Church</strong></p>
 				<p>Kras&agrave;s <em>Brundib&agrave;r</em> will be performed by the MCO and the Pembina Trails Voices children&rsquo;s choir (Ruth Wiwchar, Director). Brundib&agrave;r is a children&rsquo;s opera by Jewish Czech composer Hans Kras&agrave; with a libretto by Adolf Hoffmeister. The piece was originally performed by the children of Theresienstadt concentration camp in occupied Czechoslovakia. The name come from a Czech colloquialism for a bumble bee.</p>
 			</div>
 	</div><!-- /span6 -->
 
-	<div class="span2"> 
+	<div class="span1"> 
 		<a href="http://www.themco.ca/schools-form/"><img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /></a><br />
 		<p class="fineprint">Click above for order form.</p>
 	</div><!-- /span2 -->
+
+</div><!-- /row -->
+
+<div class="row-fluid">
+
+	<div class="span7">
+		<div class="well-white">
+			<div class="media-body">
+				<p><strong><a href="http://www.sunlife.ca/">Sun Life Financial</a><br>Arts Outreach Program: Open dress rehearsals for schools</strong></p>
+				<p>Bring your school group to Westminster Church for a 50-minute open dress rehearsal. Learn about the work that goes into presenting a professional orchestral performance. Watch musicians and conductor rehearse and be the first to hear what audiences will hear that night! <strong>Tickets $5.25 including GST.</strong> These open dress rehearsals are best suited to senior students, adults and home-schooled children. They are working rehearsals with some stopping and starting. Offered for the concerts listed <a href="http://www.themco.ca/schools-form"><strong>on this form.</strong></a></p>
+				<p><a href="http://www.sunlife.ca/"><img src="http://www.themco.ca/wp-content/uploads/2013/01/SunLife-logo.png"></a></p> 
+			</div>
+		</div>
+	</div>
 
 </div><!-- /row -->
 
