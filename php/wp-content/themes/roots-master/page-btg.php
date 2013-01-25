7dd768a66e9d3c422d6708b7b932d69423186d11
Jon Snidal <jsnidal@gmail.com>
2013-01-25 15:30:29 -0600

    BTG added

---
+++ php/wp-content/themes/roots-master/page-btg.php
@@ -0,0 +1,108 @@
+<?php
+/*
+Template Name: Page Btg Template
+*/
+?>
+<div class="container">
+
+<div class="row">
+
+	<div class="span8">
+		<img src="http://www.themco.ca/wp-content/uploads/2013/01/headbtg.jpg" alt="" width="770" height="455">
+	</div>
+	
+</div><!-- /row -->
+
+<div class="row">
+
+	<div class="span7">
+		<p>&nbsp;</p>
+		<p><b>A Study Guide for Brundib&aacute;r<br>Music by Hans Krasa<br>Original libretto by Adolf Hoffmeister<br>English translation by Tony Kushner</b></p>
+		<p><b><a href="http://www.themco.ca/wp-content/uploads/2013/01/BrundibárTeacherGuide.pdf">PDF available here.</a></b></p>
+		<p>Manitoba Chamber Orchestra  and the Pembina Trails Voices Anne Manson, Conductor Ruth Wiwchar, Director of the Pembina Trails Voices Donna Fletcher, Director</p>
+		<p>Performances March 12, 2013: Westminster Church 1:15 pm / school matinee with introduction and Q&amp;A session to follow 7:30 pm / evening concert</p>
+		<p>Funding for this project has been provided by: Manitoba Arts Council Canada Council for the Arts Winnipeg Arts Council The Winnipeg Foundation The Jewish Foundation of Manitoba  Manitoba Opera</p>
+		<p> The purpose of this guide We hope this guide will help you encourage your students&Otilde; understanding and  appreciation of this performance of Brundib&Dagger;r, and its relationship to world history.  Brundib&Dagger;r gives us an opportunity to introduce the topics of the Holocaust, racial  discrimination, and the treatment of Jewish people in the Nazi concentration town  of Terezin during World War II. Please be sure to examine any website content for age  appropriateness prior to recommending to your students. This guide will provide you with background material on the composer,  Terezin (or Theresienstadt), a synopsis of the story, and activities that  can be incorporated into your curriculum. </p>
+
+		<p><b>Table of contents</b></p>
+		<p>Meet the composer  Hans Kras&Dagger;  Websites  Timeline</p>
+
+<p>About Terezin, Czechoslovakia   About Brundib&Dagger;r  Cast   Synopsis</p>
+
+<p>Before going to Brundib&Dagger;r  Connect and motivate  Audience Etiquette</p>
+
+<p>During the performance   Look  Listen  Think</p>
+
+<p>After attending Brundib&Dagger;r  React  Retell  Write something original  Research</p>
+
+<p> Curricular connections   Aesthetics and art criticism  English, reading and writing  Science  Create, present, perform</p>
+
+<p> Meet the Composer: Hans Kras&Dagger; Hans Kras&Dagger; was born on November 30, 1899 in Prague to a Czech father, who was  a lawyer, and a Jewish German mother. He studied both the piano and the violin as  a child, and went on to study composition at the German Music Academy in Prague.  After graduating, he became a vocal coach at the Neues Deutsches Theater (New German  Theatre), where he met the composer and conductor, Alexander Zemlinsky, who had  a major impact on Kras&Dagger;&Otilde;s career. In 1942, he was transported to the Nazi concentration town of Terezin, Czechoslovakia  (now the Czech republic). While he was interned in the ghetto, Kras&Dagger; was at his most  productive, producing a number of chamber works. Due to the circumstances,  some of these have not survived. Along with fellow composers Viktor Ullmann, Pavel Haas and Gideon Klein, Kras&Dagger;  was sent to Auschwitz and, being considered too old to work and also a possible  threat to order, was killed in October 1944. He was not yet 45 years old.</p>
+
+<p> </p>
+
+<p>For more information on Hans Kras&Dagger;, visit the following websites:</p>
+
+<p>http://orelfoundation.org/index.php/composers/article/hans_krasa/</p>
+
+<p>For information on children and the Holocaust, visit the following site:</p>
+
+<p>http://fcit.usf.edu/HOLOCAUST/people/children.htm</p>
+
+<p>  </p>
+
+<p> About Terezin, Czech Republic In the late 18th century, Terezin was founded as a garrison town during the reign  of Emperor Joseph II and named after his mother, Empress Maria Theresa. However,  in 1941, the &Ocirc;Ghetto Theresienstadt&Otilde; was established and between the years of 1941  and 1945 the Nazis transferred about 140,000 Jewish people to the town. At its largest,  there were approximately 55,000 people concentrated in Terezin, a town that before  the war had housed about 5,000 residents. Children deported to Terezin were not kept with their families, but were watched over  by counselors. Education was strictly forbidden, but the counselors found ways around  this rule by telling stories, singing songs, and acting out plays to covertly teach history,  religion, reading, writing and mathematics. One of the most tragic statistics is that of the  10,000 children who made their way through Terezin, only a tiny percentage survived.  Some place the figure at 100, others at 1,000, but either way it is horrifically small.</p>
+
+<p>  The cast of Brundib&Dagger;r in a Terezin production</p>
+
+<p>For more information on Terezin, visit the following websites: http://www.jewishvirtuallibrary.org/jsource/Holocaust/terezin.html</p>
+
+<p>http://www.google.ca/search?q=terezin&amp;hl=en&amp;tbo=u&amp;tbm=isch&amp;source=univ&amp;sa=X&amp;ei =UF0AUcu9DdKE2QXM1oFo&amp;ved=0CE8QsAQ&amp;biw=1626&amp;bih=997</p>
+
+<p>About Brundib&Dagger;r Brundib&Dagger;r is a half-hour opera written to be performed by and for children. Composed by  Hans Kras&Dagger; in 1938, with lyrics by Adolf Hoffmeister (translated by Tony Kushner in  2003), as an entry for a children&Otilde;s opera competition, it received its premiere in German- occupied Prague, performed by children at the Jewish orphanage in Belgicka Street.  Brundib&Dagger;r had one additional performance before the mass transports of Bohemian and  Moravian Jews to Terezin began in 1942. In July 1943, the score of Brundib&Dagger;r was  smuggled into camp, where it was re-orchestrated by Krasa for the various  instrumentalists who were resident in the camp at that time, and the premiere of the  Terezin version took place on 23 September 1943 in the hall of the Magdeburg barracks.  Realizing the propagandistic potential of this enormously popular artistic endeavour, the  Nazis arranged a special new staging of Brundib&Dagger;r for the propaganda film  Theresienstadt?&Ntilde;?eine Dokumentarfilm aus en judische Siedlungsgebiet (directed by Kurt  Gerron), and the same production was performed for the inspection of Terezin by the  International Red Cross in September 1944. This would be the last of the fifty-five  performances in the Terezin Ghetto; two weeks later, transportation of artists began to  Auschwitz and other destinations East, silencing this, the most popular theatrical  production in Terezin. The camp audience quickly grasped that the simple plot of Brundib&Dagger;r represented all that  the Nazi regime stood for. When the children sang their final song of victory over the cruel  Brundibar, there was no doubt about the evil he personified; this was the very evil that  caused the young performers of the opera to be deported to Auschwitz and the gas  chambers. Survivors report that people would sing the victory march throughout the  streets of Terezin, raising their hopes that they, too, might find victory over a tyrant. The story and music are accessible enough for a young audience, but layered with  historical references, making the opera a poignant experience for an adult audience as  well. For more information about Brundib&Dagger;r visit:</p>
+
+<p>http://www.cbsnews.com/stories/2007/02/23/60minutes/main2508458.shtml</p>
+
+<p>http://holocaustmusic.ort.org/places/theresienstadt/Brundib&Dagger;r/</p>
+
+<p>Cast of Characters and the Pembina Trails choirs to which they belong Pepicek: a young boy: Mitch Heron &Ntilde; PTV Men Aninku: his sister: Stephanie Bell &Ntilde; Chorale Brundib&Dagger;r: the organ grinder: John Anderson &Ntilde; PTV Men Ice cream seller: Alexandra Skwarchuk &Ntilde; Chorale Baker: David Newell &Ntilde; PTV Boys Milkman: Matthew Woelk &Ntilde; PTV Boys Policewoman: Rachel Woelk &Ntilde; Cantemus Sparrow: Jane Petroff &Ntilde; Cantemus Cat: Tessa Jenkyns &Ntilde; Cantemus Dog: Ella Wiebe &Ntilde; Chorale Chorus of young people from the neighbourhood</p>
+
+<p>Synopsis Aninku and Pepicek, two little children, have a sick mother. The doctor has prescribed milk  for her health, and they go seeking milk in the town marketplace, but they have no money  with which to purchase it. Three traders hawk their wares: an ice-cream man, a baker and  a milkman. The children engage the milkman in a song, but he tells them that they need  money for milk. Suddenly, the children spot the organ-grinder, Brundib&Dagger;r, playing on the  street corner. Seeing his success, they decide to busk as well (and proceed to sing a song  about geese), much to the annoyance of the townsfolk and Brundib&Dagger;r, who chase them  away. Three animals?&Ntilde;?a sparrow, cat and dog?&Ntilde;?come to their help, and together they  recruit the other children of the neighbourhood in their plan. Night falls, the dawn comes,  the children and animals begin morning exercises and the townsfolk get ready for the day.  The plan goes ahead: the animals and children drown out Brundib&Dagger;r; they then join in a  beautiful lullaby. The townsfolk are very moved and give Aninku and Pepicek money.  Suddenly, Brundib&Dagger;r sneaks in and steals their takings. All the children and animals give  chase and recover the money. The opera concludes with a victory march sung about  defeating the evil organ-grinder.</p>
+
+<p> Before attending the concert Connect and motivate Discuss:  ? What do you know about the story of Brundib&Dagger;r? What do you think is the point of  the story? ? What is a tyrant? Do you know any tyrants? ? What do you think the opera will be like?</p>
+
+<p>Audience etiquette Discuss: ? Is it all right to laugh? Cry? ? Is it all right to applaud? When?</p>
+
+<p>Read the background information Discuss life in Europe during the Second World War ? How did life differ from the times in which we now live? ? How do you think the times may have inspired the story of the opera?</p>
+
+<p>During the performance Look What do you see? Notice the following: ? Sets and props ? Costumes ? Movement/choreography</p>
+
+<p>Listen ? How does each scene begin? ? What arias or other songs are sung? ? How does the music change the mood?</p>
+
+<p>Think ? What problems arise in the opera? ? How are the problems solved?</p>
+
+<p>After the performance React to the experience Write or talk about the following: ? Did you like the opera? Why or why not? ? What did you like the best or least? ? What character(s) did you like the best or least? ? Sets/costumes/props</p>
+
+<p> Retell the story Write something original ? Write a song for one of the characters in the opera ? Write a song for a fairytale character ? Write a story, song, poem or skit with ideas from the opera, such as:  helping friends  talking animals  your mother  money  greed  victory</p>
+
+<p>Research Find out more about one of the following: ? Terezin, Czech Republic ? Fairytales ? Hans Krasa ? Surviving cast members, such as Ella Weissberger &ETH; the original Cat</p>
+
+<p>Curricular connections </p>
+
+<p>Aesthetics and art criticism ? After reviewing the Study Guide and attending the Manitoba Chamber Orchestra&Otilde;s  production of Brundib&Dagger;r, ask students to write a review of the production, noting  how the music directly affects the emotional impact for the listener.</p>
+
+<p>English, reading and writing ? After seeing the Manitoba Chamber Orchestra&Otilde;s production of Brundib&Dagger;r, ask  students to write a journal entry or review of the show as a reflection. ? Write a sequel to Brundib&Dagger;r about what happens to the characters or life in the  town 5 years after the action in opera. ? Using persuasive writing styles, create a new ending for the opera, using known  character information to produce absolute change within one or more of the  characters. ? Ask the students to consider whether there are other stories that remind them of  Brundib&Dagger;r. Ask the students to choose a fairytale and write an opera libretto.  Remind them that they may have to streamline and/or simplify the story. It takes  longer to sing something than to say it. The libretto will consist almost entirely of  dialogue. After they have written the libretto, have them reflect on what they had  to do to take a written story and make it work as a dramatic or musical one. They  can use poetry or not, as they wish.</p>
+
+<p> Science The voice is a combination of a wind instrument and a string instrument &ETH; air passing  through the vocal cords creates a vacuum, pulling the vocal cords closed, The cords then  vibrate together and create sound. Pitch is determined by the tension of the vocal cords,  just like a violin or guitar. You can demonstrate this with a rubber band: wrap a rubber  band around your fingers. Pluck it a few times. Can you see and feel the vibrations? The  harder you pluck the rubber band, the more it will vibrate, creating a louder sound. If you  stretch the rubber band, making it longer and thinner, what do you hear? (It will be a  higher pitch.) Have your students place their hands on their throats while speaking or  singing at different pitches so they can feel the vibrations in their throats and chests.  Explore sound waves.</p>
+
+<p>Create, present, perform ? Obtain copies of the translation of the libretto for Brundib&Dagger;r. Have students read  the parts in a dramatic reading, infusing as much feeling and power into the words  as possible. The language may be awkward or embarrassing. Ask them to translate  the libretto into their own way of speaking and perform that way. Then return to  the original libretto. How does their perception of the language change &Ntilde; or not?  Do they think that music can affect how the lines sound? ? Have students work in groups to write their own &Ocirc;operas&Otilde; using popular songs,  stringing them together with dialogue. These could be performed for the class.</p>
+
+</div>
+
+</div><!-- /row -->
+
+</div><!-- /row -->
+
+<?php get_template_part('templates/content', 'btg'); ?>
+
+</div><!-- /container -->
