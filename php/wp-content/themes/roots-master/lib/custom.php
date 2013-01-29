ed84c3136ec19d31de4d69ef0f21bbfed614d7c4
Jon Snidal <jsnidal@gmail.com>
2013-01-29 10:10:22 -0600

    Howdy changed to Ola

--- php/wp-content/themes/roots-master/lib/custom.php
+++ php/wp-content/themes/roots-master/lib/custom.php
@@ -72,3 +72,13 @@ function create_event_post_types() {
 
 add_action( 'init', 'create_concert_post_types' );
 add_action( 'init', 'create_event_post_types' );
+
+function replace_howdy( $wp_admin_bar ) {
+ $my_account=$wp_admin_bar->get_node('my-account');
+ $newtitle = str_replace( 'Howdy,', 'Ola,', $my_account->title );
+ $wp_admin_bar->add_node( array(
+ 'id' => 'my-account',
+ 'title' => $newtitle,
+ ) );
+ }
+ add_filter( 'admin_bar_menu', 'replace_howdy',25 );
