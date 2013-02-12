af87524b7c11693e2f568df597e35fd38a1203f7
Jon Snidal <jsnidal@gmail.com>
2013-02-12 07:55:20 -0600

    Brundibar corr

--- php/wp-content/themes/roots-master/ConcertTxt/page-concert6.php
+++ php/wp-content/themes/roots-master/ConcertTxt/page-concert6.php
@@ -24,7 +24,7 @@ Template Name: Page Concert6 Template
 		  <p>The music is infectious, joyful, lyrical and very rhythmic.</p>
   		<small>Anne Manson</small>
 		</blockquote>
-		<p>Two entertaining works will be offered in staged presentations at this concert: Kras&aacute;&rsquo;s <em>Brundib&aacute;r</em> will be performed by the MCO and the Pembina Trails Voices children&rsquo;s choir, under the direction of Ruth Wiwchar; and Stravinsky&rsquo;s <em>l&rsquo;Histoire du Soldat</em> is a narrated work.</p>
+		<p>Two entertaining works will be offered in staged presentations at this concert: Kr&aacute;sa&rsquo;s <em>Brundib&aacute;r</em> will be performed by the MCO and the Pembina Trails Voices children&rsquo;s choir, under the direction of Ruth Wiwchar; and Stravinsky&rsquo;s <em>l&rsquo;Histoire du Soldat</em> is a narrated work.</p>
 		<p>Brundib&aacute;r is a children&rsquo;s opera by Jewish Czech composer Hans Kr&aacute;sa with a libretto by Adolf Hoffmeister, originally performed by the children of Theresienstadt concentration camp in occupied Czechoslovakia. The name comes from a Czech colloquialism for a bumblebee.</p>
 		<p>&ldquo;<em>Brundib&aacute;r</em> is a terrific piece,&rdquo; says Anne Manson. &lsquo;Ruth had proposed this piece years ago for a collaboration. It&rsquo;s a great story and very empowering for children, who take all the roles in this work. It is emblematic of resisting a fascist leader, but the music is infectious, joyful, lyrical and very rhythmic.</p>
 		<p>It is a very important piece to play and to keep playing. I&rsquo;ve done it many times with different children’s choirs all over Europe.&rdquo; This concert is designed for the whole family to enjoy together!</p>
@@ -64,7 +64,7 @@ Template Name: Page Concert6 Template
 			<div class="tab-pane fade in active" id="repertoire">
 				<p><strong>Manitoba Chamber Orchestra</strong><br />Anne Manson, Music Director and Conductor<br />Karl Stobbe, Concertmaster<br />Westminster United Church<br />12 March 2013</p>
 				<p>Pembina Trails Voices<br />Ruth Wiwchar, Director</p>
-				<p><strong>Hans Kras&aacute;</strong><br /><em>Brundib&aacute;r</em></p>
+				<p><strong>Hans Kr&aacute;sa</strong><br /><em>Brundib&aacute;r</em></p>
 				<p><strong>Igor Stravinsky</strong><br /><em>l&rsquo;Histoire du Soldat</em></p>
 				<p>Concert sponsor / <strong>Gail Asper Family Foundation</strong><br />Season sponsor / <strong><a href="http://www.greatwestlife.com/001/Home/index.htm">The Great-West Life Assurance Company</a></strong><br />Print media sponsor / <strong><a href="http://www.winnipegfreepress.com/">Winnipeg Free Press</a></strong><br />Radio media sponsors / <strong><a href="http://music.cbc.ca/#/radio2/">CBC Radio 2 98.3</a></strong>, <strong><a href="http://www.cbc.ca/programguide/daily/today/cbc_radio_one">CBC Radio One 990</a></strong>,<br /><strong><a href="http://www.espace.mu/Espace-Musique/">Espace musique 89,9</a></strong> and <strong><a href="http://www.goldenwestradio.com/">Golden West Radio</a></strong>
 			</div>
