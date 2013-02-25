69c78e9425886c181f0de54b40955e0cb6af8924
Jon Snidal <jsnidal@gmail.com>
2013-02-25 13:56:44 -0600

    Thumbnails repositioned

--- php/wp-content/themes/roots-master/templates/content.php
+++ php/wp-content/themes/roots-master/templates/content.php
@@ -6,9 +6,8 @@
   <?php get_search_form(); ?>
 <?php endif; ?>
 
-<?php the_post_thumbnail( 'thumbnail' ); ?> 
-
 <?php while (have_posts()) : the_post(); ?>
+  <?php the_post_thumbnail( 'thumbnail' ); ?> 
   <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header>
       <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
