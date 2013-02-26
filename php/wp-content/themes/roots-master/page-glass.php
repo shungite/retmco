75771a61e7bc4abc8a629a385e3186117a8e344c
Jon Snidal <jsnidal@gmail.com>
2013-02-26 10:43:49 -0600

    CD page added

---
+++ php/wp-content/themes/roots-master/page-glass.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Template Name: Page Glass Template
+*/
+?>
+<div class="container">
+
+	<div class="row">
+
+		<p>&nbsp;</p>
+
+		<div class="span6">
+
+			<p>Our newest compact disc, with the MCO conducted by <b>Anne Manson</b> and featuring pianist <b>Michael Riesman</b> and the music of <b>Philip Glass</b>, will be released on February 26th, so we&rsquo;ll all have to wait until then to give it a spin. But be sure to receive the first copies &mdash; pre-order now! <a href="http://www.themco.ca/something-awesome/"><b>Lots more about the project here.</b></a></p>
+			<p>&nbsp;</p>
+			<img alt="OMM CD cover" src="http://www.themco.ca/wp-content/uploads/2013/02/cd570.jpg" />
+			<p><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=6df00871581f461c94917dd52dca8293"><button class="btn btn-large btn-info" type="button">Click to add to cart</button></a></p>
+			<small><b>Price: $21 (inc GST)</b>; shipping and handling is $2.50; you may also arrange to pick up product at the MCO office or at a concert (please call 204.783.7377 to make arrangements after pre-ordering). Orders will be filled on a first-to-order, first-to-ship basis.</small>
+
+		</div>
+
+	</div><!-- /row -->
+
+<?php get_template_part('templates/content', 'glass'); ?>
+
+</div><!-- /container -->
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
