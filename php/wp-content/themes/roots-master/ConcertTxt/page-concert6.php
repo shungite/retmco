92177797371192c43e17b37e73312c5684780ad7
Jon Snidal <jsnidal@gmail.com>
2013-03-15 09:06:17 -0500

    Review added

--- php/wp-content/themes/roots-master/ConcertTxt/page-concert6.php
+++ php/wp-content/themes/roots-master/ConcertTxt/page-concert6.php
@@ -19,7 +19,7 @@ Template Name: Page Concert6 Template
 <div class="row">
 
 	<div class="span7">
-		<h4>Fascinating<br />and thought-provoking<br />family entertainment</h4>
+		<h4>Fascinating<br>and thought-provoking<br>family entertainment</h4>
 		<blockquote>
 		  <p>The music is infectious, joyful, lyrical and very rhythmic.</p>
   		<small>Anne Manson</small>
@@ -40,9 +40,9 @@ Template Name: Page Concert6 Template
 	</div><!-- /span7 -->
 
 	<div class="span1"> 
-		<img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /><br />
-	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=720d6e6e7bb549aa8f5532ef65ea4edd"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="left" src="http://www.themco.ca/wp-content/uploads/2013/01/AdultC.png" alt="" width="62" height="40" /></a><br />
- 	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=ce47fb63079d4f84b5907716f09cf6c7"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="left" src="http://www.themco.ca/wp-content/uploads/2013/01/SeniorC.png" alt="" width="62" height="40" /></a><br />
+		<img src="http://retmco-shungite.rhcloud.com/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /><br>
+	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=720d6e6e7bb549aa8f5532ef65ea4edd"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="left" src="http://www.themco.ca/wp-content/uploads/2013/01/AdultC.png" alt="" width="62" height="40" /></a><br>
+ 	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=ce47fb63079d4f84b5907716f09cf6c7"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="left" src="http://www.themco.ca/wp-content/uploads/2013/01/SeniorC.png" alt="" width="62" height="40" /></a><br>
 	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=a3bd1ff75de449a3a8cf1a61948316b5"><img rel="tooltip" title="Click to add student ticket to cart" data-placement="left" src="http://www.themco.ca/wp-content/uploads/2013/01/StudentC.png" alt="" width="62" height="40" /></a>
 		<p class="fineprint">Click ticket above to add to shopping cart; adjust quantity in cart and return to purchase other types of tickets.</p>
 	</div><!-- /span2 -->
@@ -63,11 +63,11 @@ Template Name: Page Concert6 Template
 		</ul>
 		<div class="tab-content">
 			<div class="tab-pane fade in active" id="repertoire">
-				<p><strong>Manitoba Chamber Orchestra</strong><br />Anne Manson, Music Director and Conductor<br />Karl Stobbe, Concertmaster<br />Westminster United Church<br />12 March 2013</p>
-				<p>Pembina Trails Voices<br />Ruth Wiwchar, Director</p>
-				<p><strong>Hans Kr&aacute;sa</strong><br /><em>Brundib&aacute;r</em></p>
-				<p><strong>Igor Stravinsky</strong><br /><em>l&rsquo;Histoire du Soldat</em></p>
-				<p>Concert sponsor / <strong>Gail Asper Family Foundation</strong><br />Season sponsor / <strong><a href="http://www.greatwestlife.com/001/Home/index.htm">The Great-West Life Assurance Company</a></strong><br />Print media sponsor / <strong><a href="http://www.winnipegfreepress.com/">Winnipeg Free Press</a></strong><br />Radio media sponsors / <strong><a href="http://music.cbc.ca/#/radio2/">CBC Radio 2 98.3</a></strong>, <strong><a href="http://www.cbc.ca/programguide/daily/today/cbc_radio_one">CBC Radio One 990</a></strong>,<br /><strong><a href="http://www.espace.mu/Espace-Musique/">Espace musique 89,9</a></strong> and <strong><a href="http://www.goldenwestradio.com/">Golden West Radio</a></strong>
+				<p><strong>Manitoba Chamber Orchestra</strong><br>Anne Manson, Music Director and Conductor<br>Karl Stobbe, Concertmaster<br>Westminster United Church<br>12 March 2013</p>
+				<p>Pembina Trails Voices<br>Ruth Wiwchar, Director</p>
+				<p><strong>Hans Kr&aacute;sa</strong><br><em>Brundib&aacute;r</em></p>
+				<p><strong>Igor Stravinsky</strong><br><em>l&rsquo;Histoire du Soldat</em></p>
+				<p>Concert sponsor / <strong>Gail Asper Family Foundation</strong><br>Season sponsor / <strong><a href="http://www.greatwestlife.com/001/Home/index.htm">The Great-West Life Assurance Company</a></strong><br>Print media sponsor / <strong><a href="http://www.winnipegfreepress.com/">Winnipeg Free Press</a></strong><br>Radio media sponsors / <strong><a href="http://music.cbc.ca/#/radio2/">CBC Radio 2 98.3</a></strong>, <strong><a href="http://www.cbc.ca/programguide/daily/today/cbc_radio_one">CBC Radio One 990</a></strong>,<br><strong><a href="http://www.espace.mu/Espace-Musique/">Espace musique 89,9</a></strong> and <strong><a href="http://www.goldenwestradio.com/">Golden West Radio</a></strong>
 			</div>
 			<div class="tab-pane fade" id="bios">
 				<p><strong>Ruth Wiwchar</strong></p>
@@ -84,7 +84,7 @@ Template Name: Page Concert6 Template
 				<p>Touring is an integral part of the program for middle and senior years choristers. Sharing choral music with audiences throughout Canada, the United States, the Caribbean and Europe has brought an incredible richness and a depth of cultural awareness to the choristers. Touring highlights over the years include performing in Newfoundland, Prague, Vienna, and at New York&rsquo;s Carnegie Hall.</p>
 			</div>
 			<div class="tab-pane fade" id="notes">
-				<p><strong>Brundib&aacute;r (The Bumblebee)<br />Hans Kr&aacute;sa</strong></p>
+				<p><strong>Brundib&aacute;r (The Bumblebee)<br>Hans Kr&aacute;sa</strong></p>
 				<p>Born in Prague of German descent, Kr&aacute;sa, like many other Jewish artists, perished in the Nazi Holocaust. His tragically brief career knew its share of success, and promised even more. In 1927, when he was 28, one of his teachers, the esteemed composer / conductor Alexander Zemlinsky, helped promote Kr&aacute;sa&rsquo;s conducting career by taking him along to Berlin as his assistant at the Kroll Opera. By that time, Kr&aacute;sa&rsquo;s Symphony for Small Orchestra had already won a favourable reception in Zurich, Boston and New York. He received conducting offers from Berlin, Paris, and Chicago, but since he was unwilling to accept a post in a foreign country, he returned to Prague. He threw himself into multiple activities, including composing his first opera, <em>Verlobung im Traum</em> (Betrothal in a Dream, 1933, from a story by the celebrated Russian author, Fyodor Dostoyevsky). It was produced in Prague under the esteemed conductor, George Szell.</p>
 				<p>Because Kr&aacute;sa was Jewish and a vocal anti-fascist, in August 1942 he was sent to the Terezín (Theresienstadt) facility near Prague, a transitional location for people destined for the death camps. The Nazis tried to disguise it as a pleasant place to stay, prettifying it for a visit by a Red Cross representative in June 1944, for example.</p>
 				<p>At first, the Terezín authorities merely tolerated artistic activity, but eventually they encouraged it. Kr&aacute;sa composed several works there, and served as music director of a group that administered the inmates&rsquo; &lsquo;leisure&rsquo; time. On 16 October 1944, he was sent on to the Auschwitz camp, where he was murdered in the gas chambers two days later.</p>
@@ -96,7 +96,7 @@ Template Name: Page Concert6 Template
 				<p><em>Brundib&aacute;r</em> was published in 1992, and has received numerous performances in many lands. On one level, the engaging music and story provide pleasant family entertainment. On another, echoes of the heart-piercing circumstances under which it was first performed, and its stirring messages of hope, cooperation, and the defiance of evil (embodied in <em>Brundib&aacute;r</em>, the Hitler-like organ grinder), give it much different and deeper resonance. This MCO performance uses an English-language text by the noted British opera director, John Abulafia. His production of <em>Brundib&aacute;r</em> for the Mecklenburg Opera Company, conducted by Anne Manson, was broadcast by the BBC in 1993.</p>
 				<p>Scene 1 : Two children, Aninka and Pep&iacute;&#269;ek, have a sick mother, who needs milk to get better. They have no money to buy it from the milkman, so they decide to imitate the wicked, bullying organ-grinder, Brundib&aacute;r, and raise money by singing on a street corner. Brundib&aacute;r and a policeman chase them away. The children spend the night on a bench. A dog, a cat, and a sparrow offer to help them.</p>
 				<p>Scene 2 : In the morning, the children and their friends, animal and human alike, drown out Brundib&aacute;r&rsquo;s organ playing and together sing a beautiful lullaby for Aninka and Pepiček&rsquo;s mother. Their performance earns them generous donations from passersby. Brundib&aacute;r steals their money, but the crowd of children chase him down and get it back. To conclude the opera, all the children sing a stirring song about friends banding together to gain victory over tyranny.</p>
-				<p><strong>L&rsquo;Histoire du soldat (The Soldier&rsquo;s Tale)<br />Igor Stravinsky</strong></p>
+				<p><strong>L&rsquo;Histoire du soldat (The Soldier&rsquo;s Tale)<br>Igor Stravinsky</strong></p>
 				<p>Stravinsky shot to fame through his brilliant collaborations with the legendary Russian impresario, Sergei Diaghilev, and his dance company, Les Ballets russes: <em>The Firebird</em> (1910), <em>Petrushka</em> (1911) and <em>The Rite of Spring</em> (1913). The First World War temporarily put a halt to the partnership. The Bolshevik Revolution of 1917, through which the imperial Russia where Stravinsky had grown up was dismantled, also made a profound impact upon him, in terms both spiritual and practical.</p>
 				<p>Settling in Switzerland, he solidified and expanded the creative relationship with an author, Charles-Ferdinand Ramuz, that had begun in 1915. The tempestuous events of recent history had virtually wiped out Stravinsky&rsquo;s income from his music and his family estates. To make ends meet, he and Ramuz cast about for a joint project that would be inexpensive to produce and could be toured profitably throughout Switzerland. A new Stravinsky style, Neoclassicism, was born of those practicalities. <em>The Soldier&rsquo;s Tale</em>, the fascinating hybrid work that was so different from virtually everything he had composed previously, showed where he was headed stylistically (and where he would remain for the most part, until the 1950s).</p>
 				<p>Stravinsky found a subject for them to work with in an anthology of Russian folk tales that he came across in the vast library of his father, Fyodor, the principal operatic bass of the Mariinsky Theatre in St. Petersburg. The story about a hapless Soldier who strikes a Faust-like, eventually tragic deal with the Devil, was set during the nineteenth-century reign of Tsar Nicholas I. In Ramuz and Stravinsky&rsquo;s version, they minimized any references to a specific time and place, in order to make their version of the allegorical fable more universal.</p>
@@ -107,7 +107,7 @@ Template Name: Page Concert6 Template
 				<p>Stravinsky&rsquo;s tart, witty and rhythmically intricate score displayed several influences: Russian folk song, gypsy fiddlers (reflecting the plot, the violin often takes the musical spotlight), klezmer groups, the popular music of Spain (The Royal March is an example of the pasodoble), and his recent exposure to jazz (the ensemble sounds like a Dixieland band at times). &ldquo;My knowledge of jazz was derived exclusively from copies of sheet music,&rdquo; he admitted, &ldquo;and as I had never actually heard any of the music performed, I borrowed its rhythmic style not as played, but as written. I could imagine jazz sound, however, or so I liked to think. Jazz meant, in any case, a wholly new sound in my music, and <em>L&rsquo;Histoire du soldat</em> marks my final break with the Russian orchestral school in which I had been fostered.&rdquo; Among the music&rsquo;s other ingredients are parodies of military marches and popular music forms such as the tango, waltz and ragtime, and even two Lutheran chorales, including <em>Ein&rsquo; feste Burg ist under Gott</em>(A Mighty Fortress Is Our God).</p>
 			</div>
 			<div class="tab-pane fade" id="extras">
-					<p><strong><a href="http://www.haleyrempel.com/">Haley Rempel</a>, two-minute talk<br />Number 32: &lsquo;On Igor Stravinsky&rsquo;</strong></p>
+					<p><strong><a href="http://www.haleyrempel.com/">Haley Rempel</a>, two-minute talk<br>Number 32: &lsquo;On Igor Stravinsky&rsquo;</strong></p>
 					<section class="row">
   					<div class="span5">
   						<div class="flex-video widescreen">
@@ -117,7 +117,21 @@ Template Name: Page Concert6 Template
   						</div>
   					</div>
   				</section>
-
+					<section class="row">
+						<div class="span5">
+							<h4>Review:<br>Children&rsquo;s opera packs emotional wallop</h4>
+							<p class="fineprintdark"><a href="http://www.winnipegfreepress.com/">Winnipeg Free Press</a>, by Holly Harris<br>Manitoba Chamber Orchestra; Earl Stafford, guest conductor<br>Pembina Trails Voices; Ruth Wiwchar, Director<br>Westminster United Church; Tue, Mar 12<br>Attendance: 764<br>Four stars out of five<br>&nbsp;</p>
+							<p>Out of the inferno of the Holocaust have come many inspiring stories of courage, strength, survival, and defiance. <i>The Diary of Anne Frank</i>, published in 1947, still resonates as an extraordinary tale of resilience.</p>
+							<p>The Manitoba Chamber Orchestra presented the seemingly innocent <i>Brundib&aacute;r</i> (The Bumblebee) Tuesday night, a poignant children&rsquo;s opera penned by Jewish Czech composer Hans Kr&aacute;sa that is a charming fairy tale and ethical manifesto dealing with the horrors of fascism.</p>
+							<p>Winnipeg&rsquo;s premi&egrave;re children/youth vocal ensemble, Pembina Trails Voices, performed the 40-minute work with youthful energy, impeccably prepared by artistic director Ruth Wiwchar with efficient stage direction by Donna Fletcher. Notably, Wiwchar also led the orchestra situated in front of the stage area, stepping in for MCO&rsquo;s Anne Manson, who had become ill at the last minute.</p>
+							<p>The story, based on Adolf Hoffmeister&rsquo;s libretto, is simple. First performed in its orchestral version in 1943 at the Theresienstadt concentration camp, it tells the tale of young Aninka and Pepicek, whose ailing mother needs milk to get well. Wicked organ grinder Brundib&aacute;r bullies them after they imitate his singing to raise money. Their fantastical friends, Dog, Cat, and Sparrow, joined by Ice Cream Seller, Baker and Milkman offer to help by singing a lullaby that earns them donations from passers-by. After Brundib&aacute;r steals their money, the children chase him down before reuniting to sing a victory song that celebrates the bonds of friendship in staring down adversity.</p>
+							<p>This moving opera has been performed in the city before; last seen as a touring production by Saskatoon Children&rsquo;s Choir in June 2006. But whether one sees the opera for the first time, or many times, it&rsquo;s safe to say it will always pack an emotional wallop of sheer magnitude, literally giving voice to the ghetto&rsquo;s children who were murdered days after each of the original 55 performances, as well as serving as a living testament to the power of art. Even its unusual orchestration reflects harsh origins: scored for the 12 musicians available &mdash; and still alive &mdash; in the camp.</p>
+							<p>Sung in English, all 78 children, and especially the 10 leads, performed with a sense of conviction their crystal-clear diction ensuring every word was heard. Their sweetly harmonized duets, trios, quartets and quintets interspersed throughout the show proved particularly heart-warming.</p>
+							<p>The youthful cast is also to be commended for carrying on like true, seasoned pros when several sound issues inadvertently arose. Bravo to all, and especially to the tireless Wiwchar for bringing this important work to life, which elicited both laughter and tears in the audience of 764.</p>
+							<p>The concert also included Stravinsky&rsquo;s <i>L&rsquo;Histoire Du Soldat</i> (The Soldier&rsquo;s Tale) featuring Winnipeg actor Arne MacPherson, who spun his Faustian tale as a master storyteller. Guest conductor Earl Stafford &mdash; again, filling in on scant notice &mdash; led the seven-piece ensemble through Stravinsky&rsquo;s rhythmically treacherous, episodic score with precision and fearless pluck.</p>
+							<p>Kudos to MCO concertmaster Karl Stobbe for his solo passages packed with double and triple stops, especially during <i>Three Dances</i>, which morphs into a tango, waltz, and ragtime dance. Other highlights included <i>Royal March</i> with trumpeter Guy Few tossing off complex riffs like child&rsquo;s play, and MacPherson&rsquo;s animated, biting delivery during <i>The Devil&rsquo;s Song</i>.</p>
+						</div><!-- /span5 -->
+					</section>
 				</div>
 
 		</div><!-- /tab-content -->
