5d34653cd95eb704fa49789b8495d91c0d701955
Jon Snidal <jsnidal@gmail.com>
2013-04-09 14:19:42 -0500

    Media zone

---
+++ php/wp-content/themes/roots-master/page-media.php
@@ -0,0 +1,71 @@
+<?php
+/*
+Template Name: Page Media Template
+*/
+?>
+<div class="container">
+
+<div class="row">
+
+  <div class="span6">
+
+	<ul class="thumbnails">
+  <li class="span2">
+    <div class="thumbnail">
+      <img data-src="holder.js/300x200" alt="">
+      <h4>Stay+play</h4>
+      <p>Media files</p>
+    </div>
+  </li>
+  <li class="span2">
+    <div class="thumbnail">
+      <img data-src="holder.js/300x200" alt="">
+      <h4>One</h4>
+      <p><a href="http://www.themco.ca/wp-content/uploads/2013/04/Brundibar2.mov">Brundib&aacute;r</a></p>
+    </div>
+  </li>
+  <li class="span2">
+    <div class="thumbnail">
+      <img data-src="holder.js/300x200" alt="">
+      <h4>Two</h4>
+      <p>File</p>
+    </div>
+  </li>
+  <li class="span2">
+    <div class="thumbnail">
+      <img data-src="holder.js/300x200" alt="">
+      <h4>Three</h4>
+      <p>File</p>
+    </div>
+  </li>
+  <li class="span2">
+    <div class="thumbnail">
+      <img data-src="holder.js/300x200" alt="">
+      <h4>Four</h4>
+      <p>File</p>
+    </div>
+  </li>
+  <li class="span2">
+    <div class="thumbnail">
+      <img data-src="holder.js/300x200" alt="">
+      <h4>Five</h4>
+      <p>File</p>
+    </div>
+  </li>
+  <li class="span2">
+    <div class="thumbnail">
+      <img data-src="holder.js/300x200" alt="">
+      <h4>Six</h4>
+      <p>File</p>
+    </div>
+  </li>
+  
+  </ul>
+
+</div>
+
+</div>
+
+<?php get_template_part('templates/content', 'media'); ?>
+
+</div><!-- /container -->
