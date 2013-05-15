71332e5d3b49f0f5c44503fc574da2f0dbd441eb
Jon Snidal <jsnidal@gmail.com>
2013-05-15 12:22:23 -0500

    Switch

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -8,12 +8,12 @@ Template Name: Page MCOhome Template
 <div class="row-fluid">
 	
 	<div class="span8">
-   	<a href="http://www.themco.ca/concert/leblanc-taylor/"><img src="http://www.themco.ca/wp-content/uploads/2013/01/head14may13.jpg" alt="LeBlanc, Taylor"></a>
-		<h4>Suzie LeBlanc &amp; Daniel Taylor<br>&mdash; &ldquo;perfectly matched&rdquo;</h4>
-		<p>There are some concerts you don&rsquo;t want to miss; there are those concerts <i>you must not miss</i>. This is definitely one of them! The <i>Winnipeg Free Press</i> (2005) continued: &ldquo;Nothing could have improved on this performance.&rdquo; Read lots more about the concert <a href="http://www.themco.ca/concert/leblanc-taylor/"><strong>here</strong></a>; if you&rsquo;re just here for a ticket, click below.<br>
-		<img src="http://www.themco.ca/wp-content/uploads/2013/01/QuikTicket.png"><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=571b7b59fe3b48009544bbf1201c5c9e"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/AdultC.png" alt="" width="62" height="40" /></a>
- 	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=c9585707ac6842088dec4791c517637f"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/SeniorC.png" alt="" width="62" height="40" /></a>
-	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=de0912da44394be8a0b0127c47906151"><img rel="tooltip" title="Click to add student ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/StudentC.png" alt="" width="62" height="40" /></a>
+   	<a href="http://www.themco.ca/concert/leblanc-taylor/"><img src="http://www.themco.ca/wp-content/uploads/2013/01/head05jun13.jpg" alt="LeBlanc, Taylor"></a>
+		<h4>40th anniversary season<br> wraps with Brit cellist Colin Carr</h4>
+		<p>Colin Carr has performed with the Manitoba Chamber Orchestra before, but not as part of an MCO season. Carr and the MCO were guests of 2011&rsquo;s inaugural International Cello Festival of Canada, and that concert became the talk of the town! Read lots more about the concert <a href="http://www.themco.ca/concert/carr/"><strong>here</strong></a>; if you&rsquo;re just here for a ticket, click below.<br>
+		<img src="http://www.themco.ca/wp-content/uploads/2013/01/QuikTicket.png"><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=0718bdddf4764a2c9b9266ee450eb5af"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/AdultC.png" alt="" width="62" height="40" /></a>
+ 	  <a href="hhttp://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=65cf18fe94b34a2aa24d7f7e56e7dd71"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/SeniorC.png" alt="" width="62" height="40" /></a>
+	  <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=374bffd27d964cf382b3d3cf17768550"><img rel="tooltip" title="Click to add student ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/StudentC.png" alt="" width="62" height="40" /></a>
 		</p>
   </div>
 
@@ -23,22 +23,6 @@ Template Name: Page MCOhome Template
 	<small>&nbsp;</small>
 </div>
 
-<div class="row-fluid">
-
-	<div class="span8">
-		<div class="well">
-		<div class="media">
-			<a class="pull-left" href="http://www.themco.ca/heartstrings/">
-			<img class="media-object img-rounded" src="http://www.themco.ca/wp-content/uploads/2013/02/thumbhs.jpg" width="96px" height="96px">
-			</a>
-		<div class="media-body">
-			<p><strong><a href="http://www.themco.ca/heartstrings/">MCO&rsquo;s Heartstrings 2013:<br>a return to formal elegance...</strong></a><br />Heartstrings returns to The Fort Garry's Crystal Ballroom and moves to a new month this year! Celebrate 40 years of the Manitoba Chamber Orchestra in May at its main event &mdash; with fine food, fine wine, plus live, silent &amp; rainbow auctions. And, of course, music!</p>
-		</div>
-	</div>
-</div>
-</div>
-
-</div><!-- /row -->
 
 <div class="row-fluid">
 
