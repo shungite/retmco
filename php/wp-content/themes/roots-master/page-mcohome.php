da22819289978fd7644446a2a8077315a5686fb3
Jon Snidal <jsnidal@gmail.com>
2013-05-27 12:00:15 -0500

    Update Viny Vault again

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -33,7 +33,7 @@ Template Name: Page MCOhome Template
 			<img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/01/Parlo1.png" width="96px" height="96px">
 			</a>
 		<div class="media-body">
-			<p><strong><a href="#">Final Vinyl Vault sale of the season:<br>Saturday, 29 May 2013, 10 am to 3 pm</strong></a><br />Rummage around in one of the city&rsquo;s largest used record collection! Find rare jazz titles, pop, classical, rock, country, folk, easy listening and much more &mdash; collectibles a speciality! Located in the basement of the Power Building, 428 Portage Avenue (near The Bay).</p> 
+			<p><strong><a href="#">Final Vinyl Vault sale of the season:<br>Saturday, 29 June 2013, 10 am to 3 pm</strong></a><br />Rummage around in one of the city&rsquo;s largest used record collection! Find rare jazz titles, pop, classical, rock, country, folk, easy listening and much more &mdash; collectibles a speciality! Located in the basement of the Power Building, 428 Portage Avenue (near The Bay).</p> 
 		</div>
 	</div>
 </div>
