7297d5ea103bfa1f36a2888295d1852d274dc2cb
Jon Snidal <jsnidal@gmail.com>
2013-06-07 09:33:26 -0500

    Typo, edits

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -30,7 +30,7 @@ Template Name: Page MCOhome Template
 			<img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/01/Parlo1.png" width="96px" height="96px">
 			</a>
 		<div class="media-body">
-			<p><strong><a href="#">Final Vinyl Vault sale of the season:<br>Saturday, 29 June 2013, 10 am to 3 pm</strong></a><br />Rummage around in one of the city&rsquo;s largest used record collection! Find rare jazz titles, pop, classical, rock, country, folk, easy listening and much more &mdash; collectibles a speciality! Located in the basement of the Power Building, 428 Portage Avenue (near The Bay).</p> 
+			<p><strong><a href="#">Final Vinyl Vault sale of the season:<br>Saturday, 29 June 2013, from 10 am to 3 pm</strong></a><br />Rummage around in one of the city&rsquo;s largest used record collections! Find rare jazz titles, pop, classical, rock, country, folk, easy listening and much more &mdash; collectibles a speciality! Located in the basement of the Power Building, 428 Portage Avenue (near The Bay).</p> 
 		</div>
 	</div>
 </div>
@@ -50,8 +50,8 @@ Template Name: Page MCOhome Template
 
 	<div class="span2">
 		<h6><i class="icon-warning-sign"></i><br />Warning!</h6>
-		<p class="fineprint">Please be patient as we rebuild our web site. We promise to reward you with better content and interactive features. In the meantime, feel free to send us some feedback...</p>
-		<a class="btn btn-mini" href="mailto:info@themco.ca?subject=Feedback ...">Complain »</a>
+		<p class="fineprint">Please be patient as we rebuild our web site. We promise to reward you with better content and interactive features. In the meantime, we&rsquo;d love to hear from you!</p>
+		<a class="btn btn-mini" href="mailto:info@themco.ca?subject=Feedback ...">Feedback »</a>
 		<p>&nbsp;</p>
 	</div><!-- /span2 -->
 
