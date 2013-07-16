aa656222bccc2372fb24dbf210b918cedf7e199f
Jon Snidal <jsnidal@gmail.com>
2013-07-16 11:58:33 -0500

    Corrections

--- php/wp-content/themes/roots-master/page-heartstrings.php
+++ php/wp-content/themes/roots-master/page-heartstrings.php
@@ -19,9 +19,9 @@ Template Name: Page Heartstrings Template
 <div class="row">
 
 	<div class="span6">
-		<h4>The MCO's gala evening<br />&mdash;2014 edition!</h4>
+		<h4>The MCO's gala evening<br />&mdash; fabulous 2014 edition!</h4>
 		<p>The main event, held in <a href="http://www.fortgarryhotel.com/" >The Fort Garry</a> on Broadway &mdash; a heavenly evening of music &amp; food in Winnipeg&rsquo;s most elegant setting.</p>
-		<p><strong>Friday, 2 May 2014 at The Fort Garry<br />7:00 pm reception, dinner at 7:30 pm <br />&mdash; live, silent &amp; rainbow auctions. (Ticket links coming soon!)</strong></p>
+		<p><strong>Friday, 2 May 2014 at The Fort Garry<br />7:00 pm reception, dinner at 7:30 pm <br />&mdash; live, silent &amp; rainbow auctions.<br>(Ticket links coming soon!)</strong></p>
 		<p><strong>&bull; buy tickets @ $175<br>&bull; buy a table of ten @ $1750<br>&bull; buy Patron tickets @ $200*</strong></p>
 		<p class="fineprint">Patrons are acknowledged in the program<br /> and receive a larger tax receipt.</p>
   	<p class="regtext"><a href="http://www.mannfinancialassurance.com/" ><img src="http://www.themco.ca/wp-content/uploads/2012/11/mannfinanciallogo.jpg" width="140" height="52" border="0" alt="Mann Financial Assurance"></a></p>
@@ -29,7 +29,7 @@ Template Name: Page Heartstrings Template
 	</div><!-- /span6 -->
 
 	<div class="span2"> 
-		<a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=417b84bfbcb543e18db623edf577a343"><img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /></a><br />
+		<img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /><br />
 		<p class="fineprint">Click above (coming soon!) to add a regular ticket to your shopping cart; adjust quantity in cart and return to purchase other tickets.</p>
 	</div><!-- /span2 -->
 
