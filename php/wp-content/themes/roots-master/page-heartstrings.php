777adf10d504fb395f949718e949c8f2325d4b98
Jon Snidal <jsnidal@gmail.com>
2013-07-16 11:52:47 -0500

    Update Heartstrings page

--- php/wp-content/themes/roots-master/page-heartstrings.php
+++ php/wp-content/themes/roots-master/page-heartstrings.php
@@ -19,10 +19,10 @@ Template Name: Page Heartstrings Template
 <div class="row">
 
 	<div class="span6">
-		<h4>The MCO's gala evening<br />&mdash;40th anniversary edition!</h4>
+		<h4>The MCO's gala evening<br />&mdash;2014 edition!</h4>
 		<p>The main event, held in <a href="http://www.fortgarryhotel.com/" >The Fort Garry</a> on Broadway &mdash; a heavenly evening of music &amp; food in Winnipeg&rsquo;s most elegant setting.</p>
-		<p><strong>Friday, 3 May 2013 at The Fort Garry<br />6:30 pm reception, dinner at 7:30 pm <br />&mdash; live, silent &amp; rainbow auctions.</strong></p>
-		<p><strong>&bull; <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=417b84bfbcb543e18db623edf577a343">buy tickets @ $175</a><br>&bull; <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=870fd03278324d0aa890e79e77752c4c">buy a table of ten @ $1750</a><br>&bull; <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=0db1a7971cc748adad01da114ceb57d1">buy Patron tickets @ $200*</a></strong></p>
+		<p><strong>Friday, 2 May 2014 at The Fort Garry<br />7:00 pm reception, dinner at 7:30 pm <br />&mdash; live, silent &amp; rainbow auctions. (Ticket links coming soon!)</strong></p>
+		<p><strong>&bull; buy tickets @ $175<br>&bull; buy a table of ten @ $1750<br>&bull; buy Patron tickets @ $200*</strong></p>
 		<p class="fineprint">Patrons are acknowledged in the program<br /> and receive a larger tax receipt.</p>
   	<p class="regtext"><a href="http://www.mannfinancialassurance.com/" ><img src="http://www.themco.ca/wp-content/uploads/2012/11/mannfinanciallogo.jpg" width="140" height="52" border="0" alt="Mann Financial Assurance"></a></p>
   	<p class="fineprint">Event sponsor / <strong><a href="http://www.mannfinancialassurance.com/" >Mann Financial Assurance</a></strong></p>
@@ -30,7 +30,7 @@ Template Name: Page Heartstrings Template
 
 	<div class="span2"> 
 		<a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=417b84bfbcb543e18db623edf577a343"><img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /></a><br />
-		<p class="fineprint">Click above to add a regular ticket to your shopping cart; adjust quantity in cart and return to purchase other tickets.</p>
+		<p class="fineprint">Click above (coming soon!) to add a regular ticket to your shopping cart; adjust quantity in cart and return to purchase other tickets.</p>
 	</div><!-- /span2 -->
 
 </div><!-- /row -->
