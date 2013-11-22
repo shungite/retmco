f5e97c17964744e4d08484aa1ebb235e71333259
Jon Snidal <jsnidal@gmail.com>
2013-11-22 14:36:45 -0600

    Change

--- php/wp-content/themes/roots-master/page-troubadour.php
+++ php/wp-content/themes/roots-master/page-troubadour.php
@@ -12,7 +12,7 @@ Template Name: Page Troubadour Template
 		<div class="span6">
 
 			<img alt="Troubadour &amp; the Nightingale" src="http://www.themco.ca/wp-content/uploads/2013/10/MCOtroubadour.jpg" />
-			<p>Our newest compact disc, with the MCO conducted by <b>Anne Manson</b> and featuring pianist <b>Michael Riesman</b> and the music of <b>Philip Glass</b>, is available now &mdash; be sure to order yours today! <a href="http://www.themco.ca/something-awesome/"><b>Lots more about the project here.</b></a></p>
+			<p>Our newest compact disc, with the MCO conducted by <b>Anne Manson</b> and featuring pianist <b>Michael Riesman</b> and the music of <b>Philip Glass</b>, is available now &mdash; be sure to order yours today! <a href="http://www.themco.ca/something-awesome/"><b>There&rsquo;s a lots more about the project here.</b></a></p>
 			<p>&nbsp;</p>
 			<p><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=94ce7b6e08cf4bb79c932d28f04ae510"><button class="btn btn-large btn-info" type="button">Click to add to cart</button></a></p>
 			<p>&nbsp;</p>
