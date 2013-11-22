a83d46ac769be77392eb55e20ce763ddffb28416
Jon Snidal <jsnidal@gmail.com>
2013-11-22 14:15:15 -0600

    Added Fab, Troubadour page

---
+++ php/wp-content/themes/roots-master/page-troubadour.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Template Name: Page Troubadour Template
+*/
+?>
+<div class="container">
+
+	<div class="row">
+
+		<p>&nbsp;</p>
+
+		<div class="span6">
+
+			<img alt="Troubadour &amp; the Nightingale" src="http://www.themco.ca/wp-content/uploads/2013/10/MCOtroubadour.jpg" />
+			<p>Our newest compact disc, with the MCO conducted by <b>Anne Manson</b> and featuring pianist <b>Michael Riesman</b> and the music of <b>Philip Glass</b>, is available now &mdash; be sure to order yours today! <a href="http://www.themco.ca/something-awesome/"><b>Lots more about the project here.</b></a></p>
+			<p>&nbsp;</p>
+			<p><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=94ce7b6e08cf4bb79c932d28f04ae510"><button class="btn btn-large btn-info" type="button">Click to add to cart</button></a></p>
+			<p>&nbsp;</p>
+			<small><b>Price: $21 (inc GST)</b>; shipping and handling is $2.50; you may also arrange to pick up product at the MCO office or at a concert (please call 204.783.7377 to make arrangements after pre-ordering). Orders will be filled on a first-to-order, first-to-ship basis.</small>
+			<p>&nbsp;</p>
+
+		</div>
+
+	</div><!-- /row -->
+
+<?php get_template_part('templates/content', 'troubadour'); ?>
+
+</div><!-- /container -->
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
