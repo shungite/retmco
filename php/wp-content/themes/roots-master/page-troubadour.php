8a013d43267d33064dfd056a01678d6784f8d4c1
Jon Snidal <jsnidal@gmail.com>
2013-11-27 13:31:22 -0600

    Product experience page

--- php/wp-content/themes/roots-master/page-troubadour.php
+++ php/wp-content/themes/roots-master/page-troubadour.php
@@ -16,15 +16,63 @@ Template Name: Page Troubadour Template
 		  	<p>&ldquo;Isabel Bayrakdarian invites you to a vibrant world of colour and poetry with music enriched by the cultures of Armenia, Greece, the Middle East and Occitane."</p>
 	 			<small>Anne Manson</small>
 			</blockquote>
-			<p>&nbsp;</p>
-			<p><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=94ce7b6e08cf4bb79c932d28f04ae510"><button class="btn btn-large btn-info" type="button">Click to add to cart</button></a></p>
-			<p>&nbsp;</p>
-			<small><b>Price: $21 (inc GST)</b>; shipping and handling is $2.50; you may also arrange to pick up product at the MCO office or at a concert (please call 204.783.7377 to make arrangements after pre-ordering). Orders will be filled on a first-to-order, first-to-ship basis.</small>
-			<p>&nbsp;</p>
 
 		</div>
 
 	</div><!-- /row -->
+
+
+<div class="row">
+
+	<div class="span8">
+
+		<ul class="nav nav-pills">
+			<li class="active"><a href="#buy" data-toggle="tab"><strong>Buy online</strong></a></li>
+			<li><a href="#about" data-toggle="tab"><strong>Back story</strong></a></li>
+			<li><a href="#reviews" data-toggle="tab"><strong>Reviews</strong></a></li>
+			<li><a href="#samples" data-toggle="tab"><strong>Hear samples</strong></a></li>
+		</ul>
+		<div class="tab-content">
+			<div class="tab-pane fade in active" id="buy">
+				<div class="well">
+								<p><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=94ce7b6e08cf4bb79c932d28f04ae510"><button class="btn btn-large btn-info" type="button">Click to add to cart</button></a></p>
+						<p>&nbsp;</p>
+							<small><b>Price: $21 (inc GST)</b>; shipping and handling is $2.50; you may also arrange to pick up product at the MCO office or at a concert (please call 204.783.7377 to make arrangements after pre-ordering). Orders will be filled on a first-to-order, first-to-ship basis.</small>
+						<p>&nbsp;</p>
+					</div>
+			</div>
+			<div class="tab-pane fade" id="about">
+				<div class="well">
+				<p><strong>Back story here</strong></p>
+				<p>...</p>
+			</div>
+			</div>
+
+			<div class="tab-pane fade" id="reviews">
+				<div class="well">
+					<article>
+						<h4>Review:<br />One</h4>
+						<h5>Subhead</h5>
+						<p class="fineprintdark"><a href="http://www.winnipegfreepress.com/">Winnipeg Free Press</a>, by Holly Harris<br />Manitoba Chamber Orchestra; Anne Manson, conductor<br />Westminster United Church; Tue, Oct 22<br />Attendance: 650<br>Three stars out of five<br>&nbsp;</p>
+						<p>Text here ...</p>
+						<p class="text-left">&#8258;</p>
+					</article>
+					
+				</div>
+			</div>
+
+			<div class="tab-pane fade" id="samples">
+				<div class="well">
+					<p><strong>Tracks</strong></p>
+					
+				</div>
+			</div>
+
+		</div><!-- /tab-content -->
+
+	</div><!-- /span6 tab area -->
+
+</div>
 
 <?php get_template_part('templates/content', 'troubadour'); ?>
 
