41519535b65bd39ddff2b419482a513a3b4800e5
Jon Snidal <jsnidal@gmail.com>
2014-02-19 10:18:24 -0600

    Concert updates

--- php/wp-content/themes/roots-master/ConcertTxt/page-concert8.php
+++ php/wp-content/themes/roots-master/ConcertTxt/page-concert8.php
@@ -8,7 +8,7 @@ Template Name: Page Concert8 Template
 <div class="row">
 
 	<div class="span8">
-		<img src="http://www.themco.ca/wp-content/uploads/2013/07/head09apr14.jpg" width="770" height="455">
+		<img src="http://www.themco.ca/wp-content/uploads/2014/02/head09apr14b.jpg" width="770" height="455">
 	</div>
 	<div class="span2">
 		<p>.</p>
