5cc71a8fd3f80da208e8d1ea4f2d93802aba82b0
Jon Snidal <jsnidal@gmail.com>
2014-09-11 10:22:39 -0500

    Switch to concert marketing

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -8,10 +8,12 @@ Template Name: Page MCOhome Template
 
 	<div class="row-fluid">
 		<div class="span8">
-	   	<a href="http://www.themco.ca/quick-links-to-1415-subscriptions/"><img src="http://www.themco.ca/wp-content/uploads/2014/05/SubscribeNow.jpg" alt="MCO concert / 12 March 2014"></a>
-	   	<p>&nbsp;</p>
+	   	<a href="http://www.themco.ca/concert/manson-gauvin/"><img src="http://www.themco.ca/wp-content/uploads/2014/06/head16sep14.jpg" alt="MCO concert / 16 September 2014"></a>
+	   	<h4>MCO season opens with soprano Karina Gauvin,<br>and arias from Handel's early operas!</h4>
+			<p>For her MCO debut, the famed Canadian soprano will sing <i>Tornami a vacheggiar</i> (Return to me to languish) and <i>Ah, mio cor</i> (Ah, my heart) from Handel&rsquo;s <i>Alcina</i>. Of the latter, MCO program note annotator <b>Don Anderson</b> writes that it is “one of the longest and most demanding arias in any Handel opera.” Get your tickets early! See program details <a href="http://www.themco.ca/concert/manson-gauvin/"><b>here</b></a>, or if you're just after a quick ticket or two, click the links below.<br><img src="http://www.themco.ca/wp-content/uploads/2013/01/QuikTicket.png" alt="QuikTicket"><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=1ee00dcca60e4814ae020dad63d74525"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/AdultC.png" alt="" width="62" height="40"></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=0b5452c610b140b0a200148327bfdfe3"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/SeniorC.png" alt="" width="62" height="40" /></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=4ac256069e254c92b1d711cce624ed30"><img rel="tooltip" title="Click to add student ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/StudentC.png" alt="" width="62" height="40" /></a></p>
 		</div>
 	</div><!-- /row -->
+
 
 	<!-- <div class="row-fluid">
 		<small>&nbsp;</small>
@@ -23,7 +25,6 @@ Template Name: Page MCOhome Template
 				<div class="media">
 					<a class="pull-left" href="#extras" data-toggle="tab">
 					<img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/07/DonorCello.jpg" width="96px" height="96px">
-					</a>
 					<div class="media-body">
 						<p><strong><a href="http://www.themco.ca/quick-links-to-1415-subscriptions/">Subscribe now to MCO&rsquo;s 2014/15 season<br>&mdash; enjoy six or nine concerts in Westminster Church!</strong></a><br />Only subscribers enjoy every benefit the MCO has to offer, like a free CD, Musicnet Subscriber Reward Card, ticket exchange privileges, discounts on extra tickets and more. Make this the season you subscribe to Canada&rsquo;s &ldquo;tiny, perfect, orchestra!&rdquo;</p> 
 					</div>
