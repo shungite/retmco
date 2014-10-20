0372a5daf95ac260a3afcab0996e31d3f6b5a22e
Jon Snidal <jsnidal@gmail.com>
2014-10-20 11:11:51 -0500

    Event added

---
+++ php/wp-content/themes/roots-master/page-bezaly.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Template Name: Page Bezaly Template
+*/
+?>
+<div class="container">
+
+<div class="row">
+
+	<div class="span8">
+		<img src="http://www.themco.ca/wp-content/uploads/2014/10/candlelight.jpg" alt="" width="770" height="455">
+	</div>
+	<div class="span2">
+		<p>.</p>
+	</div>
+
+</div><!-- /row -->
+
+<div class="row">
+
+	<div class="span6">
+		<h4>A Candlelight evening<br />with flautist Sharon Bezaly</h4>
+		<p>The Manitoba Chamber Orchestra invites you to a special Candlelight evening with flautist <b>Sharon Bezaly</b>. The event will be held at 7:30 pm, Tuesday, 25 November 2014, ,to be held at historic <b>Moss House</b>, 218 Roslyn Road, a turn-of-the-century heritage home, once again generously offered by John &amp; Nancy Embry. Tickets $75 per person (partial tax receipt). Musical performance will commence at 7:45.</p>
+		<p>As there are a very <b>limited number of tickets</b> available, please RSVP 204.783.7377 by Wednesday, November 12th, or simply <strong><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=f0f19b54fe6344eab6f5e227f462ed2b">purchase tickets here</a></strong>.</p>
+		<p>This unique salon evening includes <b>wine with sweets and savouries</b>.</p>
+		<p><strong>Sharon Bezaly</strong> Described by <i>The Times</i> as &ldquo;God&rsquo;s gift to the flute,&rdquo; Sharon Bezaly was chosen &lsquo;Instru­ment­al­ist of the Year&rsquo; by <i>Klassik Echo</i> in Ger­many in 2002 and &rsquo;Young Artist of the Year&lsquo; at the Cannes Clas­sical Awards in 2003. <i>Clas­sics Today</i> hailed her as &ldquo;a flut­ist vir­tu­ally without peer in the world today&rdquo; and <i>Inter­na­tional Record Review</i> stated &ldquo;Her record­ings and con­cert appear­ances are typ­ic­ally more than simply tri­umphs: they are defin­ing artistic events.&rdquo; Bezaly gave her debut con­cert as a soloist with the Israel Phil­har­monic Orches­tra con­duc­ted by Zubin Mehta when she was 14. She then stud­ied at the Con­ser­vatoire national superieur de musique in Paris under Alain Marion, Ray­mond Guiot and Maurice Bourge, win­ning the Academy&rsquo;s first prizes for flute and cham­ber music. Sub­sequently, she was invited by Sandor Vegh to play as prin­cipal flaut­ist in his Cam­erata Aca­dem­ica Salzburg, a pos­i­tion she held until his death in 1997. Sharon Bezaly plays a 24-carat gold flute, spe­cially built for her by the Muramatsu team, Japan.</p>
+
+	</div><!-- /span6 -->
+
+	<div class="span2"> 
+		<a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=f0f19b54fe6344eab6f5e227f462ed2b"><img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /></a><br />
+		<p class="fineprint">Click above to add a ticket to your shopping cart; adjust quantity in cart and return to purchase other tickets.</p>
+	</div><!-- /span2 -->
+
+</div><!-- /row -->
+
+<?php get_template_part('templates/content', 'bezaly'); ?>
+
+</div><!-- /container -->
