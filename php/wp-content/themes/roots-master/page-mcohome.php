a426223a2c4dc8177bb737ff7feedeadd3c3d485
Jon Snidal <jsnidal@gmail.com>
2015-01-12 11:00:03 -0600

    Typo

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -41,7 +41,7 @@ Template Name: Page MCOhome Template
 					<img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/01/Parlo1.png" width="96px" height="96px">
 					</a>
 					<div class="media-body">
-						<p><strong><a href="#">Come in for a spin at the Vinyl Vault<br>and CD Cellar! Next sale: Saturday, November 29th</strong></a><br />Browse one of the city&rsquo;s largest and most varied vinyl collections. You&rsquo;ll find thousands of titles in the jazz, pop, classical, rock, country and folk categories, and more! MCO&rsquo;s Vinyl Vault is located in the basement of the Power Building at 428 Portage Avenue (near The Bay).</p> 
+						<p><strong><a href="#">Come in for a spin at the Vinyl Vault / CD Cellar!<br> Next sale: Saturday, January 31st, 10 am to 3 pm</strong></a><br />Browse one of the city&rsquo;s largest and most varied vinyl collections. You&rsquo;ll find thousands of titles in the jazz, pop, classical, rock, country and folk categories, and more! MCO&rsquo;s Vinyl Vault is located in the basement of the Power Building at 428 Portage Avenue (near The Bay).</p> 
 					</div>
 				</div>
 			</div>
