581f4833ebc95db36d42912ffe47a891c855d1b4
Jon Snidal <jsnidal@gmail.com>
2015-01-28 15:41:40 -0600

    TD Young Composer

---
+++ php/wp-content/themes/roots-master/page-composer.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Template Name: Page Composer Template
+*/
+?>
+<div class="container">
+
+<div class="row">
+
+	<div class="span8">
+		<img src="http://www.themco.ca/wp-content/uploads/2015/01/TDYoungComposer.jpg" alt="" width="770" height="455">
+	</div>
+	<div class="span2">
+		<p>.</p>
+	</div>
+
+</div><!-- /row -->
+
+<div class="row">
+
+	<div class="span7">
+		<h4>TD Young Composer Program Challenge:<br>bring your students&rsquo; compositions to life!</h4>
+		<p>Music teachers: an exciting new province-wide program! This challenge directly connects to outcomes in the Manitoba Music Curriculum framework. Stay tuned for more information regarding specific outcomes. This is a challenge for children and youth in Grades 4+, and is open to anyone wishing to participate.</p>
+		<div class="well">
+			<p>Write a 16-measure piece in the C, F, or G pentatonic scale, in 2, 3, or 4/4 time. Start with a melody line, and add texture and harmony, too! Try to incorporate some rhythmic variety, some musical expression, and have the melody begin and end on the home tone. Please email the score, with the name of composer, name of school and grade level to the info@themco.ca, with the following subject: <b>MCO / TD Young Composer Program Challenge</b>.</p>
+		</div>
+
+			<p><a href="http://www.tdcanadatrust.com/products-services/banking/index-banking.jsp"><img src="http://www.themco.ca/wp-content/uploads/2015/01/TD-Shield_col_ns_EN_Rev.png" width="112" height="104"></p>
+
+	</div><!-- /span6 -->
+
+
+</div><!-- /row -->
+
+<?php get_template_part('templates/content', 'composer'); ?>
+
+</div><!-- /container -->
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
