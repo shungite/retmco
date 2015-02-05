ff673d829a1830b21d14fdc62de2767549a546a6
Jon Snidal <jsnidal@gmail.com>
2015-02-05 12:36:11 -0600

    Cropped TD logo

--- php/wp-content/themes/roots-master/page-composer.php
+++ php/wp-content/themes/roots-master/page-composer.php
@@ -5,32 +5,41 @@ Template Name: Page Composer Template
 ?>
 <div class="container">
 
-<div class="row">
+  <div class="row">
 
-	<div class="span8">
-		<img src="http://www.themco.ca/wp-content/uploads/2015/01/TDYoungComposer.jpg" alt="" width="770" height="455">
-	</div>
-	<div class="span2">
-		<p>.</p>
-	</div>
+    <div class="span8">
 
-</div><!-- /row -->
+      <img src="http://www.themco.ca/wp-content/uploads/2015/01/TDYoungComposer.jpg" alt="" width="770" height="455">
 
-<div class="row">
+    </div><!-- /span8 -->
 
-	<div class="span7">
-		<h4>TD Young Composer Program Challenge:<br>bring your students&rsquo; compositions to life!</h4>
-		<p>Music teachers: an exciting new province-wide program! This challenge directly connects to outcomes in the Manitoba Music Curriculum framework. Stay tuned for more information regarding specific outcomes. This is a challenge for children and youth in Grades 4+, and is open to anyone wishing to participate.</p>
-		<div class="well">
-			<p>Write a 16-measure piece in the C, F, or G pentatonic scale, in 2, 3, or 4/4 time. Start with a melody line, and add texture and harmony, too! Try to incorporate some rhythmic variety, some musical expression, and have the melody begin and end on the home tone. Please email the score, with the name of composer, name of school and grade level to the info@themco.ca, with the following subject: <b>MCO / TD Young Composer Program Challenge</b>.</p>
-		</div>
+    <div class="span2">
 
-			<p><a href="http://www.tdcanadatrust.com/products-services/banking/index-banking.jsp"><img src="http://www.themco.ca/wp-content/uploads/2015/01/TD-Shield_col_ns_EN_Rev.png" width="112" height="104"></p>
+      <p>.</p>
 
-	</div><!-- /span6 -->
+    </div><!-- /span2 -->
 
+  </div><!-- /row -->
 
-</div><!-- /row -->
+  <div class="row">
+
+    <div class="span7">
+
+      <h4>TD Young Composer Program Challenge:<br>bring your students&rsquo; compositions to life!</h4>
+      <p>Music teachers: an exciting new province-wide program! This challenge directly connects to outcomes in the Manitoba Music Curriculum framework. Stay tuned for more information regarding specific outcomes. This is a challenge for children and youth in Grades 4+, and is open to anyone wishing to participate.</p>
+
+        <div class="well">
+
+          <p>Write a 16-measure piece in the C, F, or G pentatonic scale, in 2, 3, or 4/4 time. Start with a melody line, and add texture and harmony, too! Try to incorporate some rhythmic variety, some musical expression, and have the melody begin and end on the home tone. Please email the score, with the name of composer, name of school and grade level to the info@themco.ca, with the following subject: <b>MCO / TD Young Composer Program Challenge</b>.</p>
+
+        </div><!-- /well -->
+
+        <p><a
+        href="http://www.tdcanadatrust.com/products-services/banking/index-banking.jsp"><img src="http://www.themco.ca/wp-content/uploads/2015/02/TD-Shield_col_ns_EN_Rev_Tight.png" width="112" height="110"></p>
+
+    </div><!-- /span7 -->
+
+  </div><!-- /row -->
 
 <?php get_template_part('templates/content', 'composer'); ?>
 
