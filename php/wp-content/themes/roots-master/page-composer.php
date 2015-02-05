20099da84489a68ed2e1fda8be61af89f43d667d
Jon Snidal <jsnidal@gmail.com>
2015-02-05 13:44:41 -0600

    Fix again

--- php/wp-content/themes/roots-master/page-composer.php
+++ php/wp-content/themes/roots-master/page-composer.php
@@ -32,15 +32,15 @@ Template Name: Page Composer Template
 
           <p>Write a 16-measure piece in the C, F, or G pentatonic scale, in 2, 3, or 4/4 time. Start with a melody line, and add texture and harmony, too! Try to incorporate some rhythmic variety, some musical expression, and have the melody begin and end on the home tone. Please email the score, with the name of composer, name of school and grade level to the info@themco.ca, with the following subject: <b>MCO / TD Young Composer Program Challenge</b>.</p>
 
+        <p><a href="http://www.tdcanadatrust.com/products-services/banking/index-banking.jsp"><img src="http://www.themco.ca/wp-content/uploads/2015/02/TD-Shield_col_ns_EN_Rev_Tight.png" width="112" height="110"></p>
+
         </div><!-- /well -->
 
         <p>Deadline for submission of compositions is April 30. Some written copies and recordings will be posted on our website in early June, and some works may be selected for performance or playing at our June 3 concert after intermission and at Frontier and Winnipeg area schools this spring. Additionally, a few students will be interviewed and featured on our website.</p>
 
         <p>In the links below, you will find three staves: one treble, one bass, and one treble and bass. Download these now, and let the composing begin!</p>
 
-        <p><a href="http://www.themco.ca/wp-content/uploads/2015/02/Stave-treble.pdf"><img src="http://www.themco.ca/wp-content/uploads/2015/02/Adobe_PDF_icon_128.png"></a><br>Treble stave<br><a href="http://www.themco.ca/wp-content/uploads/2015/02/Bass.pdf"><img src="http://www.themco.ca/wp-content/uploads/2015/02/Adobe_PDF_icon_128.png"></a><br>Bass stave<br><a href="http://www.themco.ca/wp-content/uploads/2015/02/keyboard.pdf"><img src="http://www.themco.ca/wp-content/uploads/2015/02/Adobe_PDF_icon_128.png"></a><br>Treble and bass</p>
-
-        <p><a href="http://www.tdcanadatrust.com/products-services/banking/index-banking.jsp"><img src="http://www.themco.ca/wp-content/uploads/2015/02/TD-Shield_col_ns_EN_Rev_Tight.png" width="112" height="110"></p>
+        <p><a href="http://www.themco.ca/wp-content/uploads/2015/02/Stave-treble.pdf"><img src="http://www.themco.ca/wp-content/uploads/2015/02/Adobe_PDF_icon_128.png"></a>&nbsp;Treble stave<br><a href="http://www.themco.ca/wp-content/uploads/2015/02/Bass.pdf"><img src="http://www.themco.ca/wp-content/uploads/2015/02/Adobe_PDF_icon_128.png"></a>&nbsp;Bass stave<br><a href="http://www.themco.ca/wp-content/uploads/2015/02/keyboard.pdf"><img src="http://www.themco.ca/wp-content/uploads/2015/02/Adobe_PDF_icon_128.png"></a>&nbsp;Treble and bass</p>
 
     </div><!-- /span7 -->
 
