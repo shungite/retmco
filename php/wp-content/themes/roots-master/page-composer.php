2c12320182e412f790205c097d343f18d70da45a
Jon Snidal <jsnidal@gmail.com>
2015-02-06 10:34:16 -0600

    Update composer

--- php/wp-content/themes/roots-master/page-composer.php
+++ php/wp-content/themes/roots-master/page-composer.php
@@ -26,21 +26,26 @@ Template Name: Page Composer Template
     <div class="span7">
 
       <h4>TD Young Composer Program Challenge:<br>bring your students&rsquo; compositions to life!</h4>
+
       <p>Music teachers: an exciting new province-wide program! This challenge directly connects to outcomes in the Manitoba Music Curriculum framework. Stay tuned for more information regarding specific outcomes. This is a challenge for children and youth in Grades 4+, and is open to anyone wishing to participate.</p>
 
         <div class="well">
 
           <p>Write a 16-measure piece in the C, F, or G pentatonic scale, in 2, 3, or 4/4 time. Start with a melody line, and add texture and harmony, too! Try to incorporate some rhythmic variety, some musical expression, and have the melody begin and end on the home tone. Please email the score, with the name of composer, name of school and grade level to the info@themco.ca, with the following subject: <b>MCO / TD Young Composer Program Challenge</b>.</p>
 
-        <p><a href="http://www.tdcanadatrust.com/products-services/banking/index-banking.jsp"><img src="http://www.themco.ca/wp-content/uploads/2015/02/TD-Shield_col_ns_EN_Rev_Tight.png" width="112" height="110"></a></p>
+          <p>Deadline for submission of compositions is April 30. Some written copies and recordings will be posted on our website in early June, and some works may be selected for performance or playing at our June 3 concert and in Frontier School Division and Winnipeg area schools this spring. Additionally, a few students will be interviewed and featured on our website.</p>
+
+        <p><b>Support materials</b> / Download these staves and use them to compose your masterpiece!</p>
+        
+        <p><span class="badge">1</span> <a href="http://www.themco.ca/wp-content/uploads/2015/02/Stave-treble.pdf"><b>Treble stave</b></a></p>
+        <p><span class="badge">2</span> <a href="http://www.themco.ca/wp-content/uploads/2015/02/Bass.pdf"><b>Bass stave</b></a></p>
+        <p><span class="badge">3</span> <a href="http://www.themco.ca/wp-content/uploads/2015/02/keyboard.pdf"><b>Treble &amp; bass stave</b></a></p>
 
         </div><!-- /well -->
 
-        <p>Deadline for submission of compositions is April 30. Some written copies and recordings will be posted on our website in early June, and some works may be selected for performance or playing at our June 3 concert after intermission and at Frontier and Winnipeg area schools this spring. Additionally, a few students will be interviewed and featured on our website.</p>
-
-        <p>In the links below, you will find three staves: one treble, one bass, and one treble and bass. Download these now, and let the composing begin!</p>
-
-        <p><a href="http://www.themco.ca/wp-content/uploads/2015/02/Stave-treble.pdf"><img src="http://www.themco.ca/wp-content/uploads/2015/02/Adobe_PDF_icon_128.png"></a>&nbsp;Treble stave<br><a href="http://www.themco.ca/wp-content/uploads/2015/02/Bass.pdf"><img src="http://www.themco.ca/wp-content/uploads/2015/02/Adobe_PDF_icon_128.png"></a>&nbsp;Bass stave<br><a href="http://www.themco.ca/wp-content/uploads/2015/02/keyboard.pdf"><img src="http://www.themco.ca/wp-content/uploads/2015/02/Adobe_PDF_icon_128.png"></a>&nbsp;Treble and bass</p>
+        <p>The Manitoba Chamber Orchestra&rsquo;s <b>TD Young Composer Program Challenge</b> is made possible with support from <a href="http://www.tdcanadatrust.com/products-services/banking/index-banking.jsp"><b>TD</b></a>.</p>
+        
+        <p><a href="http://www.tdcanadatrust.com/products-services/banking/index-banking.jsp"><img src="http://www.themco.ca/wp-content/uploads/2015/02/TD-Shield_col_ns_EN_Rev_Tight.png" width="112" height="110"></a></p>
 
     </div><!-- /span7 -->
 
