23fdf237ba230212fcf48cf93761d469228304fa
Jon Snidal <jsnidal@gmail.com>
2015-02-06 15:54:45 -0600

    Listening guide page started

---
+++ php/wp-content/themes/roots-master/page-guide18feb15.php
@@ -0,0 +1,59 @@
+<?php
+/*
+Template Name: Page guide18feb15 Template
+*/
+?>
+<div class="container">
+
+  <div class="row">
+
+    <div class="span8">
+
+      <img src="http://www.themco.ca/wp-content/uploads/2015/01/TDYoungComposer.jpg" alt="" width="770" height="455">
+
+    </div><!-- /span8 -->
+
+    <div class="span2">
+
+      <p>.</p>
+
+    </div><!-- /span2 -->
+
+  </div><!-- /row -->
+
+  <div class="row">
+
+    <div class="span7">
+
+      <h4>TD Young Composer Program Challenge:<br>bring your students&rsquo; compositions to life!</h4>
+
+      <p>Music teachers: an exciting new province-wide program! This challenge directly connects to outcomes in the Manitoba Music Curriculum framework. Stay tuned for more information regarding specific outcomes. This is a challenge for children and youth in Grades 4+, and is open to anyone wishing to participate.</p>
+
+        <div class="well">
+
+          <p>Write a 16-measure piece in the C, F, or G pentatonic scale, in 2, 3, or 4/4 time. Start with a melody line, and add texture and harmony, too! Try to incorporate some rhythmic variety, some musical expression, and have the melody begin and end on the home tone. Please email the score, with the name of composer, name of school and grade level to the info@themco.ca, with the following subject: <b>MCO / TD Young Composer Program Challenge</b>.</p>
+
+          <p>Deadline for submission of compositions is April 30. Some written copies and recordings will be posted on our website in early June, and some works may be selected for performance or playing at our June 3 concert and in Frontier School Division and Winnipeg area schools this spring. Additionally, a few students will be interviewed and featured on our website.</p>
+
+        <p><b>Support materials</b> / Download these staves and compose your masterpiece!</p>
+        
+        <p><span class="badge">1</span>&nbsp;<a href="http://www.themco.ca/wp-content/uploads/2015/02/Stave-treble.pdf"><b>&nbsp;Treble stave</b></a></p>
+        <p><span class="badge">2</span>&nbsp;<a href="http://www.themco.ca/wp-content/uploads/2015/02/Bass.pdf"><b>&nbsp;Bass stave</b></a></p>
+        <p><span class="badge">3</span>&nbsp;<a href="http://www.themco.ca/wp-content/uploads/2015/02/keyboard.pdf"><b>&nbsp;Treble &amp; bass stave</b></a></p>
+
+        </div><!-- /well -->
+
+        <p>MCO&rsquo;s <b>TD Young Composer Program Challenge</b> is made possible with support from <a href="http://www.tdcanadatrust.com/products-services/banking/index-banking.jsp"><b>TD</b></a>.</p>
+        <p>&nbsp;</p>
+        <p><a href="http://www.tdcanadatrust.com/products-services/banking/index-banking.jsp"><img src="http://www.themco.ca/wp-content/uploads/2015/02/TD-Shield_col_ns_EN_Rev_Tight.png" width="112" height="110"></a></p>
+        <p>&nbsp;</p>
+
+    </div><!-- /span7 -->
+
+  </div><!-- /row -->
+
+<?php get_template_part('templates/content', 'composer'); ?>
+
+</div><!-- /container -->
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
