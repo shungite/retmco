aa61d3663b7a57ef6752b6d3e7f53a3fb4cef5c1
Jon Snidal <jsnidal@gmail.com>
2015-02-06 09:16:06 -0600

    Board updated

--- php/wp-content/themes/roots-master/page-board.php
+++ php/wp-content/themes/roots-master/page-board.php
@@ -5,85 +5,77 @@ Template Name: Page Board Template
 ?>
 <div class="container">
 
-<div class="row">
+  <div class="row">
 
-	<div class="span6">
-		<p>&nbsp;</p>
-		<blockquote>
-			<p>To create and perform chamber orchestra repertoire at the highest artistic level possible for Manitobans, for Canadians, and for international audiences.</p>
-			<small>the Manitoba Chamber Orchestra&rsquo;s artistic mission statement</small>
-		</blockquote>
+    <div class="span6">
+      <p>&nbsp;</p>
+        <blockquote>
+          <p>To create and perform chamber orchestra repertoire at the highest artistic level possible for Manitobans, for Canadians, and for international audiences.</p>
+          <small>the Manitoba Chamber Orchestra&rsquo;s artistic mission statement</small>
+        </blockquote>
 
-		<div class="well">
-			<p><strong>Manitoba	 Chamber Orchestra<br>Board of Directors and staff</strong></p>
-			<dl class="dl">
-				<dt>President</dt>
-				<dd>Judith Hall</dd>
-				<dt>Vice-President</dt>
-				<dd>Faye Warren</dd>
-				<dt>Past President</dt>
-				<dd>William Neville</dd>
-				<dt>Treasurer</dt>
-				<dd>Gil Forrest</dd>
-				<dt>Secretary</dt>
-				<dd>Alice MacKenzie</dd>
-				<dt>Members</dt>
-				<dd>Lynne Axworthy</dd>
-				<dd>Zaz Bajon</dd>
-				<dd>Coralie Bryant</dd>
-				<dd>Jim Chapryk</dd>
-				<dd>Robert Giesbrecht</dd>
-				<dd>Joan Hardy</dd>
-				<dd>Sue Irving</dd>
-				<dd>Suzanne Kennelly</dd>
-				<dd>Elaine Margolis</dd>
-				<dd>Michael McIntyre</dd>
-				<dd>Ingrid M&ouml;hlmann</dd>
-				<dd>Sunny Oh</dd>
-				<dd>Beryl Peters</dd>
-				<dd>Nicola Schaefer</dd>
-				<dd>Moti Shojania</dd>
-				<dd>Alison Thiessen</dd>
-				<dd>Elizabeth Tippett-Pope</dd>
-			</dl>
-			<p><strong>Administration</strong></p>
-			<dl class="dl">
-				<dt>Music Director</dt>
-				<dd>Anne Manson</dd>
-				<dt>Managing Director</dt>
-				<dd>Vicki Young</dd>
-				<dt>Development Manager</dt>
-				<dd>Joan Gillis</dd>
-				<dt>Office Manager &amp; Front-of-house Manager</dt>
-				<dd>Elise Anderson</dd>
-				<dt>Administrative Assistant</dt>
-				<dd>Corinne Friesen Loewen</dd>
-				<dt>Bookeeper</dt>
-				<dd>Pat Ireton</dd>
-				<dt>Director, Systems and Design</dt>
-				<dd>Jon Snidal</dd>
-				<dt>Concert Manager</dt>
-				<dd>Boyd MacKenzie</dd>
-				<dt>Librarian</dt>
-				<dd>Merrily Peters</dd>
-				<dt>Program advertising</dt>
-				<dd>Brian Chipney</dd>
-				<dt>Stage Manager</dt>
-				<dd>David MacKenzie</dd>
-			</dl>
+        <div class="well">
+          <p><strong>Manitoba Chamber Orchestra<br>Board of Directors and staff</strong></p>
+            <dl class="dl">
+                <dt>President</dt>
+                <dd>Judith Hall</dd>
+                <dt>Vice-President</dt>
+                <dd>Faye Warren</dd>
+                <dt>Treasurer</dt>
+                <dd>Gil Forrest</dd>
+                <dt>Secretary</dt>
+                <dd>Alice MacKenzie</dd>
+                <dt>Members</dt>
+                <dd>Zaz Bajon</dd>
+                <dd>Coralie Bryant</dd>
+                <dd>Jim Chapryk</dd>
+                <dd>Robert Giesbrecht</dd>
+                <dd>Joan Hardy</dd>
+                <dd>Sue Irving</dd>
+                <dd>Suzanne Kennelly</dd>
+                <dd>Elaine Margolis</dd>
+                <dd>Michael McIntyre</dd>
+                <dd>Ingrid M&ouml;hlmann</dd>
+                <dd>Sunny Oh</dd>
+                <dd>Beryl Peters</dd>
+                <dd>Nicola Schaefer</dd>
+                <dd>Moti Shojania</dd>
+                <dd>Alison Thiessen</dd>
+                <dd>Elizabeth Tippett-Pope</dd>
+              </dl>
+            <p><strong>Administration</strong></p>
+              <dl class="dl">
+                <dt>Music Director</dt>
+                <dd>Anne Manson</dd>
+                <dt>Managing Director</dt>
+                <dd>Vicki Young</dd>
+                <dt>Development Manager</dt>
+                <dd>Joan Gillis</dd>
+                <dt>Office Manager &amp; Front-of-house Manager</dt>
+                <dd>Elise Anderson</dd>
+                <dt>Administrative Assistant</dt>
+                <dd>Corinne Friesen Loewen</dd>
+                <dt>Bookeeper</dt>
+                <dd>Pat Ireton</dd>
+                <dt>Director, Systems and Design</dt>
+                <dd>Jon Snidal</dd>
+                <dt>Concert Manager</dt>
+                <dd>Boyd MacKenzie</dd>
+                <dt>Librarian</dt>
+                <dd>Merrily Peters</dd>
+                <dt>Program advertising</dt>
+                <dd>Brian Chipney</dd>
+                <dt>Stage Manager</dt>
+                <dd>David MacKenzie</dd>
+              </dl>
 
-			<p><b>CKUW / Musicnet Radio Winnipeg, recent broadcasts</b><br><?php echo do_shortcode('[sc_embed_player fileurl="http://station.ckuw.ca/64/20141203.14.00-16.00.mp3"]'); ?>&nbsp;&nbsp;3 December 2014 show</p>
-          <p><?php echo do_shortcode('[sc_embed_player fileurl="http://station.ckuw.ca/24/20141203.14.00-16.00.mp3"]'); ?>&nbsp;&nbsp;26 November 2014 show</p>
-          <p><?php echo do_shortcode('[sc_embed_player fileurl="http://station.ckuw.ca/64/20141119.14.00-16.00.mp3"]'); ?>&nbsp;&nbsp;19 November 2014 show</p>
-          <p><?php echo do_shortcode('[sc_embed_player fileurl="http://station.ckuw.ca/64/20141112.14.00-16.00.mp3"]'); ?>&nbsp;&nbsp;12 November 2014 show</p>
-          <p><?php echo do_shortcode('[sc_embed_player fileurl="http://station.ckuw.ca/64/20141029.14.00-16.00.mp3"]'); ?>&nbsp;&nbsp;5 November 2014 show</p>
-          <p><?php echo do_shortcode('[sc_embed_player fileurl="http://station.ckuw.ca/64/20141029.14.00-16.00.mp3"]'); ?>&nbsp;&nbsp;29 October 2014 show</p>
-		</div>
-	</div>
+        </div>
 
-</div><!-- /row -->
+      </div>
 
-<?php get_template_part('templates/content', 'board'); ?>
+    </div><!-- /row -->
+
+  <?php get_template_part('templates/content', 'board'); ?>
 
 </div><!-- /container -->
 
