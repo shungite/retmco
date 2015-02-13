a5f6a3e6c9c3d01f862ecc378472e6a0fc6020da
Jon Snidal <jsnidal@gmail.com>
2015-02-13 12:36:36 -0600

    Format page-home

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -6,64 +6,60 @@ Template Name: Page MCOhome Template
 
 <div class="container">
 
-	<div class="row-fluid">
-		<div class="span8">
-	   	<a href="http://www.themco.ca/concert/manson-barnatan/"><img src="http://www.themco.ca/wp-content/uploads/2014/06/head18feb15.jpg" alt="MCO concert / 13 January 2015"></a>
-	   	<h4>Ballet and baroque music<br>mix at next MCO concert</h4>
-        <p>The <i>London Evening Standard</i> has described Israeli pianist Inon Barnatan as &ldquo;a true poet of the keyboard, refined, searching (and) unfailingly communicative,&rdquo; and he is widely known for his compelling and insightful musicianship and elegant virtuosity. Barnatan will perform Beethoven&rsquo;s Piano Concerto No. 2, in B Flat Major, Op. 19. See notes, bios and two-minute talks by Haley Rempel <a href="http://www.themco.ca/concert/manson-barnatan/"><b>here</b></a> or, if you&rsquo;re just after a quick ticket or two, click the links below.<br><img src="http://www.themco.ca/wp-content/uploads/2013/01/QuikTicket.png" alt="QuikTicket"><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=2c9f9ee1c8cd4d1383d7291f4793aea9"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/AdultC.png" alt="" width="62" height="40"></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=4c76e9c0aac741f7bc1f47d62b3d8fe3"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/SeniorC.png" alt="" width="62" height="40" /></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=8e8d99341b06457aaddf8184b9fe9edc"><img rel="tooltip" title="Click to add student ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/StudentC.png" alt="" width="62" height="40" /></a></p>
-		</div>
-	</div><!-- /row -->
+  <div class="row-fluid">
+    <div class="span8">
+      <a href="http://www.themco.ca/concert/manson-barnatan/"><img src="http://www.themco.ca/wp-content/uploads/2014/06/head18feb15.jpg" alt="MCO concert / 13 January 2015"></a>
+      <h4>Ballet and baroque music<br>mix at next MCO concert</h4>
+      <p>The <i>London Evening Standard</i> has described Israeli pianist Inon Barnatan as &ldquo;a true poet of the keyboard, refined, searching (and) unfailingly communicative,&rdquo; and he is widely known for his compelling and insightful musicianship and elegant virtuosity. Barnatan will perform Beethoven&rsquo;s Piano Concerto No. 2, in B Flat Major, Op. 19. See notes, bios and two-minute talks by Haley Rempel <a href="http://www.themco.ca/concert/manson-barnatan/"><b>here</b></a> or, if you&rsquo;re just after a quick ticket or two, click the links below.<br><img src="http://www.themco.ca/wp-content/uploads/2013/01/QuikTicket.png" alt="QuikTicket"><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=2c9f9ee1c8cd4d1383d7291f4793aea9"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/AdultC.png" alt="" width="62" height="40"></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=4c76e9c0aac741f7bc1f47d62b3d8fe3"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/SeniorC.png" alt="" width="62" height="40" /></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=8e8d99341b06457aaddf8184b9fe9edc"><img rel="tooltip" title="Click to add student ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/StudentC.png" alt="" width="62" height="40" /></a></p>
+    </div>
+  </div><!-- /row -->
 
+  <!-- <div class="row-fluid">
+    <small>&nbsp;</small>
+  </div> -->
 
-	<!-- <div class="row-fluid">
-		<small>&nbsp;</small>
-	</div> -->
+  <div class="row-fluid">
+    <div class="span8">
+      <div class="well">
+        <div class="media"><a class="pull-left" href="#extras" data-toggle="tab"><img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/07/DonorCello.jpg" width="96px" height="96px">
+          <div class="media-body">
+            <p><strong><a href="http://www.themco.ca/quick-links-to-1415-subscriptions/">Subscribe now to MCO&rsquo;s 2014/15 season<br>&mdash; enjoy six or nine concerts in Westminster Church!</strong></a><br />Only subscribers enjoy every benefit the MCO has to offer, like a free CD, Musicnet Subscriber Reward Card, ticket exchange privileges, discounts on extra tickets and more. Make this the season you subscribe to Canada&rsquo;s &ldquo;tiny, perfect, orchestra!&rdquo;</p> 
+          </div>
+        </div>
+      </div>
+    </div>
+  </div><!-- /row -->
 
-	<div class="row-fluid">
-		<div class="span8">
-			<div class="well">
-				<div class="media">
-					<a class="pull-left" href="#extras" data-toggle="tab">
-					<img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/07/DonorCello.jpg" width="96px" height="96px">
-					<div class="media-body">
-						<p><strong><a href="http://www.themco.ca/quick-links-to-1415-subscriptions/">Subscribe now to MCO&rsquo;s 2014/15 season<br>&mdash; enjoy six or nine concerts in Westminster Church!</strong></a><br />Only subscribers enjoy every benefit the MCO has to offer, like a free CD, Musicnet Subscriber Reward Card, ticket exchange privileges, discounts on extra tickets and more. Make this the season you subscribe to Canada&rsquo;s &ldquo;tiny, perfect, orchestra!&rdquo;</p> 
-					</div>
-				</div>
-			</div>
-		</div>
-	</div><!-- /row -->
+  <div class="row-fluid">
+    <div class="span8">
+      <div class="well-white">
+        <div class="media"><a class="pull-left" href="#extras" data-toggle="tab"><img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/01/Parlo1.png" width="96px" height="96px"></a>
+          <div class="media-body">
+            <p><strong><a href="#">Come in for a spin at the Vinyl Vault / CD Cellar!<br> Next sale: Saturday, February 28th, 10 am to 3 pm</strong></a><br />Browse one of the city&rsquo;s largest and most varied vinyl collections. You&rsquo;ll find thousands of titles in the jazz, pop, classical, rock, country and folk categories, and more! MCO&rsquo;s Vinyl Vault is located in the basement of the Power Building at 428 Portage Avenue (near The Bay).</p> 
+          </div>
+        </div>
+      </div>
+    </div>
+  </div><!-- /row -->
 
-	<div class="row-fluid">
-		<div class="span8">
-			<div class="well-white">
-				<div class="media">
-					<a class="pull-left" href="#extras" 	data-toggle="tab">
-					<img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/01/Parlo1.png" width="96px" height="96px">
-					</a>
-					<div class="media-body">
-						<p><strong><a href="#">Come in for a spin at the Vinyl Vault / CD Cellar!<br> Next sale: Saturday, February 28th, 10 am to 3 pm</strong></a><br />Browse one of the city&rsquo;s largest and most varied vinyl collections. You&rsquo;ll find thousands of titles in the jazz, pop, classical, rock, country and folk categories, and more! MCO&rsquo;s Vinyl Vault is located in the basement of the Power Building at 428 Portage Avenue (near The Bay).</p> 
-					</div>
-				</div>
-			</div>
-		</div>
-	</div><!-- /row -->
+  <div class="row-fluid">
 
-	<div class="row-fluid">
-		<div class="span4">
-			<div class="well">
-				<p><strong><a href="#">Sign up here for MCO&rsquo;s<br><em>Noteworthy</em> email newsletter!</strong></a></p>
-				<p>Our just-about-monthly missive is filled with information on upcoming concerts and special events. Unsubscribe at any time.</p>
-				<?php echo do_shortcode('[contact-form-7 id="1045" title="Noteworthy"]'); ?>
-			</div>
-		</div>
-		<div class="span3">
-			<p><a href="http://www.themco.ca/venue-faq/"><img src="http://www.themco.ca/wp-content/uploads/2014/11/VenueFAQ.png"></a></p>
-			<hr style="border-top: dotted 2px; color: #ddd;" />
-			<p><a href="http://www.themco.ca/troubadour-the-nightingale/"><img src="http://www.themco.ca/wp-content/uploads/2013/10/TroubadourPreOrder.jpg"></a></p>
-			<p><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=94ce7b6e08cf4bb79c932d28f04ae510"><button class="btn btn-small btn-success" type="button">Add to cart</button></a></p>
+    <div class="span4">
+      <div class="well">
+        <p><strong><a href="#">Sign up here for MCO&rsquo;s<br><em>Noteworthy</em> email newsletter!</strong></a></p>
+        <p>Our just-about-monthly missive is filled with information on upcoming concerts and special events. Unsubscribe at any time.</p>
+        <?php echo do_shortcode('[contact-form-7 id="1045" title="Noteworthy"]'); ?>
+      </div>
+    </div>
 
-		</div><!-- /span2 -->
-	</div><!-- /row -->
+    <div class="span3">
+      <p><a href="http://www.themco.ca/venue-faq/"><img src="http://www.themco.ca/wp-content/uploads/2014/11/VenueFAQ.png"></a></p>
+      <hr style="border-top: dotted 2px; color: #ddd;" />
+      <p><a href="http://www.themco.ca/troubadour-the-nightingale/"><img src="http://www.themco.ca/wp-content/uploads/2013/10/TroubadourPreOrder.jpg"></a></p>
+      <p><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=94ce7b6e08cf4bb79c932d28f04ae510"><button class="btn btn-small btn-success" type="button">Add to cart</button></a></p>
+    </div><!-- /span2 -->
+
+  </div><!-- /row-fluid -->
 
 <?php get_template_part('templates/content', 'mcohome'); ?>
 
