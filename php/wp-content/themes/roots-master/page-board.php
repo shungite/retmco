cca657b2ae7324ef62d70d46cd2142eaa24259e3
Jon Snidal <jsnidal@gmail.com>
2015-02-17 13:45:29 -0600

    File fix

--- php/wp-content/themes/roots-master/page-board.php
+++ php/wp-content/themes/roots-master/page-board.php
@@ -4,79 +4,71 @@ Template Name: Page Board Template
 */
 ?>
 <div class="container">
-
   <div class="row">
-
     <div class="span6">
       <p>&nbsp;</p>
-        <blockquote>
-          <p>To create and perform chamber orchestra repertoire at the highest artistic level possible for Manitobans, for Canadians, and for international audiences.</p>
-          <small>the Manitoba Chamber Orchestra&rsquo;s artistic mission statement</small>
-        </blockquote>
-
-        <div class="well">
-          <p><strong>Manitoba Chamber Orchestra<br>Board of Directors and staff</strong></p>
-            <dl class="dl">
-                <dt>President</dt>
-                <dd>Judith Hall</dd>
-                <dt>Vice-President</dt>
-                <dd>Faye Warren</dd>
-                <dt>Treasurer</dt>
-                <dd>Gil Forrest</dd>
-                <dt>Secretary</dt>
-                <dd>Alice MacKenzie</dd>
-                <dt>Members</dt>
-                <dd>Zaz Bajon</dd>
-                <dd>Coralie Bryant</dd>
-                <dd>Jim Chapryk</dd>
-                <dd>Robert Giesbrecht</dd>
-                <dd>Joan Hardy</dd>
-                <dd>Sue Irving</dd>
-                <dd>Suzanne Kennelly</dd>
-                <dd>Elaine Margolis</dd>
-                <dd>Michael McIntyre</dd>
-                <dd>Ingrid M&ouml;hlmann</dd>
-                <dd>Sunny Oh</dd>
-                <dd>Beryl Peters</dd>
-                <dd>Nicola Schaefer</dd>
-                <dd>Moti Shojania</dd>
-                <dd>Alison Thiessen</dd>
-                <dd>Elizabeth Tippett-Pope</dd>
-              </dl>
-            <p><strong>Administration</strong></p>
-              <dl class="dl">
-                <dt>Music Director</dt>
-                <dd>Anne Manson</dd>
-                <dt>Managing Director</dt>
-                <dd>Vicki Young</dd>
-                <dt>Development Manager</dt>
-                <dd>Joan Gillis</dd>
-                <dt>Office Manager &amp; Front-of-house Manager</dt>
-                <dd>Elise Anderson</dd>
-                <dt>Administrative Assistant</dt>
-                <dd>Corinne Friesen Loewen</dd>
-                <dt>Bookeeper</dt>
-                <dd>Pat Ireton</dd>
-                <dt>Director, Systems and Design</dt>
-                <dd>Jon Snidal</dd>
-                <dt>Concert Manager</dt>
-                <dd>Boyd MacKenzie</dd>
-                <dt>Librarian</dt>
-                <dd>Merrily Peters</dd>
-                <dt>Program advertising</dt>
-                <dd>Brian Chipney</dd>
-                <dt>Stage Manager</dt>
-                <dd>David MacKenzie</dd>
-              </dl>
-
+      <blockquote>
+        <p>To create and perform chamber orchestra repertoire at the highest artistic level possible for Manitobans, for Canadians, and for international audiences.</p>
+        <small>the Manitoba Chamber Orchestra&rsquo;s artistic mission statement</small>
+      </blockquote>
+      <div class="well">
+        <p><strong>Manitoba Chamber Orchestra<br>Board of Directors and staff</strong></p>
+          <dl class="dl">
+            <dt>President</dt>
+            <dd>Judith Hall</dd>
+            <dt>Vice-President</dt>
+            <dd>Faye Warren</dd>
+            <dt>Treasurer</dt>
+            <dd>Gil Forrest</dd>
+            <dt>Secretary</dt>
+            <dd>Alice MacKenzie</dd>
+            <dt>Members</dt>
+            <dd>Zaz Bajon</dd>
+            <dd>Coralie Bryant</dd>
+            <dd>Jim Chapryk</dd>
+            <dd>Robert Giesbrecht</dd>
+            <dd>Joan Hardy</dd>
+            <dd>Sue Irving</dd>
+            <dd>Suzanne Kennelly</dd>
+            <dd>Elaine Margolis</dd>
+            <dd>Michael McIntyre</dd>
+            <dd>Ingrid M&ouml;hlmann</dd>
+            <dd>Sunny Oh</dd>
+            <dd>Beryl Peters</dd>
+            <dd>Nicola Schaefer</dd>
+            <dd>Moti Shojania</dd>
+            <dd>Alison Thiessen</dd>
+            <dd>Elizabeth Tippett-Pope</dd>
+          </dl>
+        <p><strong>Administration</strong></p>
+          <dl class="dl">
+            <dt>Music Director</dt>
+            <dd>Anne Manson</dd>
+            <dt>Managing Director</dt>
+            <dd>Vicki Young</dd>
+            <dt>Development Manager</dt>
+            <dd>Joan Gillis</dd>
+            <dt>Office Manager &amp; Front-of-house Manager</dt>
+            <dd>Elise Anderson</dd>
+            <dt>Administrative Assistant</dt>
+            <dd>Corinne Friesen Loewen</dd>
+            <dt>Bookeeper</dt>
+            <dd>Pat Ireton</dd>
+            <dt>Director, Systems and Design</dt>
+            <dd>Jon Snidal</dd>
+            <dt>Concert Manager</dt>
+            <dd>Boyd MacKenzie</dd>
+            <dt>Librarian</dt>
+            <dd>Merrily Peters</dd>
+            <dt>Program advertising</dt>
+            <dd>Brian Chipney</dd>
+            <dt>Stage Manager</dt>
+            <dd>David MacKenzie</dd>
+          </dl>
         </div>
-
       </div>
-
     </div><!-- /row -->
-
   <?php get_template_part('templates/content', 'board'); ?>
-
 </div><!-- /container -->
 
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
--- php/wp-content/themes/roots-master/page-venue.php
+++ php/wp-content/themes/roots-master/page-venue.php
@@ -5,97 +5,96 @@ Template Name: Page Venue Template
 ?>
 <div class="container">
 
-<div class="row">
+  <div class="row">
 
-	<div class="span8">
-		<p>&nbsp;</p>
-		<blockquote>
-			<p>No need for a why, but here&rsquo;s the what, where and when of getting to a Manitoba Chamber Orchestra concert &mdash; <b>frequently asked questions</b> included!</p>
-		</blockquote>
+    <div class="span8">
+      <p>&nbsp;</p>
+      <blockquote>
+        <p>No need for a why, but here&rsquo;s the what, where and when of getting to a Manitoba Chamber Orchestra concert &mdash; <b>frequently asked questions</b> included!</p>
+      </blockquote>
 
-		<div class="well">
-			<p>&nbsp;</p>
-			<p><img src="http://www.themco.ca/wp-content/uploads/2014/11/Westminster-Map-with-adjusted-colours2.png"></p>
-			<p>&nbsp;</p>
-			<p><b>Where and when do Manitoba Chamber Orchestra concerts take place?</b></p>
-			<p>While the MCO presents many concerts throughout Manitoba, across Canada and abroad, the Orchestra&rsquo;s usual home is in historic  <a href="http://www.westminsterchurchwinnipeg.ca"><b>Westminster United Church</b></a>, located at 745 Westminster at Maryland in Winnipeg. The venue is accessible via the following bus routes:</p>
-			<dl class="dl">
-				<dt>Tach&eacute; Avenue in St. Bon­iface to Wolseley</dt>
-				<dd><a href="http://winnipegtransit.com/en/routes/10"><b>Route 10</b></a></dd>
-				<dt>Leila Avenue in West Kil­donan to the Miseri­cor­dia Hospital</dt>
-				<dd><a href="http://winnipegtransit.com/en/routes/17"><b>Route 17</b></a></dd>
-				<dt>Leighton and Watt in East Kil­donan through River Heights to Polo Park</dt>
-				<dd><a href="http://winnipegtransit.com/en/routes/20"><b>Route 20</b></a></dd>
-				<dt>Logan and Sherbrook to Pembina in Ft. Garry</dt>
-				<dd><a href="http://winnipegtransit.com/en/routes/29"><b>Route 29</b></a></dd>
-			</dl>
-			<p>If none of these routes are suitable, please consult the <a href="http://winnipegtransit.com/en/routes"><b>Winnipeg Transit route map</b></a> for more information, or use their <a href="http://winnipegtransit.com/en/navigo"><b>Navigo trip planner</b></a>. Further help is available by calling <b>311</b>.</p>
-			<p>All regular season Manitoba Chamber Orchestra concerts begin at 7:30 pm, and the doors open at 6:30 pm.</p>
-		</div><!-- /well -->
+      <div class="well">
+        <p>&nbsp;</p>
+          <p><img src="http://www.themco.ca/wp-content/uploads/2014/11/Westminster-Map-with-adjusted-colours2.png"></p>
+          <p>&nbsp;</p>
+          <p><b>Where and when do Manitoba Chamber Orchestra concerts take place?</b></p>
+          <p>While the MCO presents many concerts throughout Manitoba, across Canada and abroad, the Orchestra&rsquo;s usual home is in historic  <a href="http://www.westminsterchurchwinnipeg.ca"><b>Westminster United Church</b></a>, located at 745 Westminster at Maryland in Winnipeg. The venue is accessible via the following bus routes:</p>
+            <dl class="dl">
+              <dt>Tach&eacute; Avenue in St. Bon­iface to Wolseley</dt>
+              <dd><a href="http://winnipegtransit.com/en/routes/10"><b>Route 10</b></a></dd>
+              <dt>Leila Avenue in West Kil­donan to the Miseri­cor­dia Hospital</dt>
+              <dd><a href="http://winnipegtransit.com/en/routes/17"><b>Route 17</b></a></dd>
+              <dt>Leighton and Watt in East Kil­donan through River Heights to Polo Park</dt>
+              <dd><a href="http://winnipegtransit.com/en/routes/20"><b>Route 20</b></a></dd>
+              <dt>Logan and Sherbrook to Pembina in Ft. Garry</dt>
+              <dd><a href="http://winnipegtransit.com/en/routes/29"><b>Route 29</b></a></dd>
+            </dl>
+          <p>If none of these routes are suitable, please consult the <a href="http://winnipegtransit.com/en/routes"><b>Winnipeg Transit route map</b></a> for more information, or use their <a href="http://winnipegtransit.com/en/navigo"><b>Navigo trip planner</b></a>. Further help is available by calling <b>311</b>.</p>
+          <p>All regular season Manitoba Chamber Orchestra concerts begin at 7:30 pm, and the doors open at 6:30 pm.</p>
+      </div><!-- /well -->
 
-		<div class="well-white">
-			<p><b>Where can I get tickets to Manitoba	Chamber Orchestra concerts?</b></p>
-			<p>Season tickets may be purchased on this site or by calling the MCO Ticketline at 204-783-7377. Casual tickets are available on this site and at the door, but also at several outlets in Winnipeg. These outlets include:</p>
-			<dl class="dl">
-				<dt><a href="http://www.mcnallyrobinson.com/home" title="McNally Robinson">McNally Robinson Booksellers</a></dt>
-				<dd>1120 Grant Avenue, Winnipeg MB R3M 2A6, tel 204-475-0483</dd>
-				<dt><a href="http://www.wecc.ca/" title="West End Cultural Centre">West End Cultural Centre</a></dt>
-				<dd>586 Ellice Avenue at Sherbrook Street MB R3B 1Z8, tel 204-783-6918</dd>
-				<dt><a href="http://www.organicplanet.coop/" title="Organic Planet">Organic Planet Worker Co-op</a></dt>
-				<dd>877 Westminster Avenue, Winnipeg MB R3G 1B3, tel 204-772-8771</dd>
-			</dl>
-		</div><!-- /well -->
+      <div class="well-white">
+        <p><b>Where can I get tickets to Manitoba	Chamber Orchestra concerts?</b></p>
+        <p>Season tickets may be purchased on this site or by calling the MCO Ticketline at 204-783-7377. Casual tickets are available on this site and at the door, but also at several outlets in Winnipeg. These outlets include:</p>
+        <dl class="dl">
+          <dt><a href="http://www.mcnallyrobinson.com/home" title="McNally Robinson">McNally Robinson Booksellers</a></dt>
+          <dd>1120 Grant Avenue, Winnipeg MB R3M 2A6, tel 204-475-0483</dd>
+          <dt><a href="http://www.wecc.ca/" title="West End Cultural Centre">West End Cultural Centre</a></dt>
+          <dd>586 Ellice Avenue at Sherbrook Street MB R3B 1Z8, tel 204-783-6918</dd>
+          <dt><a href="http://www.organicplanet.coop/" title="Organic Planet">Organic Planet Worker Co-op</a></dt>
+          <dd>877 Westminster Avenue, Winnipeg MB R3G 1B3, tel 204-772-8771</dd>
+        </dl>
+      </div><!-- /well -->
 
-		<div class="well">
-			<p><b>Where can I collect my tickets?</b></p>
-			<p>Purchased tickets can be mailed out ($1.00 added to cover postage and handling), picked up downtown in the MCO office on the third floor in Portage Place (9-5 Mon-Fri) or collected at the door on concert night.</p>
-		</div><!-- /well -->
+      <div class="well">
+        <p><b>Where can I collect my tickets?</b></p>
+        <p>Purchased tickets can be mailed out ($1.00 added to cover postage and handling), picked up downtown in the MCO office on the third floor in Portage Place (9-5 Mon-Fri) or collected at the door on concert night.</p>
+      </div><!-- /well -->
 
-		<div class="well-white">
-			<p><b>I&rsquo;m a subscriber. How do I exchange tickets?</b></p>
-			<p>For subscribers, any concert ticket is good at any other show in the same season. All we ask is that you inform us at least 48 hours in advance of concert day that you would like to swap that ticket for another. This allows us to know how many seats we have available for sold out shows. Simply call 204-783-7377 at least two days before the concert. For obvious reasons, we are unable to exchange tickets into a sold-out concert.</p>
-		</div><!-- /well -->
+      <div class="well-white">
+        <p><b>I&rsquo;m a subscriber. How do I exchange tickets?</b></p>
+        <p>For subscribers, any concert ticket is good at any other show in the same season. All we ask is that you inform us at least 48 hours in advance of concert day that you would like to swap that ticket for another. This allows us to know how many seats we have available for sold out shows. Simply call 204-783-7377 at least two days before the concert. For obvious reasons, we are unable to exchange tickets into a sold-out concert.</p>
+      </div><!-- /well -->
 
-		<div class="well">
-			<p><b>When are season tickets mailed out?</b></p>
-			<p>Season tickets ordered in the spring or summer months for the upcoming season are mailed out from late-August; orders received from then on are processed and mailed immediately. If you subscribe close to the date of a concert, or if you have made a request, your tickets may be picked up at the ticket table at any concert.</p>
-		</div><!-- /well -->
+      <div class="well">
+        <p><b>When are season tickets mailed out?</b></p>
+        <p>Season tickets ordered in the spring or summer months for the upcoming season are mailed out from late-August; orders received from then on are processed and mailed immediately. If you subscribe close to the date of a concert, or if you have made a request, your tickets may be picked up at the ticket table at any concert.</p>
+      </div><!-- /well -->
 
-		<div class="well-white">
-			<p><b>Does the MCO and Westminster United Church cater to special needs?</b></p>
-			<p>The staff at the Manitoba Chamber Orchestra prides itself on the accessibility of its venue and its ability to accommodate all music lovers! A state-of-the-art ramp and lift sytem has been installed at Westminster United Church on the east door on Westminster Avenue, and staff is always available to assist. If you believe you need a specific type of assistance or simply want to let us know you&rsquo;ll be attending a concert, please contact the MCO office at 204-783-7377.</p>
-		</div><!-- /well -->
+      <div class="well-white">
+        <p><b>Does the MCO and Westminster United Church cater to special needs?</b></p>
+        <p>The staff at the Manitoba Chamber Orchestra prides itself on the accessibility of its venue and its ability to accommodate all music lovers! A state-of-the-art ramp and lift sytem has been installed at Westminster United Church on the east door on Westminster Avenue, and staff is always available to assist. If you believe you need a specific type of assistance or simply want to let us know you&rsquo;ll be attending a concert, please contact the MCO office at 204-783-7377.</p>
+      </div><!-- /well -->
 
-		<div class="well">
-			<p><b>Are there any age restrictions at MCO concerts?</b></p>
-			<p>Children under the age of 14 should be accompanied by a teacher or other adult. Generally, babes in arms are not allowed, but we won&rsquo;t stop you from bringing any member of the family! That being said, a classical music concert is best enjoyed at a certain level of maturity. Please keep children quiet and seated, and use appropriate intervals for washroom breaks.</p>
-		</div><!-- /well -->
+      <div class="well">
+        <p><b>Are there any age restrictions at MCO concerts?</b></p>
+        <p>Children under the age of 14 should be accompanied by a teacher or other adult. Generally, babes in arms are not allowed, but we won&rsquo;t stop you from bringing any member of the family! That being said, a classical music concert is best enjoyed at a certain level of maturity. Please keep children quiet and seated, and use appropriate intervals for washroom breaks.</p>
+      </div><!-- /well -->
 
-		<div class="well-white">
-			<p><b>Where can I park my car?</b></p>
-			<p>Limited parking is available in the Food Fare lot across from the Church (the two northernmost rows only). Those spots fill up pretty quickly, but there is plenty of parking in the surrounding neighbourhood, but prepare for a bit of a walk! Arrive early for top spots.</p>
-		</div><!-- /well -->
+      <div class="well-white">
+        <p><b>Where can I park my car?</b></p>
+        <p>Limited parking is available in the Food Fare lot across from the Church (the two northernmost rows only). Those spots fill up pretty quickly, but there is plenty of parking in the surrounding neighbourhood, but prepare for a bit of a walk! Arrive early for top spots.</p>
+      </div><!-- /well -->
 
-		<div class="well">
-			<p><b>Where is my lost property?</b></p>
-			<p>If you have lost something at a Manitoba Chamber Orchestra concert, please get in touch with the Westminster United Church office at 204-784-1330.</p>
-		</div><!-- /well -->
+      <div class="well">
+        <p><b>Where is my lost property?</b></p>
+        <p>If you have lost something at a Manitoba Chamber Orchestra concert, please get in touch with the Westminster United Church office at 204-784-1330.</p>
+      </div><!-- /well -->
 
-		<div class="well-white">
-			<p><b>What should I bring with me?</b></p>
-			<p>As little as possible! While we have a limited number of seat cushions for our patrons&rsquo; comfort, if you have your own, bring it along so there will be enough to go around. No food or drink is to be brought into the venue. If you did bring a cell phone, please turn off the ringer (pagers and beeping watches, too) before the concert begins. The use of recording and photographic equipment is not permitted.</p>
-		</div><!-- /well -->
+      <div class="well-white">
+        <p><b>What should I bring with me?</b></p>
+        <p>As little as possible! While we have a limited number of seat cushions for our patrons&rsquo; comfort, if you have your own, bring it along so there will be enough to go around. No food or drink is to be brought into the venue. If you did bring a cell phone, please turn off the ringer (pagers and beeping watches, too) before the concert begins. The use of recording and photographic equipment is not permitted.</p>
+      </div><!-- /well -->
 
-		<div class="well">
-			<p><img src="http://www.themco.ca/wp-content/uploads/2014/11/PizzaClubTrans.png"></p>
-			<p><b>How does MCO&rsquo;s Pizza Club work?</b></p>
-			<p>MCO&rsquo;s Pizza Club is great fun for the whole family! Reserve three days in advance of the concert date to reserve your spots. Enjoy free pizza (included in the price of your ticket) and a talk on the music you&rsquo;ll hear at the concert. Starts at 6:00 pm; call 204-783-7377 for reservations. Please enter via the easternmost door on Westminster Avenue.</p>
-		</div><!-- /well -->
-	
+      <div class="well">
+        <p><img src="http://www.themco.ca/wp-content/uploads/2014/11/PizzaClubTrans.png"></p>
+        <p><b>How does MCO&rsquo;s Pizza Club work?</b></p>
+        <p>MCO&rsquo;s Pizza Club is great fun for the whole family! Reserve three days in advance of the concert date to reserve your spots. Enjoy free pizza (included in the price of your ticket) and a talk on the music you&rsquo;ll hear at the concert. Starts at 6:00 pm; call 204-783-7377 for reservations. Please enter via the easternmost door on Westminster Avenue.</p>
+      </div><!-- /well -->
 
-	</div>
+    </div>
 
-</div><!-- /row -->
+  </div><!-- /row -->
 
 <?php get_template_part('templates/content', 'venue'); ?>
 
