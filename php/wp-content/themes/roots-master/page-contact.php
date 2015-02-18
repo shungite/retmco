20c0819a547e2f855625fc587f08574a89629e7c
Jon Snidal <jsnidal@gmail.com>
2015-02-18 12:29:15 -0600

    Contact

--- php/wp-content/themes/roots-master/page-contact.php
+++ php/wp-content/themes/roots-master/page-contact.php
@@ -3,19 +3,14 @@
 Template Name: Page Contact Template
 */
 ?>
+
 <div class="container">
-
-	<div class="row">
-
-		<p>&nbsp;</p>
-
-		<?php get_template_part('templates/content', 'contact'); ?>
-
-		<div class="span6">
-			<p><strong>Manitoba Chamber Orchestra</strong><br />Unit Y300, Portage Place<br />393 Portage Avenue<br />Winnipeg MB   R3B 3H6<br />Tel (204) 783-7377<br />Fax (204) 783-7383<br />info@themco.ca<br />themco.ca</p>
-		</div>
-
-</div><!-- /row -->
-
+  <div class="row">
+    <p>&nbsp;</p>
+    <?php get_template_part('templates/content', 'contact'); ?>
+    <div class="span6">
+      <p><strong>Manitoba Chamber Orchestra</strong><br />Unit Y300, Portage Place<br />393 Portage Avenue<br />Winnipeg MB   R3B 3H6<br />Tel (204) 783-7377<br />Fax (204) 783-7383<br />info@themco.ca<br />themco.ca</p>
+    </div>
+  </div><!-- /row -->
 </div><!-- /container -->
 
