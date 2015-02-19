5fea85795dff0c52bbe71ad0b51d1b25b704380a
Jon Snidal <jsnidal@gmail.com>
2015-02-19 11:54:04 -0600

    Format HS

--- php/wp-content/themes/roots-master/page-heartstrings.php
+++ php/wp-content/themes/roots-master/page-heartstrings.php
@@ -3,37 +3,39 @@
 Template Name: Page Heartstrings Template
 */
 ?>
+
 <div class="container">
 
-<div class="row">
+  <div class="row">
 
-	<div class="span8">
-		<img src="http://www.themco.ca/wp-content/uploads/2014/06/heartstrings.jpg" alt="" width="770" height="455">
-	</div>
-	<div class="span2">
-		<p>.</p>
-	</div>
+    <div class="span8">
+      <img src="http://www.themco.ca/wp-content/uploads/2014/06/heartstrings.jpg" alt="" width="770" height="455">
+    </div><!-- /span8 -->
 
-</div><!-- /row -->
+    <div class="span2">
+      <p>.</p>
+    </div><!-- /span2 -->
 
-<div class="row">
+    </div><!-- /row -->
 
-	<div class="span6">
-		<h4>The MCO's gala evening<br />&mdash; fabulous 2015 edition!</h4>
-		<p>The main event, held in <a href="http://www.fortgarryhotel.com/" >The Fort Garry</a> on Broadway &mdash; a heavenly evening of music &amp; food in Winnipeg&rsquo;s most elegant setting.</p>
-		<p><strong>Friday, 8 May 2015 at The Fort Garry<br />Event details to follow!</strong></p>
-		<p><strong>&bull; <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=43afb0af27a54ae18e70bf8ee56a737b">buy tickets @ $175</a><br>&bull; <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=24c6b0d77db847acbd1bcd875ee1bcec">buy a table of ten @ $1750</a><br>&bull; <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=4bf23be4b03a4b29a0f306853ce8581b">buy Patron tickets @ $200*</a></strong></p>
-		<p class="fineprint">Patrons are acknowledged in the program<br /> and receive a larger tax receipt.</p>
-  	<!-- <p class="regtext"><a href="http://www.mannfinancialassurance.com/" ><img src="http://www.themco.ca/wp-content/uploads/2012/11/mannfinanciallogo.jpg" width="140" height="52" border="0" alt="Mann Financial Assurance"></a></p>
-  	<p class="fineprint">Event sponsor / <strong><a href="http://www.mannfinancialassurance.com/" >Mann Financial Assurance</a></strong></p> -->
-	</div><!-- /span6 -->
+    <div class="row">
 
-	<div class="span2"> 
-		<img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /><br />
-		<p class="fineprint">Click above to add a regular ticket to your shopping cart; adjust quantity in cart and return to purchase other tickets.</p>
-	</div><!-- /span2 -->
+    <div class="span6">
+      <h4>The MCO's gala evening<br>&mdash; fabulous 2015 edition!</h4>
+      <p>The main event, held in <a href="http://www.fortgarryhotel.com/" >The Fort Garry</a> on Broadway &mdash; a heavenly evening of music &amp; food in Winnipeg&rsquo;s most elegant setting.</p>
+      <p><strong>Friday, 8 May 2015 at The Fort Garry<br />Event details to follow!</strong></p>
+      <p><strong>&bull; <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=43afb0af27a54ae18e70bf8ee56a737b">buy tickets @ $175</a><br>&bull; <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=24c6b0d77db847acbd1bcd875ee1bcec">buy a table of ten @ $1750</a><br>&bull; <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=4bf23be4b03a4b29a0f306853ce8581b">buy Patron tickets @ $200*</a></strong></p>
+      <p class="fineprint">Patrons are acknowledged in the program<br /> and receive a larger tax receipt.</p>
+      <!-- <p class="regtext"><a href="http://www.mannfinancialassurance.com/" ><img src="http://www.themco.ca/wp-content/uploads/2012/11/mannfinanciallogo.jpg" width="140" height="52" border="0" alt="Mann Financial Assurance"></a></p>
+      <p class="fineprint">Event sponsor / <strong><a href="http://www.mannfinancialassurance.com/" >Mann Financial Assurance</a></strong></p> -->
+    </div><!-- /span6 -->
 
-</div><!-- /row -->
+    <div class="span2"> 
+      <img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /><br />
+      <p class="fineprint">Click above to add a regular ticket to your shopping cart; adjust quantity in cart and return to purchase other tickets.</p>
+    </div><!-- /span2 -->
+
+  </div><!-- /row -->
 
 <?php get_template_part('templates/content', 'heartstrings'); ?>
 
