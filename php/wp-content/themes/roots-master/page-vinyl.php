8b9a6ce9d7ec4f3ab84f780a8dee6ec389077ffd
Jon Snidal <jsnidal@gmail.com>
2015-02-19 12:29:09 -0600

    Vinyl update

--- php/wp-content/themes/roots-master/page-vinyl.php
+++ php/wp-content/themes/roots-master/page-vinyl.php
@@ -3,38 +3,39 @@
 Template Name: Page Vinyl Template
 */
 ?>
+
 <div class="container">
 
-<div class="row">
+  <div class="row">
 
-	<div class="span8">
-		<img src="http://www.themco.ca/wp-content/uploads/2013/01/headvinyl.jpg" alt="" width="770" height="455">
-	</div>
-	
-</div><!-- /row -->
+    <div class="span8">
+      <img src="http://www.themco.ca/wp-content/uploads/2013/01/headvinyl.jpg" alt="" width="770" height="455">
+    </div>
 
-<div class="row">
+  </div><!-- /row -->
 
-	<div class="span8">
-		<h4>Check here for<br>Vinyl Vault sales dates!</h4>
-		<p>The Manitoba Chamber Orchestra&rsquo;s almost-monthly Vinyl Vault sales are your chance to rummage around in the city&rsquo;s largest used record collection! Find rare titles in jazz, pop, classical, opera, rock, country, folk, easy listening and much more &mdash; <b>collectibles a speciality!</b></p>
-		<p><b>10:00 am to 3:00 pm, Saturdays<br>Power Building basement<br>428 Portage Avenue<br>Dates for 14/15:</b></p>
-		<p>27 September 2014<br>25 October 2014<br>29 November 2014<br>31 January 2015<br>28 February 2015<br>28 March 2015<br>25 April 2015<br>30 May 2015<br>27 June 2015</p>
-		<p>&nbsp;</p>
-	</div>
+  <div class="row">
 
-</div><!-- /row -->
+    <div class="span8">
+      <h4>Check here for<br>Vinyl Vault sales dates!</h4>
+      <p>The Manitoba Chamber Orchestra&rsquo;s almost-monthly Vinyl Vault sales are your chance to rummage around in the city&rsquo;s largest used record collection! Find rare titles in jazz, pop, classical, opera, rock, country, folk, easy listening and much more &mdash; <b>collectibles a speciality!</b></p>
+      <p><b>10:00 am to 3:00 pm, Saturdays<br>Power Building basement<br>428 Portage Avenue<br>Dates for 14/15:</b></p>
+      <p>27 September 2014<br>25 October 2014<br>29 November 2014<br>31 January 2015<br>28 February 2015<br>28 March 2015<br>25 April 2015<br>30 May 2015<br>27 June 2015</p>
+      <p>&nbsp;</p>
+    </div>
 
-<div class="row">
+  </div><!-- /row -->
 
-	<div class="span6">
-		<div class="well">
-			<p><b>Donate your unwanted records to the MCO!</b></p>
-			<p>We always want new used records! If you have some you&rsquo;d like to donate, call the MCOI office at 204.783.7377 for drop-off instructions or to arrange a pick-up. Please note that pickups may take a while as we keep our volunteers pretty busy &mdash; thanks in advance!</p>
-		</div><!-- /well -->
-	</div><!-- /span3 -->
+  <div class="row">
 
-</div><!-- /row -->
+    <div class="span6">
+      <div class="well">
+        <p><b>Donate your unwanted records to the MCO!</b></p>
+        <p>We always want new used records! If you have some you&rsquo;d like to donate, call the MCOI office at 204.783.7377 for drop-off instructions or to arrange a pick-up. Please note that pickups may take a while as we keep our volunteers pretty busy &mdash; thanks in advance!</p>
+      </div><!-- /well -->
+    </div><!-- /span6 -->
+
+  </div><!-- /row -->
 
 <?php get_template_part('templates/content', 'vinyl'); ?>
 
