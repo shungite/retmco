d86529d03f3bb8f9cdd4b529a16ece8a222e2484
Jon Snidal <jsnidal@gmail.com>
2015-03-05 14:34:05 -0600

    Ed sponsors

--- php/wp-content/themes/roots-master/page-programs.php
+++ php/wp-content/themes/roots-master/page-programs.php
@@ -3,90 +3,84 @@
 Template Name: Page Programs Template
 */
 ?>
+
 <div class="container">
 
-<div class="row">
+  <div class="row">
+    <div class="span8">
+      <img src="http://www.themco.ca/wp-content/uploads/2014/10/e+o.jpg" alt="" width="770" height="455">
+    </div>
+    <div class="span2">
+      <p>.</p>
+    </div>
+  </div><!-- /row -->
 
-	<div class="span8">
-		<img src="http://www.themco.ca/wp-content/uploads/2014/10/e+o.jpg" alt="" width="770" height="455">
-	</div>
-	<div class="span2">
-		<p>.</p>
-	</div>
+  <div class="row">
+    <div class="span7">
+      <h4>Students love the excitement<br />of live chamber music!</h4>
+      <p>Whether it&rsquo;s a school band or choir, Orff group, recorder ensemble, or private studio, children learn through music! There are so many reasons to bring the young people in your life to an MCO event &mdash; they won&rsquo;t hear anything like this anywhere else.</p>
+      <p>Help them create a cultural and musical framework for themselves. Show them where their music studies can lead &mdash; they can become an audience member, composer, conductor, musician, board member, donor, educator or CD purchaser. Have fun sharing music with them and with other avid music fans.</p>
+      <p><strong>School visits &mdash; the MCO comes to you</strong> / the MCO Outreach Ensemble is a roaming troupe of MCO players consisting of violin, viola, cello, double bass and guitar. It offers workshops and mini-concerts in schools throughout the province. Call 204.783.7377 for more info or to request a visit.</p>
+      <p>&nbsp;</p>
+    </div><!-- /span7 -->
+    <div class="span1"> 
+      <a href="http://www.themco.ca/schools-form/"><img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /></a><br />
+      <p class="fineprint">Click above for order form.</p>
+    </div><!-- /span1 -->
+  </div><!-- /row -->
+  <div class="row-fluid">
+    <div class="span7">
+      <div class="well">
+        <div class="media-body">
+          <p><strong><a href="http://www.sunlife.ca/">Sun Life Financial</a><br>&lsquo;Music for all!&rsquo;program: Open dress rehearsals for schools</strong></p>
+          <p>Bring your school group to Westminster Church for a 50-minute open dress rehearsal. Learn about the work that goes into presenting a professional orchestral performance. Watch musicians and conductor rehearse and be the first to hear what audiences will hear that night! <strong>Tickets $6.00 including GST.</strong> These open dress rehearsals are best suited to senior students, adults and home-schooled children. They are working rehearsals with some stopping and starting. Offered for the concerts listed <a href="http://www.themco.ca/schools-form"><strong>on this form.</strong></a></p>
+          <p><a href="http://www.sunlife.ca/"><img src="http://www.themco.ca/wp-content/uploads/2013/01/SunLife-logo.png"></a></p> 
+        </div>
+      </div>
+    </div>
+  </div><!-- /row -->
 
-</div><!-- /row -->
+  <div class="row-fluid">
 
-<div class="row">
+    <div class="span7">
+      <div class="well-white">
+        <div class="media-body">
+          <p><strong>MCO Pizza Club</strong></p>
+          <p><strong>Come at 6:00 pm</strong> for a primer on the music that you will be hearing in that evening's concert. Pizza and a drink are included in your ticket cost if you book at least three days ahead. Appropriate for all ages, student groups and family groups. Offered before the following concerts:</p>
+          <dl class="dl">
+            <dt><del>October 28</dt>
+            <dd>Guest conductor <b>Scott Yoo</b> and great works for strings by Vivaldi and Sir Hubert Parry.</del></dd>
+            <dt>November 26</dt>
+            <dd><b>Anne Manson</b> and flautist <b>Sharon Bezaly</b>; works by Ginastera, Serebrier, Vivaldi and Haydn.</dd>
+            <dt>January 13</dt>
+            <dd>Guest conductor <b>Earl Stafford</b> and harpsichordist <b>Eric Lussier</b>; works by Bach, Rota, Stravinsky and Holst.</dd>
+            <dt>February 18</dt>
+            <dd><b>Anne Manson</b> and pianist <b>Inon Barnatan</b>; works by Mozart and Beethoven.</dd>
+            <dt>March 17</dt>
+            <dd><b>Anne Manson</b> and cellist <b>Ariel Barnes</b>; works by Jan&aacute;&#269;ek, Haydn and Heidi Ouellette.</dd>
+            <dt>April 22</dt>
+            <dd><b>Chamber Night</b>, with a focus on winds; works by Mozart, Ysa&#255;e, Beethoven and Dvo&#345;&aacute;k.</dd>
+            <dt>May 19</dt>
+            <dd>Guest conductor <b>Tania Miller</b> and classical guitarist <b>Sharon Isbin</b>; works by Geminiani, Vivaldi, Howard Shore, Gerald Barry and Aaron Jay Kernis.</dd>
+            <dt>June 3</dt>
+            <dd><b>Anne Manson</b> and piano superstar <b>Jan Lisiecki</b>; works by Jan&aacute;&#269;ek and Mozart.</dd>
+          </dl>
+          <p class="text-error"><b>Space is limited</b></p>
+          <p class="text-error">Register early by phone: call Joan at 204.783.7377 at least three days before the concert to purchase tickets and book your spot in the MCO Pizza Club. Pizza is free with your concert tickets; parents, grandparents, teachers and children welcome! <b>Concerts begin at 7:30 pm.</b></p>
+        </div>
+      </div>
+    </div>
 
-	<div class="span7">
-		<h4>Students love the excitement<br />of live chamber music!</h4>
-		<p>Whether it&rsquo;s a school band or choir, Orff group, recorder ensemble, or private studio, children learn through music! There are so many reasons to bring the young people in your life to an MCO event &mdash; they won&rsquo;t hear anything like this anywhere else.</p>
-		<p>Help them create a cultural and musical framework for themselves. Show them where their music studies can lead &mdash; they can become an audience member, composer, conductor, musician, board member, donor, educator or CD purchaser. Have fun sharing music with them and with other avid music fans.</p>
-		<p><strong>School visits &mdash; the MCO comes to you</strong> / the MCO Outreach Ensemble is a roaming troupe of MCO players consisting of violin, viola, cello, double bass and guitar. It offers workshops and mini-concerts in schools throughout the province. Call 204.783.7377 for more info or to request a visit.</p>
-		<p>&nbsp;</p>
+  </div><!-- /row -->
 
-	</div><!-- /span6 -->
-
-	<div class="span1"> 
-		<a href="http://www.themco.ca/schools-form/"><img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /></a><br />
-		<p class="fineprint">Click above for order form.</p>
-	</div><!-- /span2 -->
-
-</div><!-- /row -->
-
-<div class="row-fluid">
-
-	<div class="span7">
-		<div class="well">
-			<div class="media-body">
-				<p><strong><a href="http://www.sunlife.ca/">Sun Life Financial</a><br>&lsquo;Music for all!&rsquo;program: Open dress rehearsals for schools</strong></p>
-				<p>Bring your school group to Westminster Church for a 50-minute open dress rehearsal. Learn about the work that goes into presenting a professional orchestral performance. Watch musicians and conductor rehearse and be the first to hear what audiences will hear that night! <strong>Tickets $6.00 including GST.</strong> These open dress rehearsals are best suited to senior students, adults and home-schooled children. They are working rehearsals with some stopping and starting. Offered for the concerts listed <a href="http://www.themco.ca/schools-form"><strong>on this form.</strong></a></p>
-				<p><a href="http://www.sunlife.ca/"><img src="http://www.themco.ca/wp-content/uploads/2013/01/SunLife-logo.png"></a></p> 
-			</div>
-		</div>
-	</div>
-
-</div><!-- /row -->
-
-<div class="row-fluid">
-
-	<div class="span7">
-		<div class="well-white">
-			<div class="media-body">
-				<p><strong>MCO Pizza Club</strong></p>
-				<p><strong>Come at 6:00 pm</strong> for a primer on the music that you will be hearing in that evening's concert. Pizza and a drink are included in your ticket cost if you book at least three days ahead. Appropriate for all ages, student groups and family groups. Offered before the following concerts:</p>
-				<dl class="dl">
-  				<dt><del>October 28</dt>
-  				<dd>Guest conductor <b>Scott Yoo</b> and great works for strings by Vivaldi and Sir Hubert Parry.</del></dd>
-  				<dt>November 26</dt>
-  				<dd><b>Anne Manson</b> and flautist <b>Sharon Bezaly</b>; works by Ginastera, Serebrier, Vivaldi and Haydn.</dd>
-  				<dt>January 13</dt>
-  				<dd>Guest conductor <b>Earl Stafford</b> and harpsichordist <b>Eric Lussier</b>; works by Bach, Rota, Stravinsky and Holst.</dd>
-  				<dt>February 18</dt>
-  				<dd><b>Anne Manson</b> and pianist <b>Inon Barnatan</b>; works by Mozart and Beethoven.</dd>
-  				<dt>March 17</dt>
-  				<dd><b>Anne Manson</b> and cellist <b>Ariel Barnes</b>; works by Jan&aacute;&#269;ek, Haydn and Heidi Ouellette.</dd>
-  				<dt>April 22</dt>
-  				<dd><b>Chamber Night</b>, with a focus on winds; works by Mozart, Ysa&#255;e, Beethoven and Dvo&#345;&aacute;k.</dd>
-  				<dt>May 19</dt>
-  				<dd>Guest conductor <b>Tania Miller</b> and classical guitarist <b>Sharon Isbin</b>; works by Geminiani, Vivaldi, Howard Shore, Gerald Barry and Aaron Jay Kernis.</dd>
-  				<dt>June 3</dt>
-  				<dd><b>Anne Manson</b> and piano superstar <b>Jan Lisiecki</b>; works by Jan&aacute;&#269;ek and Mozart.</dd>
-  			</dl>
-			</div>
-		</div>
-	</div>
-
-</div><!-- /row -->
-
-<div class="row-fluid">
-	<div class="span7">
-		<div class="well-white">
-			<p class="text-error"><b>Space is limited</b></p>
-			<p class="text-error">Register early by phone: call Joan at 204.783.7377 at least three days before the concert to purchase tickets and book your spot in the MCO Pizza Club. Pizza is free with your concert tickets; parents, grandparents, teachers and children welcome! <b>Concerts begin at 7:30 pm.</b></p>
-		</div>
-	</div>
-</div>
+  <div class="row-fluid">
+    <div class="span7">
+      <div class="well-white">
+        <p>The <b>Canada Council for the Arts</b>, the <b>Manitoba Arts Council</b>, the <b>Winnipeg Arts Council</b>, <b>The Winnipeg Foundation</b>, <b>Assiniboine Credit Union</b>, the <b>Children&rsquo;s Heritage Fund</b>, <b>Friends of the Library</b>, <b>The Great-West Life Assurance Company</b>, the <b>Lloyd Carr-Harris Foundation</b>, <b>Manitoba Hydro</b>, <b>Manitoba Public Insurance</b>, <b>MB4Youth</b>, <b>MTS</b>, <b>Manitoba Teachers&rsquo; Society</b>, the <b>Musicians&rsquo; Performance Trust Fund</b>, <b>OmniTRAX Canada,</b> <b>Province of Manitoba Culture Branch</b>, the <b>Richardson Foundation</b>, the <b>Rita Menzies Endowment Fund for Arts Education</b>, the <b>Rotary Club of Winnipeg West</b>, <b>Sun Life Financial</b>, <b>TD Canada Trust</b>, the <b>Thomas Sill Foundation</b>, <b>Travel Manitoba</b>, <b>VIA Rail Canada</b>, and the <b>Winnipeg Jets True North Foundation</b>.</p>
+        <p>&nbsp;</p>
+      </div>
+    </div>
+  </div>
 
 <?php get_template_part('templates/content', 'programs'); ?>
 
