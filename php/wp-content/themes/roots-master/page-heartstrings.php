53a4de01d290d0fffcc47f63f59b91829498e58e
Jon Snidal <jsnidal@Jons-iMac.local>
2015-04-16 13:09:56 -0500

    HS page sponsors, etc

--- php/wp-content/themes/roots-master/page-heartstrings.php
+++ php/wp-content/themes/roots-master/page-heartstrings.php
@@ -21,13 +21,16 @@ Template Name: Page Heartstrings Template
     <div class="row">
 
     <div class="span6">
-      <h4>The MCO's gala evening<br>&mdash; fabulous 2015 edition!</h4>
-      <p>The main event, held in <a href="http://www.fortgarryhotel.com/" >The Fort Garry</a> on Broadway &mdash; a heavenly evening of music &amp; food in Winnipeg&rsquo;s most elegant setting.</p>
-      <p><strong>Friday, 8 May 2015 at The Fort Garry<br />Event details to follow!</strong></p>
+      <h4>The MCO&rsquo;s gala evening<br>&mdash; fabulous 2015 edition!</h4>
+      <p>Winnipeg&rsquos heavenly fundraiser is on Friday May 8th this year. As usual, it takes place in the opulent 7th-floor ballrooms at The Fort Garry Hotel, and this year, we go Argentine! Special guests include <b>Jaime Vargas</b> and <b>Vanessa Lawson</b>.</p>
+      <p>Friday, 8 May 2015<br>The Fort Garry Hotel, Seventh Floor</p>
+      <p>6:30 pm, Cava, Tapas<br>7:30 pm, Dinner and M&uacute;sica latina<br>9:30 pm, Desserts, Liqueurs and Tango<br>Live &amp; Silent Auctions<br>Tickets $175, table of ten $1,750, patron tickets $200</p>
       <p><strong>&bull; <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=43afb0af27a54ae18e70bf8ee56a737b">buy tickets @ $175</a><br>&bull; <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=24c6b0d77db847acbd1bcd875ee1bcec">buy a table of ten @ $1750</a><br>&bull; <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=4bf23be4b03a4b29a0f306853ce8581b">buy Patron tickets @ $200*</a></strong></p>
-      <p class="fineprint">Patrons are acknowledged in the program<br /> and receive a larger tax receipt.</p>
-      <!-- <p class="regtext"><a href="http://www.mannfinancialassurance.com/" ><img src="http://www.themco.ca/wp-content/uploads/2012/11/mannfinanciallogo.jpg" width="140" height="52" border="0" alt="Mann Financial Assurance"></a></p>
-      <p class="fineprint">Event sponsor / <strong><a href="http://www.mannfinancialassurance.com/" >Mann Financial Assurance</a></strong></p> -->
+                <p align="left" style="font-family: Helvetica, Arial, sans-serif; font-size:16px; line-height:10px; color: #333333;"><a href="http://advisors.nbfwm.ca/en/teams/christianson-wealth-advisors"><img src="http://www.themco.ca/wp-content/uploads/2015/04/CWANBF500.png" width="600" height="111" border=0 alt="MCO Heartstrings"></a></p>
+      <p>Heartstrings Dinner Co-Sponsor: <b>Angela and Robert Ross</b><br>Heartstrings Wine Sponsor: <a href="http://www.mbll.ca/"><b>Manitoba Liquor and Lotteries</b></a><br>Heartstrings Music Sponsor: <a href="http://www.payworks.ca/"><b>Payworks</b></a><br>Heartstrings Reception Sponsor: <a href="http://profile.rbcwealthmanagement.com/Jon.Holeman"><b>Jon Holeman and Associates, Private Wealth Management of RBC Dominion Securities</b></a><br>Heartstrings Dessert Sponsor: <b>Bill Pope and Elizabeth Tippett-Pope</b></p>
+
+      <p class="small">Patrons are acknowledged in the program<br /> and receive a larger tax receipt.</p>
+      
     </div><!-- /span6 -->
 
     <div class="span2"> 
