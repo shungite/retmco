32e1a7b60cf88d9771504a3f57b1890cea3afc42
Jon Snidal <jsnidal@Jons-iMac.local>
2015-04-17 08:24:03 -0500

    HS additions

--- php/wp-content/themes/roots-master/page-heartstrings.php
+++ php/wp-content/themes/roots-master/page-heartstrings.php
@@ -22,15 +22,15 @@ Template Name: Page Heartstrings Template
 
     <div class="span6">
       <h4>The MCO&rsquo;s gala evening<br>&mdash; fabulous 2015 edition!</h4>
-      <p>Winnipeg&rsquos heavenly fundraiser is on Friday May 8th this year. As usual, it takes place in the opulent 7th-floor ballrooms at The Fort Garry Hotel, and this year, we go Argentine! Special guests include <b>Jaime Vargas</b> and <b>Vanessa Lawson</b>.</p>
+      <p>Winnipeg&rsquo;s heavenly fundraiser is on Friday May 8th this year. As usual, it takes place in the opulent 7th-floor ballrooms at The Fort Garry Hotel, and this year, we go Argentine! Special guests include <b>Jaime Vargas</b> and <b>Vanessa Lawson</b>.</p>
       <p>Friday, 8 May 2015<br>The Fort Garry Hotel, Seventh Floor</p>
       <p>6:30 pm, Cava, Tapas<br>7:30 pm, Dinner and M&uacute;sica latina<br>9:30 pm, Desserts, Liqueurs and Tango<br>Live &amp; Silent Auctions<br>Tickets $175, table of ten $1,750, patron tickets $200</p>
       <p><strong>&bull; <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=43afb0af27a54ae18e70bf8ee56a737b">buy tickets @ $175</a><br>&bull; <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=24c6b0d77db847acbd1bcd875ee1bcec">buy a table of ten @ $1750</a><br>&bull; <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=4bf23be4b03a4b29a0f306853ce8581b">buy Patron tickets @ $200*</a></strong></p>
 <small><b>Heartrings is sponsored by</b></small>
 <p><a href="http://advisors.nbfwm.ca/en/teams/christianson-wealth-advisors"><img src="http://www.themco.ca/wp-content/uploads/2015/04/CWANBF476.png" width="476" height="92" border=0 alt="MCO Heartstrings"></a></p>
-      <p><small>Heartstrings Dinner Co-Sponsor: <b>Angela and Robert Ross</b><br>Heartstrings Wine Sponsor: <a href="http://www.mbll.ca/"><b>Manitoba Liquor and Lotteries</b></a><br>Heartstrings Music Sponsor: <a href="http://www.payworks.ca/"><b>Payworks</b></a><br>Heartstrings Reception Sponsor: <a href="http://profile.rbcwealthmanagement.com/Jon.Holeman"><b>Jon Holeman and Associates,<br>Private Wealth Management of RBC Dominion Securities</b></a><br>Heartstrings Dessert Sponsor: <b>Bill Pope and Elizabeth Tippett-Pope</b></small></p>
+      <p><small>Heartstrings Decor Co-Sponsors: <b>Angela &amp; Robert Ross</b>and <a href="http://www.fortgarryhotel.com/"><b>The Fort Garry Hotel</b></a><br>Heartstrings Wine Sponsor: <a href="http://www.mbll.ca/"><b>Manitoba Liquor and Lotteries</b></a><br>Heartstrings Music Sponsor: <a href="http://www.payworks.ca/"><b>Payworks</b></a><br>Heartstrings Reception Sponsor: <a href="http://profile.rbcwealthmanagement.com/Jon.Holeman"><b>Jon Holeman and Associates,<br>Private Wealth Management of RBC Dominion Securities</b></a><br>Heartstrings Dessert Sponsor: <b>Bill Pope and Elizabeth Tippett-Pope</b></small></p>
 
-      <p><small>Patrons are acknowledged in the program<br> and receive a larger tax receipt.</small></p>
+      <p><small>Tickets must be purchased by 3 May 2015, and will not be available at the door. Patrons are acknowledged in the program<br> and receive a larger tax receipt.</small></p>
 
     </div><!-- /span6 -->
 
