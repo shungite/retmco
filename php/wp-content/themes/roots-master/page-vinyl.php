dad4c47977b3863d745f036f9fd4b6501c9decb7
Jon Snidal <jsnidal@Jons-iMac.local>
2015-05-06 11:18:52 -0500

    Update VV dates

--- php/wp-content/themes/roots-master/page-vinyl.php
+++ php/wp-content/themes/roots-master/page-vinyl.php
@@ -19,7 +19,7 @@ Template Name: Page Vinyl Template
     <div class="span8">
       <h4>Check here for<br>Vinyl Vault sales dates!</h4>
       <p>The Manitoba Chamber Orchestra&rsquo;s almost-monthly Vinyl Vault sales are your chance to rummage around in the city&rsquo;s largest used record collection! Find rare titles in jazz, pop, classical, opera, rock, country, folk, easy listening and much more &mdash; <b>collectibles a speciality!</b></p>
-      <p><b>10:00 am to 3:00 pm, Saturdays<br>Power Building basement<br>428 Portage Avenue<br>Dates for 14/15:</b></p><p><strike>27 September 2014</strike><br><strike>25 October 2014</strike><br><strike>29 November 2014</strike><br><strike>31 January 2015</strike><br>28 February 2015<br>28 March 2015<br>25 April 2015<br>30 May 2015<br>27 June 2015</p>
+      <p><b>10:00 am to 3:00 pm, Saturdays<br>Power Building basement<br>428 Portage Avenue<br>Dates for 14/15:</b></p><p><strike>27 September 2014</strike><br><strike>25 October 2014</strike><br><strike>29 November 2014</strike><br><strike>31 January 2015</strike><br><strike>28 February 2015</strike><br><strike>28 March 2015</strike><br><strike>25 April 2015</strike><br>30 May 2015<br>27 June 2015</p>
       <p>&nbsp;</p>
     </div>
 
