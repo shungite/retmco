70b52a424ce4e4708da01e757e9e53a6fefdf2a7
Jon Snidal <jsnidal@Jons-iMac.local>
2015-06-04 10:22:45 -0500

    Update

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -8,9 +8,9 @@ Template Name: Page MCOhome Template
 
   <div class="row-fluid">
     <div class="span8">
-      <a href="http://www.themco.ca/concert/manson-lisiecki/"><img src="http://www.themco.ca/wp-content/uploads/2014/06/head03jun15.jpg" alt="MCO concert / 3 June 2015"></a>
-      <h4>Jan Lisiecki: &ldquo;Pristine, lyrical<br>and intelligent&rdquo;</h4>
-      <p>So says the <i>New York Times</i> abdout Canadian piano superstar <b>Jan Lisiecki</b>. Two previous sold-out performances with the MCO simply blew us all away. After his last 4-star concert here, the <i>Winnipeg Free Press</i> review praised his &ldquo;confidence, attack, technical fluidity and enchanting sensitivity&hellip;&rdquo; Lisiecki will perform Mozart&rsquo;s Piano Concerto No. 26. See notes, bios and two-minute talks by Haley Rempel <a href="http://www.themco.ca/concert/manson-lisiecki/"><b>here</b></a> or, if you&rsquo;re just after a quick ticket or two, click the links below.<br><img src="http://www.themco.ca/wp-content/uploads/2013/01/QuikTicket.png" alt="QuikTicket"><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=fbae07696ae04cbfaa773f9cfd7faed2"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/AdultC.png" alt="" width="62" height="40"></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=24f08d8b6a0c4f8fb3be8c9b80451539"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/SeniorC.png" alt="" width="62" height="40" /></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=393d2f396c5c495eae8bac3dbb5340bd"><img rel="tooltip" title="Click to add student ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/StudentC.png" alt="" width="62" height="40" /></a></p>
+      <a href="http://www.themco.ca/quick-links-1516-subscriptions/"><img src="http://www.themco.ca/wp-content/uploads/2015/06/SubscribeNow.jpg" alt="MCO / Subscribe now!"></a>
+      <h4>Subscribe now to MCO&rsquo;s 2015/16 season<br>&mdash; enjoy six or nine concerts in Westminster Church!</h4>
+      <p>Only subscribers enjoy every benefit the MCO has to offer, like a free CD, Musicnet Subscriber Reward Card, ticket exchange privileges, discounts on extra tickets and more. Make this the season you subscribe to Canada&rsquo;s &ldquo;tiny, perfect, orchestra!&rdquo;</p>
     </div>
   </div><!-- /row -->
 
@@ -18,7 +18,7 @@ Template Name: Page MCOhome Template
     <small>&nbsp;</small>
   </div> -->
 
-  <div class="row-fluid">
+  <!-- <div class="row-fluid">
     <div class="span8">
       <div class="well-white">
         <div class="media"><a class="pull-left" href="#extras" data-toggle="tab"><img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/07/DonorCello.jpg" width="96px" height="96px"></a>
@@ -28,7 +28,7 @@ Template Name: Page MCOhome Template
         </div>
       </div>
     </div>
-  </div><!-- /row -->
+  </div><!-- /row --> -->
 
 <div class="row-fluid">
     <div class="span8">
