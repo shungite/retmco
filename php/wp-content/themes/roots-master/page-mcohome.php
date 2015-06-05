75f41ab2cc46a98f746059172c9c5df91c3eecd3
Jon Snidal <jsnidal@Jons-iMac.local>
2015-06-05 12:21:20 -0500

    Typo

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -10,7 +10,7 @@ Template Name: Page MCOhome Template
     <div class="span8">
       <a href="http://www.themco.ca/quick-links-1516-subscriptions/"><img src="http://www.themco.ca/wp-content/uploads/2015/06/SubscribeNow2.jpg" alt="MCO / Subscribe now!"></a>
       <h4><b>Subscribe now to MCO&rsquo;s 2015/16 season<br>&mdash; enjoy six or nine concerts in Westminster Church!</h4>
-      <p></b>Only subscribers enjoy every benefit the MCO has to offer, like a free premium compact disc, a Musicnet Subscriber Reward Card, ticket exchange privileges, discounts on extra tickets and <b>savings of up to 25%!</b> Plus, design your own subscription: a 9-concert package can be three tickets to each of three concderts; a 6-concert package can be two to each of three &hellip; you do the math! Make this the season you subscribe to Canada&rsquo;s &ldquo;tiny, perfect, orchestra!&rdquo;</p>
+      <p></b>Only subscribers enjoy every benefit the MCO has to offer, like a free premium compact disc, a Musicnet Subscriber Reward Card, ticket exchange privileges, discounts on extra tickets and <b>savings of up to 25%!</b> Plus, design your own subscription: a 9-concert package can be three tickets to each of three concerts; a 6-concert package can be two to each of three &hellip; you do the math! Make this the season you subscribe to Canada&rsquo;s &ldquo;tiny, perfect, orchestra!&rdquo;</p>
         <p><a href="http://www.themco.ca/wp-content/uploads/2015/03/1516Renewal.pdf"><button class="btn btn-small btn-inverse" type="button">Renewal form</button> <a href="http://www.themco.ca/quick-links-1516-subscriptions/"><button class="btn btn-small btn-success" type="button">Quick links to 15/16 subscription tickets</button></a><br>&nbsp;</p>
     </div>
   </div><!-- /row -->
