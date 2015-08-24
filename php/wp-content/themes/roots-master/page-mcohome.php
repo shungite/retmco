dfc0371e09a4a122f06a287c34a49adf51995332
Jon Snidal <jsnidal@gmail.com>
2015-08-24 14:02:28 -0500

    Update VV sale

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -10,8 +10,8 @@ Template Name: Page MCOhome Template
     <div class="span8">
       <a href="http://www.themco.ca/quick-links-1516-subscriptions/"><img src="http://www.themco.ca/wp-content/uploads/2015/07/SubscribeNow.png" alt="MCO / Subscribe now!"></a>
       <h4><b>Subscribe now to MCO&rsquo;s 2015/16 season<br>&mdash; enjoy six or nine concerts in Westminster Church!</h4>
-      <p></b>Only subscribers enjoy every benefit the MCO has to offer, like a free premium compact disc, a Musicnet Subscriber Reward Card, ticket exchange privileges, discounts on extra tickets and <b>savings of up to 25%!</b> Plus, design your own subscription: a 9-concert package can be three tickets to each of three concerts; a 6-concert package can be two to each of three &hellip; you do the math! Make this the season you subscribe to Canada&rsquo;s &ldquo;tiny, perfect, orchestra!&rdquo;</p>
-        <p><a href="http://www.themco.ca/wp-content/uploads/2015/03/1516Renewal.pdf"><button class="btn btn-small btn-inverse" type="button">Renewal form</button> <a href="http://www.themco.ca/quick-links-1516-subscriptions/"><button class="btn btn-small btn-success" type="button">Quick links to 15/16 subscription tickets</button></a><br>&nbsp;</p>
+        <p></b>Only subscribers enjoy every benefit the MCO has to offer, like a free premium compact disc, a Musicnet Subscriber Reward Card, ticket exchange privileges, discounts on extra tickets and <b>savings of up to 25%!</b> Plus, design your own subscription: a 9-concert package can be three tickets to each of three concerts; a 6-concert package can be two to each of three &hellip; you do the math! Make this the season you subscribe to Canada&rsquo;s &ldquo;tiny, perfect, orchestra!&rdquo;</p>
+          <p><a href="http://www.themco.ca/wp-content/uploads/2015/03/1516Renewal.pdf"><button class="btn btn-small btn-inverse" type="button">Renewal form</button> <a href="http://www.themco.ca/quick-links-1516-subscriptions/"><button class="btn btn-small btn-success" type="button">Quick links to 15/16 subscription tickets</button></a><br>&nbsp;</p>
     </div>
   </div><!-- /row -->
 
@@ -36,7 +36,7 @@ Template Name: Page MCOhome Template
       <div class="well">
         <div class="media"><a class="pull-left" href="#extras" data-toggle="tab"><img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/01/Parlo1.png" width="96px" height="96px"></a>
           <div class="media-body">
-            <p><strong><a href="#">4-DAY SUMMER SIZZLE SALE! Tuesday, August 18th thru Friday, August 21st &hellip;</strong></a><br />Get your hot vinyl from 10:00 am to 2:00 pm for 4 glorious days in August! Browse to your heart's content through Winnipeg&rsquo;s finest used LP/CD collection. <b>THOUSANDS of LPs have been donated since our last sale!</b> MCO&rsquo;s Vinyl Vault is located in the basement of the Power Building at 428 Portage Avenue (near The Bay).</p>
+            <p><strong><a href="#">Next MCO Vinyl Vault &amp; CD Cellar sale:<br />Saturday, 26 September 2015</strong></a><br />Regular Vinyl Vault sales resume in September! Browse Winnipeg&rsquo;s finest used LP/CD collection, and check out our many recent donations, with thousands of titles priced to spin out the door! MCO&rsquo;s Vinyl Vault is located in the basement of the Power Building at 428 Portage Avenue (near The Bay). Sales run from 10:00 am until 3:00 pm.</p>
           </div>
         </div>
       </div>
