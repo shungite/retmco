aacc6e0be48a1df17b2d7f105a042d1276e58670
Jon Snidal <jsnidal@gmail.com>
2015-10-01 07:55:53 -0500

    Corrections

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -10,17 +10,17 @@ Template Name: Page MCOhome Template
     <div class="span8">
       <a href="http://www.themco.ca/concert/manson-hamelin/"><img src="http://www.themco.ca/wp-content/uploads/2015/07/head14oct15.png" alt="MCO / Manson, Hamelin"></a>
       <h4>Hamelin &ldquo;Urged to return<br>by a riotous ovation&rdquo;</h4>
-      <p>Ring any bells? The words from <i>The Ore­go­nian</i> sound won­der­fully famil­iar to any­one who has been at an MCO con­cert with Marc-Andr&rsquo; Hamelin. The piano super­star first appeared with us in 1991, and he has been one of the high points of every sea­son in which he has appeared, and now he opens our fabulous 15.16 season! See notes, bios and more at <a href="http://www.themco.ca/concert/manson-hamelin/"><b>here</b></a> or, if you&rsquo;re just after a quick ticket or two, click the links below.<br><img src="http://www.themco.ca/wp-content/uploads/2013/01/QuikTicket.png" alt="QuikTicket"><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=a9c9eebccf9b4b109f78cfd06bf79b57"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/AdultC.png" alt="" width="62" height="40"></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=00cc31aa243843ce93876523e2a42760"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/SeniorC.png" alt="" width="62" height="40" /></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=89845e4002f14202acef13b9f3a8b0d9"><img rel="tooltip" title="Click to add student ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/StudentC.png" alt="" width="62" height="40" /></a></p>
+      <p>Ring any bells? The words from <i>The Ore­go­nian</i> sound won­der­fully famil­iar to any­one who has been at an MCO con­cert with Marc-Andr&eacute; Hamelin. The piano super­star first appeared with us in 1991, and he has been one of the high points of every sea­son in which he has appeared, and now he opens our fabulous 2015/16 season! See program notes, artists&rsquo; bios and more at <a href="http://www.themco.ca/concert/manson-hamelin/"><b>here</b></a> or, if you&rsquo;re just after a quick ticket or two, click the links below.<br><img src="http://www.themco.ca/wp-content/uploads/2013/01/QuikTicket.png" alt="QuikTicket"><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=a9c9eebccf9b4b109f78cfd06bf79b57"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/AdultC.png" alt="" width="62" height="40"></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=00cc31aa243843ce93876523e2a42760"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/SeniorC.png" alt="" width="62" height="40" /></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=89845e4002f14202acef13b9f3a8b0d9"><img rel="tooltip" title="Click to add student ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/StudentC.png" alt="" width="62" height="40" /></a></p>
     </div>
   </div><!-- /row -->
 
   <div class="row-fluid">
     <small>&nbsp;</small>
-  </div> -->
+  </div>
 
   <div class="row-fluid">
     <div class="span8">
-      <div class="well-white">
+      <div class="well">
         <div class="media"><a class="pull-left" href="#extras" data-toggle="tab"><img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/07/DonorCello.jpg" width="96px" height="96px"></a>
           <div class="media-body">
             <p><strong><a href="http://www.themco.ca/quick-links-1516-subscriptions/">Subscribe now to MCO&rsquo;s 2015/16 season<br>&mdash; enjoy six or nine concerts in Westminster Church!</strong></a><br />Only subscribers enjoy every benefit the MCO has to offer, like a free CD, Musicnet Subscriber Reward Card, ticket exchange privileges, discounts on extra tickets and more. Make this the season you subscribe to Canada&rsquo;s &ldquo;tiny, perfect, orchestra!&rdquo;</p>
@@ -32,7 +32,7 @@ Template Name: Page MCOhome Template
 
 <div class="row-fluid">
     <div class="span8">
-      <div class="well">
+      <div class="well-white">
         <div class="media"><a class="pull-left" href="#extras" data-toggle="tab"><img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/01/Parlo1.png" width="96px" height="96px"></a>
           <div class="media-body">
             <p><strong><a href="#">Next MCO Vinyl Vault &amp; CD Cellar sale:<br />10 am to 3 pm, Saturday, 31 October 2015</strong></a><br />It&rsquo;s chilling, but true: the next Vinyl Vault sale is on Hallowe&rsquo;en! Browse Winnipeg&rsquo;s big used LP/CD collection, and check out our many recent donations, with 1000s of titles priced to spin out the door! MCO&rsquo;s Vinyl Vault is located in the basement of the Power Building at 428 Portage Avenue (near The Bay).</p>
