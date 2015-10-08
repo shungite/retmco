6c4c75a1475bfd70ad9ea9e1019b197e5189a2f5
Jon Snidal <jsnidal@gmail.com>
2015-10-08 07:54:37 -0500

    Carr well

--- php/wp-content/themes/roots-master/page-carr.php
+++ php/wp-content/themes/roots-master/page-carr.php
@@ -23,15 +23,21 @@ Template Name: Page Carr Template
 
 		<p>The Manitoba Chamber Orchestra invites you to a Candlelight Evening of music and conversation with cellist <b>Colin Carr</b>, 7:30 pm, Monday, 2 November 2015, to be held at historic <b>Moss House</b>, 218 Roslyn Road, a turn-of-the-century heritage home, once again generously offered by John and Nancy Embry. Tickets $75 per person (partial tax receipt). Musical performance will commence at 7:45. RSVP 204.783.7377 by Thursday, October 29th. Tickets also available <strong><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=8997a184a24848c6a74fe697db29bc81">here</a></strong>.</p>
 		<p>This unique salon evening includes <b>wine with sweets and savouries</b>.</p>
-		<p><strong>Colin Carr</strong> Colin Carr appears throughout the world as a soloist, chamber musician, recording artist, and teacher. He has played with major orchestras worldwide, including the Royal Concertgebouw Orchestra, The Philharmonia, Royal Philharmonic, BBC Symphony, the orchestras of Chicago, Los Angeles, Washington, Philadelphia, Montréal and all the major orchestras of Australia and New Zealand. Conductors with whom he has worked include Rattle, Gergiev, Dutoit, Elder, Skrowasczewski and Marriner. He has been a regular guest at the BBC Proms and has twice toured Australia. With his duo partner Thomas Sauer he has played recitals throughout the United States and Europe. They have recently played recitals together at the Concertgebouw in Amsterdam, Philadelphia&rsquo;s Chamber Music Society and at Wigmore Hall in London. Colin has played complete cycles of the Bach Solo Suites at Wigmore Hall, at the Chamber Music Society of Lincoln Center, the Gardner Museum in Boston and recently in Montr&eacute;al, Toronto, Ottawa and Vancouver.
-</p>
 
-	</div><!-- /span6 -->
+<div class="row-fluid">
+		<div class="span4">
+      <div class="well">
+				<p><strong>Colin Carr</strong> Colin Carr appears throughout the world as a soloist, chamber musician, recording artist, and teacher. He has played with major orchestras worldwide, including the Royal Concertgebouw Orchestra, The Philharmonia, Royal Philharmonic, BBC Symphony, the orchestras of Chicago, Los Angeles, Washington, Philadelphia, Montréal and all the major orchestras of Australia and New Zealand. Conductors with whom he has worked include Rattle, Gergiev, Dutoit, Elder, Skrowasczewski and Marriner. He has been a regular guest at the BBC Proms and has twice toured Australia. With his duo partner Thomas Sauer he has played recitals throughout the United States and Europe. They have recently played recitals together at the Concertgebouw in Amsterdam, Philadelphia&rsquo;s Chamber Music Society and at Wigmore Hall in London. Colin has played complete cycles of the Bach Solo Suites at Wigmore Hall, at the Chamber Music Society of Lincoln Center, the Gardner Museum in Boston and recently in Montr&eacute;al, Toronto, Ottawa and Vancouver.</p>
+			</div>
+	</div>
+</div>
 
-	<div class="span2">
-		<a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=8997a184a24848c6a74fe697db29bc81"><img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /></a><br />
-		<p class="fineprint">Click above to add a ticket to your shopping cart; adjust quantity in cart and return to purchase other tickets.</p>
-	</div><!-- /span2 -->
+</div><!-- /span6 -->
+
+<div class="span2">
+	<a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=8997a184a24848c6a74fe697db29bc81"><img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /></a><br />
+	<p class="fineprint">Click above to add a ticket to your shopping cart; adjust quantity in cart and return to purchase other tickets.</p>
+</div><!-- /span2 -->
 
 </div><!-- /row -->
 
