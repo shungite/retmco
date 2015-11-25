378e647aeb8f110972bb4aaf2d12425794cff06c
Jon Snidal <jsnidal@gmail.com>
2015-11-25 09:56:03 -0600

    HS updates again

--- php/wp-content/themes/roots-master/page-heartstrings.php
+++ php/wp-content/themes/roots-master/page-heartstrings.php
@@ -28,7 +28,7 @@ Template Name: Page Heartstrings Template
       <p><strong>&bull; <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=5e8d5ea5e2174f6d8876407b6b7c0efa">buy tickets @ $175</a><br>&bull; <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=ae1f89e45f4b46b18661d7397fd43bc2">buy a table of ten @ $1750</a><br>&bull; <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=8b1b400a4cd8448b91b4692661dc4299">buy Patron tickets @ $200*</a></strong></p>
 <small><b>Heartrings is sponsored by</b></small>
 <p><a href="http://advisors.nbfwm.ca/en/teams/christianson-wealth-advisors"><img src="http://www.themco.ca/wp-content/uploads/2015/04/CWANBF476.png" width="476" height="92" border=0 alt="MCO Heartstrings"></a></p>
-      <p><small>Heartstrings Wine Sponsor: <a href="http://www.mbll.ca/"><b>Manitoba Liquor and Lotteries</b></a><br>Heartstrings Music Sponsor: <a href="http://www.payworks.ca/"><b>Payworks</b></a><br>Heartstrings Reception Sponsor: <a href="http://profile.rbcwealthmanagement.com/Jon.Holeman"><b>Jon Holeman and Associates,<br>Private Wealth Management of RBC Dominion Securities</b></a></small></p>
+      <p><small>Heartstrings Wine Sponsor: <a href="http://www.mbll.ca/"><b>Manitoba Liquor and Lotteries</b></a><br>Heartstrings Music Sponsor: <a href="http://www.payworks.ca/"><b>Payworks</b></a></p>
 
       <p><small>Tickets must be purchased by 3 May 2016, and will not be available at the door. Patrons are acknowledged in the program and receive a larger tax receipt.</small></p>
 
