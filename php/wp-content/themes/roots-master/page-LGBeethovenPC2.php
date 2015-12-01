1816d0a46cab6f4abe75745ae51a17753cd57f81
Jon Snidal <jsnidal@gmail.com>
2015-12-01 10:54:18 -0600

    Two Listening Guides reworked

---
+++ php/wp-content/themes/roots-master/page-LGBeethovenPC2.php
@@ -0,0 +1,136 @@
+<?php
+/*
+Template Name: Page LGBeethovenPC2 Template
+*/
+?>
+
+<div class="container">
+
+  <div class="row">
+
+    <div class="span8">
+      <img src="http://www.themco.ca/wp-content/uploads/2015/12/ListeningGuides.jpg" alt="" width="770" height="256">
+    </div>
+    <div class="span2">
+      <p>.</p>
+    </div>
+
+  </div><!-- /row -->
+
+  <div class="row">
+
+    <div class="span7">
+
+      <p>&nbsp;<br>MCO&rsquo;s MTS Future First Listening Guides are prepared by Beryl Peters, Ph.D.</p>
+
+      <p><b>LUDWIG VAN BEETHOVEN: PIANO CONCERTO NO. 2</b><p>
+
+      <p>Ludwig van Beethoven composed the Piano Concerto No. 2 over a number of years and it was finally published in 1801. Beethoven enjoyed performing this concerto as it showed off his talents both as a piano soloist and as a composer. You can read Haley Rempel&rsquo;s comments about this work on the MCO website to find out more about Beethoven and this work <a href="http://www.themco.ca/concert/manson-barnatan/"><b>here</b></a>.</p>
+
+      <p><b>When you listen to this Piano Concerto by Beethoven, you will hear musical elements that give this work a sense of drama and contrast.</b></p>
+
+      <div class="well">
+        <p><b>FOR EDUCATORS</b></p>
+        <p><b>Manitoba Music Curricular Connections</b></p>
+        <p><b>9-12 Making:</b> The learner develops competencies for listening by listening critically with discrimination and purpose to:</p>
+        <ul>
+          <li>situate and contextualize music (e.g., cultural/ ideological/historical/social contexts, music style, genre, tradition, or praxis, etc.)</li>
+          <li>support enjoyment and understanding of music</li>
+          <li>make and interpret music expressively and creatively</li>
+          <li>inform analysis, interpretation, judgement, appreciation, and evaluation</li>
+        </ul>
+        <p><b>K-8 Understanding Music in Context:</b> Demonstrate awareness of the intended meanings and/or purposes of music encountered in own performance and listening experiences</p>
+        <p><b>9-12 Connecting:</b> The learner develops understandings about the significance of music by connecting music to diverse contexts.</p>
+      </div>
+
+      <p><b>The following questions can help you listen to this work:</b></p>
+
+      <p><span class="badge">1</span>&nbsp;Can you identify the form (organizational structure) of this piano concerto?</p>
+
+      <p>Can you hear that it is written in three different sections, or movements? They are called:</p>
+
+      <ol>
+        <li>Allegro con brio (fast, lively)</li>
+        <li>Adagio (slowly)</li>
+        <li>Rondo Molto Allegro (a music form called a rondo played very fast)</li>
+      </ol>
+
+      <p>Each of the three sections or movements also has a particular form. The first movement Allegro con brio is written in sonata form. Can you hear a main theme introduced by the orchestra at the beginning of the work that Beethoven develops in different ways throughout this movement, and then returns to at the end of the movement? This kind of compositional development is characteristic of sonata form.</p>
+
+      <p>The second Adagio slow movement is a three part form called ABA or ternary form. Can you hear themes in the first part of this movement, then a contrasting section, and then the sounds repeated from the first section again?</p>
+
+      <p>The third movement is called a rondo. In rondo form, a main theme is repeated before and after contrasting sections. The repeated theme is called the A section. The contrasting sections are called B, C, D etc., depending on how many different sections the composer decides to compose. In this rondo the repeated theme and contrasting sections create an ABACABA form. This means that the main theme (A) is heard 4 times with two different sections (B and C) heard in between the A sections.</p>
+
+      <p>Beethoven also includes a musical joke in the rondo for his audience and musicians of the time. Just before the last A theme is heard, Beethoven writes the music in a different key to what the musicians and audience would have expected at that time in Europe. Can you hear where the sounds of the rondo in the final A section sound like they change as Beethoven &lsquo;corrects&rsquo; the final key of the rondo for his audience and orchestra?</p>
+
+      <p><span class="badge">2</span>&nbsp;What <b>Instruments</b> can you hear in this concerto? How many performers are playing onstage? What string instruments do you hear (violin, viola, cello, double bass)? Can you pick out when you hear the flute, oboes, bassoons, and horns?</p>
+
+      <p>When do you first hear the piano? Do you hear the fast notes that sound very close together when the piano is first heard?</p>
+
+      <p>Do you hear the way that Beethoven treats the piano as a special, solo instrument? Can you hear when the piano is given an important part to play? Can you hear when the piano gets to play a very difficult section all by itself towards the end of the first movement? This showy section is called a cadenza. Can you hear when the orchestra plays by itself or when different instruments can be heard above the rest? Which instruments are given the predominant melodies or themes in this work?</p>
+
+      <p><span class="badge">3</span>&nbsp;Can you identify the mood Beethoven is trying to create in each movement? How does Beethoven use tempo (the speed of the music), dynamics (the volume of the music), and expression (musical elements that express certain feelings or dispositions) to create different moods?</p>
+
+      <p>Which movements use mostly fast music? Which movement uses mostly slow music? Can you hear when the tempo of the music changes and gets faster or slower? How does that affect the mood of the work?</p>
+
+      <p>Was the music played at all the same volume (dynamics)? When do you hear music played loudly? Quietly? When do the dynamics change and what effect does that create? When does the music get louder (crescendo) or get quieter (decrescendo)?</p>
+
+      <p>Do you hear musical sounds that are short sounding (staccato) or very smooth sounding (legato)? Do you hear suddenly loud sounds (accents)?</p>
+
+      <p><span class="badge">4</span>&nbsp;How is <b>rhythm</b> used in this piano concerto? Do you hear sections that have a steady rhythm or beat that you could tap to? Did any particular rhythmic pattern stand out for you that you could listen for in the work? Was there a particular pattern that you could hear repeated anywhere in the work?</p>
+
+      <p><span class="badge">5</span>&nbsp;How is <b>melody or pitch</b> used in this piano concerto? Do you hear high sounds or pitches? Do you hear low sounds or pitches? Can you pick out a particular melody played by the orchestra instruments or piano in each of the three movements? Can you hum or sing that melody? Could you draw the shape of the melody in the air? When does it move up higher and when does it move lower?</p>
+
+      <p>Do you hear a melody in a certain instrument that is then played by a different instrument?</p>
+
+      <p>Can you hear a melody that is repeated over and over again in the third section or movement of the piano concerto? A piece where a melody keeps coming back after different or contrasting sections is called a rondo.</p>
+
+      <p><span class="badge">6</span>&nbsp;Can you hear different kinds of texture in this work? Where do you hear lots of instruments playing together so that the sound is thick? Where do you hear just a few instruments playing? Can you tell which instruments are playing?</p>
+
+      <p><span class="badge">7</span>&nbsp;What kinds of instrumental timbre do you hear? Timbre is the different qualities of sound that can be heard, for example the kinds of sounds that the string instruments make when they play together. When does the timbre of the music change because Beethoven adds different instruments to the strings? Can you hear when the strings are playing alone and when they are playing together with the piano and other instruments?</p>
+
+      <div class="well">
+        <p><b>FOR EDUCATORS</b></p>
+        <p><b>Reflections and Responses</b> (K-8 Valuing; 9-12 Responding)</p>
+        <p><b>Grades 9-12 Responding</b></p>
+        <p>The learner develops and uses critical reflection and thinking for music learning:</p>
+        <ul>
+          <li>the learner generates initial reactions to music experiences</li>
+          <li>the learner critically listens to, observes, and describes music experiences</li>
+          <li>the learner analyzes and interprets music experiences</li>
+          <li>the learner constructs meanings about music experiences</li>
+        </ul>
+        <p><b>Grades K-8 Valuing</b></p>
+        <p>Students analyze, reflect on, and construct meaning in response to their own and others&rsquo; music:</p>
+        <ul>
+          <li>students analyze their own and others</li>
+          <li>musical excerpts, works, and performances</li>
+          <li>students form personal responses to and construct meaning from their own and others&rsquo; music</li>
+        </ul>
+      </div>
+
+      <p><span class="badge">1</span>&nbsp;What is your immediate response to this music? Does this music sound like any other music you have heard before? What does this music make you think of? Have you ever heard the opening of Beethoven&rsquo;s fifth symphony? It has a famous four note opening that is often used in films and on TV. If you have ever heard Beethoven&rsquo;s fifth, does it sound anything like his second piano concerto? What music elements might be similar?</p>
+
+      <p><span class="badge">2</span>&nbsp;What adjectives might describe the mood that you felt when listening to this piano concerto? Can you identify what musical elements may have created that mood for you? When did the mood change and why?</p>
+
+      <p><span class="badge">3</span>&nbsp;What musical elements did you enjoy or find interesting? Did you enjoy the melodies that you heard? Did the rhythms, dynamics, or tempos used by Beethoven help make this work exciting? Did you hear parts of the piano concerto that made you feel peaceful or calm and if so, what part of the piano concerto were they in? Were there any parts that sounded playful to you? If so, which movement did you hear them in?</p>
+
+      <p><span class="badge">4</span>&nbsp;Were there parts of the Piano Concerto that you did not enjoy? Why or why not? Can you identify which music elements made you enjoy or not enjoy the music?</p>
+
+      <p><span class="badge">5</span>&nbsp;Different people often have different responses to the same music. Ask someone else who heard the same music about their response to BeethovenÕs second piano concerto.</p>
+
+      <p><span class="badge">6</span>&nbsp;What feelings did it seem that Beethoven was trying to communicate to his audience about this work? What music elements seemed to be important to him?</p>
+
+      <p><span class="badge">7</span>&nbsp;Is there other music by Beethoven that you could listen to and compare to the sounds and experience of the Piano Concerto No. 2?</p>
+
+      <p>&nbsp;</p>
+
+    </div><!-- /span7 -->
+
+  </div><!-- /row -->
+
+  <?php get_template_part('templates/content', 'listen'); ?>
+
+</div><!-- /container -->
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
---
+++ php/wp-content/themes/roots-master/page-LGMozart40.php
@@ -0,0 +1,117 @@
+<?php
+/*
+Template Name: Page LGMozart40 Template
+*/
+?>
+
+<div class="container">
+
+  <div class="row">
+
+    <div class="span8">
+      <img src="http://www.themco.ca/wp-content/uploads/2015/12/ListeningGuides.jpg" alt="" width="770" height="256">
+    </div>
+    <div class="span2">
+      <p>.</p>
+    </div>
+
+  </div><!-- /row -->
+
+  <div class="row">
+
+    <div class="span7">
+
+      <p>&nbsp;<br>MCO&rsquo;s MTS Future First Listening Guides are prepared by Beryl Peters, Ph.D.</p>
+
+      <p><b>WOLFGANG AMADEUS MOZART: SYMPHONY NO. 40</b></p>
+
+      <p>Mozart wrote at least 41 symphonies and there is evidence that he probably wrote even more. He finished writing his 40th symphony in July 1788. This symphony is one of Mozart&rsquo;s most performed and admired symphonies and it exists in different versions. You can read Haley Rempel&rsquo;s comments about this work on the website to find out more about Mozart and the story of this work <a href="http://www.themco.ca/concert/manson-barnatan/"><b>here</b></a>.</p>
+
+      <p><b>When you listen to this symphony by Mozart, you will hear musical elements that give this work a strong emotional quality. Perhaps you might even feel there is a sense of tragedy, grief, or passionate feelings expressed in this work. The following questions can help you listen to this work.</b></p>
+
+      <p><span class="badge">1</span>&nbsp;Can you identify the form (organizational structure) of this symphony?</p>
+
+      <p>Can you hear that it is written in four different sections, or movements? These movements use forms typical for a symphony written in this classical time period. They are:</p>
+
+      <ol>
+        <li>Molto allegro (Very fast and lively)</li>
+        <li>Andante (a slower movement)</li>
+        <li>Menuetto Allegretto ( A minuet dance form)</li>
+        <li>Allegro Finale (a fast, lively finale)</li>
+      </ol>
+
+      <p>Each of the four sections or movements also has a particular form. The first, second, and fourth movements are written in sonata form. Can you hear a main theme introduced by the orchestra at the beginning of these movements that Mozart develops in different ways throughout the movement, and then returns to at the end of the movement? This kind of compositional development is characteristic of sonata form.</p>
+
+      <p>The third movement is a three part dance form called a minuet and trio. Can you hear themes in the first part of this movement, then a contrasting section, and then the sounds repeated from the first section again? This particular dance minuet was not intended for people to dance to. Could you dance to this minuet? Why or why not?</p>
+
+      <p><span class="badge">2</span>&nbsp;What <b>instruments</b> can you hear in this symphony? How many performers are playing onstage? What string instruments do you hear (violin, viola, cello, double bass)? Can you pick out other instruments? Which ones?</p>
+
+      <p><span class="badge">3</span>&nbsp;Can you identify the mood Mozart is trying to create in each movement? How does Mozart use <b>tempo</b> (the speed of the music), <b>dynamics</b> (the volume of the music), and <b>expression</b> (musical elements that express certain feelings or dispositions) to create different moods?</p>
+
+      <p>Which movements use mostly fast music? Which movement uses mostly slower music? Can you hear when the tempo of the music changes and gets faster or slower? How does that affect the mood of the work?</p>
+
+      <p>Was the music played at all the same volume? When do you hear music played loudly? Quietly? When does the volume of the music change? When does the music get louder (crescendo) or get quieter (decrescendo)?</p>
+
+      <p>Do you hear musical sounds that are short sounding (staccato) or very smooth sounding (legato)? Do you hear suddenly loud sounds (accents)?</p>
+
+      <p><span class="badge">4</span>&nbsp;How is <b>rhythm</b> used in this symphony? Do you hear sections that have a steady rhythm or beat that you could tap to?</p>
+
+      <p>There are many interesting rhythm patterns in this symphony. The opening movement begins with a repeated three note pattern that sounds like Òopen up, open up, open up now!Ó Do you hear that pattern in the first movement?</p>
+
+      <p>Can you hear any repeated rhythmic patterns in other movements? Did any particular rhythmic pattern stand out for you that you could listen for in this symphony?</p>
+
+      <p><span class="badge">5</span>&nbsp;How is <b>melody</b> used in this symphony? Do you hear high sounds or pitches? Do you hear low sounds or pitches? Can you hear when a melody suddenly jumps much higher? The opening of the first movement is sometimes known as the rocket theme because in the opening rhythmic pattern &lsquo;open up open up now,&rsquo; the note on the word &lsquo;now&rsquo; is suddenly higher than the rest.</p>
+
+      <p>There is another rocket sound at the very beginning of the fourth movement that Mozart creates by writing a series of notes that quickly go higher and higher. This was known as a &lsquo;Mannheim rocket.&rsquo; Can you hear the Mannheim rocket in the finale of Mozart&rsquo;s symphony?</p>
+
+      <p>Can you pick out other melodies or themes in each of the four movements? Can you hear melodies or themes that are repeated throughout each movement and sound quite different to each other? Can you hear melodies or themes that are louder and more dramatic than others? Do you hear themes are that calmer and gentler? Do you hear where Mozart has lets different instruments take turns playing the same melody or parts of the melody? The contrast of these different themes helps create a sense of drama in this symphony.</p>
+
+      <p><span class="badge">6</span>&nbsp;Can you hear different kinds of <b>texture</b> in this work? Where do you hear lots of instruments playing together so that the sound is thick? Where do you hear just a few instruments playing? Can you hear which instruments are playing?</p>
+
+      <p><span class="badge">7</span>&nbsp;What kinds of instrumental <b>timbre</b> do you hear? Timbre is the different qualities of sound that can be heard, for example the kinds of sounds that the string instruments make when they play together. When does the timbre of the music change because Mozart adds or takes away different instruments?</p>
+
+      <div class="well">
+        <p><b>FOR EDUCATORS</b></p>
+        <p><b>Reflections and Responses</b><br>(K-8 Valuing; 9-12 Responding)</p>
+
+        <p><b>Grades 9-12 Responding</b></p>
+        <p>The learner develops and uses critical reflection and thinking for music learning:</p>
+        <ul>
+          <li>the learner generates initial reactions to music experiences</li>
+          <li>the learner critically listens to, observes, and describes music experiences</li>
+          <li>the learner analyzes and interprets music experiences</li>
+          <li>the learner constructs meanings about music experiences</li>
+        </ul>
+        <p><b>Grades K-8 Valuing</b></p>
+        <p>Students analyze, reflect on, and construct meaning in response to their own and others&rsquo; music:</p>
+        <ul>
+          <li>students analyze their own and others&rsquo; musical excerpts, works, and performances</li>
+          <li>students form personal responses to and construct meaning from their own and others&rsquo; music</li>
+        </ul>
+      </div>
+
+      <p><span class="badge">1</span>&nbsp;What is your immediate response to this music? Does this music sound like any other music you have heard before? What does this music make you think of? Have you heard any other music by Mozart? His music has been used in many films and on TV. If you have heard other music by Mozart, is it similar or different to the Symphony No. 40?</p>
+
+      <p><span class="badge">2</span>&nbsp;What adjectives might describe the mood that you felt when listening to this symphony? Can you identify what musical elements may have created that mood for you? When did the mood change and why?</p>
+
+      <p><span class="badge">3</span>&nbsp;What musical elements did you enjoy or find interesting? Did you enjoy the melodies that you heard? Did the rhythms, dynamics, or tempos used by Mozart help make this work exciting? Did you hear parts of the symphony that made you feel peaceful or calm and if so, what part of the symphony were they in? Were there any parts that sounded particularly dramatic to you? If so, which movement did you hear them in and why did they feel dramatic to you?</p>
+
+      <p><span class="badge">4</span>&nbsp;Were there parts of the Symphony that you did not enjoy? Why or why not? Can you identify which music elements made you enjoy or not enjoy the music?</p>
+
+      <p><span class="badge">5</span>&nbsp;Different people often have different responses to the same music. Ask someone else who heard the same music about their response to Mozart&rsquo;s Symphony No. 40.</p>
+
+      <p><span class="badge">6</span>&nbsp;What feelings did it seem that Mozart was trying to communicate to his audience about this work? What music elements seemed to be important to him?</p>
+
+      <p><span class="badge">7</span>&nbsp;Is there other music by Mozart that you could listen to and compare to the sounds and experience of the Symphony No. 40?</p>
+
+      <p>&nbsp;</p>
+
+    </div><!-- /span7 -->
+
+  </div><!-- /row -->
+
+  <?php get_template_part('templates/content', 'listen'); ?>
+
+</div><!-- /container -->
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
