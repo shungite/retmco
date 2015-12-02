52e0b67f25881551a590973b778e27583864253d
Jon Snidal <jsnidal@gmail.com>
2015-12-02 08:24:25 -0600

    Video link updated

--- php/wp-content/themes/roots-master/page-media.php
+++ php/wp-content/themes/roots-master/page-media.php
@@ -27,7 +27,7 @@ Template Name: Page Media Template
 
           <p><strong>Upcoming concerts video playlist<br>from the Official MCO YouTube Channel</strong></p>
           <iframe width="80%" height="360" src="//www.youtube.com/embed/14doGQdOjVg?list=PLrh6cFljbZhEXjcvsfi69-KIfk3r1JkCA" frameborder="0" allowfullscreen></iframe>
-          <p><a href="https://www.youtube.com/playlist?list=PLrh6cFljbZhEXjcvsfi69-KIfk3r1JkCA" target="_blank"><small>Go to playlist</small></a></p>
+          <p><a href="https://www.youtube.com/embed/s7cNPjBYn5k?list=PLrh6cFljbZhEXjcvsfi69-KIfk3r1JkCA" target="_blank"><small>Go to playlist</small></a></p>
 
           <p><strong>Classic 107 playlist<br>from the Official MCO YouTube Channel</strong></p>
           <iframe width="80%" height="360" src="//www.youtube.com/embed/1GxA-8-dtIY?list=PLrh6cFljbZhH87SF1DNn1l94aLtUHAqM-" frameborder="0" allowfullscreen></iframe>
