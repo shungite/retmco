83fd21ac461a31eb574f27dcbbd21e1cc6597be0
Jon Snidal <jsnidal@gmail.com>
2016-02-22 09:01:29 -0600

    Programs updated

--- php/wp-content/themes/roots-master/page-programs.php
+++ php/wp-content/themes/roots-master/page-programs.php
@@ -23,7 +23,7 @@ Template Name: Page Programs Template
       <p><strong>School visits &mdash; the MCO comes to you</strong> / the MCO Outreach Ensemble is a roaming troupe of MCO players consisting of violin, viola, cello, double bass and guitar. It offers workshops and mini-concerts in schools throughout the province. Call 204.783.7377 for more info or to request a visit.</p>
       <p>&nbsp;</p>
     </div><!-- /span7 -->
-    <div class="span1"> 
+    <div class="span1">
       <a href="http://www.themco.ca/schools-form/"><img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /></a><br />
       <p class="fineprint">Click above for order form.</p>
     </div><!-- /span1 -->
@@ -34,7 +34,7 @@ Template Name: Page Programs Template
         <div class="media-body">
           <p><strong><a href="http://www.sunlife.ca/">Sun Life Financial</a><br>&lsquo;Music for all!&rsquo;program: Open dress rehearsals for schools</strong></p>
           <p>Bring your school group to Westminster Church for a 50-minute open dress rehearsal. Learn about the work that goes into presenting a professional orchestral performance. Watch musicians and conductor rehearse and be the first to hear what audiences will hear that night! <strong>Tickets $6.00 including GST.</strong> These open dress rehearsals are best suited to senior students, adults and home-schooled children. They are working rehearsals with some stopping and starting. Offered for the concerts listed <a href="http://www.themco.ca/schools-form"><strong>on this form.</strong></a></p>
-          <p><a href="http://www.sunlife.ca/"><img src="http://www.themco.ca/wp-content/uploads/2013/01/SunLife-logo.png"></a></p> 
+          <p><a href="http://www.sunlife.ca/"><img src="http://www.themco.ca/wp-content/uploads/2013/01/SunLife-logo.png"></a></p>
         </div>
       </div>
     </div>
@@ -48,22 +48,22 @@ Template Name: Page Programs Template
           <p><strong>MCO Pizza Club</strong></p>
           <p><strong>Come at 6:00 pm</strong> for a primer on the music that you will be hearing in that evening's concert. Pizza and a drink are included in your ticket cost if you book at least three days ahead. Appropriate for all ages, student groups and family groups. Offered before the following concerts:</p>
           <dl class="dl">
-            <dt><del>October 28</dt>
-            <dd>Guest conductor <b>Scott Yoo</b> and great works for strings by Vivaldi and Sir Hubert Parry.</del></dd>
-            <dt>November 26</dt>
-            <dd><b>Anne Manson</b> and flautist <b>Sharon Bezaly</b>; works by Ginastera, Serebrier, Vivaldi and Haydn.</dd>
-            <dt>January 13</dt>
-            <dd>Guest conductor <b>Earl Stafford</b> and harpsichordist <b>Eric Lussier</b>; works by Bach, Rota, Stravinsky and Holst.</dd>
-            <dt>February 18</dt>
-            <dd><b>Anne Manson</b> and pianist <b>Inon Barnatan</b>; works by Mozart and Beethoven.</dd>
-            <dt>March 17</dt>
-            <dd><b>Anne Manson</b> and cellist <b>Ariel Barnes</b>; works by Jan&aacute;&#269;ek, Haydn and Heidi Ouellette.</dd>
-            <dt>April 22</dt>
-            <dd><b>Chamber Night</b>, with a focus on winds; works by Mozart, Ysa&#255;e, Beethoven and Dvo&#345;&aacute;k.</dd>
-            <dt>May 19</dt>
-            <dd>Guest conductor <b>Tania Miller</b> and classical guitarist <b>Sharon Isbin</b>; works by Geminiani, Vivaldi, Howard Shore, Gerald Barry and Aaron Jay Kernis.</dd>
-            <dt>June 3</dt>
-            <dd><b>Anne Manson</b> and piano superstar <b>Jan Lisiecki</b>; works by Jan&aacute;&#269;ek and Mozart.</dd>
+            <dt>November 3</dt>
+            <dd>Anne Manson presents British cellist Colin Carr and Robert Schumann&rsquo;s enigmatic Cello Concerto in A Minor.</dd>
+            <dt>December 1</dt>
+            <dd>Guest conductor Earl Stafford, the Winnipeg Boys&rsquo; Choir and the Pembina Trails Voices Singers will put on a Christmas concert for all ages to enjoy!</dd>
+            <dt>January 12</dt>
+            <dd>Alain Trudel and friends: Victoria Sparks (percussion); Karl Stobbe and Kerry DuWors (double violin concerto); plus Patricia Evans &amp; Callie Oberheu (double horn).</dd>
+            <dt>February 23</dt>
+            <dd>Anne Manson introduces the &rsquo;enfant terrible&rsquo; of harp: Emmanuel Ceysson sweeps away the instrument&rsquo;s clich&eacute;s.</dd>
+            <dt>March 23</dt>
+            <dd>Scott Yoo will present pianist Janina Fialkowska and Beethoven&rsquo;s Piano Concerto No. 4, in G Major, Op. 58. </dd>
+            <dt>April 27</dt>
+            <dd>Anne Manson presents 16-year-old Dutch Eurovision Young Musicians recorder sensation Lucie Horsch! Lucie will play the piece that won her a spot in the 2014 finals.</dd>
+            <dt>May 18</dt>
+            <dd>It&rsquo;s Chamber Night, Deluxe Edition, as Manitoba violin superstar James Ehnes and pianist Andrew Armstrong stop in at the MCO on their national recital tour!</dd>
+            <dt>June 9</dt>
+            <dd>Anne Manson welcomes Canadian-Armenian star soprano Isabel Bayrakdarian back to the MCO for Respighi&rsquo;s Il Tramonto (text by Percy Bysshe Shelley) and Songs from the Diaspora by Roberto Sierra.</dd>
           </dl>
           <p class="text-error"><b>Space is limited</b></p>
           <p class="text-error">Register early by phone: call Joan at 204.783.7377 at least three days before the concert to purchase tickets and book your spot in the MCO Pizza Club. Pizza is free with your concert tickets; parents, grandparents, teachers and children welcome! <b>Concerts begin at 7:30 pm.</b></p>
