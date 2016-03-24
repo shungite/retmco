2cd0fec1da975ee6c52837fb5aee60ff689d057c
Jon Snidal <jsnidal@gmail.com>
2016-03-24 08:26:12 -0500

    Sub data updated

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -10,7 +10,34 @@ Template Name: Page MCOhome Template
     <div class="span8">
       <a href="http://www.themco.ca/concert/manson-horsch/"><img src="http://www.themco.ca/wp-content/uploads/2015/07/head27apr16.png" alt="MCO / Manson, Ceysson"></a>
       <h4>One of the most remarkable<br>musical talents of her generation</h4>
-      <p>Whether playing two recorders at once or blazing through Baroque melodies on Eurovision, 16 year-old recorder player <b>Lucie Horsch</b> dazzles. Our Music Director and conductor <b>Anne Manson</b> calls her &ldquo;an extraordinary prodigy, with a great deal of sophistication and taste in her playing.&rdquo; <b>One-hour <a href="http://www.themco.ca/concert/manson-horsch/">matinee mini-concert</a> also available!</b> See program notes, bios and more at <a href="http://www.themco.ca/concert/manson-horsch/"><b>here</b></a> or, if you&rsquo;re just after a quick ticket or two, click the links below.<br><img src="http://www.themco.ca/wp-content/uploads/2013/01/QuikTicket.png" alt="QuikTicket"><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=0ffc25ee994c4f6089d67d0b5a8d0483"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2015/11/AdultTx.png" alt="" width="62" height="40"></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=8c3b274b2b1e4be1bef4b63857684e3c"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2015/11/SeniorTx.png" alt="" width="62" height="40" /></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=3b87dbe669d1452a8153adc183ab8078"><img rel="tooltip" title="Click to add student ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2015/11/StudentTx.png" alt="" width="62" height="40" /></a></p>
+      <p>Whether playing two recorders at once or blazing through Baroque melodies on Eurovision, 16 year-old recorder player <b>Lucie Horsch</b> dazzles. Our Music Director and conductor <b>Anne Manson</b> calls her &ldquo;an extraordinary prodigy, with a great deal of sophistication and taste in her playing.&rdquo; <b>One-hour <a href="http://www.themco.ca/concert/manson-horsch/">matinee mini-concert</a><a href="http://www.themco.ca/wp-content/uploads/2016/03/MCO1617.pdf"><img src="http://www.themco.ca/wp-content/uploads/2016/03/MCO1617form.jpg" alt="MCO subscription form" /></a>
+
+Click above for MCO&rsquo;s 2016/17 season details and an order form, then select from the following. Subscribe now to six or nine concerts! Mix and match as you wish!
+
+<table class="table table-condensed">
+  <tr>
+  <td><img src="http://www.themco.ca/wp-content/uploads/2013/01/AdultC.png" alt="" width="62" height="40" /> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=fb7fb126ba6a4b84841cfe67812da277" title="9-concert adult">2015/16 season nine concert adult subscription</a></td>
+  </tr>
+  <tr>
+  <td><img src="http://www.themco.ca/wp-content/uploads/2013/01/SeniorC.png" alt="" width="62" height="40" /> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=40a4d04fd9144339950d9a2706d435ae" title="9-concert senior">2015/16 season nine concert senior subscription
+</a></td>
+  </tr>
+  <tr>
+  <td><img src="http://www.themco.ca/wp-content/uploads/2013/01/StudentC.png" alt="" width="62" height="40" /> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=b1380e20e67a462f9433b1d8e6b46b5e" title="9-concert student">2015/16 season nine concert student subscription</a></td>
+  </tr>
+  <tr>
+  <td><img src="http://www.themco.ca/wp-content/uploads/2013/01/AdultC.png" alt="" width="62" height="40" /> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=1904c0df2d654dd7a3c96d45a229f72c" title="6-concert adult">2015/16 season six concert adult subscription</a></td>
+  </tr>
+  <tr>
+  <td><img src="http://www.themco.ca/wp-content/uploads/2013/01/SeniorC.png" alt="" width="62" height="40" /> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=39d82ce7eb914575a075242c36a7f76d" title="6-concert senior">2015/16 season six concert senior subscription</a></td>
+  </tr>
+  <tr>
+  <td><img src="http://www.themco.ca/wp-content/uploads/2013/01/StudentC.png" alt="" width="62" height="40" /> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=a7e73399d21f4cee9fbbe707c10b7a8d" title="6-concert student">2015/16 season six concert student subscription</a></td>
+</tr>
+  <tr>
+  <td><small>Select concerts upon checkout</small></td>
+  </tr>
+</table> also available!</b> See program notes, bios and more at <a href="http://www.themco.ca/concert/manson-horsch/"><b>here</b></a> or, if you&rsquo;re just after a quick ticket or two, click the links below.<br><img src="http://www.themco.ca/wp-content/uploads/2013/01/QuikTicket.png" alt="QuikTicket"><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=0ffc25ee994c4f6089d67d0b5a8d0483"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2015/11/AdultTx.png" alt="" width="62" height="40"></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=8c3b274b2b1e4be1bef4b63857684e3c"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2015/11/SeniorTx.png" alt="" width="62" height="40" /></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=3b87dbe669d1452a8153adc183ab8078"><img rel="tooltip" title="Click to add student ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2015/11/StudentTx.png" alt="" width="62" height="40" /></a></p>
     </div>
   </div><!-- /row -->
 
@@ -23,7 +50,7 @@ Template Name: Page MCOhome Template
       <div class="well">
         <div class="media"><a class="pull-left" href="#extras" data-toggle="tab"><img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/07/DonorCello.jpg" width="96px" height="96px"></a>
           <div class="media-body">
-            <p><strong><a href="http://www.themco.ca/quick-links-1516-subscriptions/">Subscribe now to MCO&rsquo;s 2015/16 season<br>&mdash; enjoy six or nine concerts in Westminster Church!</strong></a><br />Only subscribers enjoy every benefit the MCO has to offer, like a free CD, Musicnet Subscriber Reward Card, ticket exchange privileges, discounts on extra tickets and more. Make this the season you subscribe to Canada&rsquo;s &ldquo;tiny, perfect, orchestra!&rdquo;</p>
+            <p><strong><a href="http://www.themco.ca/quick-links-to-1617-subscriptions/">Subscribe now to MCO&rsquo;s 2016/17 season<br>&mdash; enjoy six or nine concerts in Westminster Church!</strong></a><br />Only subscribers enjoy every benefit the MCO has to offer, like a free CD, Musicnet Subscriber Reward Card, ticket exchange privileges, discounts on extra tickets and more. Make this the season you subscribe to Canada&rsquo;s &ldquo;tiny, perfect, orchestra!&rdquo; <b>EARLY BIRD PRICES IN EFFECT!</b></p>
           </div>
         </div>
       </div>
