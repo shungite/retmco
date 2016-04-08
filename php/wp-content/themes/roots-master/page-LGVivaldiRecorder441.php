29a0c37810a5f394b3dc7387d46ae457333ec305
Jon Snidal <jsnidal@gmail.com>
2016-04-08 11:55:09 -0500

    Listening guide added

---
+++ php/wp-content/themes/roots-master/page-LGVivaldiRecorder441.php
@@ -0,0 +1,157 @@
+<?php
+/*
+Template Name: Page LGVivaldiRecorder441 Template
+*/
+?>
+
+<div class="container">
+
+  <div class="row">
+
+    <div class="span8">
+      <img src="http://www.themco.ca/wp-content/uploads/2015/12/ListeningGuides.jpg" alt="" width="770" height="455">
+    </div>
+    <div class="span2">
+      <p>.</p>
+    </div>
+
+  </div><!-- /row -->
+
+  <div class="row">
+
+    <div class="span7">
+
+      <p>The MCO understands the importance of music education, which is why we&rsquo;ve developed a unique instructional resource for teachers and parents. Written by music educator <b>Beryl Peters, Ph.D</b>, our <b>MTS Future First Listening Guides</b> provide a clear and thorough analysis of the repertoire featured in our concerts. They offer a perfect primer to the great works of Bach, Beethoven, Britten and beyond for K-8 and 9-12 students&thinsp;&mdash;&thinsp;with engaging historical anecdotes and simple theory for younger students, and more complex information for older students. Indeed, just about anyone can learn a thing or two from our MTS Future First Listening Guides&thinsp;&hellip;&thinsp;so dig in!</p>
+
+      <p>
+      <b>
+        ANTONIO VIVALDI: RECORDER CONCERTO IN C MINOR, RV 441
+      </b>
+      </p>
+
+      <p>Antonio Vivaldi was a renowned Baroque Italian composer, violinist, and teacher who was born in Venice, Italy in 1678 and died in 1741. Vivaldi, also an ordained priest, served for many years as string teacher, conductor and composer-in-residence at Ospedale della Piet&agrave;, a convent, orphanage, and well-regarded music school in Venice. The Piet&agrave; had exceptionally high musical standards and Vivaldi composed a large body of sacred vocal and instrumental music for the highly talented female music ensembles at Piet&agrave;. Piet&agrave; was financially very well-endowed, since many of the female &ldquo;orphans&rdquo; were actually the illegitimate children of wealthy Italian noblemen and placed there at the request of their fathers.</p>
+
+      <p>Vivaldi is best known for the over 500 concertos he wrote for orchestra and solo instruments.  A group of four Vivaldi violin concerti, each associated with a different season of the year and together known as <i>The Four Seasons</i>, have been made famous over the years in thousands of recordings and as background music for movies and advertisements. Vivaldi also wrote over 40 operas as well as works for choir and a variety of instruments such as oboe, violin, bassoon, cello, flute, and various sizes of recorder.</p>
+
+      <p>Vivaldi&rsquo;s recorder and flute concertos demand players of virtuosic talent.  The solo recorder concertos were likely written for the technically and musically advanced talents of the Piet&agrave; virtuoso musicians and performing ensembles. The Recorder Concerto in C minor RV 441 is known as the most technically demanding of Vivaldi&rsquo;s recorder concertos and as one of the most virtuosic recorder compositions in the entire Baroque recorder repertoire. It is considered a jewel among Vivaldi&rsquo;s mature style concertos.</p>
+
+      <div class="well">
+        <p><b>FOR EDUCATORS</b></p>
+        <p><b>Manitoba Music Curricular Connections</b></p>
+        <p><b>9-12 Making:</b> The learner develops competencies for listening by listening critically with discrimination and purpose to:</p>
+        <ul>
+          <li>situate and contextualize music (e.g., cultural/ ideological/historical/social contexts, music style, genre, tradition, or praxis, etc.)</li>
+          <li>support enjoyment and understanding of music</li>
+          <li>make and interpret music expressively and creatively</li>
+          <li>inform analysis, interpretation, judgement, appreciation, and evaluation</li>
+        </ul>
+        <p><b>K-8 Understanding Music in Context:</b> Demonstrate awareness of the intended meanings and/or purposes of music encountered in own performance and listening experiences</p>
+        <p><b>9-12 Connecting:</b> The learner develops understandings about the significance of music by connecting music to diverse contexts.</p>
+      </div>
+
+      <p><b>Here are some ways to help you listen to this work:</b></p>
+
+      <span class="badge">1</span> Music compositions have a certain architecture called musical form. Can you hear that the structure of this concerto is made up of three different sections or movements? The Italian composer Antonio Vivaldi (1678-1741) was known for writing structures or forms in three fast-slow-fast movements. In this concerto the movements are titled:
+
+      <ol>
+        <li>Allegro non molto (fast and lively but not too fast)</li>
+        <li>Largo (slowly and broadly)</li>
+        <li>Allegro (fast and lively)</li>
+      </ol>
+
+      <p>Each of the three sections or movements also has a particular form.  In this concerto, each movement is created in some version of ritornello form. Ritornello form is based on a recurring theme (a refrain called the ritornello) played by the orchestra and interspersed with solo episodes of contrasting or related material played by the soloist(s) or small groups of instruments.  The word ritornello is derived from the Italian word &lsquo;ritornare&rsquo; and is translated as &lsquo;return.&rsquo; </p>
+
+      <p>The ritornello typically is heard as the introduction and then returns after the solo section as an interlude and as a postlude at the end of a movement. However, in this particular concerto, can you hear the recorder join the orchestra in the ritornello that opens the first movement?  And can you hear the recorder join the orchestra in the closing ritornello that concludes the third movement? The interplay and contrasts between the orchestral ritornello and the solo recorder can be heard throughout the concerto and the dialogue between the orchestra and solo recorder is perhaps most distinctly heard in the third movement Allegro. Can you hear the tutti (meaning &lsquo;all&rsquo; or whole orchestra) sections alternate with the solo recorder player in this movement?  The alternation between tutti and solo sections is a common feature of Baroque ritornello concerto form. </p>
+
+      <p><span class="badge">2</span> What <b>instruments</b> can you hear in this concerto? A concerto is a work of music for a solo performer or a small group of solo performers and an orchestra.  In this Concerto, the solo performer is the recorder player. There are different sizes of recorder including sopranino, soprano, alto, tenor, and bass recorders. Can you tell which recorder is being played in this concerto? Typically, this concerto is played by the alto recorder although it has been transposed for other recorders as well as for the flute.</p>
+
+      <p>When do you first hear the recorder? Do you hear the way that Vivaldi focuses on the solo recorder and gives the recorder special parts to play in the concerto? Can you hear the recorder in back and forth dialogue with the orchestra? When the orchestra plays with the solo recorder, can you hear how Vivaldi uses the orchestra as an accompaniment for the soloist? Can you hear when the orchestra plays by itself and when the recorder plays the same parts as the orchestra?</p>
+
+      <p>How many other musicians are playing onstage? What string instruments do you hear (violin, viola, cello, double bass)? Can you hear a harpsichord playing? The harpsichord is part of a group of accompanying instruments that provide harmonic support, known as the <b>Basso continuo</b> or <b>continuo</b> for short. Basso continuo means &quot;continuous bass&quot; and in the Baroque period was often made up of a keyboard instrument and another lower or bass instrument, for example, the cello, double bass, or bassoon. The number and type of instruments in the continuo group varied widely according to Baroque music practices, but always included at least one instrument that could play chords (e.g., harpsichord, organ, lute, harp, or guitar) in combination with a bass register instrument or instruments. </p>
+
+      <p><span class="badge">3</span> Vivaldi uses <b>tempo</b> (the speed of the music), <b>dynamics</b> (the volume of the music), and <b>expression</b> (musical elements that express certain feelings or dispositions) to create contrast and different moods throughout the concerto.</p>
+
+      <p>Can you identify a particular mood for each movement? What kind of mood is created by the energetic dialogue between the solo recorder and the orchestra in the first and third movements? Can you hear a distinct change in mood at the opening melody of the second movement?</p>
+
+      <p>Which movements use mostly fast music?  Which movement uses mostly slow music? Can you hear when the tempo of the music changes and gets faster or slower? How does that affect the mood of the work?</p>
+
+      <p>Was the music played at all the same volume (<b>dynamics</b>)? When do you hear music played loudly? Quietly? What dynamic level is mostly used for the slow second movement? When do the dynamics change and what effect does that create? When does the music get louder (<b>crescendo</b>) or get quieter (<b>decrescendo</b>)?</p>
+
+      <p>Do you hear musical sounds that are short sounding (<b>staccato</b>) or very smooth sounding (<b>legato</b>)?  Where do you hear suddenly loud sounds (<b>accents</b>)?</p>
+
+      <p>Vivaldi also adds expressive qualities to this concerto by the use of ornamentation. Ornamentation was common in the Baroque period and performers would often add certain musical flourishes or decorations to their parts. Ornamentation was also written into parts, as for example, when Vivaldi decorates the melody notes by adding extra very short notes just above or below the main melody note, or by altering rhythms. In the opening of the first movement, can you hear a short, decorative trill-like note called an acciaccatura? Can you hear the bird-like acciaccaturas and trills in the recorder part of the third movement?</p>
+
+      <p><span class="badge">4</span> How is <b>rhythm</b> used in this concerto? Do you hear sections that have a steady rhythm or beat that you could tap to? The underlying steady pulse heard throughout the concerto is a feature of Baroque music.</p>
+
+      <p>The opening movement ritornello begins with a rhythmic pattern of sixteenth and eighth notes that sounds like &ldquo;let me in, let me in, I am knocking, I am knocking.&rdquo; Do you hear that pattern or parts of that pattern again in other parts in the first movement?</p>
+
+      <p>The first solo recorder part in the opening movement features triplet rhythms (three equally spaced notes played in the space of two). Can you hear where the recorder begins playing a running, downwards scalic pattern of triplets? Listen for triplet patterns going upwards and downwards in other places in the first movement.</p>
+
+      <p>This Vivaldi concerto is known as one of the most virtuosic concertos of the Baroque era, due in part to the challenges of the rhythmic and melodic parts. Can you hear where the recorder plays very rapid rhythmic passages in the first movement? Some sections feature the recorder playing notes that are just 1/32 of the duration of a whole note (32nd notes).</p>
+
+      <p>Can you hear how the rhythmic patterns change in the second slow movement? Can you hear notes of longer duration in the second movement of this concerto? Then can you hear a return to sequences of triplet patterns and faster rhythmic passages in the third movement?</p>
+
+      <p><span class="badge">5</span> How is <b>melody</b> or <b>pitch</b> used in this concerto? Vivaldi&rsquo;s melodic writing for the recorder part in this concerto makes this a hugely challenging work for even the most virtuosic of recorder players and is an important reason why this concerto is considered perhaps the most demanding and virtuosic recorder concerto in all the Baroque repertoire! One of the reasons this concerto is so difficult to play is because there are so many large leaps from one melody note to the next, particularly in the rapid, complex rhythmic passages.</p>
+
+      <p>Can you hear places in all three movements where the recorder plays notes that seem to make large leaps up or down to the next note? Can you hear where the melodic patterns for the recorder and the orchestra are contrasting and sound more even, like one note running quickly after another in a scalic pattern? Even the rapid scalic runs of this concerto are considered treacherous by recorder players! Can you draw the shape of these melodies in the air to show when sounds are going up or down?</p>
+
+      <p>In the first movement, can you hear the opening orchestral ritornello melody (refrain) or fragments of this melody repeated again anywhere in the first movement? </p>
+
+      <p>How could you describe the melodies heard in the slower, second movement? Does this melody sound like a processional or march-like to you? Do you hear that the second movement begins with three repeated melody notes? The first two notes are played in the same rhythm and the third note is a shorter note that leads to the same melodic sequence played on higher notes. Do you hear repeated notes anywhere else in the second movement? </p>
+
+      <p>Vivaldi writes many melodic contrasts into the third movement. What different melodic features can you hear in the third movement? Can you hear large melodic leaps? Both very high and very low notes for the recorder?  Ornamented melodic lines?  Fast scalic patterns? Triplet melodic sequences? Rapid arpeggiated passages where the recorder alternates jumping back and forth on different notes of a chord? When you listen to the solo recorder and orchestra instruments can you pick out which instruments play mostly higher notes? Which instruments in the orchestra play lower notes?  </p>
+
+      <p><span class="badge">1</span> Can you hear different kinds of <b>texture</b> in this work? Musical texture refers to the layers of sound that are heard in the music. Where do you hear lots of instruments playing together so that the sound is thick? Where do you hear just a few instruments playing to create sparser sounding texture?</p>
+
+      <p>Can you hear where the texture of this concerto is many-layered with different independent busy lines or voices played by the orchestra and solo musician? This kind of texture is called <b>polyphonic</b> or &lsquo;many voiced.&rsquo;</p>
+
+      <p>Can you hear where the texture of this concerto is not as busy or &ldquo;many-voiced&rdquo; and where the orchestra instruments sounds similar or &ldquo;like sounding&rdquo; and move in the same rhythm rather than as independent melodic lines with their own rhythms and characters? Chords that are like sounding and are used to support a main, independent melody line are known as homophonic in texture. </p>
+
+      <p>Listen for the texture in the second movement. Can you hear that the orchestra does not have as many busy, independent, layered parts but is instead more chordal and <b>homophonic</b>?  </p>
+
+      <p><span class="badge">7</span> What kinds of instrumental <b>timbre</b> do you hear? Timbre is the different qualities of sound created by different instruments. For example the kind of sound that the string instruments make when they play together is a different quality of sound to that produced by brass instruments like the trumpet or French horn. Can you hear how the timbre changes when the harpsichord plays with the strings? When the recorder plays with the strings?</p>
+
+      <p>When does the timbre of the music change because Vivaldi adds or takes away different instruments? Can you hear when the orchestra is playing alone and when the orchestra musicians are playing together with the soloist? Can you hear different qualities of sound depending on what instrument or instruments are playing? Can you tell which instruments are playing just from hearing them? </p>
+
+      <div class="well">
+        <p><b>FOR EDUCATORS</b></p>
+        <p><b>Reflections and Responses</b><br>(K-8 Valuing; 9-12 Responding)</p>
+        <p><b>Grades 9-12 Responding</b></p>
+        <p>The learner develops and uses critical reflection and thinking for music learning:</p>
+        <ul>
+          <li>the learner generates initial reactions to music experiences</li>
+          <li>the learner critically listens to, observes, and describes music experiences</li>
+          <li>the learner analyzes and interprets music experiences</li>
+          <li>the learner constructs meanings about music experiences</li>
+        </ul>
+        <p><b>Grades K-8 Valuing</b></p>
+        <p>Students analyze, reflect on, and construct meaning in response to their own and others&rsquo; music:</p>
+        <ul>
+          <li>students analyze their own and others&rsquo; musical excerpts, works, and performances</li>
+          <li>students form personal responses to and construct meaning from their own and others&rsquo; music</li>
+        </ul>
+      </div>
+
+      <p><span class="badge">1</span> What is your immediate response to this music?  Does this music sound like any other music you have heard before?  What does this music make you think of?</p>
+
+      <p><span class="badge">2</span> What adjectives might describe the mood that you felt when listening to this recorder concerto? Can you identify what musical elements may have created that mood for you? When did the mood change and why?</p>
+
+      <p><span class="badge">3</span> What musical elements did you enjoy or find interesting? Did you enjoy the melodies that you heard?  Did the rhythms, dynamics, or tempos used by Vivaldi help make this work energetic?  Did you hear parts of the recorder concerto that made you feel peaceful or calm and if so, what part of the concerto were they in?  Were there any parts that sounded playful to you? If so, which movement did you hear them in?</p>
+
+      <p><span class="badge">4</span> Were there parts of the recorder concerto that you did not enjoy?  Why or why not? Can you identify which music elements made you enjoy or not enjoy the music?</p>
+
+      <p><span class="badge">5</span> Different people often have different responses to the same music. Ask someone else who heard the same music about their response to Vivaldi&rsquo;s Recorder Concerto in C minor.</p>
+
+      <p><span class="badge">6</span> What feelings did it seem that Vivaldi was trying to communicate to his audience about this work? What music elements seemed to be important to him?</p>
+
+      <p><span class="badge">7</span> Is there other music by Vivaldi that you could listen to and compare to the sounds and experience of the Recorder Concerto in C minor? Perhaps you could listen to other music by Vivaldi, such as <i>The Four Seasons</i>? Try listening to any of the Johann Sebastian Bach concertos to see if you can hear Vivaldi influences in Bach&rsquo;s music. Bach stated that he used the Vivaldi concertos as inspiration for his writing.</p>
+
+    </div><!-- /span7 -->
+
+  </div><!-- /row -->
+
+  <?php get_template_part('templates/content', 'listen'); ?>
+
+</div><!-- /container -->
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
