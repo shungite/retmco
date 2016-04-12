dbfee247c1ed9d4c82ef9e4443759ce903fdae93
Jon Snidal <jsnidal@gmail.com>
2016-04-12 08:29:30 -0500

    Discover page tweak again

--- php/wp-content/themes/roots-master/page-horsch.php
+++ php/wp-content/themes/roots-master/page-horsch.php
@@ -7,7 +7,7 @@ Template Name: Page Horsch Template
 
 <div class="row">
 
-  <div class="span8">
+  <div class="span7">
     <p>&nbsp;</p>
     <blockquote>
       <p>Luci Horsch is an extraordinary prodigy, with a great deal of sophistication and taste in her playing.</p>
@@ -19,15 +19,15 @@ Template Name: Page Horsch Template
     <div class="well">
       <p><strong>Discover Lucie Horsch</strong></p>
 
-      <iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/0fc55d5f-b1aa-4211-86b4-32078d9af987?startpage=1&allowFullscreen=true" width="650px" height="460px" frameborder="0" allowfullscreen=""></iframe>
+      <iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/0fc55d5f-b1aa-4211-86b4-32078d9af987?startpage=1&allowFullscreen=true" width="525px" height="371px" frameborder="0" allowfullscreen=""></iframe>
 
       <p>&nbsp;<br><strong>D&eacute;couvre l'artiste Lucie Horsch</strong></p>
 
-      <iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/787e9080-d80e-47bd-af1f-3953109b6654?startpage=1&allowFullscreen=true" width="650px" height="460px" frameborder="0" allowfullscreen=""></iframe>
+      <iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/787e9080-d80e-47bd-af1f-3953109b6654?startpage=1&allowFullscreen=true" width="525px" height="371px" frameborder="0" allowfullscreen=""></iframe>
 
   </div><!-- /well -->
 
-</div><!-- /span8 -->
+</div><!-- /span7 -->
 
 </div><!-- /row -->
 
