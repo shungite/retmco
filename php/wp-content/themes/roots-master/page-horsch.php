5bb1556f539bab716ad0c61b44f40ea0c810eb65
Jon Snidal <jsnidal@gmail.com>
2016-04-12 08:01:55 -0500

    Discover Lucie Horsch page added

---
+++ php/wp-content/themes/roots-master/page-horsch.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Template Name: Page Horsch Template
+*/
+?>
+<div class="container">
+
+<div class="row">
+
+  <div class="span8">
+    <p>&nbsp;</p>
+    <blockquote>
+      <p>&hellip;an extraordinary prodigy, with a great deal of sophistication and taste in her playing.</p>
+      <small>Anne Manson</small>
+    </blockquote>
+
+    <p>&nbsp;</p>
+
+    <div class="well">
+      <p><strong>Discover Lucie Horsch</strong></p>
+
+      <iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/0fc55d5f-b1aa-4211-86b4-32078d9af987?startpage=1&allowFullscreen=true" width="525px" height="371px" frameborder="0" allowfullscreen=""></iframe>
+
+      <p><strong>D&eacute;couvre l'artiste Lucie Horsch</strong></p>
+
+      <iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/787e9080-d80e-47bd-af1f-3953109b6654?startpage=1&allowFullscreen=true" width="525px" height="371px" frameborder="0" allowfullscreen=""></iframe>
+
+  </div>
+
+</div>
+
+<?php get_template_part('templates/content', 'media'); ?>
+
+</div><!-- /container -->
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
