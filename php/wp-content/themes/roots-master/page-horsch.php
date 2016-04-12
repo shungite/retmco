397e013d0987a06a0acc5436f8a2941d495c6471
Jon Snidal <jsnidal@gmail.com>
2016-04-12 09:22:55 -0500

    Link to EN iBook added

--- php/wp-content/themes/roots-master/page-horsch.php
+++ php/wp-content/themes/roots-master/page-horsch.php
@@ -20,7 +20,7 @@ Template Name: Page Horsch Template
       <p><strong>Discover Lucie Horsch</strong></p>
 
       <iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/0fc55d5f-b1aa-4211-86b4-32078d9af987?startpage=1&allowFullscreen=true" width="525px" height="371px" frameborder="0" allowfullscreen=""></iframe>
-      <p><img src="http://www.themco.ca/wp-content/uploads/2016/04/iBookIcon50.png">&nbsp;Download iBook here (9mb)</p>
+      <p><a href="http://www.themco.ca/wp-content/uploads/2016/04/Horsch-EN.ibooks"><img src="http://www.themco.ca/wp-content/uploads/2016/04/iBookIcon50.png"></a>&nbsp;<a href="http://www.themco.ca/wp-content/uploads/2016/04/Horsch-EN.ibooks"><b>Download iBook here (9mb)</b></a></p>
 
       <p>&nbsp;<br><strong>D&eacute;couvre l'artiste Lucie Horsch</strong></p>
 
