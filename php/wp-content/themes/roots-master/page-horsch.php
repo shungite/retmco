c7dd832425e578d2e91b6638b11a6e157b8c8bca
Jon Snidal <jsnidal@gmail.com>
2016-04-12 08:41:39 -0500

    iBook

--- php/wp-content/themes/roots-master/page-horsch.php
+++ php/wp-content/themes/roots-master/page-horsch.php
@@ -7,7 +7,7 @@ Template Name: Page Horsch Template
 
 <div class="row">
 
-  <div class="span7">
+  <div class="span8">
     <p>&nbsp;</p>
     <blockquote>
       <p>Luci Horsch is an extraordinary prodigy, with a great deal of sophistication and taste in her playing.</p>
@@ -19,7 +19,7 @@ Template Name: Page Horsch Template
     <div class="well">
       <p><strong>Discover Lucie Horsch</strong></p>
 
-      <iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/0fc55d5f-b1aa-4211-86b4-32078d9af987?startpage=1&allowFullscreen=true" width="525px" height="371px" frameborder="0" allowfullscreen=""></iframe>
+      <iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/0fc55d5f-b1aa-4211-86b4-32078d9af987?startpage=1&allowFullscreen=true" width="525px" height="371px" frameborder="0" allowfullscreen=""></iframe><img src="http://www.themco.ca/wp-content/uploads/2016/04/iBookIcon200.png">
 
       <p>&nbsp;<br><strong>D&eacute;couvre l'artiste Lucie Horsch</strong></p>
 
@@ -27,7 +27,7 @@ Template Name: Page Horsch Template
 
   </div><!-- /well -->
 
-</div><!-- /span7 -->
+</div><!-- /span8 -->
 
 </div><!-- /row -->
 
