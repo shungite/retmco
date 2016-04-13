d4ff4705c782c6a638a6fa8e4638a3955b56db3e
Jon Snidal <jsnidal@gmail.com>
2016-04-13 08:38:48 -0500

    New intros

--- php/wp-content/themes/roots-master/page-horsch.php
+++ php/wp-content/themes/roots-master/page-horsch.php
@@ -17,10 +17,36 @@ Template Name: Page Horsch Template
     <p>&nbsp;</p>
 
     <div class="well">
-      <p><strong>Discover Lucie Horsch</strong></p>
+
+      <p><b>MCO Learning Guides</b></p>
+
+      <p>The MCO understands the importance of music education, which is why we&rsquo;ve created a variety of unique instructional resources such as the bilingual, interactive MCO Discover Learning Guide featuring the virtuosic, young recorder player Lucie Horsch and the Baroque composer Antonio Vivaldi. Designed by Julie Mongeon-Ferr&eacute; and written by <b>Julie Mongeon-Ferr&eacute;</b> and <b>Beryl Peters</b>, the bilingual Discover Learning Guide is an exciting way for all audience members from Kindergarten to Grade 12 and beyond, to prepare for and engage with the inspiring music and musicians featured in the April 27, 2016 Manitoba Chamber Orchestra matinee concert. The Discover Learning Guide will help educators and learners to connect with the Manitoba Music K-12 curriculum framework through a variety of interactive experiences that explore the life and music of Lucie Horsch and Antonio Vivaldi. The Discover Learning Guide also suggests creative music possibilities for all learners inspired by the music of Vivaldi, and provides supporting material about the recorder family and innovative ways to interact with and respond to the music and musicians of this concert.</p>
+
+      <p>The Discover Learning Guide is available in three formats:</p>
+      <ul>
+        <li>electronic book ibook (iPad, iPhone, iPod touch or Mac)</li>
+        <li>online version (viewable with internet connection on most devices and browsers)</li>
+        <li>PDF (for reading and printing, no interactive components, available from within online version)</li>
+      </ul>
+      <p>Enjoy your musical discoveries!</p>
+
+      <p><strong>Learning Guide April 27</strong></p>
 
       <iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/0fc55d5f-b1aa-4211-86b4-32078d9af987?startpage=1&allowFullscreen=true" width="525px" height="371px" frameborder="0" allowfullscreen=""></iframe>
       <p><a href="http://www.themco.ca/wp-content/uploads/2016/04/Horsch-EN.ibooks"><img src="http://www.themco.ca/wp-content/uploads/2016/04/iBookIcon50.png"></a>&nbsp;<a href="http://www.themco.ca/wp-content/uploads/2016/04/Horsch-EN.ibooks"><b>Download iBook here</b> (9MB)</a></p>
+
+      <p><b>Guides d&rsquo;apprentissage OCM</b></p>
+
+      <p>L&rsquo;Orchestre de chambre du Manitoba (OCM) comprend l&apos;importance de l&apos;&eacute;ducation musicale, c&rsquo;est pourquoi nous avons cr&eacute;&eacute; une vari&eacute;t&eacute; de ressources p&eacute;dagogiques uniques tel que le guide d&apos;apprentissage OCM D&eacute;couverte, &agrave; la fois bilingue et interactif, mettant en vedette l&rsquo;interpr&egrave;te virtuose de fl&ucirc;te &agrave; bec Lucie Horsch et le compositeur baroque Antonio Vivaldi. Conceptualis&eacute; par Julie Mongeon-Ferr&eacute; et r&eacute;dig&eacute; par <b>Julie Mongeon-Ferr&eacute;</b> et <b>Beryl Peters, Ph.D.</b> le guide bilingue d&apos;apprentissage D&eacute;couverte est un outil stimulant  pour l&rsquo;auditoire d&rsquo;&eacute;l&egrave;ves la maternelle &agrave; la 12e ann&eacute;e et m&ecirc;me pour les adultes. Le guide peut servir &agrave; les pr&eacute;parer et &agrave; d&eacute;couvrir la musique inspirante et les musiciens en vedette lors de la matin&eacute;e du 27 Avril 2016 &agrave; l&rsquo;Orchestre de chambre du Manitoba. Le guide d&apos;apprentissage D&eacute;couverte aidera les apprenants et les &eacute;ducateurs &agrave; faire les liens avec les programmes d&rsquo;&eacute;tudes manitobains en musique de la maternelle &agrave; la 12e ann&eacute;e par le biais d&rsquo;une vari&eacute;t&eacute; d&apos;exp&eacute;riences interactives explorant la vie et la musique de Lucie Horsch et d&rsquo;Antonio Vivaldi. Le guide d&apos;apprentissage D&eacute;couverte sugg&egrave;re &eacute;galement &agrave; tous les apprenants des possibilit&eacute;s musicales cr&eacute;atives inspir&eacute;es par la musique de Vivaldi. Le guide offre &eacute;galement de l&rsquo;information au sujet de la famille des fl&ucirc;tes &agrave; bec et de fa&ccedil;on innovatrice, permet d&apos;interagir et de r&eacute;agir &agrave; la musique et d&rsquo;en apprendre au sujet des musiciens de ce concert.</p>
+
+      <p>Le guide d&apos;apprentissage D&eacute;couverte est accessible en trois formats :</p>
+      <ul>
+        <li>livre &eacute;lectronique ibook (iPad, iPhone, iPod touch ou Mac)</li>
+        <li>version en ligne (lisible avec une connexion Internet sur tous les navigateurs et tous les  appareils &eacute;lectroniques)</li>
+        <li>PDF (pour lecture et impression, sans composantes interactives, est disponible &agrave; partir de la version en ligne)</li>
+      </ul>
+
+      <p>Bonnes découvertes musicales!</p>
 
       <p>&nbsp;<br><strong>D&eacute;couvre l'artiste Lucie Horsch</strong></p>
 
