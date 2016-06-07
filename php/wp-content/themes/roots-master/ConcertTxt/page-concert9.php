6249ea2b35d1842674a731f29bd9a83582aaf271
Jon Snidal <jsnidal@gmail.com>
2016-06-07 07:45:21 -0500

    Sponsor added

--- php/wp-content/themes/roots-master/ConcertTxt/page-concert9.php
+++ php/wp-content/themes/roots-master/ConcertTxt/page-concert9.php
@@ -73,13 +73,13 @@ Template Name: Page Concert6 Template
 			<div class="tab-pane fade in active" id="repertoire">
 				<p><strong>Manitoba Chamber Orchestra</strong><br />Anne Manson, Music Director<br />Karl Stobbe, Concertmaster<br />Westminster United Church<br>9 June 2016</p>
 				<p>Isabel Bayrakdarian, soprano</p>
-				<p><b>B&eacute;la Bart&oacute;k</b><br><i>Romanian Folk Dances</i></p>
+				<p><b>B&eacute;la Bart&oacute;k</b><br><i>Rumanian Folk Dances</i></p>
 				<p><b>George Frideric Handel</b><br><i>Brilla nell’ alma</i>, from <i>Alessandro</i></p>
 				<p><b>Johann Adolph Hasse</b><br><i>Padre ingiusto, sposo ingrato</i>, from <i>Cajo Fabrizio</i></p>
 				<p><b>John Burge</b><br><i>Upper Canada Fiddle Suite</i></p>
 				<p><b>Ralph Vaughan Williams</b><br><i>Fantasia on a Theme of Thomas Tallis</i></p>
 				<p><b>Ottorino Respighi (arr. A. Trudel)</b><br><i>Il Tramonto</i> (The Sunset), text by Percy Bysshe Shelley</p>
-				<p>Concert sponsor / <b>Gail Asper Family Foundation</b><br />Concertmaster sponsor / <b>Elaine and Neil Margolis</b><br />Print media sponsor / <strong><a href="http://www.winnipegfreepress.com/">Winnipeg Free Press</a></strong><br />Radio media sponsors / <a href="http://www.icimusique.ca/" title="ICI musique"><strong>ICI musique 89.9</strong></a>, <a href="http://www.classic107.com/"><strong>Classic 107</a></strong> and <a href="http://www.goldenwestradio.com" title="Golden West Radio"><strong>Golden West Radio</strong></a>.
+				<p>Concert sponsor / <b>Gail Asper Family Foundation</b><br />Guest artist sponsorship / <b>in memory of David Riesen</b><br />Concertmaster sponsor / <b>Elaine and Neil Margolis</b><br />Print media sponsor / <strong><a href="http://www.winnipegfreepress.com/">Winnipeg Free Press</a></strong><br />Radio media sponsors / <a href="http://www.icimusique.ca/" title="ICI musique"><strong>ICI musique 89.9</strong></a>, <a href="http://www.classic107.com/"><strong>Classic 107</a></strong> and <a href="http://www.goldenwestradio.com" title="Golden West Radio"><strong>Golden West Radio</strong></a>
 			</div>
 		  <div class="tab-pane fade" id="bios">
 				<p><b>Isabel Bayrakdarian</b></p>
