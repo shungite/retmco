47e048745bb472113ae2205239f7be1fe1dfaa7d
Jon Snidal <jsnidal@gmail.com>
2016-07-11 09:49:46 -0500

    Corrections

--- php/wp-content/themes/roots-master/page-programs.php
+++ php/wp-content/themes/roots-master/page-programs.php
@@ -29,32 +29,50 @@ Template Name: Page Programs Template
       <div class="tab-content">
         <div class="tab-pane fade in active" id="en">
 
-          <b>Students love the excitement<br />of live chamber music!</b>
+          <p><b>Students love the excitement<br />of live chamber music!</b></p>
           <p>Bring your students to one or both of these special matinee performances! Use the order form on the reverse to book your tickets today or call 204-783-7377 for more information.</p>
           <p><span class="badge">1</span> <b>The MCO performs with the Frontier Fiddlers, February 14</b></p>
           <p>Made up of students in the Frontier School Division, the Frontier Fiddlers represents <i>la cr&egrave;me de la cr&egrave;me</i> of young fiddle players in the province. They come from as far away as Grand Rapids, Matheson Island, Berens River, and Norway House. They play Red River jigs with breakneck speed. And all of them are teenagers or younger. </p>
           <p>At this concert, the Frontier Fiddlers will treat us to a spectacular medley of fiddle tunes with a Red River flavour. With the orchestra, they&rsquo;ll also perform selections from Bart&oacute;k and Bach, and a folk-inspired new work by favourite Winnipeg composer Jim Hiscott.</p>
+          <p>1:00 pm matinee start time (doors open 12:30)<br>Tuesday, 14 February 2017<br>Anne Manson, Music Director<br>Karl Stobbe, Concertmaster<br>Westminster United Church<br>745 Westminster Avenue</p>
+          <p>Selections from:</p>
+          <p>Fiddle Medley (traditional)</p>
+          <p>Jim Hiscott<br>New composition<br>Manitoba Arts Council commission<br>World premiere performance</p>
+          <p>Johann Sebastian Bach<br>Violin Concerto no. 2 in E Major, BWV&thinsp;1042</p>
+          <p>B&eacute;la Bart&oacute;k<br>Divertimento for String Orchestra (movement)</p>
           <p><b><span class="badge">2</span> MCO, Th&eacute;&acirc;tre Cercle Molière &mdash; music and theatre, June 7 and 8 </b></p>
           <p>Nanabush is the trickster figure of Anishinaabe mythology&thinsp;&mdash;&thinsp;a cultural hero who, according to legend, is responsible for dogs&rsquo; proclivity to sniff one another&rsquo;s tails and for rescuing the forest from Paul Bunyan.</p>
           <p><i>Nanabush and the Drum&thinsp;/&thinsp;Nanabozho et le tambour</i>, a music and theatre collaboration between the MCO and Th&eacute;&acirc;tre Cercle Moli&egrave;re, envisions Nanabush&rsquo;s role in the foundation of the M&eacute;tis. He sets up playful obstacles for a hapless French voyageur, as he attempts to navigate the land and woo his more savvy Indigenous love interest.</p>
           <p>While celebrating Canada&rsquo;s 150th birthday, the piece illuminates the diversity of our heritage with a sense of humour and spectacle. The work is bilingual, but accessible to French and English students equally.</p>
+          <p>10:30 am matinee start times (doors open 10:00)<br>Wednesday and Thursday, 7 and 8 June 2017<br>Anne Manson, Music Director<br>Karl Stobbe, Concertmaster<br>Westminster United Church<br>745 Westminster Avenue</p>
+          <p>Genevi&egrave;ve Pelletier, Director<br>Anne Manson, Conductor</p>
+          <p><i>Nanabush and the Drum&thinsp;/&thinsp;Nanabozho et le tambour</i><br>Bilingual script by Rh&eacute;al Cenerini<br>Music by Michael Oesterle</p>
           <p>&nbsp;</p>
 
         </div>
 
-
       <div class="tab-pane fade" id="fr">
         <div>
 
-          <b>Les jeunes aiment l&rsquo;enthousiasme<br>de la musique en direct&thinsp;!</b>
+          <p><b>Les jeunes aiment l&rsquo;enthousiasme<br>de la musique en direct&thinsp;!</b></p>
           <p>Amenez vos &eacute;l&egrave;ves &agrave; l&rsquo;une ou aux deux pr&eacute;sentations de ces matin&eacute;es sp&eacute;ciales ! Utilisez le formulaire de commande &agrave; l&rsquo;endos pour r&eacute;server vos billets d&egrave;s aujourd&rsquo;hui ou composez le 204-783-7377 pour de plus amples renseignements.</p>
           <p><span class="badge">1</span> <b>l&rsquo;OCM joue avec les Frontier Fiddlers, le 14 f&eacute;vrier</b></p>
           <p>Compos&eacute; d&rsquo;&eacute;l&egrave;ves de la division scolaire Frontier, le groupe Frontier Fiddlers repr&eacute;sente la cr&egrave;me de la cr&egrave;me des jeunes joueurs de violon dans la province. Ils viennent d&rsquo;aussi loin que Grand Rapids, l&rsquo;&Icirc;le Matheson, Berens River et Norway House. Ils jouent des gigues de la rivi&egrave;re Rouge &agrave; une vitesse vertigineuse. Et tous sont des adolescents ou plus jeunes.</p>
           <p>Lors de ce concert, le groupe Frontier Fiddlers nous g&acirc;tera avec un m&eacute;lange spectaculaire d&rsquo;airs de violon avec une saveur de la rivi&egrave;re Rouge. Accompagn&eacute;s de l&rsquo;orchestre, les &eacute;l&egrave;ves pr&eacute;senteront aussi une s&eacute;lection de pi&egrave;ces de Bart&oacute;k et de Bach et une nouvelle &oelig;uvre d&rsquo;inspiration folklorique du compositeur favori de Winnipeg, Jim Hiscott.</p>
+          <p>Matin&eacute;e d&eacute;butant &agrave; 13 h<br>(portes ouvertes d&egrave;s 12 h 30)<br>Le mardi 14 f&eacute;vrier 2017<br>Anne Manson, directrice musicale<br>Karl Stobbe, premier violon<br>Westminster United Church<br>745, avenue Westminster</p>
+          <p>Pi&egrave;ces de :</p>
+          <p>Pot-pourri pour violon (traditionnel)<br>Jim Hiscott<br> Nouvelle &oelig;uvre<br>Commande du Conseil des arts du Manitoba<br>Premi&egrave;re mondiale</p>
+          <p>Johann Sebastian Bach<br>Concerto pour violon no 2 en mi majeur, BWV&thinsp;1042</p>
+          <p>B&eacute;la Bart&oacute;k<br>Divertimento pour orchestre &agrave; cordes (mouvement)</p>
           <p><b><span class="badge">2</span> l&rsquo;OCM, Th&eacute;&acirc;tre Cercle Moli&egrave;re&thinsp;&mdash;&thinsp;musique et th&eacute;&acirc;tre, les 7 et 8 juin</b></p>
           <p>Nanabozho, connu dans la mythologie des Anishinaabe comme &eacute;tant le Filou, est un h&eacute;ros culturel qui, selon la l&eacute;gende, est responsable de la propension des chiens &agrave; se renifler la queue et du sauvetage de la for&ecirc;t de Paul Bunyan.</p>
           <p><i>Nanabozho et le tambour&thinsp;/&thinsp;Nanabush and the Drum</i>, une collaboration musicale et th&eacute;&acirc;trale entre l&rsquo;OCM et le Th&eacute;&acirc;tre Cercle Moli&egrave;re, imagine le r&ocirc;le de Nanabozho dans la cr&eacute;ation des M&eacute;tis. Il met en place des obstacles amusants pour un pauvre voyageur qui tente de se frayer un chemin sur la terre et de faire la cour &agrave; sa bien-aim&eacute;e autochtone plus rus&eacute;e.</p>
           <p>Tout en c&eacute;l&eacute;brant le 150e anniversaire du Canada, la pi&egrave;ce met en &eacute;vidence la diversit&eacute; de notre patrimoine avec un sens d&rsquo;humour et du spectacle. L&rsquo;&oelig;uvre est bilingue, mais accessible tant pour les &eacute;tudiants francophones qu&rsquo;anglophones.</p>
+
+          <p>Matin&eacute;e d&eacute;butant &agrave; 10 h 30<br>(portes ouvertes d&egrave;s 10 h)<br>Le mercrexdi 7 juin et le jeudi 8 juin 2017<br>Anne Manson, directrice musicale<br>Karl Stobbe, premier violon<br>Westminster United Church<br>745, avenue Westminster</p>
+          <p>Genevi&egrave;ve Pelletier, metteure en sc&egrave;ne<br>Anne Manson, chef d&rsquo;orchestre</p>
+          <p><i>Nanabozho et le tambour&thinsp;/&thinsp;Nanabush and the Drum</i><br>Texte bilingue par Rh&eacute;al Cenerini<br>Musique par Michael &oelig;sterle</p>
+
           <p>&nbsp;</p>
 
         </div>
