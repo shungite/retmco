a346ef3a87a677fa4eb9bdfcfd06d9b39d7e564a
Jon Snidal <jsnidal@gmail.com>
2016-09-14 11:11:25 -0500

    BBedit project file

--- php/wp-content/themes/roots-master/ConcertTxt/page-concert7.php
+++ php/wp-content/themes/roots-master/ConcertTxt/page-concert7.php
@@ -3,37 +3,49 @@
 Template Name: Page Concert7 Template
 */
 ?>
-
 <div class="container">
-
-<div class="row">
-
-	<div class="span8">
-		<img src="http://www.themco.ca/wp-content/uploads/2016/06/head26apr17.jpg" width="770" height="455" alt="Stafford, Casseday">
+	<div class="row">
+		<div class="span8">
+			<img src="http://www.themco.ca/wp-content/uploads/2016/06/head26apr17.jpg" width="770" height="455" alt="Stafford, Casseday"> 
+		</div>
+		<div class="span2">
+			<p>
+				. 
+			</p>
+		</div>
 	</div>
-	<div class="span2">
-		<p>.</p>
-	</div>
-
-</div><!-- /row -->
-
-<div class="row">
-
-	<div class="span7">
-		<h4><div>Measha Brueggergosman<br>&amp; the genius of Strauss</div></h4>
-		<p>Measha Brueggergosman embodies the term &lsquo;diva&rsquo; in the best sense. Who else could boast of both singing opera on the world&rsquo;s most prestigious stages and acting as a judge on Project Runway?</p>
-		<p>Winnipeggers and the MCO have been with Brueggergosman since the beginning. As an up-and-coming soloist, the soprano performed an emotional concert with the MCO two weeks after the 2001 World Trade Center attacks. While establishing herself as an international celeb over the course of the next 15 years, she would perform with the MCO no fewer than five times, as well as record a CD with our orchestra for CBC Records.</p>
-		<p>This season, the Juno-winner sings the exquisite Four Last Songs of Richard Strauss in an intimate arrangement for piano quintet. With Earl Stafford conducting, the MCO also performs Strauss&rsquo; Metamorphosen and a new work by local composer Luke Nickel.</p>
-		<p><b>More Measha</b></p>
-		<p>N&eacute;e Measha Gosman, Measha and her husband combined their last names when they married to form Brueggergosman. Never one to follow the beaten path, the celebrated opera singer has recorded Ron Sexsmith and Frank Sinatra tunes, appeared on Much Music&rsquo;s Video on Trial, and performed the <i>Olympic Hymn</i> at the Opening Ceremonies of the 2010 Olympic Games. The vocally acrobatic performance inspired the NBC host to remark with a laugh, &ldquo;That&rsquo;s a hymn for you, right there.&rdquo;</p>
-
-		<div>
-	    <p><img src="http://www.themco.ca/wp-content/uploads/2013/01/PreConcert.png"><button id="pizza" class="btn btn-small btn-inverse" rel="popover" data-placement="top" data-original-title="Pizza Club" data-content="Book at least three days in advance for free pizza and a pre-concert presentation by Haley Marie. This is included with any concert ticket purchase, space permitting. Call (204)783-7377 for reservations. Audience members are also invited to come for the talk alone, if they&rsquo;ve already eaten dinner.">Pizza Club</button></p>
-	  </div>
-
-		<p><b>The concerts begin at 7:30 pm on April 26th in <a href="http://www.westminsterchurchwinnipeg.ca">Westminster United Church</a>, 745 Westminster at Maryland. Tickets are $34 for adults, $32 for seniors and $10 for students, including GST, at <a href="http://www.mcnallyrobinson.com/home" title="McNally Robinson">McNally Robinson</a>, the <a href="http://www.wecc.ca/" title="West End Cultural Centre">West End Cultural Centre</a> (586 Ellice at Sherbrook), <a href="http://www.organicplanet.coop/" title="Organic Planet">Organic Planet</a> (877 Westminster Ave) or MCO's Ticketline (204-783-7377).</b></p>
-
-		<!-- <div class="media">
+<!-- /row -->
+	<div class="row">
+		<div class="span7">
+			<h4>
+				<div>
+					Measha Brueggergosman<br>&amp; the genius of Strauss 
+				</div>
+			</h4>
+			<p>
+				Measha Brueggergosman embodies the term &lsquo;diva&rsquo; in the best sense. Who else could boast of both singing opera on the world&rsquo;s most prestigious stages and acting as a judge on Project Runway? 
+			</p>
+			<p>
+				Winnipeggers and the MCO have been with Brueggergosman since the beginning. As an up-and-coming soloist, the soprano performed an emotional concert with the MCO two weeks after the 2001 World Trade Center attacks. While establishing herself as an international celeb over the course of the next 15 years, she would perform with the MCO no fewer than five times, as well as record a CD with our orchestra for CBC Records. 
+			</p>
+			<p>
+				This season, the Juno-winner sings the exquisite Four Last Songs of Richard Strauss in an intimate arrangement for piano quintet. With Earl Stafford conducting, the MCO also performs Strauss&rsquo; Metamorphosen and a new work by local composer Luke Nickel. 
+			</p>
+			<p>
+				<b>More Measha</b> 
+			</p>
+			<p>
+				N&eacute;e Measha Gosman, Measha and her husband combined their last names when they married to form Brueggergosman. Never one to follow the beaten path, the celebrated opera singer has recorded Ron Sexsmith and Frank Sinatra tunes, appeared on Much Music&rsquo;s Video on Trial, and performed the <i>Olympic Hymn</i> at the Opening Ceremonies of the 2010 Olympic Games. The vocally acrobatic performance inspired the NBC host to remark with a laugh, &ldquo;That&rsquo;s a hymn for you, right there.&rdquo; 
+			</p>
+			<div>
+				<p>
+					<img src="http://www.themco.ca/wp-content/uploads/2013/01/PreConcert.png"><button id="pizza" class="btn btn-small btn-inverse" rel="popover" data-placement="top" data-original-title="Pizza Club" data-content="Book at least three days in advance for free pizza and a pre-concert presentation by Haley Marie. This is included with any concert ticket purchase, space permitting. Call (204)783-7377 for reservations. Audience members are also invited to come for the talk alone, if they&rsquo;ve already eaten dinner.">Pizza Club</button> 
+				</p>
+			</div>
+			<p>
+				<b>The concerts begin at 7:30 pm on April 26th in <a href="http://www.westminsterchurchwinnipeg.ca">Westminster United Church</a>, 745 Westminster at Maryland. Tickets are $34 for adults, $32 for seniors and $10 for students, including GST, at <a href="http://www.mcnallyrobinson.com/home" title="McNally Robinson">McNally Robinson</a>, the <a href="http://www.wecc.ca/" title="West End Cultural Centre">West End Cultural Centre</a> (586 Ellice at Sherbrook), <a href="http://www.organicplanet.coop/" title="Organic Planet">Organic Planet</a> (877 Westminster Ave) or MCO's Ticketline (204-783-7377).</b> 
+			</p>
+<!-- <div class="media">
 					<a class="pull-left" href="#extras" data-toggle="tab">
 					<img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/01/Don_Anderson_small.jpg">
 				</a>
@@ -42,67 +54,115 @@ Template Name: Page Concert7 Template
 					<p class="fineprint">&nbsp;<p>
 				</div>
 				</div> -->
-	</div><!-- /span7 -->
-
-	<div class="span1">
-
-		<img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /><br />
-	  	<a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=fd013d4be91941ffa1e56cbf0340a81e"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="left" src="http://www.themco.ca/wp-content/uploads/2015/11/AdultTxSm.png" alt="" width="62" height="30" /></a><br />
- 	  	<a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=c0ed99b4cc7848d89e525fb890cfbea1"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="left" src="http://www.themco.ca/wp-content/uploads/2015/11/SeniorTxSm.png" alt="" width="62" height="30" /></a><br />
-	  	<a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=768beb8510244ff69c1cf18383a98f11"><img rel="tooltip" title="Click to add student ticket to cart" data-placement="left" src="http://www.themco.ca/wp-content/uploads/2015/11/StudentTxSm.png" alt="" width="62" height="30" /></a>
-
-			<p class="fineprint">Click above to add ticket to cart; adjust quantity in cart; click &lsquo;Continue Shopping&rsquo; for other tickets.</p>
-
-	</div> <!-- /span1 -->
-
-</div><!-- /row -->
-
-<div class="row">
-
-	<div class="span6">
-
-		<div class="well"><!-- tab content well -->
-
-		<ul class="nav nav-pills">
-			<li class="active"><a href="#repertoire" data-toggle="tab"><strong>Repertoire</strong></a></li>
-			<li><a href="#bios" data-toggle="tab"><strong>Artists</strong></a></li>
-			<li><a href="#notes" data-toggle="tab"><strong>Notes</strong></a></li>
-		</ul>
-		<div class="tab-content">
-
-			<div class="tab-pane fade in active" id="repertoire">
-				<p><strong>Manitoba Chamber Orchestra</strong><br />Anne Manson, Music Director<br />Karl Stobbe, Concertmaster<br />Westminster United Church<br>29 March 2017</p>
-				<p>Earl Stafford, conductor<br>Measha Brueggergosman, soprano</p>
-				<p><b>Luke Nickel</b><br>New composition<br>Manitoba Arts Council commission<br>World premiere performance</p>
-				<p><b>Richard Strauss</b><br><i>Metamorphosen</i></p>
-				<p><b>Richard Strauss</b><br><i>Four Last Songs</i> (quintet version)</p>
-				<p>Concert sponsor / <a href="https://www.canadahelps.org/en/charities/the-gail-asper-family-foundation-inc/?gclid=CjwKEAjwmMS-BRCm5dn51JLbp1wSJACc61tFw3QwWIC0TqZiU-b970wX5FWstUYwP-ixSt1dy_0YhRoCo0fw_wcB"><b>Gail Asper Family Foundation</b></a><br>Concert media sponsor / <b>Neighbours of Tuxedo (<a href="http://www.bestversionmedia.com/">Best Version Media</a>)</b></p>
-			</div>
-
-		  <div class="tab-pane fade" id="bios">
-
-				<p><b>Coming soon!</b></p>
-
-			</div>
-
-			<div class="tab-pane fade" id="notes">
-
-				<p><b>Coming soon!</b></p>
-
-			</div>
-
-		</div><!-- /tab-content -->
-
-		</div><!-- /tab content well -->
-
-	</div><!-- /span6 tab area -->
-
-	<div class="span2 offset1">
-		<p class="fineprint">.</p>
+		</div>
+<!-- /span7 -->
+		<div class="span1">
+			<img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /><br />
+			<a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&amp;pid=fd013d4be91941ffa1e56cbf0340a81e"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="left" src="http://www.themco.ca/wp-content/uploads/2015/11/AdultTxSm.png" alt="" width="62" height="30" /></a><br />
+			<a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&amp;pid=c0ed99b4cc7848d89e525fb890cfbea1"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="left" src="http://www.themco.ca/wp-content/uploads/2015/11/SeniorTxSm.png" alt="" width="62" height="30" /></a><br />
+			<a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&amp;pid=768beb8510244ff69c1cf18383a98f11"><img rel="tooltip" title="Click to add student ticket to cart" data-placement="left" src="http://www.themco.ca/wp-content/uploads/2015/11/StudentTxSm.png" alt="" width="62" height="30" /></a> 
+			<p class="fineprint">
+				Click above to add ticket to cart; adjust quantity in cart; click &lsquo;Continue Shopping&rsquo; for other tickets. 
+			</p>
+		</div>
+<!-- /span1 -->
 	</div>
-
-</div><!-- /row -->
-
+<!-- /row -->
+	<div class="row">
+		<div class="span6">
+			<div class="well">
+<!-- tab content well -->
+				<ul class="nav nav-pills">
+					<li class="active"><a href="#repertoire" data-toggle="tab"><strong>Repertoire</strong></a></li>
+					<li><a href="#bios" data-toggle="tab"><strong>Artists</strong></a></li>
+					<li><a href="#notes" data-toggle="tab"><strong>Notes</strong></a></li>
+				</ul>
+				<div class="tab-content">
+					<div class="tab-pane fade in active" id="repertoire">
+						<p>
+							<strong>Manitoba Chamber Orchestra</strong><br />
+							Anne Manson, Music Director<br />
+							Karl Stobbe, Concertmaster<br />
+							Westminster United Church<br>29 March 2017 
+						</p>
+						<p>
+							Earl Stafford, conductor<br>Measha Brueggergosman, soprano 
+						</p>
+						<p>
+							<b>Luke Nickel</b><br>New composition<br>Manitoba Arts Council commission<br>World premiere performance 
+						</p>
+						<p>
+							<b>Richard Strauss</b><br><i>Metamorphosen</i> 
+						</p>
+						<p>
+							<b>Richard Strauss</b><br><i>Four Last Songs</i> (quintet version) 
+						</p>
+						<p>
+							Concert sponsor / <a href="https://www.canadahelps.org/en/charities/the-gail-asper-family-foundation-inc/?gclid=CjwKEAjwmMS-BRCm5dn51JLbp1wSJACc61tFw3QwWIC0TqZiU-b970wX5FWstUYwP-ixSt1dy_0YhRoCo0fw_wcB"><b>Gail Asper Family Foundation</b></a><br>Concert media sponsor / <b>Neighbours of Tuxedo (<a href="http://www.bestversionmedia.com/">Best Version Media</a>)</b> 
+						</p>
+					</div>
+					<div class="tab-pane fade" id="bios">
+						<p>
+							<b>Earl Stafford</b> 
+						</p>
+						<p>
+							Earl began his music education at the age of eight, making his debut two years later as a piano soloist with orchestra. Since then, he has played around the world, receiving critical accolades for his performances. The Globe and Mail wrote, &ldquo;His pianistic artistry was sheer poetry.&rdquo; He was awarded the gold medal as best accompanist at the 1980 International Ballet Competition in Varna, Bulgaria. 
+						</p>
+						<p>
+							In 1984, Earl was appointed Music Director and Principal Conductor for Canada&rsquo;s Royal Winnipeg Ballet, a position he held until 2009. He has also served as music director for the Saskatoon Symphony, and has held positions on the faculties of the University of Manitoba and The Banff Centre. Earl was invited to conduct for Queen Elizabeth&rsquo;s Royal Jubilee Gala in 2002 and was music director for the Governor General&rsquo;s Awards with the National Arts Centre Orchestra. He has made numerous recordings for CBC and has appeared as a piano soloist on Bravo and A&amp;E. 
+						</p>
+						<p>
+							Earl&rsquo;s work with the Royal Winnipeg Ballet earned him a sterling reputation, which has resulted in numerous repeat invitations as guest conductor. He has worked with many of Canada&rsquo;s finest orchestras including the Toronto Symphony, the National Arts Centre Orchestra, the Symphonies of Vancouver, Victoria, Kitchener-Waterloo, Winnipeg and Saskatoon and the Orchestre M&eacute;tropolitain du Grand Montr&eacute;al. 
+						</p>
+						<p>
+							Internationally, Earl has appeared with the Tokyo Philharmonic, the Hungarian State Opera, the Scottish Chamber Orchestra, the Queen&rsquo;s Hall Orchestra of London and the Theatre Harmony Orchestra of Moscow. Earl has also collaborated with many of the world&rsquo;s finest artists, including James Ehnes, Janina Fialkowska, Desmond Hoebig, Angela Hewitt, Angela Cheng, Josh Groban, Evelyn Hart, Oscar Peterson, Joel Quarrington, Alain Trudel, Stephen Sitarski, Kyoko Takezawa, Mel Torm&eacute;, Ron Paley, Edith Wiens, Mark Zeltzer, Ian Tyson, Neil Sedaka, Fernando Varela, Jann Arden, Anne Murray and more. 
+						</p>
+						<p>
+							Earl is comfortable conducting and playing a variety of styles of music&mdash;from the classics to the avant-garde, and from chamber music to pops. He is a versatile conductor who isn&rsquo;t afraid to take chances. 
+						</p>
+						<p>
+							<b>Measha Brueggergosman</b> 
+						</p>
+						<p>
+							Noted by the San Francisco Chronicle as &ldquo;a singer of rare gifts and artistic intensity&rdquo; and by the Miami Herald for possessing &ldquo;a superb voice capable of just about everything,&rdquo; Canadian soprano Measha Brueggergosman has emerged as one of the most magnificent performers and vibrant personalities of the day. She is critically acclaimed by the international press as much for her innate musicianship and voluptuous voice as for a sovereign stage presence. 
+						</p>
+						<p>
+							3.2 billion television viewers worldwide witnessed the Opening Ceremonies of the Vancouver 2010 Olympic and Paralympic Winter Games, and all heard the lauded soprano&rsquo;s epic performance of the &lsquo;Olympic Hymn.&rsquo; 
+						</p>
+						<p>
+							Notable performances of the recent past include: Mahler&rsquo;s Des knaben Wunderhorn conducted by Jaap van Zweden with the Chicago Symphony; Cage&rsquo;s Aria with Renga under Michael Tilson Thomas at the debut concert of the YouTube Symphony at Carnegie Hall; Messiaen&rsquo;s Poemes pour Mi with Daniel Harding and the London Symphony and Mendelssohn&rsquo;s Elijah with Yannick N&eacute;z&eacute;t-Seguin and the Orchestre M&eacute;tropolitain du Montr&eacute;al. Measha also performed at Gustavo Dudamel&rsquo;s inaugural concert as Music Director of the Los Angeles Philharmonic. 
+						</p>
+						<p>
+							Deeply committed to the art of song, Measha Brueggergosman has given solo recitals at London&rsquo;s Wigmore Hall, Roy Thomson Hall, Carnegie Hall, Spivey Hall, the Kennedy Center, and the Palais des Beaux-Arts in Brussels, and many other venues across North America and Europe. Always happy to try something new, she participated in the Verbier Festival&rsquo;s one-off presentation of Rufus Wainwright Goes Classical, sharing performances of works from the classical repertoire with Rufus Wainwright and Angelika Kirchschlager, arranged especially for the occasion. 
+						</p>
+						<p>
+							Ms Brueggergosman has been honoured to participate in a number of special events including the gala re-openings of Roy Thomson Hall and the University of Michigan&rsquo;s Hill Auditorium, Canada Day celebrations from Parliament Hill in Ottawa, and the opening ceremonies of the XVI International AIDS Conference in Toronto&mdash;sharing the stage with Bill Gates and former President Bill Clinton. She has given a Royal Command Performance for Queen Elizabeth II and has sung for, amongst others, the Prince of Wales, Queen Margrethe II of Denmark, Queen Sonja of Norway, President Tarja Halonen of Finland, Nelson Mandela and Kofi Annan. In 2012 she served as a judge for the inaugural season of Canada&rsquo;s Got Talent. Measha has hosted other cultural variety shows and was the subject of a full-length CBC feature documentary, Spirit in her Voice. 
+						</p>
+						<p>
+							Beyond the great concert halls of the world, Ms Brueggergosman lends her voice, passion, and energy to social and environmental causes as a Canadian goodwill ambassador for three international organizations: the African Medical and Research Foundation; Learning Through the Arts; and the World Wildlife Fund. Her commitments to these organizations have taken her on a broad spectrum of missions&mdash;from primary schools in New Brunswick to internally displaced persons camps of northern Uganda. 
+						</p>
+						<p>
+							In April 2015 Measha and her husband Markus welcomed to their family Sterling Markus, a baby brother for three year old Shepherd Peter. 
+						</p>
+					</div>
+					<div class="tab-pane fade" id="notes">
+						<p>
+							<b>Coming soon!</b> 
+						</p>
+					</div>
+				</div>
+<!-- /tab-content -->
+			</div>
+<!-- /tab content well -->
+		</div>
+<!-- /span6 tab area -->
+		<div class="span2 offset1">
+			<p class="fineprint">
+				. 
+			</p>
+		</div>
+	</div>
+<!-- /row -->
 <?php get_template_part('templates/content', 'concert'); ?>
-
-</div><!-- /container -->
+</div>
+<!-- /container -->
