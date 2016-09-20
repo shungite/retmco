4fc8f9b85286970b47c98804de7c703c9bc7344a
Jon Snidal <jsnidal@gmail.com>
2016-09-20 15:41:29 -0500

    FOTL correction

--- php/wp-content/themes/roots-master/page-fotl.php
+++ php/wp-content/themes/roots-master/page-fotl.php
@@ -45,21 +45,6 @@ Template Name: Page FOTL Template
 		</div>
 	</div>
 <!-- /row -->
-	<div class="row">
-		<div class="span6">
-			<div class="well">
-				<p>
-					<b>Donate your unwanted records to the MCO!</b>
-				</p>
-				<p>
-					We always want new used records! If you have some you&rsquo;d like to donate, call the MCO office at 204.783.7377 for drop-off instructions or to arrange a pick-up. Please note that pickups may take a while as we keep our volunteers pretty busy &mdash; thanks in advance!
-				</p>
-			</div>
-<!-- /well -->
-		</div>
-<!-- /span6 -->
-	</div>
-<!-- /row -->
 <?php get_template_part('templates/content', 'fotl'); ?>
 </div>
 <!-- /container -->
