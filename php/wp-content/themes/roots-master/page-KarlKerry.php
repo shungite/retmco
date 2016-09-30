2f4bd4d6f88404d6a38a7a4f0157db4324cc3ddd
Jon Snidal <jsnidal@gmail.com>
2016-09-30 16:25:39 -0500

    Candlelight page fix again

--- php/wp-content/themes/roots-master/page-KarlKerry.php
+++ php/wp-content/themes/roots-master/page-KarlKerry.php
@@ -6,7 +6,7 @@ Template Name: Page KarlKerry Template
 <div class="container">
 	<div class="row">
 		<div class="span8">
-			<img src="" alt="" width="770" height="455"> 
+			<img src="http://www.themco.ca/wp-content/uploads/2016/09/KarlKerry.jpg" alt="" width="770" height="455"> 
 		</div>
 		<div class="span2">
 			<p>
@@ -21,18 +21,15 @@ Template Name: Page KarlKerry Template
 				A Candlelight evening with violinists<br>Karl Stobbe &amp; Kerry DuWors 
 			</h4>
 			<p>
-				The Manitoba Chamber Orchestra invites you to a Candlelight Evening of music and conversation with violinists <strong>Karl Stobbe</strong> and <strong>Kerry DuWors</strong>, 7:30 pm, Thursday, 20 October 2016, to be held at historic <b>Moss House</b>, 218 Roslyn Road, a turn-of-the-century heritage home. Tickets $75 per person (partial tax receipt). Musical performance will commence at 7:45. RSVP 204.783.7377 by Monday, October 17th. Tickets also available <strong><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=9d1087ee6a25498da8c658d4562f4e0e">here</a></strong>. 
-			</p>
-			<p>
-				This unique salon evening includes <b>wine with sweets and savouries</b>. 
+				The Manitoba Chamber Orchestra invites you to a Candlelight Evening of music and conversation with violinists <strong>Karl Stobbe</strong> and <strong>Kerry DuWors</strong>, 7:30 pm, Thursday, 20 October 2016, to be held at historic <b>Moss House</b>, 218 Roslyn Road. Tickets $75 per person (partial tax receipt). Musical performance will commence at 7:45. RSVP 204.783.7377 by Monday, October 17th. Tickets also available <strong><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&amp;pid=9d1087ee6a25498da8c658d4562f4e0e">here</a></strong>. 
 			</p>
 			<div class="row-fluid">
 				<div class="well">
 					<p>
-						Karl Stobbe is recognized as one of Canada&rsquo;s most accomplished violinists, noted for his generous, rich sound, and long, poignant phrasing. As a concertmaster, soloist, chamber musician, and recording musician he has long been an audience favourite. London&rsquo;s <em>Sunday Times</em> calls Karl &ldquo;a master soloist, recalling the golden age of violin playing&thinsp;&hellip;&thinsp;producing a breathtaking range of tone colours.&rdquo; 
+						<strong>Karl Stobbe</strong> is recognized as one of Canada&rsquo;s most accomplished violinists, noted for his generous, rich sound, and long, poignant phrasing. As a concertmaster, soloist, chamber musician, and recording musician he has long been an audience favourite. London&rsquo;s <em>Sunday Times</em> calls Karl &ldquo;a master soloist, recalling the golden age of violin playing&thinsp;&hellip;&thinsp;producing a breathtaking range of tone colours.&rdquo; 
 					</p>
 					<p>
-						Violinist Kerry DuWors has earned accolades for her &ldquo;poise and maturity&rdquo; and &ldquo;spellbinding expression,&rdquo; and has been praised for always finding &ldquo;the music behind the notes&rdquo; (<em>Winnipeg Free Press</em>). Winner of the 26th Eckhardt-Gramatt&eacute; Competition, she is a dedicated mentor and teacher and is Associate Professor of Violin and Chamber Music at Brandon University. 
+						Violinist <strong>Kerry DuWors</strong> has earned accolades for her &ldquo;poise and maturity&rdquo; and &ldquo;spellbinding expression,&rdquo; and has been praised for always finding &ldquo;the music behind the notes&rdquo; (<em>Winnipeg Free Press</em>). Winner of the 26th Eckhardt-Gramatt&eacute; Competition, she is a dedicated mentor and teacher and is Associate Professor of Violin and Chamber Music at Brandon University. 
 					</p>
 					<p>
 						Both Karl and Kerry have performed in some of North America&rsquo;s most famous concert halls, including New York&rsquo;s Carnegie Hall and Boston&rsquo;s Jordan Hall. 
@@ -42,7 +39,7 @@ Template Name: Page KarlKerry Template
 		</div>
 <!-- /span6 -->
 		<div class="span2">
-			<a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=9d1087ee6a25498da8c658d4562f4e0e"><img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /></a><br />
+			<a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&amp;pid=9d1087ee6a25498da8c658d4562f4e0e"><img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /></a><br />
 			<p class="fineprint">
 				Click above to add a ticket to your shopping cart; adjust quantity in cart and return to purchase other tickets. 
 			</p>
