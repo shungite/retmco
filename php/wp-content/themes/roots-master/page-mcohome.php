bf592656be312a090c4c1fd82b827bacdfa78dec
Jon Snidal <jsnidal@gmail.com>
2016-10-19 11:54:51 -0500

    Revert

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -6,7 +6,7 @@ Template Name: Page MCOhome Template
 <div class="container">
 	<div class="row-fluid">
 		<div class="span8">
-			[video_lightbox_youtube video_id="ORmQwkn7SGc" width="640" height="480" anchor="http://www.themco.ca/wp-content/uploads/2016/10/head08nov16vid.jpg"] 
+			<a href="http://www.themco.ca/concert/manson-von-oeyen/"><img src="http://www.themco.ca/wp-content/uploads/2016/10/head08nov16c.jpg" alt="MCO / Manson, von Oeyen "></a> 
 			<h4>
 				&ldquo;Can do absolutely anything<br>he likes with a keyboard&rdquo;
 			</h4>
