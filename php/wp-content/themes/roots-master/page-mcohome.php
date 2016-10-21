af6c77b33e22529b20a23a649ed23bffc63594cb
Jon Snidal <jsnidal@gmail.com>
2016-10-21 10:57:07 -0500

    Image on top

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -6,10 +6,8 @@ Template Name: Page MCOhome Template
 <div class="container">
 	<div class="row-fluid">
 		<div class="span8">
-			<a href="http://www.themco.ca/concert/manson-von-oeyen/"><img src="http://www.themco.ca/wp-content/uploads/2016/10/head08nov16c.jpg" alt="MCO / Manson, von Oeyen "></a> 
-			<div>
-				<a href="http://www.themco.ca/concert/manson-von-oeyen/"><img src="http://www.themco.ca/wp-content/uploads/2014/10/YouTube-social-circle_red_48px.png" alt="Play"></a> 
-			</div>
+			<a href="http://www.themco.ca/concert/manson-von-oeyen/"><img src="http://www.themco.ca/wp-content/uploads/2016/10/head08nov16c.jpg" alt="MCO / Manson, von Oeyen "></a>
+				<a href="http://www.themco.ca/concert/manson-von-oeyen/"><img src="http://www.themco.ca/wp-content/uploads/2014/10/YouTube-social-circle_red_48px.png" alt="Play"></a>
 		</div>
 	</div>
 	<div class="row-fluid">
