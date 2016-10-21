01c2548871d1fa71be4c4053faa1abcf1efb9de7
Jon Snidal <jsnidal@gmail.com>
2016-10-21 11:01:22 -0500

    Image coord

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -6,8 +6,8 @@ Template Name: Page MCOhome Template
 <div class="container">
 	<div class="row-fluid">
 		<div class="span8">
-			<a href="http://www.themco.ca/concert/manson-von-oeyen/"><img src="http://www.themco.ca/wp-content/uploads/2016/10/head08nov16c.jpg" alt="MCO / Manson, von Oeyen "></a>
-				<a href="http://www.themco.ca/concert/manson-von-oeyen/"><img src="http://www.themco.ca/wp-content/uploads/2014/10/YouTube-social-circle_red_48px.png" alt="Play"></a>
+			<a href="http://www.themco.ca/concert/manson-von-oeyen/"><img src="http://www.themco.ca/wp-content/uploads/2016/10/head08nov16c.jpg" alt="MCO / Manson, von Oeyen" style="position: relative; top: 0; left: 0;"></a>
+				<a href="http://www.themco.ca/concert/manson-von-oeyen/"><img src="http://www.themco.ca/wp-content/uploads/2014/10/YouTube-social-circle_red_48px.png" style="position: absolute; top: 30px; left: 70px;" alt="Play"></a>
 		</div>
 	</div>
 	<div class="row-fluid">
