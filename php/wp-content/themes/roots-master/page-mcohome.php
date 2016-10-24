86aae5e75a207f444d53fc674c3be287a385aa7f
Jon Snidal <jsnidal@gmail.com>
2016-10-24 13:06:17 -0500

    CSS exp

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -8,7 +8,7 @@ Template Name: Page MCOhome Template
 		<div class="span8">
 			<div class="covercontainer">
 				<a href="http://www.themco.ca/concert/manson-von-oeyen/"><img src="http://www.themco.ca/wp-content/uploads/2016/10/head08nov16c.jpg" class="coverheader" alt="MCO / Manson, von Oeyen"></a> 
-				<div class="playbutton&quot;">
+				<div class="playbutton">
 					<a href="http://www.themco.ca/concert/manson-von-oeyen/"><img src="http://www.themco.ca/wp-content/uploads/2014/10/YouTube-social-circle_red_48px.png" alt="Play"></a> 
 				</div>
 			</div>
