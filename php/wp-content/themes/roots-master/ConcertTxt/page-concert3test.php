315f74429d5dbfb399b5190021522a90be44ccf1
Jon Snidal <jsnidal@gmail.com>
2016-10-28 07:38:51 -0500

    Musicians updated

--- php/wp-content/themes/roots-master/ConcertTxt/page-concert3test.php
+++ php/wp-content/themes/roots-master/ConcertTxt/page-concert3test.php
@@ -101,7 +101,7 @@ Template Name: Page Concert3 Template
 							<b>Wolfgang Amadeus Mozart</b><br>Symphony No. 29, in A Major, K 201 
 						</p>
 						<p>
-							Concert sponsor / <a href="http://www.pollardbanknote.com/"><b>Pollard Banknote</b></a><br>Concertmaster sponsor / <a href="https://nesbittburns.bmo.com/joel.cogan"><strong>Joel B. Cogen, BMO Nesbitt Burns</strong><br></a>Piano sponsors / <a href="http://www.stjohnsmusic.com/"><b>St. John&rsquo;s Music</b></a> and <a href="http://ca.yamaha.com/"><b>Yamaha Canada</b></a> 
+							Concert sponsor / <a href="http://www.pollardbanknote.com/"><b>Pollard Banknote</b></a><br>Music Director sponsor / <a href="http://www.prolific.ca/"><strong>The Prolific Group</strong></a><br>Concertmaster sponsor / <a href="https://nesbittburns.bmo.com/joel.cogan"><strong>Joel B. Cogan, BMO Nesbitt Burns</strong><br></a>Piano sponsors / <a href="http://www.stjohnsmusic.com/"><b>St. John&rsquo;s Music</b></a> and <a href="http://ca.yamaha.com/"><b>Yamaha Canada</b></a> 
 						</p>
 					</div>
 					<div class="tab-pane fade" id="bios">
