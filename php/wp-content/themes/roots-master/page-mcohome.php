ca353c748b9eda3477b81409830291194474acf4
Jon Snidal <jsnidal@gmail.com>
2016-11-01 12:25:05 -0500

    Remove extra row-fluid fix

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -9,7 +9,7 @@ Template Name: Page MCOhome Template
     <div class="covercontainer">
       <a href="http://www.themco.ca/concert/manson-von-oeyen/"><img src="http://www.themco.ca/wp-content/uploads/2016/10/head14feb17cover.jpg" alt="MCO / Manson, von Oeyen"></a>
       <div class="playbutton">
-        <a href="http://www.themco.ca/concert/manson-von-oeyen/"><img src="http://www.themco.ca/wp-content/uploads/2016/10/YouTube-social-circle_red_92px.png" alt="Play"></a>
+        <a href="http://www.themco.ca/concert/manson-von-oeyen/"><img src="http://www.themco.ca/wp-content/uploads/2016/11/YouTube-social-circle_red_96px.png" alt="Play"></a>
       </div>
     </div>
   </div>
