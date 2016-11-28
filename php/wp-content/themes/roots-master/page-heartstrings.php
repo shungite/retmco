6ba39aadf952a218b95c535114f3d85bed5aad78
Jon Snidal <jsnidal@gmail.com>
2016-11-28 13:56:55 -0600

    HS update

--- php/wp-content/themes/roots-master/page-heartstrings.php
+++ php/wp-content/themes/roots-master/page-heartstrings.php
@@ -3,45 +3,65 @@
 Template Name: Page Heartstrings Template
 */
 ?>
-
 <div class="container">
+	<div class="row">
+		<div class="span8">
+			<img src="http://www.themco.ca/wp-content/uploads/2016/11/HShead.jpg" alt="MCO Heartstrings 2017" width="770" height="455"> 
+		</div>
 
-  <div class="row">
-
-    <div class="span8">
-      <img src="http://www.themco.ca/wp-content/uploads/2015/11/HShead.jpg" alt="" width="770" height="455">
-    </div><!-- /span8 -->
-
-    <div class="span2">
-      <p>.</p>
-    </div><!-- /span2 -->
-
-    </div><!-- /row -->
-
-    <div class="row">
-
-    <div class="span6">
-      <h4>The MCO&rsquo;s gala evening<br>&mdash; fabulous 2017 edition!</h4>
-      <p>Winnipeg&rsquo;s heavenly fundraiser is on Friday, May 5th this year. As usual, it takes place in the opulent 7th-floor ballrooms at The Fort Garry Hotel, and this year, it&rsquo;s LA DOLCE VITA!</p>
-      <p>Friday, 5 May 2017<br>The Fort Garry Hotel, Seventh Floor</p>
-      <p>6:30 pm, hors d&rsquo;oeuvres<br>7:30 pm, Dinner<br>9:30 pm, Desserts and Liqueurs<br>Live &amp; Silent Auctions<br>Tickets $175, table of ten $2,000, patron tickets $200</p>
-      <p><strong>&bull; <a href="#">buy tickets @ $175</a><br>&bull; <a href="#">buy a table of ten @ $2000</a><br>&bull; <a href="#">buy Patron tickets @ $200*</a></strong></p>
-<small><b>Heartrings is sponsored by</b></small>
-<p><a href="http://advisors.nbfwm.ca/en/teams/christianson-wealth-advisors"><img src="http://www.themco.ca/wp-content/uploads/2015/04/CWANBF476.png" width="476" height="92" border=0 alt="Event sponsor"></a></p>
-      <p><small>Heartstrings Wine Sponsor: <a href="http://www.mbll.ca/"><b>Manitoba Liquor and Lotteries</b></a><br>Heartstrings Music Sponsor: <a href="http://www.payworks.ca/"><b>Payworks</b></a></p>
-      <p>Heartstrings Reception Sponsors:<br><a href="https://profile.rbcwealthmanagement.com/Jon.Holeman"><img src="http://www.themco.ca/wp-content/uploads/2016/03/16_90783_NT6_002_Holeman-Sponsor-Logo300.png" width="300" height="137" alt="Reception sponsor"></a></p>
-
-      <p><small>Tickets must be purchased by 2 May 2017, and will not be available at the door. Patrons are acknowledged in the program and receive a larger tax receipt.</small></p>
-
-    </div><!-- /span6 -->
-
-    <div class="span2">
-      <a href="#s"><img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /></a><br />
-      <p class="fineprint">Click above to add a regular ticket to your shopping cart; adjust quantity in cart and return to purchase other tickets.</p>
-    </div><!-- /span2 -->
-
-  </div><!-- /row -->
-
+<!-- /span8 -->
+		<div class="span2">
+			<p>
+				.
+			</p>
+		</div>
+<!-- /span2 -->
+	</div>
+<!-- /row -->
+	<div class="row">
+		<div class="span6">
+			<h4>
+				The MCO&rsquo;s gala evening<br>&mdash; fabulous 2017 edition!
+			</h4>
+			<p>
+				The MCO&rsquo;s magnificent gala dinner is a memorable evening of fundraising to be enjoyed with clients and friends. As usual, the event takes place in the opulent 7th-floor ballrooms at The Fort Garry Hotel where guests will be treated to outstanding entertainment, an elegant dining experience, and lively conversations and auction sessions. The funds raised by the event help to ensure that the MCO can continue presenting some of the world&rsquo;s best musicians and music to Winnipeg audiences and delivering vital music education to underserved communities throughout the province. 
+			</p>
+			<p>
+				This year the theme is la dolce vita, after the sumptuous Italy of Fellini&rsquo;s films.
+			</p>
+			<p>
+				Friday, 5 May 2017<br>The Fort Garry Hotel, Seventh Floor
+			</p>
+			<p>
+				6:30 pm, hors d&rsquo;oeuvres<br>7:30 pm, Dinner<br>9:30 pm, Desserts and Liqueurs<br>Live &amp; Silent Auctions<br>Tickets $175
+			</p>
+			<p>
+				<strong>&bull; <a href="#">buy tickets @ $175</a><br>&bull; <a href="#">buy a table of ten @ $2000</a><br>&bull; <a href="#">buy Patron tickets @ $200*</a></strong>
+			</p>
+			<small><b>Heartrings is sponsored by</b></small> 
+			<p>
+				<a href="http://advisors.nbfwm.ca/en/teams/christianson-wealth-advisors"><img src="http://www.themco.ca/wp-content/uploads/2015/04/CWANBF476.png" width="476" height="92" border="0" alt="Event sponsor"></a>
+			</p>
+			<p>
+				<small>Heartstrings Wine Sponsor: <a href="http://www.mbll.ca/"><b>Manitoba Liquor and Lotteries</b></a><br>Heartstrings Music Sponsor: <a href="http://www.payworks.ca/"><b>Payworks</b></a>
+			</p>
+			<p>
+				Heartstrings Reception Sponsors:<br><a href="https://profile.rbcwealthmanagement.com/Jon.Holeman"><img src="http://www.themco.ca/wp-content/uploads/2016/03/16_90783_NT6_002_Holeman-Sponsor-Logo300.png" width="300" height="137" alt="Reception sponsor"></a>
+			</p>
+			<p>
+				<small>Tickets must be purchased by 2 May 2017, and will not be available at the door. Patrons are acknowledged in the program and receive a larger tax receipt.</small>
+			</p>
+		</div>
+<!-- /span6 -->
+		<div class="span2">
+			<a href="#s"><img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /></a><br />
+			<p class="fineprint">
+				Click above to add a regular ticket to your shopping cart; adjust quantity in cart and return to purchase other tickets.
+			</p>
+		</div>
+<!-- /span2 -->
+	</div>
+<!-- /row -->
 <?php get_template_part('templates/content', 'heartstrings'); ?>
-
-</div><!-- /container -->
+</div>
+<!-- /container -->
