63dd12cc1188ac702a5040599990f179f6eff519
Jon Snidal <jsnidal@gmail.com>
2017-01-11 13:11:22 -0600

    Minstrel page

---
+++ php/wp-content/themes/roots-master/page-minstrel.php
@@ -0,0 +1,83 @@
+<?php
+/*
+Template Name: Page Minstrel Template
+*/
+?>
+<div class="container">
+	<div class="row">
+		<div class="span8">
+			<img src="http://www.themco.ca/wp-content/uploads/2017/01/Minstrel.jpg" alt="Minstrel Booking Agency" width="770" height="455"> 
+		</div>
+<!-- /span8 -->
+		<div class="span2">
+			<p>
+				. 
+			</p>
+		</div>
+<!-- /span2 -->
+	</div>
+<!-- /row -->
+	<div class="row">
+		<div class="span6">
+			<h4>
+				Planning an event? 
+			</h4>
+			<p>
+				Minstrel is a booking agency that brings central Canada&rsquo;s finest musicians to you. Whether you&rsquo;re looking to hire a flamenco guitarist for a wedding, a string quartet for a staff party or a full orchestra to back up a rock, hip hop, or jazz group &ndash; we do it all. Many of our artists have had illustrious careers as orchestral musicians, but our roster includes musicians of all styles and instrumental backgrounds. 
+			</p>
+			<p>
+				Administered by the Manitoba Chamber Orchestra, Minstrel divides its work into three areas: 
+			</p>
+			<ol>
+				<li>Accompaniment for other musicians</li>
+				<li>Corporate functions, including receptions, conferences, and fundraisers</li>
+				<li>Personal events, like weddings, birthdays, and home parties</li>
+			</ol>
+			<p>
+				Under the first category, Minstrel has had collaborative engagements with Wayne Newton, Rod Stewart, Kanye West, Anne Murray, kd lang, and many other household names. 
+			</p>
+			<p>
+				But don&rsquo;t let that fool you: our rates are competitive, and we work with our clients to determine the most efficient use of our musicians&rsquo; time to keep your costs low. Families, local bands, and small businesses hire us just as often as the stars do. And if you&rsquo;re looking to hire musicians to accompany you or an artist you represent, we have accomplished arrangers we work with who are experienced in preparing orchestral arrangements of music in all genres under tight deadlines. 
+			</p>
+			<p>
+				Under the second category, past clientele include but are not limited to: 
+			</p>
+			<ul>
+				<li>Lyric Theatre</li>
+				<li>Royal Canadian Mint</li>
+				<li>Smith Carter Architects</li>
+				<li>Investors Group</li>
+				<li>Great West Life</li>
+				<li>Fillmore Riley</li>
+				<li>Banville + Jones Wine</li>
+				<li>Princess Auto</li>
+				<li>Horizon Kia</li>
+				<li>Winnipeg Free Press</li>
+				<li>Manitoba Lotteries</li>
+				<li>CancerCare Manitoba</li>
+				<li>ISIS Research Network</li>
+				<li>Mayberry Gallery</li>
+				<li>Festival Du Voyageur</li>
+				<li>Robertson College</li>
+				<li>Agence nationale et internationale du Manitoba (ANIM)</li>
+				<li>Synyshyn Architects</li>
+				<li>Manitoba Club</li>
+				<li>University of Manitoba</li>
+				<li>Ft. Garry Hotel</li>
+				<li>College of Registered Nurses of MB</li>
+				<li>Red River Porsche Club</li>
+			</ol>
+		</div>
+<!-- /span6 -->
+		<div class="span2">
+			<a href="#s"><img src="http://www.themco.ca/wp-content/uploads/2012/10/BuyTicketsHereHi.png" alt="" width="75" height="150" /></a><br />
+			<p class="fineprint">
+				Click above to add a regular ticket to your shopping cart; adjust quantity in cart and return to purchase other tickets. 
+			</p>
+		</div>
+<!-- /span2 -->
+	</div>
+<!-- /row -->
+<?php get_template_part('templates/content', 'minstrel'); ?>
+</div>
+<!-- /container -->
