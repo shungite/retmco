6c9b063d26b50c8ff6b91314e5c744e666fce113
Jon Snidal <jsnidal@gmail.com>
2017-01-12 13:50:12 -0600

    Front FACTOR logos added

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -5,19 +5,19 @@ Template Name: Page MCOhome Template
 ?>
 <div class="container">
 	<div class="row-fluid">
-  <div class="span8">
-    <div class="covercontainer">
-      <a href="http://www.themco.ca/concert/frontier-fiddlers/"><img src="http://www.themco.ca/wp-content/uploads/2016/12/head14feb17cover.jpg" alt="MCO / Stobbe, Frontier Fiddlers"></a>
-      <div class="playbutton">
-        <?php echo do_shortcode('[video_lightbox_youtube video_id="Q0e3uV69LWg" width="640" height="480" anchor="http://www.themco.ca/wp-content/uploads/2016/11/YouTube-social-circle_red_96px.png"]'); ?>
-      </div>
-    </div>
-  </div>
-</div>
+		<div class="span8">
+			<div class="covercontainer">
+				<a href="http://www.themco.ca/concert/frontier-fiddlers/"><img src="http://www.themco.ca/wp-content/uploads/2016/12/head14feb17cover.jpg" alt="MCO / Stobbe, Frontier Fiddlers"></a> 
+				<div class="playbutton">
+<?php echo do_shortcode('[video_lightbox_youtube video_id="Q0e3uV69LWg" width="640" height="480" anchor="http://www.themco.ca/wp-content/uploads/2016/11/YouTube-social-circle_red_96px.png"]'); ?>
+				</div>
+			</div>
+		</div>
+	</div>
 	<div class="row-fluid">
 		<div class="span8">
 			<h4>
-				﻿Frontier Fiddlers&thinsp;&mdash;&thinsp;Red River Reverie
+				﻿Frontier Fiddlers&thinsp;&mdash;&thinsp;Red River Reverie 
 			</h4>
 			<p>
 				How lucky we are to welcome back the Frontier Fiddlers! Made up of students in the Frontier School Division, the group represents <i>la cr&egrave;me de la cr&egrave;me</i> of young fiddle players in the province. They&rsquo;ll treat us to a spectacular medley of Red River jigs and traditional folk music. The Frontier Fiddlers and the orchestra will also together perform a new work by favourite Winnipeg composer Jim Hiscott. Matinee tickets and further details <a href="http://www.themco.ca/concert/frontier-fiddlers/"><b>here</b></a> or, if you&rsquo;re just after a quick ticket or two to the evening concert, click these links.<br><img src="http://www.themco.ca/wp-content/uploads/2013/01/QuikTicket.png" alt="QuikTicket"><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&amp;pid=9f2d0c6ce9e4485884d98d0f993fe03c"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/AdultC.png" alt="" width="62" height="40"></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&amp;pid=f6ed94335d8347a7954f2ba5f5fa7730"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/SeniorC.png" alt="" width="62" height="40" /></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&amp;pid=a0f06eb73e3c426da3282c9ff488eff8"><img rel="tooltip" title="Click to add student ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2013/01/StudentC.png" alt="" width="62" height="40" /></a> 
@@ -33,9 +33,8 @@ Template Name: Page MCOhome Template
 					<div class="media-body">
 						<p>
 							<strong><a href="http://www.themco.ca/quick-links-to-1617-subscriptions/">Subscribe now to MCO&rsquo;s 2016/17 season<br>&mdash;&thinsp;enjoy six or nine concerts in Westminster Church!</strong></a><br />
-							Only subscribers enjoy every benefit the MCO has to offer, like a free CD, Musicnet Subscriber Reward Card, ticket exchange privileges, discounts on extra tickets and more. This season, subscribe to Canada&rsquo;s &ldquo;tiny, perfect, orchestra!&rdquo;
-							<?php echo do_shortcode('[video_lightbox_youtube video_id="2ruCxwyal5U" width="640" height="480" anchor="<strong>Video: the season-at-a-glance!</strong>"]'); ?>
-							
+							Only subscribers enjoy every benefit the MCO has to offer, like a free CD, Musicnet Subscriber Reward Card, ticket exchange privileges, discounts on extra tickets and more. This season, subscribe to Canada&rsquo;s &ldquo;tiny, perfect, orchestra!&rdquo; 
+<?php echo do_shortcode('[video_lightbox_youtube video_id="2ruCxwyal5U" width="640" height="480" anchor="<strong>Video: the season-at-a-glance!</strong>"]'); ?>
 						</p>
 					</div>
 				</div>
@@ -85,19 +84,26 @@ Template Name: Page MCOhome Template
 <!-- /span2 -->
 	</div>
 <!-- /row-fluid -->
-
 	<div class="row-fluid">
 		<div class="span6">
 			<div class="well-white">
-<p>FACTOR</p>
-</div>
-</div>
-</div>
-
+				<p>
+					<a href="https://www.factor.ca/"><img src="http://www.themco.ca/wp-content/uploads/2017/01/FACTOR200.png"></a>&nbsp;&nbsp;<a href="https://www.canada.ca/en.html"><img src="http://www.themco.ca/wp-content/uploads/2017/01/CanadaWordmark200.png"></a> 
+				</p>
+				<p>
+					<small>Some of these projects were funded in part by FACTOR, the Government of Canada and Canada&rsquo;s private radio broadcasters.</small>
+				</p>
+				<p>
+					<small>Certains de ces projets ont &eacute;t&eacute; financ&eacute;s en partie par FACTOR, le gouvernement du Canada et les radiodiffuseurs prive&#x301;s du Canada.</small>
+				</p>
+			</div>
+		</div>
+	</div>
 <?php get_template_part('templates/content', 'mcohome'); ?>
 </div>
 <!-- /container -->
 <script type="text/javascript">
+
 
 $(document).ready(function () {
   $("[rel=tooltip]").tooltip();
