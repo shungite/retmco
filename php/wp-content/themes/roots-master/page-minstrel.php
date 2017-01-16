58ea11e8e091521b8ccf9f709c4ac4a2bd100d72
Jon Snidal <jsnidal@gmail.com>
2017-01-16 11:00:33 -0600

    Minstrel header

--- php/wp-content/themes/roots-master/page-minstrel.php
+++ php/wp-content/themes/roots-master/page-minstrel.php
@@ -6,7 +6,7 @@ Template Name: Page Minstrel Template
 <div class="container">
 	<div class="row">
 		<div class="span8">
-			<img src="http://www.themco.ca/wp-content/uploads/2017/01/MinstrelAgency.jpg" alt="Minstrel Booking Agency" width="770" height="455"> 
+			<img src="http://www.themco.ca/wp-content/uploads/2017/01/Minstrel.jpg" alt="Minstrel Booking Agency" width="770" height="455"> 
 		</div>
 <!-- /span8 -->
 		<div class="span2">
