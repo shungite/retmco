c62af320e0bb5ac3a300796276bf198cb672100a
Jon Snidal <jsnidal@gmail.com>
2017-02-02 13:32:03 -0600

    Interactive guide page added

---
+++ php/wp-content/themes/roots-master/page-frontier.php
@@ -0,0 +1,105 @@
+<?php
+/*
+Template Name: Page Frontier Template
+*/
+?>
+<div class="container">
+	<div class="row">
+		<div class="span8">
+			<p>
+				&nbsp; 
+			</p>
+			<blockquote>
+				<p>
+					They come from as far away as Grand Rapids, Matheson Island, Berens River, and Norway House. They play Red River jigs with breakneck speed. And all of them are teenagers or younger. 
+				</p>
+			</blockquote>
+			<p>
+				&nbsp; 
+			</p>
+			<div class="well">
+				<p>
+					<b>MCO Learning Guides</b> 
+				</p>
+				<p>
+					The MCO understands the importance of music education, which is why we&rsquo;ve created a variety of unique instructional resources such as the bilingual, interactive MCO Discover Learning Guide featuring Manitoba&rsquo;s talented Frontier Fiddlers, and the works of Canadian composer Jim Hiscott, J.S. Bach, and B&eacute;la Bart&oacute;k. 
+				</p>
+				<p>
+					Designed by Julie Mongeon-Ferr&eacute; and written by Julie Mongeon-Ferr&eacute; and Beryl Peters, the bilingual Discover Learning Guide is an exciting way for all audience members from Kindergarten to Grade 12 and beyond, to prepare for and engage with the inspiring music and musicians featured in the February 14, 2017 Manitoba Chamber Orchestra matinee concert. 
+				</p>
+				<p>
+					The Discover Learning Guide will help educators and learners to connect with the Manitoba Music K-12 curriculum framework through a variety of interactive experiences that explore the life and music of the Frontier Fiddlers, Jim Hiscott, Bach, and Bart&oacute;k. 
+				</p>
+				<p>
+					The Discover Learning Guide also suggests creative music possibilities for all learners inspired by the music of the Frontier Fiddlers, Jim Hiscott, and the sounds of Manitoba nature. The Learning Guide provides supporting material about the string/fiddle family and innovative ways to interact with and respond to the music and musicians of this concert. 
+				</p>
+				<p>
+					The Discover Learning Guide is available in three formats: 
+				</p>
+				<ul>
+					<li>electronic book ibook (iPad, iPhone, iPod touch or Mac)</li>
+					<li>online version (viewable with internet connection on most devices and browsers)</li>
+					<li>PDF (for reading and printing, no interactive components, available from within online version)</li>
+				</ul>
+				<p>
+					<b>Enjoy your musical discoveries!</b> 
+				</p>
+				<p>
+					&nbsp; 
+				</p>
+				<p>
+					<strong>Learning Guide</strong> 
+				</p>
+				<iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/0fc55d5f-b1aa-4211-86b4-32078d9af987?startpage=1&amp;allowFullscreen=true" width="525px" height="371px" frameborder="0" allowfullscreen=""></iframe> 
+				<p>
+					<a href="http://www.themco.ca/wp-content/uploads/2017/02/Frontier-Fiddlers_EN.ibooks"><img src="http://www.themco.ca/wp-content/uploads/2016/04/iBookIcon50.png"></a>&nbsp;<a href="http://www.themco.ca/wp-content/uploads/2016/04/Horsch-EN.ibooks"><b>Download iBook here</b> (9MB)</a> 
+				</p>
+				<p>
+					&nbsp; 
+				</p>
+				<p>
+					<b>Guides d&rsquo;apprentissage OCM</b> 
+				</p>
+				<p>
+					L&rsquo;Orchestre de chambre du Manitoba (OCM) comprend l'importance de l'&eacute;ducation musicale, c&rsquo;est pourquoi nous avons cr&eacute;&eacute; une vari&eacute;t&eacute; de ressources p&eacute;dagogiques uniques tel que le guide d'apprentissage OCM D&eacute;couverte, &agrave; la fois bilingue et interactif, mettant en vedette le groupe des Frontier Fiddlers et des &oelig;uvres de Bach, de Bartok et du compositeur canadien Jim Hiscott. 
+				</p>
+				<p>
+					Conceptualis&eacute; par Julie Mongeon-Ferr&eacute; et r&eacute;dig&eacute; par Julie Mongeon-Ferr&eacute; et Beryl Peters le guide bilingue d'apprentissage D&eacute;couverte est un outil stimulant pour l&rsquo;auditoire d&rsquo;&eacute;l&egrave;ves la maternelle &agrave; la 12e ann&eacute;e et m&ecirc;me pour les adultes. Le guide peut servir &agrave; les pr&eacute;parer et &agrave; d&eacute;couvrir la musique inspirante et les musiciens en vedette lors de la matin&eacute;e du 14 f&eacute;vrier 2017 &agrave; l&rsquo;Orchestre de chambre du Manitoba. 
+				</p>
+				<p>
+					Le guide d'apprentissage D&eacute;couverte aidera les &eacute;l&egrave;ves et les enseignants &agrave; faire les liens avec les programmes d&rsquo;&eacute;tudes manitobains en musique de la maternelle &agrave; la 12e ann&eacute;e par le biais d&rsquo;une vari&eacute;t&eacute; d'exp&eacute;riences interactives explorant la vie et la musique des Frontier Fiddlers, de Jim Hiscott, de Bach, et de Bart&oacute;k. 
+				</p>
+				<p>
+					Le guide d'apprentissage D&eacute;couverte sugg&egrave;re &eacute;galement des possibilit&eacute;s musicales cr&eacute;atives inspir&eacute;es par la musique des Frontier Fiddlers, de Jim Hiscott et de la nature manitobaine. Le guide offre &eacute;galement de l&rsquo;information au sujet du violon et de la famille des cordes et, de fa&ccedil;on innovatrice, permet d'interagir et de r&eacute;agir &agrave; la musique et d&rsquo;en apprendre au sujet des musiciens de ce concert. 
+				</p>
+				<p>
+					Le guide d&apos;apprentissage D&eacute;couverte est accessible en trois formats : 
+				</p>
+				<ul>
+					<li>livre &eacute;lectronique ibook (iPad, iPhone, iPod touch ou Mac)</li>
+					<li>version en ligne (lisible avec une connexion Internet sur tous les navigateurs et tous les appareils &eacute;lectroniques)</li>
+					<li>PDF (pour lecture et impression, sans composantes interactives, est disponible &agrave; partir de la version en ligne)</li>
+				</ul>
+				<p>
+					<b>Bonnes découvertes musicales!</b> 
+				</p>
+				<p>
+					&nbsp;<br>
+					<strong>Guide d&rsquo;apprentissage</strong> 
+				</p>
+				<iframe style="border: 1px solid #777;" src="https://indd.adobe.com/embed/787e9080-d80e-47bd-af1f-3953109b6654?startpage=1&amp;allowFullscreen=true" width="525px" height="371px" frameborder="0" allowfullscreen=""></iframe> 
+				<p>
+					<a href="http://www.themco.ca/wp-content/uploads/2017/02/Frontier-Fiddlers_FR.ibooks"><img src="http://www.themco.ca/wp-content/uploads/2016/04/iBookIcon50.png"></a>&nbsp;<a href="http://www.themco.ca/wp-content/uploads/2016/04/Horsch-FR.ibooks"><b>T&eacute;l&eacute;charger iBook ici</b> (9MB)</a> 
+				</p>
+			</div>
+<!-- /well -->
+		</div>
+<!-- /span8 -->
+	</div>
+<!-- /row -->
+<?php get_template_part('templates/content', 'frontier'); ?>
+</div>
+<!-- /container -->
+<script>
+	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+</script>
