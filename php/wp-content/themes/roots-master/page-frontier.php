805cf939e2ed88a312ac9ffd0497d6ff2d466f24
Jon Snidal <jsnidal@gmail.com>
2017-02-10 11:03:51 -0600

    Updates

--- php/wp-content/themes/roots-master/page-frontier.php
+++ php/wp-content/themes/roots-master/page-frontier.php
@@ -13,11 +13,9 @@ Template Name: Page Frontier Template
 				<p>
 					The Frontier Fiddlers come from as far away as Grand Rapids, Matheson Island, Berens River, and Norway House. They play Red River jigs with breakneck speed. And all of them are teenagers or younger&thinsp;&hellip; 
 				</p>
-				&
 				<p>
-					nbsp
-				</p>
-				; 
+				&nbsp;
+				</p> 
 				<p>
 					Le groupe Frontier Fiddlers viennent d&rsquo;aussi loin que Grand Rapids, l&rsquo;&Icirc;le Matheson, Berens River et Norway House. Ils jouent des gigues de la rivi&egrave;re Rouge &agrave; une vitesse vertigineuse. Et tous sont des adolescents ou plus jeunes&thinsp;&hellip; 
 				</p>
