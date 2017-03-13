72d9cec17aa95c6015a030e59676792bf6827741
Jon Snidal <jsnidal@gmail.com>
2017-03-13 12:20:43 -0500

    AIHM

--- php/wp-content/themes/roots-master/page-aihm.php
+++ php/wp-content/themes/roots-master/page-aihm.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Page Aih Template
+Template Name: Page Aihm Template
 */
 ?>
 <div class="container">
@@ -23,15 +23,19 @@ Template Name: Page Aih Template
 				Artists in Healthcare Manitoba 
 			</h4>
 			<p>
-				The Manitoba Chamber Orchestra partners in Artists in Healthcare Manitoba&rsquo;s music programs, bringing live music to patients, families and staffs in the St. Boniface Hospital Atrium monthly. <a href="http://www.artistsinhealthcare.com/"><b>AIHM</b></a> describes its programming as providing &ldquo;important benefits for patients, families, and staff as an oasis from stress and anxiety often found in healthcare environments.&rdquo;
+				The Manitoba Chamber Orchestra partners in Artists in Healthcare Manitoba&rsquo;s music programs, bringing live music to patients, families and staffs in the St. Boniface Hospital Atrium monthly. <a href="http://www.artistsinhealthcare.com/"><b>AIHM</b></a> describes its programming as providing &ldquo;important benefits for patients, families, and staff as an oasis from stress and anxiety often found in healthcare environments.&rdquo; 
 			</p>
 			<p>
 				Check back for a list of upcoming performances being offered by the MCO in regional hospitals through this program. Members of the general public are also warmly invited to attend these performances. 
 			</p>
-			<div class="well">
-				<p>
-					<img src="http://www.themco.ca/wp-content/uploads/2017/03/MCO-D.jpg"> 
-				</p>
+			<p><small>Flow image from AIHM website.</small></p>
+			<p>&nbsp;</p>
+			<div class="span6">
+				<div class="well">
+					<p>
+						<img src="http://www.themco.ca/wp-content/uploads/2017/03/MCO-D.jpg"> 
+					</p>
+				</div>
 			</div>
 		</div>
 <!-- /span6 -->
@@ -40,6 +44,6 @@ Template Name: Page Aih Template
 <!-- /span2 -->
 	</div>
 <!-- /row -->
-<?php get_template_part('templates/content', 'aih'); ?>
+<?php get_template_part('templates/content', 'aihm'); ?>
 </div>
 <!-- /container -->
