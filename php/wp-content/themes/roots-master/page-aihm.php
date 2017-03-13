aa09c0356a7ae1189d9862082a4b1c9fd5624d03
Jon Snidal <jsnidal@gmail.com>
2017-03-13 12:16:33 -0500

    AIHM

--- php/wp-content/themes/roots-master/page-aihm.php
+++ php/wp-content/themes/roots-master/page-aihm.php
@@ -1,12 +1,12 @@
 <?php
 /*
-Template Name: Page Aihm Template
+Template Name: Page Aih Template
 */
 ?>
 <div class="container">
 	<div class="row">
 		<div class="span8">
-			<img src="http://www.themco.ca/wp-content/uploads/2017/01/Minstrel.jpg" alt="Minstrel Booking Agency" width="770" height="455"> 
+			<img src="http://www.themco.ca/wp-content/uploads/2017/03/headaihm.jpg" alt="Artists in Healthcare Manitoba" width="770" height="455"> 
 		</div>
 <!-- /span8 -->
 		<div class="span2">
@@ -20,38 +20,22 @@ Template Name: Page Aihm Template
 	<div class="row">
 		<div class="span7">
 			<h4>
-				Planning an event? 
+				Artists in Healthcare Manitoba 
 			</h4>
 			<p>
-				Minstrel is a booking agency that brings central Canada&rsquo;s finest musicians to you. Whether you&rsquo;re looking to hire a flamenco guitarist for a wedding, a string quartet for a staff party or a full orchestra to back up a rock, hip hop, or jazz group &ndash; we do it all. Many of our artists have had illustrious careers as orchestral musicians, but our roster includes musicians of all styles and instrumental backgrounds. 
+				The Manitoba Chamber Orchestra partners in Artists in Healthcare Manitoba&rsquo;s music programs, bringing live music to patients, families and staffs in the St. Boniface Hospital Atrium monthly. <a href="http://www.artistsinhealthcare.com/"><b>AIHM</b></a> describes its programming as providing &ldquo;important benefits for patients, families, and staff as an oasis from stress and anxiety often found in healthcare environments.&rdquo;
 			</p>
 			<p>
-				Administered by the Manitoba Chamber Orchestra, Minstrel divides its work into three areas: 
-			</p>
-			<ol>
-				<li>Accompaniment for other musicians</li>
-				<li>Corporate functions, including receptions, conferences, and fundraisers</li>
-				<li>Personal events, like weddings, birthdays, and home parties</li>
-			</ol>
-			<p>
-				Minstrel has had collaborative engagements with Wayne Newton, Rod Stewart, Kanye West, Anne Murray, kd lang, and many other household names. 
-			</p>
-			<p>
-				But don&rsquo;t let that fool you: our rates are competitive, and we work with our clients to determine the most efficient use of our musicians&rsquo; time to keep your costs low. Families, local bands, and small businesses hire us just as often as the stars do. And if you&rsquo;re looking to hire musicians to accompany you or an artist you represent, we have accomplished arrangers we work with who are experienced in preparing orchestral arrangements of music in all genres under tight deadlines. 
+				Check back for a list of upcoming performances being offered by the MCO in regional hospitals through this program. Members of the general public are also warmly invited to attend these performances. 
 			</p>
 			<div class="well">
 				<p>
-					For more information or a specific quote on your event, start here. Use this form to provide as many details as possible, and we&rsquo;ll start planning your music!
+					<img src="http://www.themco.ca/wp-content/uploads/2017/03/MCO-D.jpg"> 
 				</p>
-<?php echo do_shortcode('[contact-form-7 id="1940" title="Minstrel event form"]'); ?>
 			</div>
 		</div>
 <!-- /span6 -->
 		<div class="span1">
-			<a href="mailto:bmackenzie5@shaw.ca?subject=Minstrel%20web%20inquiry&thinsp;&hellip;"><img src="http://www.themco.ca/wp-content/uploads/2017/03/mail.png" alt="" width="75" height="150" /></a><br />
-			<p class="fineprint">
-				Click above to email Boyd MacKenzie, or call (204)955-5350 for further information. 
-			</p>
 		</div>
 <!-- /span2 -->
 	</div>
