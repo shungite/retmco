4a0a07134fc8cdf6ee848e6ac145a63c7c47c2c5
Jon Snidal <jsnidal@gmail.com>
2017-03-20 11:10:03 -0500

    AIHM dates

--- php/wp-content/themes/roots-master/page-aihm.php
+++ php/wp-content/themes/roots-master/page-aihm.php
@@ -26,7 +26,10 @@ Template Name: Page Aihm Template
 				The Manitoba Chamber Orchestra partners in Artists in Healthcare Manitoba&rsquo;s music programs, bringing live music to patients, families and staffs in the St. Boniface Hospital Atrium monthly. <a href="http://www.artistsinhealthcare.com/"><b>AIHM</b></a> describes its programming as providing &ldquo;important benefits for patients, families, and staff as an oasis from stress and anxiety often found in healthcare environments.&rdquo; 
 			</p>
 			<p>
-				Check back for a list of upcoming performances being offered by the MCO in regional hospitals through this program. Members of the general public are also warmly invited to attend these performances. 
+				Here is a list of this season&rsquo;s performances by the MCO in regional hospitals through this program. Members of the general public are also warmly invited to attend these performances. 
+			</p>
+			<p>
+				&nbsp;
 			</p>
 			<table class="table table-striped">
 				<tr>
@@ -141,10 +144,11 @@ Template Name: Page Aihm Template
 			<p>
 				&nbsp; 
 			</p>
-			<div class="span6">
+			<div class="span7">
 				<p>
 					<img src="http://www.themco.ca/wp-content/uploads/2017/03/MCO-D.jpg"> 
 				</p>
+				<p>&nbsp;</p>
 			</div>
 		</div>
 <!-- /span6 -->
