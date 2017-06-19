6f6a941f4df013a85bb61d0bed566bdadc96647a
Jon Snidal <jsnidal@gmail.com>
2017-06-19 11:02:42 -0500

    First concert edited

--- php/wp-content/themes/roots-master/ConcertTxt/page-concert1.php
+++ php/wp-content/themes/roots-master/ConcertTxt/page-concert1.php
@@ -42,7 +42,7 @@ Template Name: Page Concert2 Template
 			</p>
 			<div>
 				<p>
-					<img src="http://www.themco.ca/wp-content/uploads/2013/01/PreConcert.png"> <button id="preconcert" class="btn btn-small btn-inverse" rel="popover" data-placement="top" data-original-title="Pre-concert event" data-content="Arrive early for more music! Percussion students of Victoria Sparks; 6:45 pm.">Event</button> <button id="pizza" class="btn btn-small btn-inverse" rel="popover" data-placement="top" data-original-title="Pizza Club" data-content="Book at least three days in advance for free pizza and a pre-concert presentation by Haley Marie. This is included with any concert ticket purchase, space permitting. Call (204)783-7377 for reservations. Audience members are also invited to come for the talk alone, if they&rsquo;ve already eaten dinner.">Pizza Club</button> 
+					<img src="http://www.themco.ca/wp-content/uploads/2013/01/PreConcert.png"> <button id="preconcert" class="btn btn-small btn-inverse" rel="popover" data-placement="top" data-original-title="Pre-concert event" data-content="Stay tuned, ,details coming soon!">Event</button> 
 				</p>
 			</div>
 			<p>
