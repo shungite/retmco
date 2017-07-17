b0ce627798a109f1b6feb90f68e301003018a831
Jon Snidal <jsnidal@gmail.com>
2017-07-17 09:19:06 -0500

    VV page again

--- php/wp-content/themes/roots-master/page-vinyl.php
+++ php/wp-content/themes/roots-master/page-vinyl.php
@@ -3,44 +3,65 @@
 Template Name: Page Vinyl Template
 */
 ?>
-
 <div class="container">
-
-  <div class="row">
-
-    <div class="span8">
-    
-    <img src="http://www.themco.ca/wp-content/uploads/2017/07/VVBlack.png" width="770" height="455" alt="MCO / 2017/18 season">
-				<div class="playbutton">
+	<div class="row">
+		<div class="span8">
+			<img src="http://www.themco.ca/wp-content/uploads/2017/07/VVBlack.png" width="770" height="455" alt="MCO / 2017/18 season"> 
+			<div class="playbutton">
 <?php echo do_shortcode('[video_lightbox_youtube video_id="RNt073ms2AM" width="640" height="480" anchor="http://www.themco.ca/wp-content/uploads/2016/10/YouTube-social-circle_red_92px.png"]'); ?>
-    
-	 
-    </div>
-
-  </div><!-- /row -->
-
-  <div class="row">
-
-    <div class="span8">
-      <h4>Check here for<br>Vinyl Vault sales dates!</h4>
-      <p>The Manitoba Chamber Orchestra&rsquo;s mostly-monthly Vinyl Vault sales are your chance to rummage around in the city&rsquo;s largest used record collection! Find rare titles in jazz, pop, classical, opera, rock, country, folk, easy listening and much more &mdash; <b>collectibles a speciality!</b></p>
-      <p><b>10:00 am to 3:00 pm, Saturdays<br>Power Building basement<br>428 Portage Avenue<br>Dates for 17/18:</b></p><p>26 August 2017<br>30 September<br>28 October<br>25 November<br>27 January 2018<br>24 February<br>31 March<br>28 April<br>26 May<br>23 June<br>July sale (dates TBA)</p>	
-      <p>&nbsp;</p>
-    </div>
-
-  </div><!-- /row -->
-
-  <div class="row">
-
-    <div class="span6">
-      <div class="well">
-        <p><b>Donate your unwanted records to the MCO!</b></p>
-        <p>We always want new used records! If you have some you&rsquo;d like to donate, call the MCO office at 204.783.7377 for drop-off instructions or to arrange a pick-up. Please note that pickups may take a while as we keep our volunteers pretty busy &mdash; thanks in advance!</p>
-      </div><!-- /well -->
-    </div><!-- /span6 -->
-
-  </div><!-- /row -->
-
+			</div>
+		</div>
+	</div>
+<!-- /row -->
+	<div class="row">
+		<div class="span8">
+			<h4>
+				Check here for<br>
+				Vinyl Vault sales dates!
+			</h4>
+			<p>
+				The Manitoba Chamber Orchestra&rsquo;s mostly-monthly Vinyl Vault sales are your chance to rummage around in the city&rsquo;s largest used record collection! Find rare titles in jazz, pop, classical, opera, rock, country, folk, easy listening and much more &mdash; <b>collectibles a speciality!</b>
+			</p>
+			<p>
+				<b>10:00 am to 3:00 pm, Saturdays<br>
+				Power Building basement<br>
+				428 Portage Avenue<br>
+				Dates for 17/18:</b>
+			</p>
+			<p>
+				26 August 2017<br>
+				30 September<br>
+				28 October<br>
+				25 November<br>
+				27 January 2018<br>
+				24 February<br>
+				31 March<br>
+				28 April<br>
+				26 May<br>
+				23 June<br>
+				July sale (dates TBA)
+			</p>
+			<p>
+				&nbsp;
+			</p>
+		</div>
+	</div>
+<!-- /row -->
+	<div class="row">
+		<div class="span6">
+			<div class="well">
+				<p>
+					<b>Donate your unwanted records to the MCO!</b>
+				</p>
+				<p>
+					We always want new used records! If you have some you&rsquo;d like to donate, call the MCO office at 204.783.7377 for drop-off instructions or to arrange a pick-up. Please note that pickups may take a while as we keep our volunteers pretty busy &mdash; thanks in advance!
+				</p>
+			</div>
+<!-- /well -->
+		</div>
+<!-- /span6 -->
+	</div>
+<!-- /row -->
 <?php get_template_part('templates/content', 'vinyl'); ?>
-
-</div><!-- /container -->
+</div>
+<!-- /container -->
