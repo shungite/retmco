b1838e095062baf186dc15493ff3cda6caeb639b
Jon Snidal <jsnidal@gmail.com>
2017-08-28 14:21:12 -0500

    Switch to casual

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -7,101 +7,105 @@ Template Name: Page MCOhome Template
 	<div class="row-fluid">
 		<div class="span8">
 			<div class="covercontainer">
-				<a href="http://www.themco.ca/quick-links-to-1718-subscriptions/"><img src="http://www.themco.ca/wp-content/uploads/2017/06/Season1718.png" width="770" height="455" alt="MCO / 2017/18 season"></a> 
+				<a href="http://www.themco.ca/concert/manson-barnes-2/"><img src="http://www.themco.ca/wp-content/uploads/2017/06/h26sep17.png" width="770" height="455" alt="MCO / 2017/18 season"></a> 
 				<div class="playbutton">
-<?php echo do_shortcode('[video_lightbox_youtube video_id="47FAYvCecrM" width="640" height="480" anchor="http://www.themco.ca/wp-content/uploads/2016/10/YouTube-social-circle_red_92px.png"]'); ?>
+<?php echo do_shortcode('[video_lightbox_youtube video_id="7iCzxWr_qSM" width="640" height="480" anchor="http://www.themco.ca/wp-content/uploads/2016/10/YouTube-social-circle_red_92px.png"]'); ?>
 				</div>
 			</div>
 		</div>
 	</div>
-	<div class="row-fluid">
-		<div class="span8">	
-			<h4>
-				Subscribe now to MCO’s 2017/18 season<br>&mdash;&thinsp;enjoy six or nine concerts in Westminster Church!
-			</h4>
-			<p>
-				Only subscribers enjoy every benefit the MCO has to offer, like a free compact disc, Musicnet Subscriber Reward Card, ticket exchange privileges, discounts on extra tickets and more. This season, subscribe to Canada&rsquo;s &ldquo;tiny, perfect, orchestra!&rdquo; <a href="http://www.themco.ca/quick-links-to-1718-subscriptions/"><b>Build your package here.</b></a><br><a href="http://www.themco.ca/quick-links-to-1718-subscriptions/"><img src="http://www.themco.ca/wp-content/uploads/2015/11/AdultTx.png" alt="" width="62" height="40">&nbsp;<img src="http://www.themco.ca/wp-content/uploads/2015/11/SeniorTx.png" alt="" width="62" height="40" />&nbsp;<img src="http://www.themco.ca/wp-content/uploads/2015/11/StudentTx.png" alt="" width="62" height="40" /></a> 
-			</p>
-		</div>
+	div class="row-fluid"> 
+	<div class="span8">
+		<a href="http://www.themco.ca/quick-links-to-1617-subscriptions/"><img src="http://www.themco.ca/wp-content/uploads/2016/06/Subscribe.png" alt="MCO / Subscribe now!"></a> 
+		<h4>
+			MCO&rsquo;s 2017/18 season opens<br>with Canada&rsquo;s cellist, Ariel Barnes!
+		</h4>
+		<p>
+			Winnipeg audiences seem to have a unique appreciation for the cello, and Ariel Barnes is uniquely qualified to satisfy that appetite. A player of dazzling virtuosity and magnetic expressivity, Barnes is, in the words of Maestro Bramwell Tovey, &ldquo;the outstanding Canadian cellist of his generation.&rdquo;. See program notes, bios and more at <a href="http://www.themco.ca/concert/manson-barnes-2/"><b>here</b></a> or, if you&rsquo;re just after a quick ticket or two, click the links below to add tickets to cart.<br><img src="http://www.themco.ca/wp-content/uploads/2013/01/QuikTicket.png" alt="QuikTicket"><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=3b8ff793221540d292b1e73163abd418"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2015/11/AdultTx.png" alt="" width="62" height="40"></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=e11c964d0f9147d8bb6f000ed64e13bf"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2015/11/SeniorTx.png" alt="" width="62" height="40" /></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&pid=e0a4dbcc5583467b92be57a58b48d497"><img rel="tooltip" title="Click to add under-30 ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2015/11/StudentTx.png" alt="" width="62" height="40" /></a>
+		</p>
 	</div>
+</div>
 <!-- /row -->
-
 <div class="row-fluid">
-		<div class="span8">
-			<div class="well">
-				<div class="media">
-					<a class="pull-left" href="#extras" data-toggle="tab"><img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2017/06/GlassSq.png" width="96px" height="96px"></a> 
-					<div class="media-body">
-						<p>
-							<strong><a href="http://www.themco.ca/concert/manson-dinnerstein/">Casual tickets now on sale for the MCO&rsquo;s premiere of their Philip Glass commission! </strong></a><br />
-							The MCO has co-commissioned a piano concerto by Philip Glass, arguably the most important living composer. The concerto will have its Canadian premiere in an exciting concert on January 23rd, 2018. Celebrated American pianist Simone Dinnerstein performs the work&rsquo;s solo. Get your tickets before they sell out!
+	<div class="span8">
+		<div class="well">
+			<div class="media">
+				<a class="pull-left" href="#extras" data-toggle="tab"><img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2017/06/GlassSq.png" width="96px" height="96px"></a> 
+				<div class="media-body">
+					<p>
+						<strong><a href="http://www.themco.ca/concert/manson-dinnerstein/">Casual tickets now on sale for the MCO&rsquo;s premiere of their Philip Glass commission! </strong></a><br />
+						The MCO has co-commissioned a piano concerto by Philip Glass, arguably the most important living composer. The concerto will have its Canadian premiere in an exciting concert on January 23rd, 2018. Celebrated American pianist Simone Dinnerstein performs the work&rsquo;s solo. Get your tickets before they sell out! 
 <?php echo do_shortcode('[video_lightbox_youtube video_id="QL2gf4n8uuA" width="640" height="480" anchor="<strong>Video preview here!</strong>"]'); ?>
-						</p>
-					</div>
+					</p>
 				</div>
 			</div>
 		</div>
 	</div>
-
-	<div class="row-fluid">
-    <div class="span8">
-      <div class="well-white">
-        <div class="media"><a class="pull-left" href="#extras" data-toggle="tab"><img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/01/Parlo1.png" width="96px" height="96px"></a>
-          <div class="media-body">
-            <p><strong><a href="http://www.themco.ca/vinyl-vault/">Next MCO Vinyl Vault sale: Saturday, 26 August 2017</strong></a><br />From 10:00 am to 3:00 pm on the last Saturday of each month, browse away in the city&rsquo;s finest used LP/CD collections. You&rsquo;ll find 1000s of titles in the jazz, pop, classical, rock, country and folk categories, and more! MCO&rsquo;s Vinyl Vault is located in the basement of the Power Building at 428 Portage Avenue (near The Bay).</p> 
-          </div>
-        </div>
-      </div>
-    </div>
-  </div><!-- /row -->
-  
-	<div class="row-fluid">
-		<div class="span4">
-			<div class="well">
-				<p>
-					<strong><a href="#">Sign up here for MCO&rsquo;s<br><em>Noteworthy</em> email newsletter!</strong></a> 
-				</p>
-				<p>
-					Our just-about-monthly missive is filled with information on upcoming concerts and special events. Unsubscribe at any time. 
-				</p>
-<?php echo do_shortcode('[contact-form-7 id="1045" title="Noteworthy"]'); ?>
+</div>
+<div class="row-fluid">
+	<div class="span8">
+		<div class="well-white">
+			<div class="media">
+				<a class="pull-left" href="#extras" data-toggle="tab"><img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/01/Parlo1.png" width="96px" height="96px"></a> 
+				<div class="media-body">
+					<p>
+						<strong><a href="http://www.themco.ca/vinyl-vault/">Next MCO Vinyl Vault sale: Saturday, 26 August 2017</strong></a><br />
+						From 10:00 am to 3:00 pm on the last Saturday of each month, browse away in the city&rsquo;s finest used LP/CD collections. You&rsquo;ll find 1000s of titles in the jazz, pop, classical, rock, country and folk categories, and more! MCO&rsquo;s Vinyl Vault is located in the basement of the Power Building at 428 Portage Avenue (near The Bay).
+					</p>
+				</div>
 			</div>
 		</div>
-		<div class="span3">
+	</div>
+</div>
+<!-- /row -->
+<div class="row-fluid">
+	<div class="span4">
+		<div class="well">
 			<p>
-				<a href="http://www.themco.ca/venue-faq/"><img src="http://www.themco.ca/wp-content/uploads/2014/11/VenueFAQ.png"></a> 
-			</p>
-			<hr style="border-top: dotted 2px; color: #ddd;" />
-			<p>
-				<a href="http://www.themco.ca/troubadour-the-nightingale/"><img src="http://www.themco.ca/wp-content/uploads/2013/10/TroubadourPreOrder.jpg"></a> 
+				<strong><a href="#">Sign up here for MCO&rsquo;s<br><em>Noteworthy</em> email newsletter!</strong></a> 
 			</p>
 			<p>
-				<a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&amp;pid=94ce7b6e08cf4bb79c932d28f04ae510"><button class="btn btn-small btn-success" type="button">Add to cart</button></a> 
+				Our just-about-monthly missive is filled with information on upcoming concerts and special events. Unsubscribe at any time. 
 			</p>
+<?php echo do_shortcode('[contact-form-7 id="1045" title="Noteworthy"]'); ?>
 		</div>
+	</div>
+	<div class="span3">
+		<p>
+			<a href="http://www.themco.ca/venue-faq/"><img src="http://www.themco.ca/wp-content/uploads/2014/11/VenueFAQ.png"></a> 
+		</p>
+		<hr style="border-top: dotted 2px; color: #ddd;" />
+		<p>
+			<a href="http://www.themco.ca/troubadour-the-nightingale/"><img src="http://www.themco.ca/wp-content/uploads/2013/10/TroubadourPreOrder.jpg"></a> 
+		</p>
+		<p>
+			<a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&amp;pid=94ce7b6e08cf4bb79c932d28f04ae510"><button class="btn btn-small btn-success" type="button">Add to cart</button></a> 
+		</p>
+	</div>
 <!-- /span2 -->
-	</div>
+</div>
 <!-- /row-fluid -->
-	<div class="row-fluid">
-		<div class="span4">
-			<p>
-				<a href="https://www.factor.ca/"><img src="http://www.themco.ca/wp-content/uploads/2017/01/FACTOR150.png"></a>&nbsp;&nbsp;<a href="https://www.canada.ca/en.html"><img src="http://www.themco.ca/wp-content/uploads/2017/01/CanadaWordmark150.png"></a> 
-			</p>
-			<p>
-				<small>2017/18 recording and touring projects were funded in part by FACTOR, the Government of Canada and Canada&rsquo;s private radio broadcasters.</small> 
-			</p>
-			<p>
-				<small>Les projets d'enregistrement et les projets de tournage en 2016-2017 ont &eacute;t&eacute; financ&eacute;s en partie par FACTOR, le gouvernement du Canada et les radiodiffuseurs prive&#x301;s du Canada.</small> 
-			</p>
-			<p>
-				&nbsp; 
-			</p>
-		</div>
+<div class="row-fluid">
+	<div class="span4">
+		<p>
+			<a href="https://www.factor.ca/"><img src="http://www.themco.ca/wp-content/uploads/2017/01/FACTOR150.png"></a>&nbsp;&nbsp;<a href="https://www.canada.ca/en.html"><img src="http://www.themco.ca/wp-content/uploads/2017/01/CanadaWordmark150.png"></a> 
+		</p>
+		<p>
+			<small>2017/18 recording and touring projects were funded in part by FACTOR, the Government of Canada and Canada&rsquo;s private radio broadcasters.</small> 
+		</p>
+		<p>
+			<small>Les projets d'enregistrement et les projets de tournage en 2016-2017 ont &eacute;t&eacute; financ&eacute;s en partie par FACTOR, le gouvernement du Canada et les radiodiffuseurs prive&#x301;s du Canada.</small> 
+		</p>
+		<p>
+			&nbsp; 
+		</p>
 	</div>
+</div>
 <?php get_template_part('templates/content', 'mcohome'); ?>
 </div>
 <!-- /container -->
 <script type="text/javascript">
+
 
 
 
