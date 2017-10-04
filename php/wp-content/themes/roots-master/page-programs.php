8a91bfe6148e1c613adf236be5d1ca7b1b94a83d
Jon Snidal <jsnidal@gmail.com>
2017-10-04 12:39:44 -0500

    E+O page update

--- php/wp-content/themes/roots-master/page-programs.php
+++ php/wp-content/themes/roots-master/page-programs.php
@@ -8,7 +8,7 @@ Template Name: Page Programs Template
 
   <div class="row">
     <div class="span8">
-      <img src="http://www.themco.ca/wp-content/uploads/2016/12/e-o1617.jpg" alt="" width="770" height="455">
+      <img src="http://www.themco.ca/wp-content/uploads/2017/10/eo1718.png" alt="Education and outreach" width="770" height="455">
     </div>
     <div class="span2">
       <p>.</p>
@@ -29,33 +29,38 @@ Template Name: Page Programs Template
       <div class="tab-content">
         <div class="tab-pane fade in active" id="en">
 
-          <p><h4>Students love the excitement<br />of live chamber music!</h4></p>
-          <p>Bring your students to one or both of these special matinee performances! Use the order form on the reverse to book your tickets today or call 204-783-7377 for more information.</p>
-          <p><span class="badge">1</span> <b>The MCO performs with the Frontier Fiddlers, February 14</b></p>
-          <p>Made up of students in the Frontier School Division, the Frontier Fiddlers represents <i>la cr&egrave;me de la cr&egrave;me</i> of young fiddle players in the province. They come from as far away as Grand Rapids, Matheson Island, Berens River, and Norway House. They play Red River jigs with breakneck speed. And all of them are teenagers or younger. </p>
-          <p>At this concert, the Frontier Fiddlers will treat us to a spectacular medley of fiddle tunes with a Red River flavour. With the orchestra, they&rsquo;ll also perform selections from Bart&oacute;k and Bach, and a folk-inspired new work by favourite Winnipeg composer Jim Hiscott.</p>
+          <p><h4>﻿Student tickets<br>now nearly 50% off!</h4></p>
+          
+          <p><span class="badge">1</span> <b>﻿MARC-ANDR&Eacute; HAMELIN (PIANO), 1:00 pm, WEDNESDAY, 21 FEBRUARY 2018 <a href="https://www.youtube.com/watch?v=sQf1LtXDBVU&feature=youtu.be"> VIDEO</a></b></p>
+          
+          <p>Montr&eacute;aler Marc-Andr&eacute; Hamelin is easily one of the world&rsquo;s greatest living pianists; someone whose hands the <em>New Yorker</em> has called &ldquo;among the wonders of the musical world.&rdquo; In this concert, those hands bring their magic to bear on a number of beautiful, accessible solo piano works by such composers as Liszt and Debussy, the musical impressionist. The effect is something of a tour through late 19th century Europe. Don&rsquo;t miss this chance to have your students experience one of the piano&rsquo;s living giants.</p>
+          
           <div class="well">
-            <p>1:00 pm matinee start time (doors open 12:30)<br>Tuesday, 14 February 2017<br>Anne Manson, Music Director<br>Karl Stobbe, Concertmaster<br>Westminster United Church<br>745 Westminster Avenue</p>
-            <p>Selections from:</p>
-            <p>Fiddle Medley (traditional)</p>
-            <p>Jim Hiscott<br>New composition<br>Manitoba Arts Council commission<br>World premiere performance</p>
-            <p>Johann Sebastian Bach<br>Violin Concerto no. 2 in E Major, BWV&thinsp;1042</p>
-            <p>B&eacute;la Bart&oacute;k<br>Divertimento for String Orchestra (movement)</p>
+            <p>1:00 pm matinee start time (doors open 12:30)<br>Wednesday, 21 February 2018<br>Westminster United Church, 745 Westminster Ave.</p>
+            <p>Franz Liszt<br>Hungarian Rhapsody No. 13 (S 244/13)<br>B&eacute;n&eacute;diction de Dieu dans la solitude<br>Fantasy and Fugue on the Theme B-A-C-H (S 529/2)</p>
+            <p>Samuil Feinberg<br>Piano Sonata No. 4, Op. 6</p>
+            <p>Claude Debussy<br>Images, Book I</p>
+            <p>Leopold GodowskyM<br>Wine, Women and Song</p>
           </div>
-          <p><b><span class="badge">2</span> MCO, Th&eacute;&acirc;tre Cercle Molière &mdash; music and theatre, June 7 and 8 </b></p>
-          <p>Nanabush is the trickster figure of Anishinaabe mythology&thinsp;&mdash;&thinsp;a cultural hero who, according to legend, is responsible for dogs&rsquo; proclivity to sniff one another&rsquo;s tails and for rescuing the forest from Paul Bunyan.</p>
-          <p><i>Nanabush and the Drum&thinsp;/&thinsp;Nanabozho et le tambour</i>, a music and theatre collaboration between the MCO and Th&eacute;&acirc;tre Cercle Moli&egrave;re, envisions Nanabush&rsquo;s role in the foundation of the M&eacute;tis. He sets up playful obstacles for a hapless French voyageur, as he attempts to navigate the land and woo his more savvy Indigenous love interest.</p>
-          <p>While celebrating Canada&rsquo;s 150th birthday, the piece illuminates the diversity of our heritage with a sense of humour and spectacle. The work is bilingual, but accessible to French and English students equally.</p>
+          
+          <p><b><span class="badge">2</span> ﻿LUCIE HORSCH (RECORDER), 1:00 PM, TUESDAY, 24 APRIL 2018 <a href="https://www.youtube.com/watch?v=x3yLCXiDiL4&feature=youtu.be">VIDEO</a> </b></p>
+          
+          <p>Teenage Lucie Horsch is the &ldquo;latest big thing in recorder playing&rdquo; (<em>The Guardian</em>) who possesses &ldquo;fearsome virtuosity and superb technique&rdquo; (<em>BBC Music Magazine</em>). Who could be a more inspiring model for students of the recorder? Hear her demonstrate this popular instrument&rsquo;s dazzling musical possibilities at our April 2018 concert. You and your students may never see another recorder player, or teenager, as musically gifted as her.</p>
+          
           <div class="well">
-            <p>10:30 am matinee start times (doors open 10:00)<br>Wednesday and Thursday, 7 and 8 June 2017<br>Anne Manson, Music Director<br>Karl Stobbe, Concertmaster<br>Westminster United Church<br>745 Westminster Avenue</p>
-            <p>Genevi&egrave;ve Pelletier, Director<br>Anne Manson, Conductor</p>
-            <p><i>Nanabush and the Drum&thinsp;/&thinsp;Nanabozho et le tambour</i><br>Bilingual script by Rh&eacute;al Cenerini<br>Music by Michael Oesterle</p>
+						<p>1:00 pm matinee start time (doors open 12:30)<br>Tuesday, 24 April 2018<br>Westminster United Church, 745 Westminster Ave.</p>
+						<p>Antonio Vivaldi<br>La Tempesta di Mare</p>
+						<p>Johann Sebastian Bach<br>Oboe Concerto in D&#x2008;Minor&mdash;tr. for recorder</p>
+						<p>Giovanni Battista Sammartini<br>Recorder Concerto in F Major</p>
+						<p>Dag Wir&eacute;n<br>Serenade for Strings, Op. 11</p>
+						<p>PLUS: special student play-along composition! Teachers will receive music in advance.</p>
           </div>
 
           <p><b>Bus subsidies</b></p>
-          <p>Please note that bus subsidies are available for transporting your students to our concerts. Please contact the MCO at 204-783-7377 for more details.</p>
+          <p>Having trouble meeting concert costs? Ask us about our ticket and bus subsidies. Please contact the MCO at 204-783-7377 for more details.</p>
+          
           <p><b>MTS Future First Listening Guides</b></p>
-          <p>Have you seen our MTS Future First Listening Guides? Prime your students for our matinee concerts with our play-by-play analysis of concert repertoire. Listening guides are generally available on the education section at themco.ca the week before the concert.</p>
+          <p>﻿Have you seen our interactive bilingual Learning Guides? Here’s a sample guide from a past <a href="http://www.themco.ca/discover-decouvre/"><b>matinee</b></a>. Look, listen, and discover the curricular music learning possibilities!</p>
 
           <div class="well-white">
             <p>The MCO Outreach Program is funded by the <a href="http://www.canadacouncil.ca/"><b>Canada Council for the Arts</b></a>, the <a href="http://artscouncil.mb.ca/"><b>Manitoba Arts Council</b></a>, the <a href="http://winnipegarts.ca/"><b>Winnipeg Arts Council</b></a>, <a href="http://www.wpgfdn.org/"><b>The Winnipeg Foundation</b></a>, the <a href="https://www.winnipegsd.ca/CHF/Pages/Default.aspx"><b>Children&rsquo;s Heritage Fund</b></a>, <a href="https://www.assiniboine.mb.ca/"><b>Assiniboine Credit Union</b></a>, <a href="http://friendswpl.ca/"><b>Friends of the Library</b></a>, <a href="http://www.greatwestlife.com/001/Home/index.htm"><b>The Great-West Life Assurance Company</b></a>, the <a href="http://www.lchf.ca/"><b>Lloyd Carr-Harris Foundation</b></a>, <a href="http://www.mpi.mb.ca/en/Pages/default.aspx"><b>Manitoba Public Insurance</b></a>, <a href="http://www.gov.mb.ca/cyo/youth/"><b>MB4Youth</b></a>, <a href="https://www3.mts.ca/mts/future+first"><b>MTS</b></a>, <a href="http://www.mbteach.org/"><b>Manitoba Teachers&rsquo; Society</b></a>, the <b>Musicians&rsquo; Performance Trust Fund</b>, <a href="http://www.gov.mb.ca/chc/artsbranch/index.html?print"><b>Province of Manitoba Culture Branch</b></a>, the <a href="http://www.richardson.ca/community/richardson-foundation"><b>Richardson Foundation</b></a>, the <b>Rita Menzies Endowment Fund for Arts Education</b>, the <a href="http://www.clubrunner.ca/Portal/Home.aspx?accountid=1004"><b>Rotary Club of Winnipeg West</b></a>, <a href="http://www.sunlife.ca/"><b>Sun Life Financial</b></a> and <a href="http://www.tdcanadatrust.com/products-services/banking/index-banking.jsp"><b>TD</b></a>.</p>
@@ -68,37 +73,7 @@ Template Name: Page Programs Template
       <div class="tab-pane fade" id="fr">
         <div>
 
-          <p><h4>Les jeunes aiment l&rsquo;enthousiasme<br>de la musique en direct&thinsp;!</h4></p>
-          <p>Amenez vos &eacute;l&egrave;ves &agrave; l&rsquo;une ou aux deux pr&eacute;sentations de ces matin&eacute;es sp&eacute;ciales ! Utilisez le formulaire de commande &agrave; l&rsquo;endos pour r&eacute;server vos billets d&egrave;s aujourd&rsquo;hui ou composez le 204-783-7377 pour de plus amples renseignements.</p>
-          <p><span class="badge">1</span> <b>l&rsquo;OCM joue avec les Frontier Fiddlers, le 14 f&eacute;vrier</b></p>
-          <p>Compos&eacute; d&rsquo;&eacute;l&egrave;ves de la division scolaire Frontier, le groupe Frontier Fiddlers repr&eacute;sente la cr&egrave;me de la cr&egrave;me des jeunes joueurs de violon dans la province. Ils viennent d&rsquo;aussi loin que Grand Rapids, l&rsquo;&Icirc;le Matheson, Berens River et Norway House. Ils jouent des gigues de la rivi&egrave;re Rouge &agrave; une vitesse vertigineuse. Et tous sont des adolescents ou plus jeunes.</p>
-          <p>Lors de ce concert, le groupe Frontier Fiddlers nous g&acirc;tera avec un m&eacute;lange spectaculaire d&rsquo;airs de violon avec une saveur de la rivi&egrave;re Rouge. Accompagn&eacute;s de l&rsquo;orchestre, les &eacute;l&egrave;ves pr&eacute;senteront aussi une s&eacute;lection de pi&egrave;ces de Bart&oacute;k et de Bach et une nouvelle &oelig;uvre d&rsquo;inspiration folklorique du compositeur favori de Winnipeg, Jim Hiscott.</p>
-          <div class="well">
-            <p>Matin&eacute;e d&eacute;butant &agrave; 13 h<br>(portes ouvertes d&egrave;s 12 h 30)<br>Le mardi 14 f&eacute;vrier 2017<br>Anne Manson, directrice musicale<br>Karl Stobbe, premier violon<br>Westminster United Church<br>745, avenue Westminster</p>
-            <p>Pi&egrave;ces de :</p>
-            <p>Pot-pourri pour violon (traditionnel)<br>Jim Hiscott<br> Nouvelle &oelig;uvre<br>Commande du Conseil des arts du Manitoba<br>Premi&egrave;re mondiale</p>
-            <p>Johann Sebastian Bach<br>Concerto pour violon no 2 en mi majeur, BWV&thinsp;1042</p>
-            <p>B&eacute;la Bart&oacute;k<br>Divertimento pour orchestre &agrave; cordes (mouvement)</p>
-          </div>
-          <p><b><span class="badge">2</span> l&rsquo;OCM, Th&eacute;&acirc;tre Cercle Moli&egrave;re&thinsp;&mdash;&thinsp;musique et th&eacute;&acirc;tre, les 7 et 8 juin</b></p>
-          <p>Nanabozho, connu dans la mythologie des Anishinaabe comme &eacute;tant le Filou, est un h&eacute;ros culturel qui, selon la l&eacute;gende, est responsable de la propension des chiens &agrave; se renifler la queue et du sauvetage de la for&ecirc;t de Paul Bunyan.</p>
-          <p><i>Nanabozho et le tambour&thinsp;/&thinsp;Nanabush and the Drum</i>, une collaboration musicale et th&eacute;&acirc;trale entre l&rsquo;OCM et le Th&eacute;&acirc;tre Cercle Moli&egrave;re, imagine le r&ocirc;le de Nanabozho dans la cr&eacute;ation des M&eacute;tis. Il met en place des obstacles amusants pour un pauvre voyageur qui tente de se frayer un chemin sur la terre et de faire la cour &agrave; sa bien-aim&eacute;e autochtone plus rus&eacute;e.</p>
-          <p>Tout en c&eacute;l&eacute;brant le 150e anniversaire du Canada, la pi&egrave;ce met en &eacute;vidence la diversit&eacute; de notre patrimoine avec un sens d&rsquo;humour et du spectacle. L&rsquo;&oelig;uvre est bilingue, mais accessible tant pour les &eacute;tudiants francophones qu&rsquo;anglophones.</p>
-          <div class="well">
-            <p>Matin&eacute;e d&eacute;butant &agrave; 10 h 30<br>(portes ouvertes d&egrave;s 10 h)<br>Le mercredi 7 juin et le jeudi 8 juin 2017<br>Anne Manson, directrice musicale<br>Karl Stobbe, premier violon<br>Westminster United Church<br>745, avenue Westminster</p>
-            <p>Genevi&egrave;ve Pelletier, metteure en sc&egrave;ne<br>Anne Manson, chef d&rsquo;orchestre</p>
-            <p><i>Nanabozho et le tambour&thinsp;/&thinsp;Nanabush and the Drum</i><br>Texte bilingue par Rh&eacute;al Cenerini<br>Musique par Michael &oelig;sterle</p>
-          </div>
-          <p><b>Subventions pour autobus</b></p>
-          <p>Veuillez noter que des subventions pour autobus sont disponibles pour transporter vos &eacute;l&egrave;ves &agrave; nos concerts. Veuillez communiquer avec l&rsquo;OCM au 204-783-7377 pour de plus amples d&eacute;tails.</p>
-          <p><b>Guides d&rsquo;apprentissage de MTS Future First</b></p>
-          <p>Avez-vous vu nos Guides d&rsquo;apprentissage de MTS Future First ? Pr&eacute;parez vos &eacute;l&egrave;ves pour nos matin&eacute;es avec une analyse d&eacute;taill&eacute;e du r&eacute;pertoire du concert. Les Guides d&rsquo;apprentissage sont g&eacute;n&eacute;ralement disponibles dans la section &eacute;ducative &laquo; Education &raquo; &agrave; themco.ca la semaine avant le concert.</p>
-
-          <div class="well-white">
-            <p>Le programme de sensibilisation de l&rsquo;OCM est financ&eacute; par le <a href="http://conseildesarts.ca/"><b>Conseil des arts du Canada</b></a>, le <a href="http://artscouncil.mb.ca/fr/accueil/"><b>Conseil des arts du Manitoba</b></a>, le <a href="http://winnipegarts.ca/"><b>Winnipeg Arts Council</b></a>, <a href="http://www.wpgfdn.org/"><b>The Winnipeg Foundation</b></a>, le <a href="https://www.winnipegsd.ca/CHF/Pages/Default.aspx"><b>Children&rsquo;s Heritage Fund</b></a>, <a href="https://www.assiniboine.mb.ca/"><b>Assiniboine Credit Union</b></a>, <a href="http://friendswpl.ca/"><b>Friends of the Winnipeg Public Library</b></a>, <a href="http://www.lagreatwest.com/001/Accueil/index.htm"><b>La Great-West, compagnie d&rsquo;assurance-vie</b></a>, la <a href="http://www.lchf.ca/"><b>Lloyd Carr-Harris Foundation,</b></a> <a href="http://www.mpi.mb.ca/fr/Pages/default.aspx"><b>Soci&eacute;t&eacute; d&rsquo;assurance publique du Manitoba</b></a>, <a href="http://www.gov.mb.ca/cyo/youth/index.fr.html"><b>Jeunesse Manitoba</b></a>, <a href="https://www3.mts.ca/mts/future+first"><b>MTS</b></a>, <a href="https://www.mbteach.org/"><b>Manitoba Teachers&rsquo; Society</b></a>, le <b>Music Performance Trust Fund</b>, <a href="http://www.gov.mb.ca/chc/artsbranch/index.fr.html"><b>minist&egrave;re de la culture de la Province du Manitoba</b></a>, la <a href="https://www.richardson.ca/our-community/richardson-foundation/"><b>Richardson Foundation</b></a>, le <b>Rita Menzies Endowment Fund for Arts Education</b>, le <a href="http://www.clubrunner.ca/Portal/Home.aspx?accountid=1004"><b>Rotary Club of Winnipeg West</b></a>, <a href="https://www.sunlife.ca/ca?vgnLocale=fr_CA"><b>Financi&egrave;re Sun Life</b></a> et <a href="https://www.tdcanadatrust.com/francais/produits-et-services/operations-bancaires/index-banking.jsp"><b>TD</b></a>. Translation services: <a href="http://fr-ca.parentyreitmeier.com/"><b>Parenty Reitmeier</b></a>.</p>
-          </div>
-
-          <p>&nbsp;</p>
+          <p>Restez &agrave; l'&eacute;coute!</p>
 
         </div>
       </div>
