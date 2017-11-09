3652be205326576a1b5a41df612f3fa3572a275b
Jon Snidal <jsnidal@gmail.com>
2017-11-09 14:04:38 -0600

    Space for French programs added

--- php/wp-content/themes/roots-master/page-programs.php
+++ php/wp-content/themes/roots-master/page-programs.php
@@ -33,7 +33,7 @@ Template Name: Page Programs Template
 						</h4>
 					</p>
 					<p>
-						<span class="badge">1</span> <b>﻿MARC-ANDR&Eacute; HAMELIN (PIANO), 1:00 pm, WEDNESDAY, 21 FEBRUARY 2018 <a href="https://www.youtube.com/watch?v=sQf1LtXDBVU&amp;feature=youtu.be"> VIDEO</a></b>
+						<span class="badge">1</span> <b>﻿MARC-ANDR&Eacute; HAMELIN (PIANO), 1:00 pm, WEDNESDAY, 21 FEBRUARY 2018 <a href="https://www.youtu"#8DD9D9"be.com/watch?v=sQf1LtXDBVU&amp;feature=youtu.be"> VIDEO</a></b>
 					</p>
 					<p>
 						Montr&eacute;aler Marc-Andr&eacute; Hamelin is easily one of the world&rsquo;s greatest living pianists; someone whose hands the <em>New Yorker</em> has called &ldquo;among the wonders of the musical world.&rdquo; In this concert, those hands bring their magic to bear on a number of beautiful, accessible solo piano works by such composers as Liszt and Debussy, the musical impressionist. The effect is something of a tour through late 19th century Europe. Don&rsquo;t miss this chance to have your students experience one of the piano&rsquo;s living giants.
@@ -134,11 +134,114 @@ Template Name: Page Programs Template
 					</p>
 				</div>
 				<div class="tab-pane fade" id="fr">
-					<div>
+				<div class="tab-pane fade in active" id="en">
+					<p>
+						<h4>
+							﻿Student tickets<br>
+							now nearly 50% off!
+						</h4>
+					</p>
+					<p>
+						<span class="badge">1</span> <b>﻿MARC-ANDR&Eacute; HAMELIN (PIANO), 1:00 pm, WEDNESDAY, 21 FEBRUARY 2018 <a href="https://www.youtu"#8DD9D9"be.com/watch?v=sQf1LtXDBVU&amp;feature=youtu.be"> VIDEO</a></b>
+					</p>
+					<p>
+						Montr&eacute;aler Marc-Andr&eacute; Hamelin is easily one of the world&rsquo;s greatest living pianists; someone whose hands the <em>New Yorker</em> has called &ldquo;among the wonders of the musical world.&rdquo; In this concert, those hands bring their magic to bear on a number of beautiful, accessible solo piano works by such composers as Liszt and Debussy, the musical impressionist. The effect is something of a tour through late 19th century Europe. Don&rsquo;t miss this chance to have your students experience one of the piano&rsquo;s living giants.
+					</p>
+					<div class="well">
 						<p>
-							Restez &agrave; l'&eacute;coute!
+							1:00 pm matinee start time (doors open 12:30)<br>
+							Wednesday, 21 February 2018<br>
+							Westminster United Church, 745 Westminster Ave.
+						</p>
+						<p>
+							Franz Liszt<br>
+							Hungarian Rhapsody No. 13 (S 244/13)<br>
+							<em>B&eacute;n&eacute;diction de Dieu dans la solitude</em><br>
+							Fantasy and Fugue on the Theme B-A-C-H (S 529/2)
+						</p>
+						<p>
+							Samuil Feinberg<br>
+							Piano Sonata No. 4, Op. 6
+						</p>
+						<p>
+							Claude Debussy<br>
+							<em>Images, Book I</em>
+						</p>
+						<p>
+							Leopold GodowskyM<br>
+							<em>Wine, Women and Song</em>
 						</p>
 					</div>
+					<p>
+						<b><span class="badge">2</span> ﻿LUCIE HORSCH (RECORDER), 1:00 PM, TUESDAY, 24 APRIL 2018 <a href="https://www.youtube.com/watch?v=x3yLCXiDiL4&amp;feature=youtu.be">VIDEO</a> </b>
+					</p>
+					<p>
+						Teenage Lucie Horsch is the &ldquo;latest big thing in recorder playing&rdquo; (<em>The Guardian</em>) who possesses &ldquo;fearsome virtuosity and superb technique&rdquo; (<em>BBC Music Magazine</em>). Who could be a more inspiring model for students of the recorder? Hear her demonstrate this popular instrument&rsquo;s dazzling musical possibilities at our April 2018 concert. You and your students may never see another recorder player, or teenager, as musically gifted as her.
+					</p>
+					<div class="well">
+						<p>
+							1:00 pm matinee start time (doors open 12:30)<br>
+							Tuesday, 24 April 2018<br>
+							Westminster United Church, 745 Westminster Ave.
+						</p>
+						<p>
+							Antonio Vivaldi<br>
+							<em>La Tempesta di Mare</em>
+						</p>
+              Johann Sebastian Bach<br>
+							Oboe Concerto in D&#x2008;Minor&mdash;tr. for recorder
+						</p>
+						<p>
+							Giovanni Battista Sammartini<br>
+							Recorder Concerto in F Major
+						</p>
+						<p>
+							Dag Wir&eacute;n<br>
+							<em>Serenade for Strings</em>, Op. 11
+						</p>
+						<p>
+							<strong>PLUS: special student play-along composition! Teachers will receive music in advance.</strong>
+						</p>
+					</div>
+					<p>
+						<strong>The Learning Butterfly</strong>
+					</p>
+					<p>
+						The MCO&rsquo;s one-hour matinees and accompanying interactive bilingual Learning Guides bring the Manitoba Music curriculum alive for K-12 students and teachers. The Learning Guides connect our concerts to a variety of core curricular areas, and employ the four wings of the Learning Butterfly (Making, Creating, Connecting and Responding) to deepen the learning experiences. 
+					</p>
+					<p>
+						<strong>Connecting</strong>&thinsp;&mdash;&thinsp;Through a variety of interactive experiences (sounds, questions, pictures, videos, etc.), students develop understandings about people and practices in music. 
+					</p>
+					<p>
+						<strong>Responding</strong>&thinsp;&mdash;&thinsp;By listening, observing, describing, analyzing, and interpreting students are invited to critically reflect and respond to music experiences. 
+					</p>
+					<p>
+						<strong>Making</strong>&thinsp;&mdash;&thinsp;Students develop language and practices for making music through interactive ways to listen to and perform to concert repertoire and music elements. 
+					</p>
+					<p>
+						<strong>Creating</strong>&thinsp;&mdash;&thinsp;Students are invited to explore and create their own music inspired by music elements of the concert repertoire. 
+					</p>
+					<p>
+						<b>Bus subsidies</b>
+					</p>
+					<p>
+						Having trouble meeting concert costs? Ask us about our ticket and bus subsidies. Please contact the MCO at 204-783-7377 for more details.
+					</p>
+					<p>
+						<b>MTS Future First Listening Guides</b>
+					</p>
+					<p>
+						﻿Have you seen our interactive bilingual Learning Guides? Here’s a sample guide from a past <a href="http://www.themco.ca/discover-decouvre/"><b>matinee</b></a>. Look, listen, and discover the curricular music learning possibilities!
+					</p>
+					<div class="well-white">
+						<p>
+							The MCO Outreach Program is funded by the <a href="http://www.canadacouncil.ca/"><b>Canada Council for the Arts</b></a>, the <a href="http://artscouncil.mb.ca/"><b>Manitoba Arts Council</b></a>, the <a href="http://winnipegarts.ca/"><b>Winnipeg Arts Council</b></a>, <a href="http://www.wpgfdn.org/"><b>The Winnipeg Foundation</b></a>, the <a href="https://www.winnipegsd.ca/CHF/Pages/Default.aspx"><b>Children&rsquo;s Heritage Fund</b></a>, <a href="https://www.assiniboine.mb.ca/"><b>Assiniboine Credit Union</b></a>, <a href="http://www.greatwestlife.com/001/Home/index.htm"><b>The Great-West Life Assurance Company</b></a>, the <a href="http://www.lchf.ca/"><b>Lloyd Carr-Harris Foundation</b></a>, <a href="http://www.macdon.com/"><b>MacDon Industries</b></a>, <a href=" https://www.hydro.mb.ca/"><b>Manitoba Hydro</b></a>, <a href="http://www.gov.mb.ca/cyo/youth/"><b>MB4Youth</b></a>, <a href="http://www.mbschoolboards.ca/"><b>Manitoba School Boards Association</b></a>, <a href="http://www.mbteach.org/"><b>Manitoba Teachers&rsquo; Society</b></a>, <a href="http://www.maximinc.com/"><b>Maxim Truck and Trailer</b></a>, the <b>Musicians&rsquo; Performance Trust Fund</b>, <a href="https://www.newflyer.com/"><b>New Flyer Industries</b></a>, <a href="http://www.gov.mb.ca/chc/artsbranch/index.html?print"><b>Province of Manitoba Culture Branch</b></a>, the <a href="http://www.richardson.ca/community/richardson-foundation"><b>Richardson Foundation</b></a>, the <b>Rita Menzies Endowment Fund for Arts Education</b>, the <a href="http://www.clubrunner.ca/Portal/Home.aspx?accountid=1004"><b>Rotary Club of Winnipeg West</b></a>, <a href="http://www.sunlife.ca/"><b>Sun Life Financial</b></a> and <a href="http://www.tdcanadatrust.com/products-services/banking/index-banking.jsp"><b>TD</b></a>.
+						</p>
+					</div>
+					<p>
+						&nbsp;
+					</p>
+				</div>
 				</div>
 			</div>
 <!-- /tab content -->
