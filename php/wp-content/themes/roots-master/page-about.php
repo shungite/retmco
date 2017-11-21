dc144851ad2c00bffa79e7b2aac15462b77f813e
Jon Snidal <jsnidal@gmail.com>
2017-11-21 10:46:09 -0600

    About video

--- php/wp-content/themes/roots-master/page-about.php
+++ php/wp-content/themes/roots-master/page-about.php
@@ -20,52 +20,10 @@ Template Name: Page About Template
 			</p>
 			<div class="well">
 				<p>
-					<strong>Complete! Bart&oacute;k&rsquo;s Divertimento for String Orchestra<br>
-					from the Official MCO YouTube Channel</strong> 
+					<strong>MCO Music Director Anne Manson<br>
+					introduces the Manitoba Chamber Orchestra!</strong> 
 				</p>
-				<iframe width="80%" height="360" src="//www.youtube.com/embed/9DDdyZGPwPk" frameborder="0" allowfullscreen></iframe> 
-				<p>
-					<strong>MCO&rsquo;s Vicki Young announces the 2017/18 season<br>
-					on Classic 107&rsquo;s Morning Light program</strong> 
-				</p>
-				<iframe width="80%" height="360" src="//www.youtube.com/embed/aN1rbalUqtE" frameborder="0" allowfullscreen></iframe> 
-				<p>
-					<strong>Complete! Vivaldi&rsquo;s lively RV&thinsp;443<br>
-					performed by Dutch recorder star Lucie Horsch</strong> 
-				</p>
-				<iframe width="80%" height="360" src="//www.youtube.com/embed/knOUxSQhcNQ" frameborder="0" allowfullscreen></iframe> 
-				<p>
-					<strong>Complete! Mozart&rsquo;s 40th Symphony<br>
-					from the Official MCO YouTube Channel</strong> 
-				</p>
-				<iframe width="80%" height="360" src="//www.youtube.com/embed/_22L-N6w_Mw" frameborder="0" allowfullscreen></iframe> 
-				<p>
-					<a href="http://www.themco.ca/mozart-symphony-no-40/" target="_blank"><small>Go to MTS Future First Listening Guide</small></a> 
-				</p>
-				<p>
-					<strong>Classic 107 playlist<br>
-					from the Official MCO YouTube Channel</strong> 
-				</p>
-				<iframe width="80%" height="360" src="//www.youtube.com/embed/AG2e1383biE?list=PLrh6cFljbZhH87SF1DNn1l94aLtUHAqM-" frameborder="0" allowfullscreen></iframe> <iframe width="80%" height="360" src="//www.youtube.com/embed/NiUOmT8pxQs?list=PLrh6cFljbZhH87SF1DNn1l94aLtUHAqM-" frameborder="0" allowfullscreen></iframe> 
-				<p>
-					<a href="https://www.youtube.com/playlist?list=PLrh6cFljbZhH87SF1DNn1l94aLtUHAqM-" target="_blank"><small>Go to playlist</small></a> 
-				</p>
-				<p>
-					<strong>Education + outreach playlist<br>
-					from the Official MCO YouTube Channel</strong> 
-				</p>
-				<iframe width="80%" height="360" src="//www.youtube.com/embed/Vwr58yd0dww?list=PLrh6cFljbZhG3TzWqRMzEzaLNe2xE965K" frameborder="0" allowfullscreen></iframe> 
-				<p>
-					<a href="https://www.youtube.com/playlist?list=PLrh6cFljbZhG3TzWqRMzEzaLNe2xE965K" target="_blank"><small>Go to playlist</small></a> 
-				</p>
-				<p>
-					<strong>Other videos playlist<br>
-					from the Official MCO YouTube Channel</strong> 
-				</p>
-				<iframe width="80%" height="360" src="//www.youtube.com/embed/SgD6xwonMys?list=PLrh6cFljbZhExham6y4DC7Tj_NtJWVq7P" frameborder="0" allowfullscreen></iframe> 
-				<p>
-					<a href="https://www.youtube.com/playlist?list=PLrh6cFljbZhExham6y4DC7Tj_NtJWVq7P" target="_blank"><small>Go to playlist</small></a> 
-				</p>
+				<iframe width="80%" height="360" src="//www.youtube.com/embed/KRrpyHL11Wc" frameborder="0" allowfullscreen></iframe> 
 			</div>
 		</div>
 <!-- /well -->
