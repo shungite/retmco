d737c2928af64943c7d1c1f3925d7463714d0854
Jon Snidal <jsnidal@gmail.com>
2017-12-01 08:55:00 -0600

    Typo

--- php/wp-content/themes/roots-master/page-mcohome.php
+++ php/wp-content/themes/roots-master/page-mcohome.php
@@ -21,7 +21,7 @@ Template Name: Page MCOhome Template
 				Alexander Weimann &amp; Guy Few with the MCO<br>&mdash;&thinsp;a Baroque Pageant
 			</h4>
 			<p>
-				<strong>Guy Few</strong> is not only an outrageously gifted musician, he's also a consummate showman. The press have looked upon his performances with astonishment: "simply phenomenal" (<em>Le Devoir</em>, Montreal) and &ldquo;sheer brilliance&rdquo; (<em>LA Times</em>). <strong>Alexander Weimann</strong>, one of the most sought-after ensemble directors and piano soloists of his generation, leads the concert from the harpsichord. See program notes, bios and more at <a href="http://www.themco.ca/concert/few-weimann/"><b>here</b></a> or, if you&rsquo;re just after a quick ticket or two, click the links below to add tickets to cart.<br><img src="http://www.themco.ca/wp-content/uploads/2013/01/QuikTicket.png" alt="QuikTicket"><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&amp;pid=b051853741534b8e9a3832b68138f20f"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2015/11/AdultTx.png" alt="" width="62" height="40"></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&amp;pid=cefe650405e44e75a5efd41b5f64dfca"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2015/11/SeniorTx.png" alt="" width="62" height="40" /></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&amp;pid=322f4494a7964b52b93fd450792aaf26"><img rel="tooltip" title="Click to add under-30 ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2017/08/Under30Tx.png" alt="" width="62" height="40" /></a> 
+				<strong>Guy Few</strong> is not only an outrageously gifted musician, he's also a consummate showman. The press have looked upon his performances with astonishment: "simply phenomenal" (<em>Le Devoir</em>, Montreal) and &ldquo;sheer brilliance&rdquo; (<em>LA Times</em>). <strong>Alexander Weimann</strong>, one of the most sought-after ensemble directors and piano soloists of his generation, leads the concert from the harpsichord. See program notes, bios and more at <a href="http://www.themco.ca/concert/few-weimann/"><b>here</b></a> or, if you&rsquo;re just after a quick ticket or two, click the links below to add tickets to cart.<br><img src="http://www.themco.ca/wp-content/uploads/2013/01/QuikTicket.png" alt="QuikTicket"><a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&amp;pid=b051853741534b8e9a3832b68138f20f"><img rel="tooltip" title="Click to add adult ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2015/11/AdultTx.png" alt="" width="62" height="40"></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&amp;pid=cefe650405e44e75a5efd41b5f64dfca"><img rel="tooltip" title="Click to add senior ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2015/11/SeniorTx.png" alt="" width="62" height="40" /></a> <a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&amp;pid=322f4494a7964b52b93fd450792aaf26"><img rel="tooltip" title="Click to add under-30 ticket to cart" data-placement="top" src="http://www.themco.ca/wp-content/uploads/2017/08/Under30Tx.png" alt="" width="62" height="40" /></a>
 			</p>
 		</div>
 	</div>
@@ -30,27 +30,27 @@ Template Name: Page MCOhome Template
 		<div class="span8">
 			<div class="well">
 				<div class="media">
-					<a class="pull-left" href="#extras" data-toggle="tab"><img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/07/DonorCello.jpg" width="96px" height="96px"></a> 
+					<a class="pull-left" href="#extras" data-toggle="tab"><img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/07/DonorCello.jpg" width="96px" height="96px"></a>
 					<div class="media-body">
 						<p>
 							<strong><a href="http://www.themco.ca/quick-links-to-1718-subscriptions/">Subscribe now to MCO&rsquo;s 2017/18 season<br>&mdash; enjoy six or nine concerts in Westminster Church!</strong></a><br />
-							Onl"#BBD1F2"y subscribers enjoy every benefit the MCO has to offer, like a free CD, Musicnet Subscriber Reward Card, ticket exchange privileges, discounts on extra tick"#BBD1F2"ets and more. This season, subscribe to Canada&rsquo;s &ldquo;tiny, perfect, orchestra!&rdquo; <a href="http://www.themco.ca/wp-content/uploads/2017/06/Brochure.pdf"><b>Get a brochure!</b></a>
+							Only subscribers enjoy every benefit the MCO has to offer, like a free CD, Musicnet Subscriber Reward Card, ticket exchange privileges, discounts on extra tick"#BBD1F2"ets and more. This season, subscribe to Canada&rsquo;s &ldquo;tiny, perfect, orchestra!&rdquo; <a href="http://www.themco.ca/wp-content/uploads/2017/06/Brochure.pdf"><b>Get a brochure!</b></a>
 						</p>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	
+
 	<div class="row-fluid">
 		<div class="span8">
 			<div class="well-white">
 				<div class="media">
-					<a class="pull-left" href="#extras" data-toggle="tab"><img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/01/Parlo1.png" width="96px" height="96px"></a> 
+					<a class="pull-left" href="#extras" data-toggle="tab"><img class="media-object img-circle" src="http://www.themco.ca/wp-content/uploads/2013/01/Parlo1.png" width="96px" height="96px"></a>
 					<div class="media-body">
 						<p>
 							<strong><a href="http://www.themco.ca/vinyl-vault/">Next MCO Vinyl Vault sale: Saturday, 25 November 2017</strong></a><br />
-							From 9:30 am to 3:30 pm on the last Saturday of most months, browse away in the city&rsquo;s finest used LP/CD collections. You&rsquo;ll find 1000s of titles in the jazz, pop, classical, rock, country and folk categories, and more! MCO&rsquo;s Vinyl Vault is located in the basement of the Power Building at 428 Portage Avenue (near The Bay). 
+							From 9:30 am to 3:30 pm on the last Saturday of most months, browse away in the city&rsquo;s finest used LP/CD collections. You&rsquo;ll find 1000s of titles in the jazz, pop, classical, rock, country and folk categories, and more! MCO&rsquo;s Vinyl Vault is located in the basement of the Power Building at 428 Portage Avenue (near The Bay).
 						</p>
 					</div>
 				</div>
@@ -62,24 +62,24 @@ Template Name: Page MCOhome Template
 		<div class="span4">
 			<div class="well">
 				<p>
-					<strong><a href="#">Sign up here for MCO&rsquo;s<br><em>Noteworthy</em> email newsletter!</strong></a> 
+					<strong><a href="#">Sign up here for MCO&rsquo;s<br><em>Noteworthy</em> email newsletter!</strong></a>
 				</p>
 				<p>
-					Our just-about-monthly missive is filled with information on upcoming concerts and special events. Unsubscribe at any time. 
+					Our just-about-monthly missive is filled with information on upcoming concerts and special events. Unsubscribe at any time.
 				</p>
 <?php echo do_shortcode('[contact-form-7 id="1045" title="Noteworthy"]'); ?>
 			</div>
 		</div>
 		<div class="span3">
 			<p>
-				<a href="http://www.themco.ca/venue-faq/"><img src="http://www.themco.ca/wp-content/uploads/2014/11/VenueFAQ.png"></a> 
+				<a href="http://www.themco.ca/venue-faq/"><img src="http://www.themco.ca/wp-content/uploads/2014/11/VenueFAQ.png"></a>
 			</p>
 			<hr style="border-top: dotted 2px; color: #ddd;" />
 			<p>
-				<a href="http://www.themco.ca/troubadour-the-nightingale/"><img src="http://www.themco.ca/wp-content/uploads/2013/10/TroubadourPreOrder.jpg"></a> 
+				<a href="http://www.themco.ca/troubadour-the-nightingale/"><img src="http://www.themco.ca/wp-content/uploads/2013/10/TroubadourPreOrder.jpg"></a>
 			</p>
 			<p>
-				<a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&amp;pid=94ce7b6e08cf4bb79c932d28f04ae510"><button class="btn btn-small btn-success" type="button">Add to cart</button></a> 
+				<a href="http://www.1shoppingcart.com/SecureCart/SecureCart.aspx?mid=292CFDD8-2004-4B27-8455-B26B8A4D841F&amp;pid=94ce7b6e08cf4bb79c932d28f04ae510"><button class="btn btn-small btn-success" type="button">Add to cart</button></a>
 			</p>
 		</div>
 <!-- /span2 -->
@@ -88,16 +88,16 @@ Template Name: Page MCOhome Template
 	<div class="row-fluid">
 		<div class="span4">
 			<p>
-				<a href="https://www.factor.ca/"><img src="http://www.themco.ca/wp-content/uploads/2017/01/FACTOR150.png"></a>&nbsp;&nbsp;<a href="https://www.canada.ca/en.html"><img src="http://www.themco.ca/wp-content/uploads/2017/01/CanadaWordmark150.png"></a> 
+				<a href="https://www.factor.ca/"><img src="http://www.themco.ca/wp-content/uploads/2017/01/FACTOR150.png"></a>&nbsp;&nbsp;<a href="https://www.canada.ca/en.html"><img src="http://www.themco.ca/wp-content/uploads/2017/01/CanadaWordmark150.png"></a>
 			</p>
 			<p>
-				<small>2017/18 recording and touring projects were funded in part by FACTOR, the Government of Canada and Canada&rsquo;s private radio broadcasters.</small> 
+				<small>2017/18 recording and touring projects were funded in part by FACTOR, the Government of Canada and Canada&rsquo;s private radio broadcasters.</small>
 			</p>
 			<p>
-				<small>Les projets d'enregistrement et les projets de tournage en 2016-2017 ont &eacute;t&eacute; financ&eacute;s en partie par FACTOR, le gouvernement du Canada et les radiodiffuseurs prive&#x301;s du Canada.</small> 
+				<small>Les projets d'enregistrement et les projets de tournage en 2016-2017 ont &eacute;t&eacute; financ&eacute;s en partie par FACTOR, le gouvernement du Canada et les radiodiffuseurs prive&#x301;s du Canada.</small>
 			</p>
 			<p>
-				&nbsp; 
+				&nbsp;
 			</p>
 		</div>
 	</div>
