96bed4ddfa370b317699dd0e8899255103da2b5e
Jon Snidal <jsnidal@gmail.com>
2017-12-06 14:36:22 -0600

    VV update
    Removed November sale date

--- php/wp-content/themes/roots-master/page-vinyl.php
+++ php/wp-content/themes/roots-master/page-vinyl.php
@@ -7,7 +7,7 @@ Template Name: Page Vinyl Template
 	<div class="row">
 		<div class="span8">
 			<div class="covercontainer">
-				<img src="http://www.themco.ca/wp-content/uploads/2017/07/VinylVault.png" width="770" height="461" alt="MCO / 2017/18 season"> 
+				<img src="http://www.themco.ca/wp-content/uploads/2017/07/VinylVault.png" width="770" height="461" alt="MCO / 2017/18 season">
 				<div class="playbutton">
 <?php echo do_shortcode('[video_lightbox_youtube video_id="RNt073ms2AM" width="640" height="480" anchor="http://www.themco.ca/wp-content/uploads/2016/10/YouTube-social-circle_red_92px.png"]'); ?>
 				</div>
@@ -19,53 +19,53 @@ Template Name: Page Vinyl Template
 		<div class="span6">
 			<h4>
 				Check here for<br>
-				Vinyl Vault sales dates! 
+				Vinyl Vault sales dates!
 			</h4>
 			<p>
-				The Manitoba Chamber Orchestra&rsquo;s mostly-monthly Vinyl Vault sales are your chance to rummage around in the city&rsquo;s largest used record collection! Find rare titles in jazz, pop, classical, opera, rock, country, folk, easy listening and much more &mdash; <b>collectibles a speciality!</b> 
+				The Manitoba Chamber Orchestra&rsquo;s mostly-monthly Vinyl Vault sales are your chance to rummage around in the city&rsquo;s largest used record collection! Find rare titles in jazz, pop, classical, opera, rock, country, folk, easy listening and much more &mdash; <b>collectibles a speciality!</b>
 			</p>
 			<p>
 				<b>9:30 am to 3:30 pm, Saturdays<br>
 				Power Building basement<br>
 				428 Portage Avenue<br>
-				Dates for 17/18:</b> 
+				Dates for 17/18:</b>
 			</p>
 			<p>
 				<strike>26 August 2017</strike><br>
 				<strike>30 September</strike><br>
 				<strike>28 October</strike><br>
-				25 November<br>
+				<strike>25 November<br></strike>
 				27 January 2018<br>
 				24 February<br>
 				31 March<br>
 				28 April<br>
 				26 May<br>
 				23 June<br>
-				July sale (dates TBA) 
+				July sale (dates TBA)
 			</p>
 			<p>
-				&nbsp; 
+				&nbsp;
 			</p>
 		</div>
-		
+
 		<div class="span2">
 			<a href="#s"><img src="http://www.themco.ca/wp-content/uploads/2017/07/UsedRecordsWanted.png" alt="" width="75" height="150" /></a><br />
 			<p class="fineprint">
-				If you have some you&rsquo;d like to donate, call the MCO office at 204.783.7377 to arrange a pick-up. 
+				If you have some you&rsquo;d like to donate, call the MCO office at 204.783.7377 to arrange a pick-up.
 			</p>
 		</div>
-		
-		
+
+
 	</div>
 <!-- /row -->
 	<div class="row">
 		<div class="span4">
 			<div class="well">
 				<p>
-					<b>Donate your unwanted records to the MCO!</b> 
+					<b>Donate your unwanted records to the MCO!</b>
 				</p>
 				<p>
-					We always want new used records! If you have some you&rsquo;d like to donate, call the MCO office at 204.783.7377 to arrange a pick-up. Please note that pickups may take a while as we keep our volunteers pretty busy &mdash; thanks in advance! 
+					We always want new used records! If you have some you&rsquo;d like to donate, call the MCO office at 204.783.7377 to arrange a pick-up. Please note that pickups may take a while as we keep our volunteers pretty busy &mdash; thanks in advance!
 				</p>
 			</div>
 <!-- /well -->
